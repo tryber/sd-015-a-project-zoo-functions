21347a3531e849d517f0f3c02a4d16c6fd6698e5
Caio Lima <caiojlimah@gmail.com>
2021-09-10 20:35:20 -0300

    Requisitos 1 ao 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,18 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (!Array.isArray(entrants)) return {};
+  const child = entrants.filter(({ age }) => age < 18).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+  return { child, adult, senior };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = []) {
+  const obj = countEntrants(entrants);
+  const { child = 0, adult = 0, senior = 0 } = obj;
+  return (prices.child * child) + (prices.adult * adult) + (prices.senior * senior);
 }
 
 module.exports = { calculateEntry, countEntrants };
