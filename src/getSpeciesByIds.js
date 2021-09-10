a655aa2cacc535a2396423e9750314eec8964384
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-10 20:37:49 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const filtering = (x) => data.species.filter((element) => element.id === x)[0];
+  return ids.map(filtering);
 }
 
 module.exports = getSpeciesByIds;
