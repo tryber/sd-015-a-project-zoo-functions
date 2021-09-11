fd819e32b60ea70ded23effa7c8e35117639917b
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 04:32:47 -0300

    Segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalSelecionado = data.species.find((elemento) => elemento.name === animal);
+  return animalSelecionado.residents.every((bixinho) => bixinho.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
