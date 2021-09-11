f1897b652bb70ce79e19a732c42b2095f717ab5a
Caio Lima <caiojlimah@gmail.com>
2021-09-11 00:11:43 -0300

    Requisito 7 e 8!

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,50 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function makeObjWithoutNames() {
+  return {
+    NE: species.filter((elem) => elem.location === 'NE').map((name) => name.name),
+    NW: species.filter((elem) => elem.location === 'NW').map((name) => name.name),
+    SE: species.filter((elem) => elem.location === 'SE').map((name) => name.name),
+    SW: species.filter((elem) => elem.location === 'SW').map((name) => name.name),
+  };
+}
+
+function makeArray(array, sex, sort) {
+  if (sort) {
+    if (sex === 'all') {
+      return array.map((elm) => ({
+        [elm]: species.find((e) =>
+          e.name === elm).residents.map((e) => e.name).sort() }));
+    }
+    return array.map((elm) => ({
+      [elm]: species.find((e) =>
+        e.name === elm).residents.filter((el) => el.sex === sex).map((e) => e.name).sort() }));
+  }
+  if (sex === 'all') {
+    return array.map((elm) => ({
+      [elm]: species.find((e) =>
+        e.name === elm).residents.map((e) => e.name) }));
+  }
+  return array.map((elm) => ({
+    [elm]: species.find((e) =>
+      e.name === elm).residents.filter((el) => el.sex === sex).map((e) => e.name) }));
+}
+
+function makeObjWithNames(sort = false, sex) {
+  const obj = makeObjWithoutNames();
+  obj.NE = makeArray(obj.NE, sex, sort);
+  obj.NW = makeArray(obj.NW, sex, sort);
+  obj.SE = makeArray(obj.SE, sex, sort);
+  obj.SW = makeArray(obj.SW, sex, sort);
+  return obj;
+}
+
+function getAnimalMap({ includeNames = false, sorted = false, sex = 'all' } = {}) {
+  if (includeNames === false) {
+    return makeObjWithoutNames();
+  }
+  return makeObjWithNames(sorted, sex);
 }
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,46 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+function checkingIfIsDayOrAnimal(dayAnimal) {
+  const ani = species.map((e) => e.name);
+  const days = Object.keys(hours);
+  if (days.some((e) => e === dayAnimal)) {
+    return {
+      [dayAnimal]: {
+        officeHour: `Open from ${hours[dayAnimal].open}am until ${hours[dayAnimal].close}pm`,
+        exhibition: species.filter((e) => e.availability.includes(dayAnimal)).map((e) => e.name),
+      },
+    };
+  }
+  if (ani.some((e) => e === dayAnimal)) {
+    return species.find((e) => e.name === dayAnimal).availability;
+  }
+  return false;
+}
+
+function ifIsMonday() {
+  return {
+    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+  };
+}
+
+function showAllSchedule() {
+  const days = Object.keys(hours);
+  return Object.assign(days.reduce((acc, day) => {
+    Object.assign(acc, checkingIfIsDayOrAnimal(day));
+    return acc;
+  }, {}), ifIsMonday());
+}
+
+function getSchedule(scheduleTarget = false) {
+  const isDayOrAnimal = checkingIfIsDayOrAnimal(scheduleTarget);
+  if (scheduleTarget === 'Monday') {
+    return ifIsMonday();
+  }
+  if (!isDayOrAnimal) {
+    return showAllSchedule();
+  }
+  return isDayOrAnimal;
 }
 
 module.exports = getSchedule;
