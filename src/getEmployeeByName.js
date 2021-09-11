651d236cc47ebe74823e65d441ca45c34be3e17d
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 04:33:51 -0300

    Terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let resposta = data.employees.find((ze) => {
+    const retorno = ze.firstName === employeeName || ze.lastName === employeeName;
+    return retorno;
+  });
+  if (resposta === undefined) {
+    resposta = {};
+  }
+  return resposta;
 }
 
 module.exports = getEmployeeByName;
