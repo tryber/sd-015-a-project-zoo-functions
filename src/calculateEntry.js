772a4b316427c3d8b5a51487c9ce9f85045c072e
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 05:39:26 -0300

    Requisito seis

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function valorarAsChaves(entradas, IdadeMinima, IdadeMaxima) {
+  return entradas.map((pessoa) => pessoa.age).reduce((acc, curr) => {
+    const retornoReduce = curr < IdadeMaxima && curr >= IdadeMinima ? acc + 1 : acc;
+    return retornoReduce;
+  }, 0);
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function countEntrants(entrants) {
+  let resposta = {};
+  if (entrants.length > 0) {
+    resposta.child = valorarAsChaves(entrants, 0, 18);
+    resposta.adult = valorarAsChaves(entrants, 18, 50);
+    resposta.senior = valorarAsChaves(entrants, 50, 900);
+  } else {
+    resposta = [];
+  }
+  return resposta;
+}
+
+function calculateEntry(entrants = {}) {
+  const objetoEntradas = countEntrants(entrants);
+  let resposta = 0;
+  if (objetoEntradas.adult !== undefined) {
+    resposta += objetoEntradas.adult * data.prices.adult;
+  }
+  if (objetoEntradas.senior !== undefined) {
+    resposta += objetoEntradas.senior * data.prices.senior;
+  }
+  if (objetoEntradas.child !== undefined) {
+    resposta += objetoEntradas.child * data.prices.child;
+  }
+  return resposta;
 }
 
 module.exports = { calculateEntry, countEntrants };
