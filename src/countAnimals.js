a7953936f7ac1288e0a09030d471b543d82b96ff
Guilherme Polippo <luispolippo@hotmail.com>
2021-09-11 17:10:44 -0300

    Adicionado requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+const listAnimals = () => {
+  const objAnimals = {};
+  const { species } = data;
+  species.forEach((specie) => {
+    objAnimals[specie.name] = specie.residents.length;
+  });
+  return objAnimals;
+};
+
+function countAnimals(animal = {}) {
   // seu código aqui
+  const { specie, gender } = animal;
+  const { species } = data;
+
+  if (gender === undefined && specie !== undefined) {
+    return species.find((specieElem) => specieElem.name === specie).residents
+      .length;
+  }
+  if (gender !== undefined) {
+    const chosenSpicie = species.find(
+      (specieElem) => specieElem.name === specie,
+    );
+    return chosenSpicie.residents.filter((resident) => resident.sex === gender)
+      .length;
+  }
+  return listAnimals();
 }
 
 module.exports = countAnimals;
