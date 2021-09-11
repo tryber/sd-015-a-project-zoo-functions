f9e3139c8ce54b77622f82a04714d537b533973c
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 04:59:02 -0300

    Requisito cinco

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function contarAnimalEspecifico(animal) {
+  let resposta;
+  const animais = data.species.find((elemento) => elemento.name === animal.specie).residents;
+  if (animal.gender !== undefined) {
+    resposta = animais.reduce((contador, bixo) => {
+      const retornoReduce = bixo.sex === animal.gender ? contador + 1 : contador;
+      return retornoReduce;
+    }, 0);
+  } else {
+    resposta = animais.length;
+  }
+  return resposta;
+}
+
+function countAnimals(bixin) {
+  let resposta = {};
+  if (bixin === undefined) {
+    const especies = data.species.map((elemento) => elemento.name);
+    especies.forEach((elemento) => {
+      resposta[elemento] = data.species.find((bixinho) => {
+        const retornoFind = bixinho.name === elemento;
+        return retornoFind;
+      }).residents.length;
+    });
+  } else {
+    resposta = contarAnimalEspecifico(bixin);
+  }
+  return resposta;
 }
 
 module.exports = countAnimals;
