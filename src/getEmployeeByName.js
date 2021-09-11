6dd5e857d20e0a5b74ab737ed70bc694adbbfe21
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-10 21:21:41 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const toFilter = (x) => x.firstName === employeeName || x.lastName === employeeName;
+  const seiLá = data.employees.find(toFilter);
+  return seiLá === undefined ? {} : data.employees.find(toFilter);
 }
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
