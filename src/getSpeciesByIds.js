5742c2e90c2fd2790f61e7c104d65052df265403
Luan Vitor <luan_vittor@hotmail.com>
2021-09-11 18:19:40 -0300

    Third one done

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,9 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) { 
+function getSpeciesByIds(...ids) {
   return species.filter((specie) => ids.includes(specie.id));
 }
 // adicionei os '...' (parametro rest), pois assim os parametros ficam salvos em uma array
 
 module.exports = getSpeciesByIds;
-
-// const array = species.filter((especie) => {
-//   if  (ids.includes(especie.id)) {
-//     return
-//   }
