5840cc766e59dc9118debb1d08c56d568b5501f7
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-11 19:16:48 -0400

    Soluçao do requisito 2 - project-zoo-functions

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-}
-
+  return species.filter((animal1) => (animal1.name === animal))[0].residents.every((resident) => resident.age >= age);
+  // const residents = animal2[0].residents;
+  // return residents.every((resident) => resident.age >= age);
+};
+console.log(getAnimalsOlderThan('otters',7));
 module.exports = getAnimalsOlderThan;
