b4d96899a80f412ec908ed7283213465acd0cfd6
Caio Lima <caiojlimah@gmail.com>
2021-09-11 00:31:31 -0300

    Requisito 9!

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const idA = employees.find((e) => e.id === id).responsibleFor[0];
+  return species.find((e) => e.id === idA).residents.reduce((o, a) =>
+    ((o[2] > a.age) ? o : [a.name, a.sex, a.age]), [0, 0, 0]);
 }
+
+console.log(getOldestFromFirstSpecies(employees[0].id));
 
 module.exports = getOldestFromFirstSpecies;
