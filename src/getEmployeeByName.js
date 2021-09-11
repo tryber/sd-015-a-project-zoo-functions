2da0ceaed632e33d75a20c2b806f3c1c0a5383de
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-11 08:22:02 -0300

    add getEmployeeByName function

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName) {
+    return data.employees.find((person) =>
+      person.firstName === employeeName || person.lastName === employeeName);
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
