9c656c7679bb7ef65e3ad63c188e81b14eed2629
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 06:22:50 -0300

    Requisito oito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,53 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
+const calendario = {
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  },
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  },
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  },
+  Friday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['tigers', 'otters', 'frogs', 'elephants', 'giraffes'],
+  },
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
+      'lions', 'tigers',
+      'bears', 'penguins',
+      'otters', 'frogs',
+      'snakes', 'elephants',
+    ],
+  },
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  },
+  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
+};
+
+function getSchedule(dayName) {
+  let resposta = {};
+  const diasDaSemana = Object.keys(calendario);
+  const animais = data.species.map((animal) => animal.name);
+  if (dayName === undefined || (!diasDaSemana.includes(dayName) && !animais.includes(dayName))) {
+    resposta = calendario;
+  } else if (diasDaSemana.includes(dayName)) {
+    resposta[dayName] = calendario[dayName];
+  } else {
+    resposta = Object.keys(calendario).filter((dia) => {
+      const respostaFilter = calendario[dia].exhibition.includes(dayName);
+      return respostaFilter;
+    });
+  }
+  return resposta;
 }
 
 module.exports = getSchedule;
