be75de5abbba5f0ec69068d5ea682d4f9094bb04
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 23:17:39 -0300

    Criando dinamicamento o calendario do requisito oito

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,40 +1,36 @@
 const data = require('../data/zoo_data');
 
-const calendario = {
-  Tuesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
-  },
-  Wednesday: {
-    officeHour: 'Open from 8am until 6pm',
-    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
-  },
-  Thursday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
-  },
-  Friday: {
-    officeHour: 'Open from 10am until 8pm',
-    exhibition: ['tigers', 'otters', 'frogs', 'elephants', 'giraffes'],
-  },
-  Saturday: {
-    officeHour: 'Open from 8am until 10pm',
-    exhibition: [
-      'lions', 'tigers',
-      'bears', 'penguins',
-      'otters', 'frogs',
-      'snakes', 'elephants',
-    ],
-  },
-  Sunday: {
-    officeHour: 'Open from 8am until 8pm',
-    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
-  },
-  Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-};
+function horarios(abre, fecha) {
+  return abre === fecha && abre === 0 ? 'CLOSED' : `Open from ${abre}am until ${fecha}pm`;
+}
+
+function animaisExibidos(dia) {
+  let retorno;
+  if (dia !== 'Monday') {
+    retorno = data.species.filter((animal) => animal.availability.includes(dia)).map((bixo) => {
+      const retornoMap = bixo.name;
+      return retornoMap;
+    });
+  } else {
+    retorno = 'The zoo will be closed!';
+  }
+  return retorno;
+}
+
+function contruirCalendario() {
+  const resposta = {};
+  Object.keys(data.hours).forEach((dia) => {
+    resposta[dia] = {
+      officeHour: horarios(data.hours[dia].open, data.hours[dia].close),
+      exhibition: animaisExibidos(dia),
+    };
+  });
+  return resposta;
+}
 
 function getSchedule(dayName) {
   let resposta = {};
+  const calendario = contruirCalendario();
   const diasDaSemana = Object.keys(calendario);
   const animais = data.species.map((animal) => animal.name);
   if (dayName === undefined || (!diasDaSemana.includes(dayName) && !animais.includes(dayName))) {
