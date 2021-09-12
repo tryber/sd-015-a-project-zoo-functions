76170548206d2d7afbd5e496e0a77061009c54f3
Marcelo Adriano <marcelo.gomes.adriano@gmail.com>
2021-09-11 21:35:11 -0300

    Requisito Quatro

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  const empregado = data.employees.find((ze) => ze.id === id);
+  const retorno = managers.some((gerentes) => gerentes === empregado.id);
+  return retorno;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const empregados = data.employees.filter((ze) => ze.managers.includes(managerId));
+  const retornoGetRelated = empregados.map((ze) => `${ze.firstName} ${ze.lastName}`);
+  return retornoGetRelated;
 }
 
 module.exports = { isManager, getRelatedEmployees };
