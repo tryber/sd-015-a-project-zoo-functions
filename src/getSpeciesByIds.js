5de6ae94d1bacafaeba2e7dc022e68df6be90b73
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-12 02:53:12 -0300

    add getRelatedEmployees function

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,6 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
 function getSpeciesByIds(...ids) {
-  // seu código aqui
   return ids.map((id) => data.species.find((specie) => specie.id === id));
 }
 module.exports = getSpeciesByIds;
