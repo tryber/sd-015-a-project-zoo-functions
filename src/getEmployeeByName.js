300087e21be7976d170a3217d5a19ef02b7d204c
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-12 16:06:54 -0300

    requisito 3 ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find(
+    (worker) =>
+      worker.lastName === employeeName || worker.firstName === employeeName,
+  );
 }
 
+// console.log(getEmployeeByName('Emery'));
 module.exports = getEmployeeByName;
