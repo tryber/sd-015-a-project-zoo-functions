623811b43cbf45911578d1183e9341da2ad67b67
Arthur Simões <arthursimoes155@gmail.com>
2021-09-12 12:14:24 -0300

    Adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,15 +1,18 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(id) {}
+function getSpeciesByIds(...ids) {
+  const ArrayObject = ids.map((id) => species.find((element) => element.id === id));
+  return ArrayObject;
+}
 
-/* const actual = getSpeciesByIds(
+const actual = getSpeciesByIds(
   'baa6e93a-f295-44e7-8f70-2bcdc6f6948d',
   '0938aa23-f153-4937-9f88-4858b24d6bce',
   'e8481c1d-42ea-4610-8e11-1752cfc05a46',
   'bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5',
 );
 
-console.log(actual); */
+console.log(actual);
 
 module.exports = getSpeciesByIds;
