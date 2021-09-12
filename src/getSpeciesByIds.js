d4b01697e64993ecf3d9cb7e09f5544dc976e288
Augusto Alves <augusto.malves@yahoo.com.br>
2021-09-12 19:44:40 -0300

    Add 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  return ids.map((id) => data.species.find((species) => id === species.id));
 }
 
 module.exports = getSpeciesByIds;
