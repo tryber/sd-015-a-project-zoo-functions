e24a19dbf15193d84823960d8039c910af011112
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-12 17:49:31 -0300

    requisito 4 ok

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.find((specie) => 
-  specie.name === animal).residents.every((exemplar) => 
-  exemplar.age >= age);
+  return data.species
+    .find((specie) => specie.name === animal)
+    .residents.every((exemplar) => exemplar.age >= age);
 }
 
 // console.log(getAnimalsOlderThan('otters', 7))
