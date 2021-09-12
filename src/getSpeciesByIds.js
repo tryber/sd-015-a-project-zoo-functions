fd922563af8b8a54fd4092a46e46c8bfee2dfa0f
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-12 15:37:01 -0300

    requisito 2 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return ids.reduce((acc, idCur) => (!ids) ? [] : acc.concat(data.species
-    .find((element) => element.id === idCur)), []);
+  // return ids.reduce((acc, idCur) => (!ids) ? [] : acc.concat(data.species
+  //   .find((element) => element.id === idCur)), []);
+  return data.species.filter((spiecie) => ids.includes(spiecie.id));
 }
 
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'))
 module.exports = getSpeciesByIds;
