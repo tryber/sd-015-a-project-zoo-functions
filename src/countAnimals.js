e2f165e1756f0c2cfd8cb90ff1b4aa3f965adef5
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-13 13:50:15 -0300

    lint 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,9 +4,11 @@ const data = require('../data/zoo_data');
 function countAnimals(animal) {
   const allAnimals = {};
   if (animal === undefined) {
-    species.forEach((element) =>  allAnimals[element.name] = element.residents.length); 
-}
-return allAnimals;
+    species.forEach((element) => {
+      allAnimals[element.name] = element.residents.length;
+    });
+  }
+  return allAnimals;
 }
 
 module.exports = countAnimals;
