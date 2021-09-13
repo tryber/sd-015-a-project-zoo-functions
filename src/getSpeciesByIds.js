8139e76b6837aa10bf9ba05271d30b05481b8bad
Leandro Hespanhol <leandrohespanhol@gmail.com>
2021-09-13 14:28:03 -0300

    leandro-hespanhol-zoo-functions

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
+  if (ids === undefined) {
+    return [];
+  }
+  return console.log(
+    data.species.filter(
+      (ele) => ele.id === '0938aa23-f153-4937-9f88-4858b24d6bce',
+    ),
+  );
 }
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
+
+// Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids;
