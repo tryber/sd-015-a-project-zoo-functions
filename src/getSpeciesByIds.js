31ceea8a40f42ae60333effdb7c9ceaada3f53fa
leonardo <leonardo.siepierski@outlook.com>
2021-09-13 17:16:44 -0300

    Consertar readme

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  
+  let empty = [];
+  let objects;
+  if (ids === undefined) return empty;
+  else {
+    objects = species.filter((animal) => (animal.id === ids));
+    return objects;
+  }
 }
 
 module.exports = getSpeciesByIds;
