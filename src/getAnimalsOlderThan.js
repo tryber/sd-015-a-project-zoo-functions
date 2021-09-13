c889d79254d017413f27371caffa5ce0f7ce83c1
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-09-13 14:00:53 -0300

    requisitos 1 e 2 done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const findAnimal = species.find((specie) => specie.name === animal).residents;
+  const countAge = findAnimal.every((resident) => resident.age >= age);
+  return countAge;
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  // if (ids === undefined) return [];
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
