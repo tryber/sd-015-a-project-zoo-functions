5a422a1fb6feaa0eb64cd5940e5e8dca4c22f739
arturbacilla <artur.bacilla1@gmail.com>
2021-09-13 18:56:05 -0300

    Requisite 3 - Find employee by name (either last or first)

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  return !employeeName ? {} : data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
