adc0a0541db5bb8985e1d56763fc44e7a4573dbe
Maria Laura <marialaurap27@gmail.com>
2021-09-13 18:18:10 -0300

    r2 e r3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const nomeAnimal = species.filter((item) => item.name === animal);
+  const residentes = nomeAnimal.map((item2) => item2.residents);
+  return residentes.pop().every((item3) => item3.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const filtro = employees.filter((item) =>
+    item.firstName === employeeName || item.lastName === employeeName);
+  return filtro.pop();
 }
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
