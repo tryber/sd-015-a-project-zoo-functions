75c7c50d5696071ea0fc113e5be6dcf3cf83ba0b
neandermenezes <neander2801@gmail.com>
2021-09-13 15:50:59 -0300

    requisito 2 e 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const {
+  species,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((type) => type.name === animal).residents
+    .every((yearsOld) => yearsOld.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const {
+  employees,
+} = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return employees.find((id) => id.firstName === employeeName || id.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
