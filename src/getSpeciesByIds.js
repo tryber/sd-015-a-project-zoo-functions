6d72fa1ad0cb6f95374f90a9650dbe0f047c9619
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-13 18:28:02 -0300

    starts requirement 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const getSpeciesByIds = (ids) => {
+
+  return species.filter((specie) => specie.id === ids);
+
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log();
