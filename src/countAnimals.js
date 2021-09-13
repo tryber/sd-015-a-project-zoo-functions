e5e1c2a4de7bbcefb3f4cac6248d6750237252d9
Fernando Sena <fernandosenacruz@gmail.com>
2021-09-13 18:57:31 -0300

    requisito 5 ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,24 @@
 const data = require('../data/zoo_data');
 
-// function xibil (animal) {
-//   return data.species.find((specie) => specie.name === animal.specie).residents.length;
-// }
+function xibil(animal) {
+  return data.species.find((specie) => specie.name === animal.specie).residents
+    .length;
+}
+function xibil2(animal) {
+  return data.species.find(
+    (specie) => specie.name === animal.specie,
+  ).residents.filter((resident) => resident.sex === animal.gender).length;
+}
 
-// function countAnimals(animal) {
-//   let obj = {};
-//   return data.species.map((specie) => {
-//   if (animal === undefined) obj.specie = xibil(specie);
-//   return obj;
-// }
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return data.species.reduce((acc, specie) =>
+      Object.assign(acc, { [specie.name]: specie.residents.length }), {});
+  } if (animal.gender) {
+    return xibil2(animal);
+  }
+  return xibil(animal);
+}
 
-// console.log(countAnimals());
-// // module.exports = countAnimals;
+// console.log(countAnimals({ specie: 'elephants', gender: 'male' }));
+module.exports = countAnimals;
