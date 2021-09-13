1420e15c4444a2ba92f4c1939e725e94fbf431e4
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-09-13 17:54:55 -0300

    Fixing lint issues

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 // Function to read the array 'ids', find each id in the list of species and return the object referred to that
 function getSpeciesByIds(...ids) {
-  return ids.map((animalId) => data.species.find((specie) => specie.id === animalId));  
+  return ids.map((animalId) => data.species.find((specie) => specie.id === animalId));
 }
 
 module.exports = getSpeciesByIds;
