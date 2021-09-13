b96cb3a0438e9fa76976707753068bb4fc001660
João Felipe P <jf.pelliccione@gmail.com>
2021-09-13 15:40:30 -0300

    Conclusão do requisito 4!

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const managersArray = data.employees.map((employee) => employee.managers); // Se está nesse Macroarray, significa que a pessoa é manager de alguém.
+
+  const organizedManagersArray = managersArray.reduce((orgManArray, microArray) =>
+    orgManArray.concat(microArray));
+
+  return organizedManagersArray.some((manager) => manager === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const relatedEmployees = data.employees.filter((employee) =>
+      employee.managers.includes(managerId) === true);
+    return relatedEmployees.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
