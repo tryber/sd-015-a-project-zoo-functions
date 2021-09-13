85283d64a73c16f20ef15df78a427eafad4099d6
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-13 15:34:24 -0300

    requisito 3 c/ lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const getEmployee = employees.filter((employee) => {
+    if (employee.lastName === employeeName || employee.firstName === employeeName) {
+      return employee;
+    } return null;
+  });
+  return (getEmployee[0]) ? getEmployee[0] : {};
 }
+
+console.log(getEmployeeByName('Emery'));
 
 module.exports = getEmployeeByName;
