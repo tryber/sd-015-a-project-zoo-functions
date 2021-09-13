dc6a647adab86a745d9b79e598358a136fd7731f
Lucas Macedo <lucasrrm22@gmail.com>
2021-09-13 15:30:56 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) {
+    return [];
+  } return data.species.filter((element) => ids.includes(element.id));
 }
 
 module.exports = getSpeciesByIds;
