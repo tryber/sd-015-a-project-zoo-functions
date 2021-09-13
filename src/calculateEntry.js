50b99ab811ec2b6a3c76e67844a0b2b5c84e2bd0
lucas-morais <lucasmoraisss@gmail.com>
2021-09-13 19:18:15 -0300

    requisito 6 - calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const people = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      people.child += 1;
+    } else if (person.age >= 50) {
+      people.senior += 1;
+    } else {
+      people.adult += 1;
+    }
+  });
+  return people;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = {}) {
+  if (Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  console.log(countEntrants(entrants));
+  const { child: numberChild, adult: numberAdult, senior: numberSenior } = countEntrants(entrants);
+  const { child: priceChild, adult: priceAdult, senior: priceSenior } = data.prices;
+  return priceChild * numberChild + priceAdult * numberAdult + priceSenior * numberSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
