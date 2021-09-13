fa27e7adc88817b1aafc62541ab602fde9e8eea3
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-09-13 18:55:56 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = data.species.find((element) => element.name === animal).residents;
+  // console.log(findAnimal);
+  return findAnimal.every((element) => element.age >= age);
 }
 
+// eslint-disable-next-line sonarjs/no-use-of-empty-return-value
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
