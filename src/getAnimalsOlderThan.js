53da32611a050dfaa3b394a5e34957671c7b7ecc
Omar Merljak <omerljak@gmail.com>
2021-09-13 17:33:20 -0300

    requisito 2 getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
-
+const getAnimalsOlderThan = (animal, age) => data.species
+.find((specie) => specie.name === animal).residents
+.every((resident) => resident.age > age);
+//transformada a função em uma arrow-function.. que recebe dois parametros(animal,age).. busca coma função find entre as especies o nome da especie. Logo busca entre os residentes daquela espécie, se todos tem a idade minima especificada.
+console.log(getAnimalsOlderThan('otters', 7))//ERRO NESTE TESTE
 module.exports = getAnimalsOlderThan;
