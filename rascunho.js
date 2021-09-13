2f8d624cde0f3fb59494710814d5f63c15b1f5d2
Italo Sergio <italosergio@graduate.org>
2021-09-13 14:48:01 -0300

    rm rascunho.js

--- rascunho.js
+++
@@ -1,9 +0,0 @@
-const selectSpecie = species
-    .filter((element) => element.name === animal);
-    console.log(selectSpecie);
-  const selectResidents = species
-  .map((element) => element.residents);
-    console.log(selectResidents);
-  const minAge = selectResidents.some((element) => element.age < age);
-    console.log(minAge);
-    // return minAge; 
