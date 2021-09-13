c5ebbc83a164e50a408cafcddc1c6fabb18715ac
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-13 19:26:10 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let filterByName = {};
+  if (employeeName !== undefined) {
+    filterByName = data.employees.filter((worker) => {
+      let workerData = null;
+      if (worker.firstName === employeeName || worker.lastName === employeeName) {
+        workerData = worker;
+      }
+      return workerData;
+    });
+    return filterByName[0];
+  }
+  return filterByName;
 }
 
 module.exports = getEmployeeByName;
