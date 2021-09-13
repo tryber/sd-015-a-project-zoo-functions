df4e57133d8fd2e88c250c109922c254faeaec1b
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-09-13 19:35:10 -0300

    Exercise 3 solved

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 
-// Function to read the array 'ids', find each id in the list of species and return the object referred to that
+// Function to read the array 'ids', find each id in the list of species and return the referent object
 function getSpeciesByIds(...ids) {
   return ids.map((animalId) => data.species.find((specie) => specie.id === animalId));
 }
