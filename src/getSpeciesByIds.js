65b8259028b60766bc7d3f03c8733cf9b8e132de
Ismael Peccin <ismaelpeccin@gmail.com>
2021-09-13 18:12:37 -0300

    Corrigindo requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.find(({ id }) => ids.includes(id));
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
