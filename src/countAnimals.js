ce9eca059213c0e4583c19bf4f5a9aa7d004a74e
Victor Araujo <victor_emanuel9@hotmail.com>
2021-09-13 20:15:18 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
-
+//  https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/fromEntries
 function countAnimals(animal) {
-  // seu código aqui
+  const { species } = data;
+  if (!animal) {
+    const allAnimals = Object.fromEntries(species.map((e) => [e.name, e.residents.length]));
+    return allAnimals;
+  }
+  const { specie: name, gender } = animal;
+  if (name && !gender) {
+    const num = species.find((e) => e.name === name);
+    return num.residents.length;
+  }
+  const animalWithGender = species.find((e) => e.name === name)
+    .residents.filter((e) => e.sex === gender).length;
+  return animalWithGender;
 }
-
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
