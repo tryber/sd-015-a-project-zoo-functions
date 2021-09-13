d555a1927316e20d5b4682427ab5c036fa23e7f9
GTmudin <testederealismo@gmail.com>
2021-09-13 16:41:00 -0300

    teste lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  dasfdasfsafas
+function getSpeciesByIds(...ids) {
+  const resultado = species.filter(elemento => ids.includes(elemento.id));
+  return resultado;
 }
-
 module.exports = getSpeciesByIds;
