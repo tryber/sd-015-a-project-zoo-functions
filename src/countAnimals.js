77521c95bc39cb0d89ade9dccded8f1842b6070d
William Schmidt <9williamschmidt@gmail.com>
2021-09-13 17:56:16 -0300

    ajuste

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,6 +1,19 @@
 const data = require('../data/zoo_data');
+// 5. Implemente a função countAnimals
+// Esta função é responsável por contabilizar a quantidade de animais de cada espécie.
 
-function countAnimals(animal) {
+// Observações técnicas
+
+// Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
+// Com o argumento { specie: 'penguins' }, retorna um número, a quantidade de pinguins no zoológico;
+// Com o argumento { specie: 'giraffes', gender: 'female' }, retorna um número, a quantidade de girafas do gênero feminino.
+// O que será avaliado
+
+// Sem parâmetros, retorna as espécies e sua quantidade;
+// Recebendo como parâmetro um objeto com a chave specie, retorna um número, a quantidade de animais daquela espécie;
+// Recebendo como parâmetro um objeto com a chave specie e gender, retorna um número, a quantidade de animais daquela espécie, no gênero selecionado.
+
+function countAnimals(...animal) {
   // seu código aqui
 }
 
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,15 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+// 1 - isManager - que será responsável por verificar se uma pessoa colaboradora é gerente ou não. O retorno dessa função deve ser um booleano: true ou false;
+const isManager = (id) => {
+  data.employees.find((e) => e.managers.find(());
+};
+isManager();
+
+// 2 - getRelatedEmployees - que utiliza a primeira função para apresentar as seguintes saídas:
+
+// se for uma pessoa colaboradora gerente, deve retornar um array contendo os nomes das pessoas colaboradoras que ela é responsável;
+// se não for uma pessoa colaboradora gerente, deverá ser lançado um erro gerado com a função construtora Error da biblioteca padrão do JavaScript com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
