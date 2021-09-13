9ccbefdc5cd40694d87fe5b9d3ad5b089d5e4619
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-13 18:01:55 -0300

    requisito 5 c/ lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+function countAnimals(obj) {
   // seu código aqui
+  const everySpecie = {};
+  const gender = {};
+  species.forEach((specie) => {
+    const { name, residents } = specie;
+    if (obj) gender[name] = residents.filter((resident) => resident.sex === obj.gender).length;
+    everySpecie[name] = residents.length;
+  });
+
+  if (!obj) {
+    return everySpecie;
+  } if (obj.gender) {
+    return gender[obj.specie];
+  } return everySpecie[obj.specie];
 }
 
+console.log(countAnimals({ specie: 'penguins', gender: 'female' }));
 module.exports = countAnimals;
