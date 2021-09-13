06515652278e757501f177824ee9b8df19f6d56e
William <william.mk1@outlook.com>
2021-09-13 18:33:30 -0300

    .

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,7 +1,9 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const managersIds = ['9e7d4524-363c-416a-8759-8aa7e50c0992', 'fdb2543b-5662-46a7-badc-93d960fdc0a8', '0e7b460e-acf4-4e17-bcb3-ee472265db83'];
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managersIds = [stephanieId, olaId, burlId];
 
 function isManager(id) {
   return managersIds.includes(id);
