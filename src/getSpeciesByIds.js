ac9ba1617b92ab1773f1451bbd39fec1bf028f3e
Gabriel <gncorreia@protonmail.com>
2021-09-13 16:21:36 -0300

    requisito 1 feito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-}
+  const species = [];
 
+  if (ids.length === 0) return species;
+
+  // Consultei o repositório do João Felipe Pelliccione para resolver essa parte: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/42/files
+  ids.forEach((id) => species.push(data.species.find((animal) => animal.id === id)));
+
+  return species;
+}
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '01422318-ca2d-46b8-b66c-3e9e188244ed'));
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
