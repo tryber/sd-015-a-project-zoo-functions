691cee0b4dde0009fe77c31a0501f2b5aa7cf9da
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-13 15:09:50 -0300

    Termina req 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const [...species] = data.species;
+  const animals = species.find((specie) => specie.name === animal);
+  const [...residents] = animals.residents;
+  const allIsOlderThan = residents.every((resident) => resident.age >= age);
+  return allIsOlderThan;
 }
 
+console.log(getAnimalsOlderThan('penguins', 10));
 module.exports = getAnimalsOlderThan;
