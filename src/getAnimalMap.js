8f0ca9da7209bcdb5dbc79180705a0651ec80f2b
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-13 18:20:38 -0300

    Loading...

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,46 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
+// pegando localização
+const ne = species.filter((element) => element.location === 'NE');
+const nw = species.filter((element) => element.location === 'NW');
+const se = species.filter((element) => element.location === 'SE');
+const sw = species.filter((element) => element.location === 'SW');
+
+const getLocation = () => {
+  return ({
+    NE: ne.map((value) => value.name),
+    NW: nw.map((value) => value.name),
+    SE: se.map((value) => value.name),
+    SW: sw.map((value) => value.name),
+  })
+}
+// pegando residentes por localização e nome
+const namesByLocation = () => {
+  return ({
+    NE: ne.map((element) => ({
+      [element.name]: element.residents.map((value) => value.name)
+    })),
+    NW: nw.map((element) => ({
+      [element.name]: element.residents.map((value) => value.name)
+    })),
+    SE: se.map((element) => ({
+      [element.name]: element.residents.map((value) => value.name)
+    })),
+    SW: sw.map((element) => ({
+      [element.name]: element.residents.map((value) => value.name)
+    })),
+  })
+}
+
+console.log(namesByLocation());
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return getLocation();
+  }
+  else if (options.includeNames) {
+    return namesByLocation();
+  }
 }
 
 module.exports = getAnimalMap;
