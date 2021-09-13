a5e3bf22f9398da84d450f74dda0387e12d06ed7
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-09-13 15:50:35 -0300

    requisito3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName !== undefined) {
+    return data.employees.find((Names) => Names
+      .firstName === employeeName || Names.lastName === employeeName);
+  }
+  return {};
 }
 
 module.exports = getEmployeeByName;
