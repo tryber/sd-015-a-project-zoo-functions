da389bce6e9a2322697d3687e5c30ba6d8866ff5
Guarize <pedroguarize@hotmail.com>
2021-09-13 10:04:13 -0300

    Minor change - calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -5,7 +5,7 @@ const countEntrants = (entrants) => {
   if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) {
     return 0;
   }
-  const ages = entrants.map((elem) => elem.age);
+  const ages = entrants.map((visitor) => visitor.age);
   return ({
     child: ages.filter((child) => child < 18).length,
     adult: ages.filter((adult) => adult >= 18 && adult < 50).length,
@@ -16,7 +16,7 @@ const countEntrants = (entrants) => {
 const calculateEntry = (entrants) => {
   const obj = countEntrants(entrants);
   return Object.keys(obj)
-    .reduce(((x, y) => x + obj[y] * prices[y]), 0);
+    .reduce(((acc, curr) => acc + obj[curr] * prices[curr]), 0);
 };
 
 module.exports = { calculateEntry, countEntrants };
