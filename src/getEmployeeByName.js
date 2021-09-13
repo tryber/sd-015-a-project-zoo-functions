cc66df8300c9a1cc83d98465b7c10f7d05ead1a3
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-09-13 16:50:06 -0300

    3º requisito, getEmployeeByName, concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  let result = data.employees.find((employee) => employee.firstName === employeeName);
+  if (typeof result === 'undefined') {
+    result = data.employees.find((employee) => employee.lastName === employeeName);
+  }
+  if (typeof result === 'undefined') {
+    result = {};
+  }
+  return result;
 }
 
 module.exports = getEmployeeByName;
