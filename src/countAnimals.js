aae8a80fa2aa89d036ffa8f4e407d4753314cf05
Renato Moreno <renats.mc@gmail.com>
2021-09-13 18:46:12 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,19 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  // ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Guide/Working_with_Objects
+  const totalOfAnimals = {};
+  if (animal === undefined) {
+    data.species.forEach((type) => { totalOfAnimals[type.name] = type.residents.length; });
+    return totalOfAnimals;
+  }
+  const { specie, gender } = animal;
+  const animalSpecie = data.species.find((name) => name.name === specie);
+  const genderAnimal = animalSpecie.residents.filter((kind) => kind.sex === gender);
+  if (animal.specie && animal.gender) {
+    return genderAnimal.length;
+  }
+  return animalSpecie.residents.length;
 }
 
 module.exports = countAnimals;
