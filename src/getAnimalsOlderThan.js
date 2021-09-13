ff0548d4573a318aff6c647f9b8bbb294a952aca
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-13 13:43:04 -0300

    Correção no requisito 1 e realização do requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const namedAnimal = data.species.filter((specie) => specie.name === animal);
+  let returnable = true;
+  namedAnimal[0].residents.forEach((resident) => {
+    if (age > resident.age) returnable = false;
+  });
+  return returnable;
 }
+
+console.log(getAnimalsOlderThan('lions', 1000));
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,18 +2,13 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...id) {
   // seu código aqui
-  const { species } = data;
   const allIds = id;
   let returnable = [];
+  console.log(data.species.residents);
   allIds.forEach((currId) => {
-    returnable = [...returnable, ...species.filter((specie) => {
-      if (specie.id === currId) return true;
-      return false;
-    })];
+    returnable = [...returnable, ...data.species.filter((specie) => specie.id === currId)];
   });
   return returnable;
 }
-
-getSpeciesByIds(1, 2);
 
 module.exports = getSpeciesByIds;
