68507400947fd740367f95cb9fc968f3e7c5020f
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-13 17:30:01 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
+function getSpeciesByIds(...ids) {
+  const dataById = [];
+  if (ids.length > 0) {
+    for (let index = 0; index < ids.length; index += 1) {
+      dataById.push(species.filter((specie) => ids[index] === specie.id));
+    }
+    const result = dataById.reduce((total, current) => total.concat(current), []);
+    return result;
+  }
 }
 
 module.exports = getSpeciesByIds;
