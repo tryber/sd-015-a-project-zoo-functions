bdeedaecbe41e1a585ee84a2ff3a75198ed1aebe
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-13 15:36:57 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const species = ids.map((id) => data.species.find(element => element.id === id));
+  return species;
 }
 
 module.exports = getSpeciesByIds;
