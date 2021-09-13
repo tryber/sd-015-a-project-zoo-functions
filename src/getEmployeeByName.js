360b35ef75f32ea1233c4c0aea00d0626ffc1b62
Eric Cruz <effolex@gmail.com>
2021-09-13 12:58:22 -0300

    terceiro requisito concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  // must return an object in any case
+  if (!employeeName) return {};
+
+  const { employees } = data;
+
+  return employees.find(
+    ({ firstName, lastName }) => employeeName === firstName || employeeName === lastName,
+  );
 }
 
 module.exports = getEmployeeByName;
