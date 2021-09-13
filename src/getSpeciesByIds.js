ee3d3df79f3a38adaf162780fbc46a7919b3a7ae
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-09-13 17:46:53 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  // seu código aqui  
+  return data.species.filter((element) => ids.includes(element.id));
 }
 
 module.exports = getSpeciesByIds;
