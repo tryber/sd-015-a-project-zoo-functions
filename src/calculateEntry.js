ec950eb25bcd1574adaa30083610aeb533268ecc
Gabriel <gncorreia@protonmail.com>
2021-09-13 14:19:16 -0300

    iniciando o projeto

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,12 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  console.log(entrants);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  console.log(entrants);
 }
 
 module.exports = { calculateEntry, countEntrants };
