c03cabbcacf75a179ad9cb9a9fe1d0d9fa7c4465
Giovanni Duarte <gikigiio@gmail.com>
2021-09-13 18:02:01 -0300

    Requisito 4 não finalizado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,8 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
-const stephanieId = "9e7d4524-363c-416a-8759-8aa7e50c0992";
-const olaId = "fdb2543b-5662-46a7-badc-93d960fdc0a8";
-const burlId = "0e7b460e-acf4-4e17-bcb3-ee472265db83";
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 const managers = [stephanieId, olaId, burlId];
 
 function isManager(id) {
@@ -12,12 +12,12 @@ function isManager(id) {
 function getRelatedEmployees(managerId) {
   const managerResult = isManager(managerId);
   if (managerResult === false) {
-    throw new Error("O id inserido não é de uma pessoa colaboradora gerente!");
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  let getResponsabilites = data.employees.find(
-    (elem) => elem.id === managerId
+  const getResponsibilities = data.employees.find(
+    (elem) => elem.id === managerId,
   ).responsibleFor;
-  console.log(getResponsabilites);
+  console.log(getResponsibilities);
 }
 
 module.exports = { isManager, getRelatedEmployees };
