35d251029d2984cdee4e2af8c8a66bc9c62cd36d
Lygia Dias <lygiiadias@gmail.com>
2021-09-13 18:38:44 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (typeof employeeName === 'undefined') return {}; // Se o parametro for indefnido, retorna objeto vazio
+  return data.employees.find((personName) => personName.firstName === employeeName
+  || personName.lastName === employeeName);
+  // Retorna a primeira condição verdadeira em que o parâmetro seja igual o firstName ou lastName dentro do array employees.
 }
 
 module.exports = getEmployeeByName;
