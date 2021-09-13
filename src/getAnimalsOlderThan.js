58735c8e15215314659ff9b60a711f616c889130
Valdeci <moura97.valdeci@gmail.com>
2021-09-13 16:00:12 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalFinder = species.find((specie) => specie.name === animal);
+  return animalFinder.residents.every((currentAnimal) => currentAnimal.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
