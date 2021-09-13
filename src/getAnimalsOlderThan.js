06e25be024952323dc8fc87010b8905111d7d760
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-13 16:31:29 -0300

    segundo requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
-
+const getAnimalsOlderThan = (animal, age) =>
+  data.species
+    .find((specie) => specie.name === animal)
+    .residents.every((residentAge) => residentAge.age > age);
 module.exports = getAnimalsOlderThan;
