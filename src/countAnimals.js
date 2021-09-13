f46b15c7490a2b8c29a7de39d43447be362d223e
bruholanda <brunaholandacunha7@gmail.com>
2021-09-13 18:26:23 -0300

    Requirement 5; Finished

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const objAnimals = {};
+    data.species.forEach((value) => {
+      objAnimals[value.name] = value.residents.length;
+    });
+    return objAnimals;
+  }
+
+
+  const findAnimal = data.species.find((value) => value.name === animal.specie);
+  if (Object.keys(animal).length === 1) {
+    return findAnimal.residents.length;
+  }
+
+  const genderPopularity = findAnimal.residents.filter((value) => value.sex === animal.gender);
+  return genderPopularity.length;
 }
 
 module.exports = countAnimals;
