83d669c4d4a04ec0e57cc176f5a87423f8045611
Gabriel <gncorreia@protonmail.com>
2021-09-13 17:04:59 -0300

    requisito 2 lint fix

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,13 +3,9 @@ const data = require('../data/zoo_data');
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
   // usar o every
-  const animals = data.species.find((species) => {
-    return species.name === animal;
-  });
+  const animals = data.species.find((species) => species.name === animal);
 
-  return animals.residents.every((species) => {
-    return species.age > age;
-  });
+  return animals.residents.every((species) => species.age > age);
 }
 console.log(getAnimalsOlderThan('otters', 7));
 console.log(getAnimalsOlderThan('lions', 5));
