6a6f25ba65128ed948731e6ef1257d3301b29119
Gustavo Lima <lima_gus@outlook.com>
2021-09-13 20:46:23 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  //  não recebe argumento
+  if (animal === undefined) {
+    const obj = {};
+    data.species.forEach((specie) => { obj[specie.name] = specie.residents.length; });
+    return obj;
+  }
+  //  object destructuring - consultado no material do course
+  const { specie, gender } = animal;
+  const getSpecie = data.species.find((addSpecie) => addSpecie.name === specie);
+  const getGender = getSpecie.residents.filter((addGender) => addGender.sex === gender);
+
+  //  recebe dois argumentos
+  if (specie && gender) {
+    return getGender.length;
+  }
+  //  recebe um argumento
+  return getSpecie.residents.length;
 }
 
 module.exports = countAnimals;
