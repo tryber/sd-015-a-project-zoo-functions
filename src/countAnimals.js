4524f770c546317d2065f253da381524a32a259b
William <william.mk1@outlook.com>
2021-09-13 20:26:18 -0300

    .

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const findAnimal = (animalSpecie) => data.species.find((chosenAnimal) => chosenAnimal.name === animalSpecie);
 
+const getAnimalGender = (animal) => findAnimal(animal.specie).residents.filter((getGender) => getGender.sex === animal.gender).length;
+
+function countAnimals(animal) {
+  if (animal === undefined) {
+    return data.species.reduce((animals, cur) => Object.assign(animals, {[cur.name]: cur.residents.length}), {});
+  } 
+  else if (animal.specie && !animal.gender) {
+    return findAnimal(animal.specie).residents.length;
+  }
+  else if (animal.gender) {
+    return getAnimalGender(animal);
+  }
+}
+console.log(countAnimals({ specie: 'penguins', gender: 'female' }));
 module.exports = countAnimals;
