77180222e86c1796067b64e37c229ded0fc0ef88
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-09-13 16:18:38 -0300

    Requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const idade = species
+    .filter((specie) => specie.name === animal)
+    .map((specie) => specie.age === age)
+    .every((specie) => specie.age > 7);
+
+  return idade;
 }
+
+console.log(getAnimalsOlderThan('otters', 7));
 
 module.exports = getAnimalsOlderThan;
