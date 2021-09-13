d0025f48ea7cd8a3fd37a9d3592e5ac16f26c780
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-13 16:12:23 -0300

    Termina req 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const [...employees] = data.employees;
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  // Usando some pois retorna True or False
+  return managers.some((manager) => manager === id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employeeName = employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employeeByManager) => `${employeeByManager.firstName} ${employeeByManager.lastName}`);
+  return employeeName;
 }
 
 module.exports = { isManager, getRelatedEmployees };
