1f079d893cf8c769ecbf7f7a923de2e144a6b793
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-13 17:14:27 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalSearched = data.species.filter((specie) => specie.name === animal);
+  return animalSearched[0].residents.every((being) => being.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
