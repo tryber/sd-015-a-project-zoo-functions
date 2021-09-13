9d59a74ef7cd6323dc398463451aca4ef140a733
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-09-13 20:01:33 -0300

    6º requisito, calculateEntry, concluido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,27 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const result = {
+    child: entrants.filter((visitor) => visitor.age < 18).length,
+    adult: entrants.filter((visitor) => visitor.age > 17 && visitor.age < 50).length,
+    senior: entrants.filter((visitor) => visitor.age > 49).length,
+  };
+  return result;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  let result = 0;
+  const visitors = countEntrants(entrants);
+  const priceTable = data.prices;
+  Object.keys(priceTable).forEach((key) => {
+    result += priceTable[key] * visitors[key];
+  });
+
+  return result;
 }
 
 module.exports = { calculateEntry, countEntrants };
