9a455f2901b7bc56c00981386b5905238cacbb8a
Braien <braienmp@outlook.com>
2021-09-13 15:25:27 -0300

    corrige lint da funçao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,9 @@
-const zoo_data = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   return (ids.length === 0) ? [] : ids.map((id) =>
-    zoo_data.species.find((specie) => specie.id === id));
+    data.species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
