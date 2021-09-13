fa3027cc5c597ae4fee8cf1f4b4ff69e91b41b00
Braien <braienmp@outlook.com>
2021-09-13 15:16:35 -0300

    implementa funçao getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const zoo_data = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return (ids.length === 0) ? [] : ids.map((id) =>
+    zoo_data.species.find((specie) => specie.id === id));
 }
 
 module.exports = getSpeciesByIds;
