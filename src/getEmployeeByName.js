a715e809b4fa60fdb92c354e12429ae6e544bfeb
Anderson Alencar <anderson23alencar@gmail.com>
2021-09-13 18:05:38 -0300

    Requisito 3 - getEmpoyleeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(...employeeName) {
-  return data.employees.find((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
+function getEmployeeByName(employeeName) {
+  if (typeof employeeName === 'undefined') {
+    return {};
+  }
+  return employees.find((emp) => emp.firstName === employeeName || emp.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -8,5 +8,3 @@ function getSpeciesByIds(...ids) {
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
-
-// INCOMPLETO
