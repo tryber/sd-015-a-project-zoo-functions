918d45672b27868bff829d2652978082089e1115
onoue9 <filipeflom@gmail.com>
2021-09-13 16:58:32 -0300

    requisito 5 realizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
+const nullParam = () => {
+  const allAnimals = {};
+  data.species.forEach((element) => { (allAnimals[element.name] = element.residents.length); });
+  return allAnimals;
+};
+
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) return nullParam();
+  const animSpecie = data.species.find((element) => element.name === animal.specie);
+  if (animal.gender === undefined) return animSpecie.residents.length;
+  const animGender = animSpecie.residents.filter((element) => element.sex === animal.gender);
+  return animGender.length;
 }
 
 module.exports = countAnimals;
