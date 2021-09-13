036f00acf3489d3e3fa90b7fd3d882753f6318a2
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-13 18:03:41 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,29 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const entrantsCount = {
+    child: 0,
+    adult: 0,
+    senior: 0,
+  };
+  entrants.forEach((entrant) => {
+    if (entrant.age >= 50) {
+      entrantsCount.senior += 1;
+    } else if (entrant.age < 18) {
+      entrantsCount.child += 1;
+    } else {
+      entrantsCount.adult += 1;
+    }
+  });
+  return entrantsCount;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (!entrants || !entrants.keys) return 0;
+  const entrantscount = Object.values(countEntrants(entrants));
+  const price = [20.99, 49.99, 24.99];
+  return entrantscount.map((entrant, i) => entrant * price[i]).reduce((a,b) => a + b);
 }
 
 module.exports = { calculateEntry, countEntrants };
