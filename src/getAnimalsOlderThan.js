70d1864cbcd830ae9612561329b1ddec0a180995
Lygia Dias <lygiiadias@gmail.com>
2021-09-13 18:15:22 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((specie) => specie.name === animal).residents // Através do find retorna o primeiro objeto (dentro de espécies) em que o nome da espécie seja igual ao parâmetro.
+    .every((specieAge) => specieAge.age >= age); // Every vai percorrer todo o array (resident) da especie retornada e verificar se o parametro corresponde com a idade mínima estabelecida.
 }
-
 module.exports = getAnimalsOlderThan;
