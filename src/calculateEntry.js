40114f228c7db05c1093ebf1e021dbf44d12b95a
Alex <alex_cubas1.0@hotmail.com>
2021-09-13 19:37:22 -0300

    exercise 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
+function countEntrants(entrants1) {
+  const pessoas = {
+    child: entrants1.filter((element) => element.age < 18).length,
+    adult: entrants1.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants1.filter((element) => element.age >= 50).length,
+  };
+  return pessoas;
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants1) {
+  if (entrants1 === undefined || Object.entries(entrants1).length === 0) {
+    return 0;
+  }
+  const valores = Object.entries(countEntrants(entrants1));
+  console.log(valores);
+  const precos = valores.reduce((result, element) => result + prices[element[0]] * element[1], 0);
+  return precos;
 }
-
+// console.log(calculateEntry(entrants));
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.map((element) => ({ [element.name]: element.residents.length,
+    }));
+  }
 }
 
 module.exports = countAnimals;
