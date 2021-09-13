d6fc24ab6354462ce80d582ea3345ac88aea0eef
lucas-morais <lucasmoraisss@gmail.com>
2021-09-13 15:04:35 -0300

    requisito 2 - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { residents } = data.species.find((animalSpecie) => animalSpecie.name === animal);
+  return residents.every((resident) => resident.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
