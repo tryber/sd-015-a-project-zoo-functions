9a718981f73929fac45ce8afcfb5eeb2c2786700
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-13 16:10:00 -0300

    getAnimalsOlderThan complete

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const filteredArr = data.species.find((animalSpecie) => animalSpecie.name.includes(animal));
+  return filteredArr.residents.every((aux) => aux.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
+// const filteredArr2 = data.species.find((animalSpecie) => animalSpecie.name.includes('lion'));
+// console.log(filteredArr2);
+console.log(getAnimalsOlderThan('lion', 10));
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
+const zooData = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+/* Esta função é responsável pela busca das
+espécies de animais por id. Ela retorna um array contendo
+as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids. */
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const especiesData = zooData.species;
+  return especiesData.filter((animals, index) => (
+    animals.id === ids[index]
+  ));
 }
 
 module.exports = getSpeciesByIds;
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
+// console.log(zooData.species);
