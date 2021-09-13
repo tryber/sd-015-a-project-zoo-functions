7695f6c4acd64eb04d45a9c569a9bce8584c609f
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-09-13 17:03:25 -0300

    Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+    const arraySpecie =  species.filter((elemento) => ids.includes(elemento.id));
+    return arraySpecie
+  }
 
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
