841c6224e81b60ad0a229d7124970695a8050726
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-09-13 16:24:10 -0300

    2-requisito

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// retorna a espécie referente ao nome
+const filterEspecie = (especie) => data.species.find((elemento) => elemento.name === especie);
+
+const getAnimalsOlderThan = (animal, age) =>
+  (filterEspecie(animal).residents.every((elemento) => elemento.age > age));
 
 module.exports = getAnimalsOlderThan;
