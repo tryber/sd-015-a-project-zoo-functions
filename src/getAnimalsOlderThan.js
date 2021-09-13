832d67c80d5231b4c9759ea07cc402a56bda3cc2
gabriel <gabriel.f.takaki@gmail.com>
2021-09-13 14:58:02 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// necessário verificar se todos os animais passados como parametro (name) tem idade maior ou igual ao parametro paassado (age)
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animalDesejado = species.find((object) => object.name === animal);
+  return animalDesejado.residents.every((element) => element.age >= age)
+  // realaizei um find pra gerar um array com os animais filtrados pelo name e depois verifiquei a idade dos residentes da especie com o every() 
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
+  
   // seu código aqui
 }
 
