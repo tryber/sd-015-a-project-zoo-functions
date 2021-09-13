6f2b4ca4617ac6c995614f5f6b9703afeef1551f
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-13 11:31:34 -0300

    add calculateEntry function

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const entrantsObj = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      entrantsObj.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      entrantsObj.adult += 1;
+    } else {
+      entrantsObj.senior += 1;
+    }
+  });
+  return entrantsObj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  let total = 0;
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return total;
+  }
+  const entrantsByAge = countEntrants(entrants);
+  total = entrantsByAge.adult * data.prices.adult
+  + entrantsByAge.child * data.prices.child
+  + entrantsByAge.senior * data.prices.senior;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
