a7b86d330c3178ae9342d10fa60da787be052474
Arthur <arthurteixeira0805@gmail.com>
2021-09-13 20:02:02 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
+const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
-}
+  const specs = data.species;
+  const findAnimal = specs.find((spec) => spec.name === animal);
+  const checkAge = findAnimal.residents.every((resident) => resident.age > age);
+  return checkAge;
+};
 
 module.exports = getAnimalsOlderThan;
