cd205579f431e1753b54237ecde1943702123a88
Lucas Pontes Arruda <paluke94@gmail.com>
2021-09-13 21:42:04 +0200

    Requisitos 5 e 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,25 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const people = {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
+  return people;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const ppl = countEntrants(entrants);
+  const priceChild = ppl.child * prices.child;
+  const priceAdult = ppl.adult * prices.adult;
+  const priceSenior = ppl.senior * prices.senior;
+  const totalPrice = priceChild + priceAdult + priceSenior;
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
