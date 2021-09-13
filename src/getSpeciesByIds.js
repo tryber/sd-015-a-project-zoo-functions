b6044da95a29e7f96131a69c7f13b0482e016d7c
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-09-13 17:58:15 -0300

    Requisito 01 - v2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,8 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((s) => {
-    return ids.includes(s.id)
-});
+  return species.filter((s) => ids.includes(s.id));
 }
+
 module.exports = getSpeciesByIds;
