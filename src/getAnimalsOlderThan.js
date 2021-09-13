cc29e4393bf86137c88fe5a19885b6564d35439f
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-13 09:36:52 -0300

    Arrumando lint 01, 02 e 03

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-return species.find((element) => element.name === animal).residents.every((value) => value.age >= age);
+  return species.find((element) => element.name === animal)
+    .residents.every((value) => value.age >= age);
 }
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const resultAnimals = [];
-
 function getSpeciesByIds(...ids) {
   return species.filter((element) => ids.includes(element.id));
-  }
+}
 
 module.exports = getSpeciesByIds;
