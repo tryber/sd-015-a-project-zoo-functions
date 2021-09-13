24460ffb546bc9f1b37870bea7604da9ed4bebc2
carla <carlabazevedoa@gmail.com>
2021-09-13 18:25:34 -0300

    getIds ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-
-}
+const getSpeciesByIds = (...ids) => ids.reduce((animalArray, id)) => 
+animalArray.concat(species.find((animal) => animal.id === id), []);
 
 module.exports = getSpeciesByIds;
