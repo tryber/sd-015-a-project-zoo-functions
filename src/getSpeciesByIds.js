df5e187fbbdeed56e4426888ed74fee94c79164d
Thiago <thiago_ssfr@hotmail.com>
2021-09-13 15:07:55 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return ids;
+  return data.species.filter((animal) => ids.find((primeiro) => animal.id === primeiro));
 }
 
 module.exports = getSpeciesByIds;
