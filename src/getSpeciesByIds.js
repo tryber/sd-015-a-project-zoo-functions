38a2a60026ab6dfb1a37dcc0fabc6dd14674fa38
Gabriel <gncorreia@protonmail.com>
2021-09-13 17:56:53 -0300

    requisito 1 lint fix

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,9 +5,7 @@ function getSpeciesByIds(...ids) {
 
   // Consultei o repositório do Apolo Wilker para resolver essa parte:
   // Link: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/9/files
-  return data.species.filter((animals) => {
-    return ids.includes(animals.id); // O includes é um método que procura se determinado conjunto de strings pode ser encontrado dentro de outra string, e irá retornar true, caso positivo, e false, caso negativo. Neste caso, estamos perguntando se os ids que estão dentro de animals estão presentes dentro do array, criado através do parâmetro rest, ...ids.
-  });
+  return data.species.filter((animals) => ids.includes(animals.id)); // O includes é um método que procura se determinado conjunto de strings pode ser encontrado dentro de outra string, e irá retornar true, caso positivo, e false, caso negativo. Neste caso, estamos perguntando se os ids que estão dentro de animals estão presentes dentro do array, criado através do parâmetro rest, ...ids.
 }
 
 module.exports = getSpeciesByIds;
