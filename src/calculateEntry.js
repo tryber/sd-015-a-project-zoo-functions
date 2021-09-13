00e7cd9f34ad67c5d0f75a824125cbd7fe970a8f
deobrafantinif <deboraferfan@gmail.com>
2021-09-13 16:08:20 -0300

    resolucao do requisito 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,32 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // s
+  // const age = {};
+  // entrants.map((entrant) => {
+  //   if (entrant.age < 18) {
+  //     age['child'] = entrant.age;
+  //   } else if (entrant.age < 50) {
+  //     age['adult'] = entrant.age;
+  //   } else {
+  //     age['senior'] = entrant.age;
+  //   }
+  // });
+
+  // return age;
 }
 
-function calculateEntry(entrants) {
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+console.log(countEntrants(entrants));
+
+function calculateEntry(entrantx) {
   // seu código aqui
 }
 
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,8 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  let manager = false;
+  const numberManagers = employees.find((employee) => employee.id === id)
+    .managers.length;
+  console.log(numberManagers);
+  if (numberManagers < 0) {
+    manager = true;
+  }
+  return manager;
 }
+
+isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad');
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
