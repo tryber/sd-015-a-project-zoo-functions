1a2e78a16abc5be92eaf0ff8a6ba473512f7dafd
William <william.mk1@outlook.com>
2021-09-13 15:15:39 -0300

    .

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,5 +3,5 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   return data.species.filter((specie) => ids.includes(specie.id));
 }
- 
+
 module.exports = getSpeciesByIds;
