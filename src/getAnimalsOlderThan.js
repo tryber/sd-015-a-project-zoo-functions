1e5268a07632331743ad6045af79a52da3fae583
Lucas Pontes Arruda <paluke94@gmail.com>
2021-09-13 12:20:13 +0200

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const findAnimal = data.species.find((creature) => creature.name === animal);
+  const checkAge = findAnimal.residents.every((each) => each.age >= age);
+  return checkAge;
 }
 
 module.exports = getAnimalsOlderThan;
