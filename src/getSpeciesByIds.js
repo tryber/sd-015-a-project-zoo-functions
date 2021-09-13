e14394ab5ad82ca020c8916d58bc09ab2dbd963c
MuriloAmaral <muralha2000@gmail.com>
2021-09-13 15:43:28 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqu
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
