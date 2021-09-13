c9354b16496cc75c4dfa011fdfc7df5fd793a104
Matheus <mathspa98@gmail.com>
2021-09-12 21:37:26 -0300

    iniciando + requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = data.species.find(element => element.name===animal).residents;
+  return getAnimal.every(element=> element.age>=age)
 }
 
 module.exports = getAnimalsOlderThan;
+ 
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...args) {
+  return species.filter(element, index => element.id===args[index]);
 }
 
 module.exports = getSpeciesByIds;
+ 
