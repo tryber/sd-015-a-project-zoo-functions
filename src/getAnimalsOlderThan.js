4736908899af3168a1a7b44adfdea4f238ea88dc
Regiane Santos <re.cfs235@gmail.com>
2021-09-13 15:25:53 -0300

    Requisito 2 getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  // O método find() retorna o valor do primeiro elemento do array data.species que satisfaz o teste. O método find() retorna o valor do primeiro elemento de um array que retornar true para uma função de teste fornecida. Se não houver alguma, retorna undefined.
+  // residents.every vai testar se a condição é verdadeira para todos os anumais dessa especie (data.species.residents.age)
+  // O método every() testa se todos os elementos do array passam pelo teste implementado pela função fornecida. arr.every(callback[, thisArg])
+
+  return data.species.find((object) => object.name === animal).residents
+    .every((object) => object.age > age);
+
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/find
+  // Para ficar fácil de entender, pense que é como filter(), mas, ao invés de retornar outro array/objeto com os correspondentes, retorna somente o primeiro valor/objeto que passe no teste (que é uma função).
+  // https://desenvolvimentoparaweb.com/javascript/every-some-find-includes-javascript/
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/every
 }
 
 module.exports = getAnimalsOlderThan;
