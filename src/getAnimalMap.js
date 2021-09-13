dc4a4fecda0c4ae891aacde2fa78f2ac2cd0eb07
Guarize <pedroguarize@hotmail.com>
2021-09-13 08:15:52 -0300

    Requirement [7] - getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,45 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
-}
+const speciesMap = {
+  NE: species.filter((anim) => anim.location === 'NE').map((specie) => specie.name),
+  NW: species.filter((anim) => anim.location === 'NW').map((specie) => specie.name),
+  SE: species.filter((anim) => anim.location === 'SE').map((specie) => specie.name),
+  SW: species.filter((anim) => anim.location === 'SW').map((specie) => specie.name),
+};
+
+const residentsSearch = (sorted, sex, elem) => {
+  const basicSearch = species.find((anim) => anim.name === elem).residents;
+  if (sorted && sex !== undefined) {
+    return basicSearch.filter((resident) => resident.sex === sex)
+      .map((specie) => specie.name).sort();
+  }
+  if (sorted) {
+    return basicSearch.map((specie) => specie.name).sort();
+  }
+  if (sex !== undefined) {
+    return basicSearch.filter((resident) => resident.sex === sex).map((specie) => specie.name);
+  }
+  return basicSearch.map((specie) => specie.name);
+};
+
+const getAnimalMapOptions = (sorted, sex) => {
+  const names = Object.keys(speciesMap).reduce((acc, curr) => {
+    acc[curr] = speciesMap[curr].map((elem) => ({
+      [elem]: residentsSearch(sorted, sex, elem),
+    }));
+    return acc;
+  }, {});
+  return names;
+};
+
+const getAnimalMap = (options) => {
+  if (typeof options === 'undefined') return speciesMap;
+
+  const { includeNames, sorted, sex } = options;
+  if (includeNames) return getAnimalMapOptions(sorted, sex);
+
+  return speciesMap;
+};
 
 module.exports = getAnimalMap;
