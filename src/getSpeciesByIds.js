d3387e68ee970524fe9753d7c0a7fc95de0eed71
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-09-13 14:59:02 -0300

    começo do projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  if (ids.length === 0) {
+    return [];
+  }
+  return data.species.filter((species) => ids.includes(species.id));
 }
 
 module.exports = getSpeciesByIds;
