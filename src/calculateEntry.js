0a271102753be4b35a407d120465990f3b4039a6
Thiago <thiago_ssfr@hotmail.com>
2021-09-13 20:07:10 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { adult: 0, child: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      obj.child += 1;
+    } else if (element.age < 50) {
+      obj.adult += 1;
+    } else {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.values(entrants).length === 0) return 0;
+  const people = countEntrants(entrants);
+  const adult = (people.adult) * data.prices.adult;
+  const senior = (people.senior) * data.prices.senior;
+  const child = (people.child) * data.prices.child;
+
+  return adult + senior + child;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const obj = {};
+    data.species.forEach(({ name, residents }) => { obj[name] = residents.length; });
+    return obj;
+  }
 }
 
+console.log(countAnimals({ specie: 'penguins' }));
 module.exports = countAnimals;
