315824ed58da2d8808eb5d3a7f6c857d11aa975a
Guarize <pedroguarize@hotmail.com>
2021-09-13 09:51:46 -0300

    Refactoring - getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,18 +1,23 @@
 const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const getSpecies = (employee) => employee.responsibleFor.map((ani) => species
+  .find((spe) => spe.id === ani).name);
+const getLocation = (employee) => employee.responsibleFor.map((ani) => species
+  .find((spe) => spe.id === ani).location);
+
 const singleEmployee = (emplo) => ({
   id: emplo.id,
   fullName: `${emplo.firstName} ${emplo.lastName}`,
-  species: emplo.responsibleFor.map((ani) => species.find((spe) => spe.id === ani).name),
-  locations: emplo.responsibleFor.map((ani) => species.find((spe) => spe.id === ani).location),
+  species: getSpecies(emplo),
+  locations: getLocation(emplo),
 });
 
 const allEmployees = () => employees.map((emp) => ({
   id: emp.id,
   fullName: `${emp.firstName} ${emp.lastName}`,
-  species: emp.responsibleFor.map((ani) => species.find((spe) => spe.id === ani).name),
-  locations: emp.responsibleFor.map((ani) => species.find((spe) => spe.id === ani).location),
+  species: getSpecies(emp),
+  locations: getLocation(emp),
 }));
 
 const getEmployeesCoverage = (idOrName) => {
