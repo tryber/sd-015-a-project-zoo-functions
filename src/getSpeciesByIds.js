3b2b7f234c6cce03001c1df7334d2311437bcfbd
William Schmidt <9williamschmidt@gmail.com>
2021-09-13 15:59:20 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,8 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const getSpeciesByIds = (...ids) => {
-  return data.species.filter((e) => console.log(e.id === ids));
+  if (!ids) return [];
+  return species.filter((e) => (ids.includes(e.id)));
 };
-getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
 
 module.exports = getSpeciesByIds;
