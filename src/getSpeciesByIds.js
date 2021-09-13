f68c8c2e0a02082d997f91ad410d522c7f6c049c
Gabriel Carvalho Maia <maiagabrielc@gmail.com>
2021-09-13 16:52:23 -0300

    requisito 1 ok

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqu
+function getSpeciesByIds(...ids) {
+  const { species } = data;
+  // console.log(species);
+  return species.filter(({ id }) => ids.includes(id));
 }
 
 module.exports = getSpeciesByIds;
