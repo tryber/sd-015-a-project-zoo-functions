de0d22ab9d0d98e6e5c506feb97e73d06bf21bd3
Cla Dávila <clarice.coliveira@yahoo.com.br>
2021-09-13 18:37:30 -0300

    Requisito 6 - Implementa a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const people = {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
+  return people;
+}
+
+/** Source: https://coderwall.com/p/_g3x9q/how-to-check-if-javascript-object-is-empty */
+function isEmpty(obj) {
+  for (const key in obj) {
+    if (obj.hasOwnProperty(key)) { return false; }
+  }
+  return true;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (isEmpty(entrants) || entrants === undefined) {
+    return 0;
+  }
+  const { child, adult, senior } = countEntrants(entrants);
+  return child * data.prices.child + adult * data.prices.adult + senior * data.prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
