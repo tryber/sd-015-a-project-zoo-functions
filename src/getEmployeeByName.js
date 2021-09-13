4017ec811c90422ba2efc52853439425f37f3a74
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-09-13 16:11:10 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const getEmployeeByName = (employeeName) => {
+  const employee = employees.find(({ firstName, lastName }) =>
+    employeeName === firstName || employeeName === lastName);
+
+  if (employee === undefined) return {};
+
+  return employee;
+};
 
 module.exports = getEmployeeByName;
