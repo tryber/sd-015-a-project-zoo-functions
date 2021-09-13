5d69030021a3dd335cadbe40bd5d46359a45afa6
Adadezer <adadezer@gmail.com>
2021-09-13 17:25:36 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === '') return []; // se ids não tiver nenhum valor, retorna um array vazio
+  return species.filter((element) => ids.includes(element.id)); // se no element.id incluir (tiver) o ids (parâmetro), retorna o objeto com aquele id
 }
-
+// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 module.exports = getSpeciesByIds;
