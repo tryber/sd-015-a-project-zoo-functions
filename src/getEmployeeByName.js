ef5decc87fd13f0d8d6c4d0095109f47e578acca
gabriel <gabriel.f.takaki@gmail.com>
2021-09-13 15:01:58 -0300

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  
+  if (employeeName === undefined) return {};
+  return employees.find((fun) => fun.firstName === employeeName || fun.lastName === employeeName);
   // seu código aqui
 }
 
