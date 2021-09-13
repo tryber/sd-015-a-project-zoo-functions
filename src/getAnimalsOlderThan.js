c7321123a4e2bd8151cb3422f78dd5aa29150e23
Guarize <pedroguarize@hotmail.com>
2021-09-13 10:00:24 -0300

    Minor change - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => species.find((elem) => elem.name === animal).residents
-  .every((x) => x.age > age);
+const getAnimalsOlderThan = (animal, age) => species.find((anim) => anim.name === animal).residents
+  .every((resident) => resident.age > age);
 
 module.exports = getAnimalsOlderThan;
