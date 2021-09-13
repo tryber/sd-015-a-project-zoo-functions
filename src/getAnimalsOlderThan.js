44fce8049ef7e65a5fb8a675091761100c4e54d3
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-13 09:20:45 -0300

    Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,6 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.every((value) => (value.name === animal && value.residents.age > age));
-};
-
+return species.find((element) => element.name === animal).residents.every((value) => value.age >= age);
+}
 module.exports = getAnimalsOlderThan;
