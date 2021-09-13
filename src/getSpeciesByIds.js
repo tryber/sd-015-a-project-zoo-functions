4c9bf6e4a4ba65fccfa18e9d5c0b4997f23dbcdb
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2021-09-13 15:57:18 -0300

    correçoes lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,12 +1,11 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  let IdObjects = ids.map(species)
-  if (ids.length === 0) {
-    return [];
-  }
-  return ids
+function getSpeciesByIds(...ids) {
+  // if (ids === undefined) {
+  //   return [];
+  // }
+  // return species.filter((animals, index) => animals.id === ids[index]);
 }
 
 module.exports = getSpeciesByIds;
