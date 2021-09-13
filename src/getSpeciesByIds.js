bdd18e3610d8dafef3c6e6340fb62f04e510a9b2
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-13 14:59:23 -0300

    Termina req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const [...species] = data.species;
+  // const searchById = species.find((specie) => specie.id === ids);
+  const searchById = species.filter((specie) => ids.find((id) => id === specie.id));
+  // const searchById = species.find((specie) => ids.filter((id) => id));
+  return ids === undefined ? [] : searchById;
 }
-
 module.exports = getSpeciesByIds;
