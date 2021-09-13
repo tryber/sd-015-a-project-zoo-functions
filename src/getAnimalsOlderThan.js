eb8e1231fb1f04bebc707055ea276735293798fa
Omar Merljak <omerljak@gmail.com>
2021-09-13 17:42:40 -0300

    requisito 2 getAnimalsOlderThan lint erro

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => data.species
-.find((specie) => specie.name === animal).residents
-.every((resident) => resident.age > age);
-//transformada a função em uma arrow-function.. que recebe dois parametros(animal,age).. busca coma função find entre as especies o nome da especie. Logo busca entre os residentes daquela espécie, se todos tem a idade minima especificada.
-console.log(getAnimalsOlderThan('otters', 7))//ERRO NESTE TESTE
+  .find((specie) => specie.name === animal).residents
+  .every((resident) => resident.age > age);
+
+/* transformada a função em uma arrow-function.. que recebe dois parametros(animal,age).. busca coma função find entre as especies o nome da especie. Logo busca entre os residentes daquela espécie, se todos tem a idade minima especificada.
+console.log(getAnimalsOlderThan('otters', 7)) */
 module.exports = getAnimalsOlderThan;
