2ad6df0ca9acdae44ba0b9656148af33c7feafba
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-09-13 16:13:41 -0300

    Primeiro requisito, getSpeciesByIds, concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+const findSpecie = (id) => data.species.find((specie) => specie.id === id);
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const result = [];
+  ids.forEach((id) => {
+    const specie = findSpecie(id);
+    if (typeof specie !== 'undefined') {
+      result.push(specie);
+    }
+  });
+  return result;
 }
 
 module.exports = getSpeciesByIds;
+
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce','533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
