41280d34ab6050ea06b62028d6edb9525aa824b2
douglas oliveira <douglasgama0@gmail.com>
2021-09-13 16:30:27 -0300

    requisito 1 incompleto, avançando para o requisito 2

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
-const data = require("../data/zoo_data");
+const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  //inicio do projeto
+function getSpeciesByIds(...ids) {
   // seu código aqui
 }
 
