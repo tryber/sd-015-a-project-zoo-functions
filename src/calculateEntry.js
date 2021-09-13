5f21d298a299597ebca615ac0bb8c8875ce3531e
Rafael Augusto Scherer <rafaelaugustscherer@gmail.com>
2021-09-13 18:28:37 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants = []) => {
+  if (!Array.isArray(entrants)) return [];
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+  return entrants.reduce(
+    (acc, { age }) => {
+      if (age < 18) acc.child += 1;
+      else if (age < 50) acc.adult += 1;
+      else acc.senior += 1;
+
+      return acc;
+    },
+    { child: 0, adult: 0, senior: 0 },
+  );
+};
+
+const calculateEntry = (entrants) => {
+  let totalPrice = 0;
+  Object.entries(countEntrants(entrants)).forEach((entrant) => {
+    Object.entries(prices).forEach((price) => {
+      if (price[0] === entrant[0]) totalPrice += price[1] * entrant[1];
+    });
+  });
+
+  return totalPrice;
+};
 
 module.exports = { calculateEntry, countEntrants };
