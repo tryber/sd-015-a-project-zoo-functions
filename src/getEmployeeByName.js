4968bb5248d114592f11351a78005a343c8f1c6d
GTmudin <testederealismo@gmail.com>
2021-09-13 17:38:17 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  
+  if (employeeName === undefined) {
+    return {};
+  }
+
+  const resultado = employees.find((elemento) => elemento.firstName.includes(employeeName)
+                                              || elemento.lastName.includes(employeeName));
+  return resultado;
 }
 
 module.exports = getEmployeeByName;
