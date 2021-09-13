d011a6cb7215e969993ec2427448bba28e6f61cc
Matheus Vaz <matheus.vaz@outlook.com>
2021-09-13 17:36:27 -0300

    requirement 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const firsLastName = employees.find((employee) => employee.firstName === employeeName | employee.lastName === employeeName);
+  return firsLastName;
 }
+
+console.log(getEmployeeByName('Orloff'));
 
 module.exports = getEmployeeByName;
