38bffb0b73a1c75741e91df702b3eae8f8ff2774
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-13 15:03:55 -0300

    Adicionando os requisitos 1 2 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = data.species.filter((element) => element.name === animal);
+  return getAnimal[0].residents.every((element) => element.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return data.employees.find((cur) =>
+    cur.firstName === employeeName || cur.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
