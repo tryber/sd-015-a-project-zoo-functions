e1f3f40972a7d71628aad8fa1e305a1c9415774b
Guilherme Polippo <luispolippo@hotmail.com>
2021-09-13 17:59:51 -0300

    Implementado requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -39,31 +39,55 @@ const listAnimals = () => {
   return { NE, NW, SE, SW };
 };
 
-const getResidents = (location, obj) =>
-  obj[location].map((animalType) => {
-    const names = animalType.residents.map((resident) => resident.name);
+const getResidents = (location, obj, sex) => {
+  let names;
+  if (sex !== undefined) {
+    return obj[location].map((animalType) => {
+      names = animalType.residents
+        .filter((resident) => resident.sex === sex)
+        .map((resident) => resident.name);
+      const newObj = {};
+      newObj[animalType.name] = names;
+      return newObj;
+    });
+  }
+  return obj[location].map((animalType) => {
+    names = animalType.residents.map((resident) => resident.name);
     const newObj = {};
     newObj[animalType.name] = names;
     return newObj;
   });
+};
 
-const getResidentsSorted = (location, obj) =>
-  obj[location].map((animalType) => {
-    const names = animalType.residents.map((resident) => resident.name).sort();
+const getResidentsSorted = (location, obj, sex) => {
+  let names;
+  if (sex !== undefined) {
+    return obj[location].map((animalType) => {
+      names = animalType.residents
+        .filter((resident) => resident.sex === sex)
+        .map((resident) => resident.name).sort();
+      const newObj = {};
+      newObj[animalType.name] = names;
+      return newObj;
+    });
+  }
+  return obj[location].map((animalType) => {
+    names = animalType.residents.map((resident) => resident.name).sort();
     const newObj = {};
     newObj[animalType.name] = names;
     return newObj;
   });
+};
 
-const listAnimalsWithResidents = (sort) => {
+const listAnimalsWithResidents = (sort, sex) => {
   const animals = listAnimalsByLocation();
   const keys = Object.keys(animals);
   const newObj = {};
   keys.forEach((key) => {
     if (sort) {
-      newObj[key] = getResidentsSorted(key, animals);
+      newObj[key] = getResidentsSorted(key, animals, sex);
     } else {
-      newObj[key] = getResidents(key, animals);
+      newObj[key] = getResidents(key, animals, sex);
     }
   });
   return newObj;
@@ -72,11 +96,9 @@ const listAnimalsWithResidents = (sort) => {
 function getAnimalMap(options) {
   // seu código aqui
   if (options === undefined) return listAnimals();
-  const { includeNames, sorted } = options;
-  if (includeNames === true && sorted !== true) return listAnimalsWithResidents();
-  if (includeNames === true && sorted === true) return listAnimalsWithResidents(sorted);
+  const { includeNames, sorted, sex } = options;
+  if (includeNames === true) return listAnimalsWithResidents(sorted, sex);
+  return listAnimals();
 }
-
-console.log(getAnimalMap({ includeNames: true }));
 
 module.exports = getAnimalMap;
