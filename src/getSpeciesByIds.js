c62920a007681ebe989a5aaeb39f05cdf8c65d81
onoue9 <filipeflom@gmail.com>
2021-09-13 14:57:35 -0300

    requisito 1 realizado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  return 'vqv';
+  if (ids[0] === '' && ids[1] === '') return [];
+  return data.species.filter((element) => element.id === ids[0] || element.id === ids[1]);
 }
 
 module.exports = getSpeciesByIds;
