37c35b2e4d9696368a14f559f5743d91d2c425dc
Guarize <pedroguarize@hotmail.com>
2021-09-13 08:09:59 -0300

    Requirement [2] - getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+const getAnimalsOlderThan = (animal, age) => species.find((elem) => elem.name === animal).residents
+  .every((x) => x.age > age);
 
 module.exports = getAnimalsOlderThan;
