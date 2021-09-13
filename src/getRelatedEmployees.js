a24c90438c738fc743ea9ae2369579f5cce59e9b
bruholanda <brunaholandacunha7@gmail.com>
2021-09-13 17:08:48 -0300

    Requirement 4; Finished

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
 const data = require('../data/zoo_data');
 
+let colaborators = [];
+
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((value) => value.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  colaborators = data.employees.filter((value) => value.managers.includes(managerId));
+  colaborators = colaborators.map((value) => `${value.firstName} ${value.lastName}`);
+  return colaborators;
 }
 
 module.exports = { isManager, getRelatedEmployees };
