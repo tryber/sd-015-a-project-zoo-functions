a8fa9d9a1024126c0004e328cf4f00bf02ffa70b
Matheus Vaz <matheus.vaz@outlook.com>
2021-09-13 15:55:16 -0300

    requirement 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  console.log()
+  const arraySpecies = ids.map((id) => species.find((specie) => specie.id === id));
+  return arraySpecies;
 }
+
+console.log(getSpeciesByIds('baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
 
 module.exports = getSpeciesByIds;
