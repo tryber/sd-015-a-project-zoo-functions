f8a721df75da82c9e9c8fcce1908561c5bf99ec9
Augusto Alves <augusto.malves@yahoo.com.br>
2021-09-12 21:47:47 -0300

    add 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employees) => employees.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return data.employees.filter((employees) => employees.managers.includes(managerId))
+      .map((employe) => `${employe.firstName} ${employe.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
