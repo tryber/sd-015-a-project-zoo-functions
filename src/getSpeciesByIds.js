c461fb8c40d1854523f0f4ccfbe18d8e86194ed4
Alex <alex_cubas1.0@hotmail.com>
2021-09-13 16:09:59 -0300

    exercicio 3

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,14 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  console.log(ids);
+// console.log(data.species[2].name);
+
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  return species.filter((animais, index) => animais.id === ids[index]);
 }
+// console.log(getSpeciesByIds(species));
 
 module.exports = getSpeciesByIds;
