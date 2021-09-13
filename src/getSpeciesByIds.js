1b8e65e5d007559ae07fbc398b4edfe6dae093dd
DevJunior21 <junior-wmw@hotmail.com>
2021-09-13 19:35:04 -0400

    inicio e primeiro requisito completo

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { species, id } = require('../data/zoo_data')
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+/* function getSpeciesByIds(...ids) {
+    const log = species.filter(({ id }) => ids.includes(id))
+    return log
+}  */
+
+function getSpeciesByIds(...ids) {
+   
+    const log = species.filter(({ id }) => ids.includes(id))
+    return log
 }
+
 
 module.exports = getSpeciesByIds;
