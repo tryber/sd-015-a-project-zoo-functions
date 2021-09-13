24837b6d918c5e094110ecc4c474686f98d6586c
Joao-Ortega <joao_ortega17@hotmail.com>
2021-09-13 15:54:46 -0300

    Requisito 05

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,8 +7,11 @@ function countAnimals(animal) {
     species.forEach((element) => {
       allAnimals[element.name] = element.residents.length;
     });
+    return allAnimals;
   }
-  return allAnimals;
+  const { specie, gender } = animal;
+  const animals = species.find((value) => value.name === specie).residents;
+  const animalsResident = animals.reduce((acc, crr) => (crr.sex === gender ? acc + 1 : acc), 0);
+  return (gender === undefined) ? animals.length : animalsResident;
 }
-
 module.exports = countAnimals;
