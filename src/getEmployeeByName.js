0a84115344797dc3c91a7eb5ca9197281bf95427
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-13 16:24:15 -0300

    getEmployeeByName complete

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const employeeData = data.employees;
+  if (employeeName === undefined) {
+    return {
+
+    };
+  }
+  return employeeData.find((aux) => aux.firstName === employeeName || aux.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
