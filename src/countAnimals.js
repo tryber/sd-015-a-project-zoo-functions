147501ea4693288def75bef0f5ba2b9014cf419a
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-13 10:51:13 -0300

    add countAnimals function

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+function getResidentsNumWithoutGender(animal) {
+  const spec = data.species.find((specie) => specie.name === animal.specie);
+  return spec.residents.length;
+}
+
+function getResidentsNumWithGender(animal) {
+  const spec = data.species.find((specie) => specie.name === animal.specie);
+  return spec.residents.filter((resident) => resident.sex === animal.gender).length;
+}
+function getAllResidents() {
+  const residentsBySpecie = {};
+  for (let index = 0; index < data.species.length; index += 1) {
+    residentsBySpecie[data.species[index].name] = data.species[index].residents.length;
+  }
+  return residentsBySpecie;
+}
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal) {
+    switch (Object.keys(animal).length) {
+    case 1:
+      return getResidentsNumWithoutGender(animal);
+    default:
+      return getResidentsNumWithGender(animal);
+    }
+  }
+  return getAllResidents();
 }
 
 module.exports = countAnimals;
