f0a3167d68ef620117b9c08135e23f4d97f0d971
William <william.mk1@outlook.com>
2021-09-13 20:34:13 -0300

    .

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,16 +1,17 @@
 const data = require('../data/zoo_data');
 
-const findAnimal = (animalSpecie) => 
-data.species.find((chosenAnimal) => 
-chosenAnimal.name === animalSpecie);
+const findAnimal = (animalSpecie) =>
+  data.species.find((chosenAnimal) =>
+  chosenAnimal.name === animalSpecie);
 
-const getAnimalGender = (animal) => 
-findAnimal(animal.specie).residents.filter((getGender) => 
-getGender.sex === animal.gender).length;
+const getAnimalGender = (animal) =>
+findAnimal(animal.specie).residents.filter((getGender) =>
+  getGender.sex === animal.gender).length;
 
 function countAnimals(animal) {
   if (animal === undefined) {
-    return data.species.reduce((animals, cur) => Object.assign(animals, { [cur.name]: cur.residents.length }), {});
+    return data.species.reduce((animals, cur) =>
+      Object.assign(animals, { [cur.name]: cur.residents.length }), {});
   }
   if (animal.specie && !animal.gender) {
     return findAnimal(animal.specie).residents.length;
