3d67abad8c003693e9d57e3874a44c98f7964b02
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-09-13 15:10:17 -0300

    adiciona requisito 2

--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -2,8 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const animals = data.species.find((bicho) => bicho.name === animal);
-  const resultado = animals.residents.every((criatura) => criatura.age >= age);
-  return resultado;
+  return console.log(animals);
 }
 
 module.exports = getAnimalsOlderThan;
