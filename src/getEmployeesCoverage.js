246143af5312d95cf7dcc87d1fef68ce1eab3f69
João Felipe P <jf.pelliccione@gmail.com>
2021-09-13 18:39:08 -0300

    Iniciando requisito 10 (voltar depois).

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+/*
+function getSpecieLocation(whatSpecie) {
+  return data.species.find((specie) => specie.id === whatSpecie).location;
+}
+
+function employeesList() {
+  let newObj = {};
+  const resultArray = [];
+
+  data.employees.forEach((employee) => {
+    newObj = { id: employee.id, fullName: `${employee.firstName} ${employee.lastName}`, species: employee.responsibleFor };
+    resultArray.push(newObj);
+    newObj = {};
+  });
+  return resultArray;
+}
+
+console.log(employeesList());
+
+function getEmployeesCoverage(inputObj) {
+  // Escreva sua função aqui
 }
 
 module.exports = getEmployeesCoverage;
+*/
