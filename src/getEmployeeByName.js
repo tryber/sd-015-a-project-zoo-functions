f82d00a284830c37336f2fb4aba433da95ddf44e
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-13 14:03:49 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  const { employees } = data;
+  let returnable = {};
+  employees.forEach((emp) => {
+    if (emp.lastName === employeeName || emp.firstName === employeeName) returnable = emp;
+  });
+  return returnable;
 }
 
 module.exports = getEmployeeByName;
