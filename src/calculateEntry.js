db3fb3d6fec6fcd3918517ce487b70ee93b24c93
Bernardocdr <bernardocdr@gmail.com>
2021-09-13 17:26:17 -0300

    Requisitos 4 e 6 concluidos.

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const obj = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((person) => {
+    if (person.age < 18) {
+      obj.child += 1;
+    } else if (person.age >= 18 && person.age < 50) {
+      obj.adult += 1;
+    } else if (person.age >= 50) {
+      obj.senior += 1;
+    }
+  });
+  return obj;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants) return 0;
+  if (!Object.keys(entrants).length) return 0;
+  const listOfGuests = countEntrants(entrants);
+  let total = 0;
+
+  total += (listOfGuests.child * data.prices.child);
+  total += (listOfGuests.adult * data.prices.adult);
+  total += (listOfGuests.senior * data.prices.senior);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  //Criando o Objeto com cada espécie como chave e a quantidade como valor
+  const obj = {};
+  species.forEach((item) => { obj[item.name] = item.residents.length; });
+
+  //sem parâmetros, retorna as espécies e sua quantidade
+  if (!animal) return obj;
+
+  const { specie, gender } = animal;
+  if (specie && gender) {
+    return species
+      .find((item) => item.name === specie).residents
+      .filter((item) => item.sex === gender).length;
+  }
+
+  //recebendo como parâmetro um objeto com a chave \'specie\', retorna um número, a quantidade de animais daquela espécie'
+  if (specie) return obj[specie];
 }
 
 module.exports = countAnimals;
