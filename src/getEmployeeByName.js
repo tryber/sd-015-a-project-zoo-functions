38c381738554df0750828028b479f4deea968392
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-09-13 19:06:11 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  return employees.find(
+    (element) =>
+      element.firstName === employeeName || element.lastName === employeeName,
+  );
 }
-
+// console.log(getEmployeeByName('Bethea'));
 module.exports = getEmployeeByName;
