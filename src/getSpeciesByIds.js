c329b1fdab6a7a4f69913eb79d76eae25d32ca76
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-13 15:00:26 -0300

    Requisito 1. VQV

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  let arr = [];
+  const getSpecie = ids.forEach(id => species.filter((specie) => {
+    if (specie.id === id) {
+      arr.push(specie);
+    }
+  }))
+  return arr;
 }
 
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
