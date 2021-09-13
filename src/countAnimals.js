741ca8da6cc4ba1155faee2199a162b39d386a84
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-09-13 17:38:40 -0300

    adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animais = {};
+    data.species.forEach((element) => { animais[element.name] = element.residents.length; });
+    return animais;
+  }
+  const especie = data.species.find((anim) => anim.name === animal.specie);
+  if (animal.specie && animal.gender) {
+    const especieGenero = especie.residents.filter((anim) => anim.sex === animal.gender);
+    return especieGenero.length;
+  }
+  return especie.residents.length;
 }
 
 module.exports = countAnimals;
