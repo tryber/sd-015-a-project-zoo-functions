58a5a7c6dc8bdef4f84734e552317e9f36ba03c9
Lui Grosz <luigr@pop-os.localdomain>
2021-09-13 14:30:36 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids[0] === undefined) {
+    const arraySpecies = [];
+    return arraySpecies;
+  }
 }
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
---
+++ src/testes.js
@@ -0,0 +1,5 @@
+const data = require('../data/zoo_data');
+
+const aaaaaa = data.id.find(('0938aa23-f153-4937-9f88-4858b24d6bce'));
+
+console.log(aaaaaa);
