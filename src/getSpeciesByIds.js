027109b23fdada5f76fc4f111a05dffe47a070f2
Valdeci <moura97.valdeci@gmail.com>
2021-09-13 15:23:13 -0300

    Corrige lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,9 @@
 const data = require('../data/zoo_data');
 
-const {species} = data;
+const { species } = data;
 
 function getSpeciesByIds(...ids) {
-  return species.filter(specie => ids.includes(specie.id));
-};
+  return species.filter((specie) => ids.includes(specie.id));
+}
 
 module.exports = getSpeciesByIds;
