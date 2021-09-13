6f1f1716608ce4724203c13e30faf3c235bd5497
William Schmidt <9williamschmidt@gmail.com>
2021-09-13 15:45:40 -0300

    teste

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+const getSpeciesByIds = (...ids) => {
+  return data.species.filter((e) => console.log(e.id === ids));
+};
+getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5');
 
 module.exports = getSpeciesByIds;
