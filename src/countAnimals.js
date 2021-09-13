8a305b2c223be045b5371e800e3cd6d3b162135a
guilhermecd15 <guilhermecd15@gmail.com>
2021-09-13 17:37:08 -0300

    5. Implemente a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+function countAnimals({ specie = undefined, gender = undefined } = '') {
+  // não sei porque precisa do = '', peguei na mentoria para resolver erro.
   // seu código aqui
+  if (specie === undefined) {
+    return species.reduce((acc, { name, residents: { length } }) => {
+      Object.assign(acc, { [name]: length });
+      return acc;
+    }, {});
+  }
+  if (gender === undefined) {
+    return species.find((crr) => crr.name === specie).residents.length;
+  }
+  return species.find((crr) =>
+    crr.name === specie).residents.filter((resident) =>
+    resident.sex === gender).length;
 }
 
 module.exports = countAnimals;
