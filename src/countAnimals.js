470515dca4bf502204e844771d93e9530df9b870
Guarize <pedroguarize@hotmail.com>
2021-09-13 08:13:39 -0300

    Requirement [5] - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (typeof animal === 'undefined') {
+    const allAnimals = {};
+    species.forEach((elem) => { allAnimals[elem.name] = elem.residents.length; });
+    return allAnimals;
+  }
+  const { specie, gender } = animal;
+  const anim = species.find((elem) => elem.name === specie).residents;
+  const genCount = anim.reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
+  return gender === undefined ? anim.length : genCount;
+};
 
 module.exports = countAnimals;
