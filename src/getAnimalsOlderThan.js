a4e9aad479325abab16ee567e8860a7c6438c003
douglas oliveira <douglasgama0@gmail.com>
2021-09-13 16:55:02 -0300

    finalizaçao do requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const nameAnimal = species.find((AniamlSpecie) => AniamlSpecie.name === animal);
+  return nameAnimal.residents.every((animalAge) => animalAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
