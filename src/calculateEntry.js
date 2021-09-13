f59375b70041a77c4447bf0f5b706d612b04a24b
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-09-13 18:32:19 -0300

    adiciona o requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const pessoas = entrants.map((element) => element.age);
+  const resultado = ({
+    child: pessoas.filter((child) => child < 18).length,
+    adult: pessoas.filter((adult) => adult >= 18 && adult < 50).length,
+    senior: pessoas.filter((senior) => senior >= 50).length,
+  });
+  return resultado;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const entradas = countEntrants(entrants);
+  const kids = entradas.child * data.prices.child;
+  const adult = entradas.adult * data.prices.adult;
+  const senior = entradas.senior * data.prices.senior;
+  const somaTotal = kids + adult + senior;
+  return somaTotal;
 }
 
 module.exports = { calculateEntry, countEntrants };
