c533c550233a2ee2e1acebad823a3cb023b02e1d
Gabriel <gncorreia@protonmail.com>
2021-09-13 16:21:56 -0300

    requisito 1 fix

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -11,7 +11,5 @@ function getSpeciesByIds(...ids) {
 
   return species;
 }
-console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '01422318-ca2d-46b8-b66c-3e9e188244ed'));
-// console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
-console.log(getSpeciesByIds());
+
 module.exports = getSpeciesByIds;
