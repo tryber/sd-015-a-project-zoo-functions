ab753cb711659983a57c71ecb278db6a5014fca0
Arthur Simões <arthursimoes155@gmail.com>
2021-09-13 14:44:30 -0300

    Adiciona requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function animalsLocation() {
+  const object = {};
+  species.forEach((element) => {
+    const sameLocation = species.filter((specie) => element.location === specie.location);
+    object[element.location] = sameLocation.map((value) => value.name);
+  });
+  return object;
+}
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    return animalsLocation();
+  }
 }
 
 module.exports = getAnimalMap;
