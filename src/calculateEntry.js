d0dfe1fcdfb3227ef1f8c2198428b46c4a1c7ce4
guilhermecd15 <guilhermecd15@gmail.com>
2021-09-13 19:51:05 -0300

    6. Implemente a função calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const child = entrants.filter((aux) => aux.age < 18).length;
+  const adult = entrants.filter((aux) => aux.age >= 18 && aux.age < 50).length;
+  const senior = entrants.filter((aux) => aux.age >= 50).length;
+  const obj = { child, adult, senior };
+  return obj;
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  if (typeof entrants === 'undefined' || Object.values(entrants).length < 1) {
+    return 0;
+  }
+  const result = countEntrants(entrants);
+  return result.child * prices.child + result.adult * prices.adult + result.senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
