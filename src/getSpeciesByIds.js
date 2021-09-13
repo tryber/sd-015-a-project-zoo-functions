d99a34ef0cc11f4fbe4a680a09eff90235f5308a
gabriel <gabriel.f.takaki@gmail.com>
2021-09-13 14:47:50 -0300

    primeiro requisito inicando projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 
 // necessário retornar um array com os animasi filtrados pelo ID
 function getSpeciesByIds(ids) {
-   return species.filter((animais, index) => animais.id === ids[index])
+  return species.filter((animais, index) => animais.id === ids[index]);
 }
 // usei a HOF filter para criar um novo array com os animais que puxei da lista JSON "species"
 module.exports = getSpeciesByIds;
