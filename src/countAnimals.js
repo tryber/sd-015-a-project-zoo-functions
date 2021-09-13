c4815c483f3ffdb73f82b6d21c64ddb05ec14ae4
Braien <braienmp@outlook.com>
2021-09-13 17:31:04 -0300

    implementa a função countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+function countAnimals(animal = undefined) {
   // seu código aqui
+  const count = {};
+  data.species.forEach((eachEspecie) => {
+    count[eachEspecie.name] = eachEspecie.residents.length;
+  });
+  if (!animal) {
+    return count;
+  }
+  const { specie, gender } = animal;
+  return (!gender) ? count[specie] : data.species.find((elem) =>
+    elem.name === specie).residents.filter((resident) =>
+    resident.sex === gender).reduce(((acc, number) => acc + 1), 0);
 }
 
 module.exports = countAnimals;
