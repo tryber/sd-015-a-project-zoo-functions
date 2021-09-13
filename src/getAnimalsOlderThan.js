54b25b479f978a401f000b11bbd4fd7907bff08f
guilhermecd15 <guilhermecd15@gmail.com>
2021-09-13 14:50:31 -0300

    2. Implemente a função getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const animalParam = species.find((specie) => specie.name === animal);
+  return animalParam.residents.every((animaldoParam) => animaldoParam.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
