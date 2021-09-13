8ca511d6d39944a645edfe65f12866847416baf4
João V. Dias <diasvictorj@gmail.com>
2021-09-13 18:10:31 -0300

    Primeiro Requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
 
+
+function getSpeciesByIds(...ids) {
+  // seu código aqui
+ const speciesFound = species.filter(({ id }) => ids.includes(id));
+ return speciesFound;
+}
 module.exports = getSpeciesByIds;
+
+
