b733c6173579a500923cf8ee0cb9b50e331b01e1
Ismael Peccin <ismaelpeccin@gmail.com>
2021-09-13 16:08:56 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return species.find((animalName) =>
+    animalName.name === animal).residents.every((ageAnimal) =>
+    ageAnimal.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
