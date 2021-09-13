49e5c4abf81508db3bef663285308c9a11da7c67
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-13 12:45:15 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...id) {
   // seu código aqui
+  const { species } = data;
+  const allIds = id;
+  let returnable = [];
+  allIds.forEach((currId) => {
+    returnable = [...returnable, ...species.filter((specie) => {
+      if (specie.id === currId) return true;
+      return false;
+    })];
+  });
+  return returnable;
 }
+
+getSpeciesByIds(1, 2);
 
 module.exports = getSpeciesByIds;
