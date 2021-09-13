f41765fb0a8c94d5ec73bb6fd6d73b32c2c9add3
Emerson Oliveira <emerson.contato@gmail.com>
2021-09-13 18:06:26 -0300

    Requisito 2 inserido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+    .find((especie) => especie.name === animal)
+    .residents.every((idadeMin) => idadeMin.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
