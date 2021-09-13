c605d17a4d0eaec442c764a8346e4869de0db02a
Luan Vitor <luan_vittor@hotmail.com>
2021-09-13 16:39:51 -0300

    Sixth one done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return ({
+    child: entrants.filter((element) => element.age < 18).length,
+    adult: entrants.filter((element) => element.age >= 18 && element.age < 50).length,
+    senior: entrants.filter((element) => element.age >= 50).length,
+  });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const people = countEntrants(entrants);
+  const { prices } = data;
+  const childrenPrice = people.child * prices.child;
+  const adultPrice = people.adult * prices.adult;
+  const seniorPrice = people.senior * prices.senior;
+  const price = childrenPrice + adultPrice + seniorPrice;
+  return price;
 }
 
 module.exports = { calculateEntry, countEntrants };
