d08ff4e312d9f4581d2272d41bc2c7551010e010
alvarogularte <alvaro.gularte@gmail.com>
2021-09-13 17:06:32 -0300

    Requirimment 5 completed

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const countAnimals = (animal) => {
+  if (typeof animal === 'undefined') {
+    const getAnimals = {};
+    species.forEach((elem) => { getAnimals[elem.name] = elem.residents.length; });
+    return getAnimals;
+  }
+  const { specie, gender } = animal;
+  const animals = species.find((element) => element.name === specie).residents;
+  const genderCount = animals.reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
+  return gender === undefined ? animals.length : genderCount;
+};
 
 module.exports = countAnimals;
