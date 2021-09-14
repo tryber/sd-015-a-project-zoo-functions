1184f4b646c50b43f49107d5d986fc8378c471e4
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-09-14 16:49:15 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return data.species.find((objeto) => objeto.name.includes(animal)).residents.every((objeto) => objeto.age > age);
+  return data.species.find((objeto) => objeto.name.includes(animal))
+    .residents.every((objeto) => objeto.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
