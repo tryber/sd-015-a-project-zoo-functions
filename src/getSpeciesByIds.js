35b9d00912fd5f1aabc19dad72d31fc024162f0a
Davio Lopes <davioplp@gmail.com>
2021-09-13 22:22:28 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  test.
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const animals = [];
+  ids.forEach((id) => {
+    animals.push(species.find((iten) => iten.id === id));  
+  });
+  return animals; 
 }
-
+console.log(getSpeciesByIds());
 module.exports = getSpeciesByIds;
