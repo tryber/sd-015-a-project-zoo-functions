9668ad8b6a2bca4b0ae1f488541a4c62ac11d797
Emerson Oliveira <emerson.contato@gmail.com>
2021-09-14 19:08:10 -0300

    Requisito 5 inserido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allSpecies = {};
+    species.forEach((nameSpecies) => 
+    { allSpecies[nameSpecies.name] = nameSpecies.residents.length; });
+    return allSpecies;
+  } if (animal.gender) {
+    const animals = species.find((animalName) => animal.specie === animalName.name);
+    const sex = animals.residents.filter((maleFemale) => animal.gender === maleFemale.sex);
+    return sex.length;
+  }
+  const nameOfAnimal = species.find((elem) => animal.specie === elem.name);
+  return nameOfAnimal.residents.length;
 }
+
 
 module.exports = countAnimals;
