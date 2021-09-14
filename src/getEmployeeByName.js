a0614974692289b0752e6a78a0b5ca256c6e6fba
Arthur <arthurteixeira0805@gmail.com>
2021-09-14 18:22:38 -0300

    3/10

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
+const getEmployeeByName = (employeeName) => {
   // seu código aqui
-}
+  if (!employeeName) return {};
+  return data.employees
+    .find((employee) => employee.lastName === employeeName || employee.firstName === employeeName);
+};
 
 module.exports = getEmployeeByName;
