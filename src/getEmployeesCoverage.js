13870e7c4a80eec3148aad1481c6d6b003e4d487
Braien <braienmp@outlook.com>
2021-09-14 19:09:43 -0300

    inicia implementaçao de getEmployeesCoverage

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,36 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const getPerson = (name, id) => employees.find((employee) =>
+  (employee.firstName === name) || (employee.lastName === name) || (employee.id === id));
+
+const getAnimalById = (id) => data.species.find((specie) => specie.id === id);
+
+const getLocation = (id) => data.species.find((specie) => specie.id === id);
+
+const getSingleCoverage = (person) => {
+  const obj = {
+    id: person.id,
+    fullName: `${person.firstName} ${person.lastName}`,
+    species: person.responsibleFor.map((animal) => getAnimalById(animal).name),
+    // locations: obj.species.map((animal) =>)
+  };
+};
+
+function getEmployeesCoverage(obj) {
   // seu código aqui
+  const { name, id } = obj;
+  const person = getPerson(name, id);
+  const { firstName, lastName, responsibleFor } = person;
+  const singleCoverage = {
+    id: person.id,
+    fullName: `${firstName} ${lastName}`,
+    species: responsibleFor.map((animal) => getAnimalById(animal).name),
+    locations: responsibleFor.map((animal) => getLocation(animal).location),
+  };
+  return singleCoverage;
 }
 
 module.exports = getEmployeesCoverage;
+
+console.log(getEmployeesCoverage({ id: '4b40a139-d4dc-4f09-822d-ec25e819a5ad' }));
