0471c6ebb8e879996b5d979f71f0ca160720dcdb
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-09-14 15:27:06 -0300

    requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function emptyObject() {
+  const newObject = {};
+  species.forEach((element) => { newObject[element.name] = element.residents.length; });
+  return newObject;
 }
+function countAnimals(animal) {
+  if (!animal) {
+    return emptyObject();
+  }
+  if (!(animal.gender)) {
+    return species.find((element) => element.name.includes(animal.specie)).residents.length;
+  }
+
+  const filterGender = species.find((element) => element.name.includes(animal.specie));
+  return filterGender.residents.filter((element) => element.sex === animal.gender).length;
+}
+console.log(countAnimals({ specie: 'elephants', gender: 'male' }));
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,8 +5,13 @@ const { employees } = data;
 function isManager(id) {
   return employees.some((element) => element.managers.includes(id));
 }
+console.log(isManager('stephanieId'));
 
 function getRelatedEmployees(managerId) {
+  if(isManager() === true) {
+    return employees.map((element) => element.managers);
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
+console.log(getRelatedEmployees('stephanieId'));
 module.exports = { isManager, getRelatedEmployees };
