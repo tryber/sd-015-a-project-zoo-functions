8405687985e19b0130c92f8907e9cfdc6bab0e1a
Davio Lopes <davioplp@gmail.com>
2021-09-14 16:05:19 -0300

    requisito 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,9 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    species.forEach((animals) => console.log(`${animals.name} : ${animals.residents.length}`));
 }
-
+}
+countAnimals();
 module.exports = countAnimals;
