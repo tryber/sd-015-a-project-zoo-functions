41aaea9ef987031cf373e83e01caa83063d4d6ed
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-09-13 21:48:34 -0300

    requisito 8 done

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,34 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function createBase() {
+  const dayWeeks = Object.keys(hours);
+  const hourPerDay = Object.values(hours);
+  const hourTable = dayWeeks.reduce((acc, act, i, array) => {
+    if (i !== array.length - 1) {
+      acc[act] = { officeHour: `Open from ${hourPerDay[i].open}am until ${hourPerDay[i].close}pm`,
+        exhibition: species.filter((spe) =>
+          spe.availability.includes(act)).map((animal) => animal.name) };
+      return acc;
+    }
+    acc[act] = { officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!' };
+    return acc;
+  }, {});
+  return hourTable;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const dayWeeks = Object.keys(hours);
+  const animalsArray = species.map((ani) => ani.name);
+  if (dayWeeks.includes(scheduleTarget)) {
+    return { [scheduleTarget]: createBase()[scheduleTarget] };
+  }
+  if (animalsArray.includes(scheduleTarget)) {
+    const findAnimal = species.find((spe) => spe.name === scheduleTarget);
+    return findAnimal.availability;
+  }
+  return createBase();
 }
 
 module.exports = getSchedule;
