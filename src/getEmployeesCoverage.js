1d0e1a248235e766f2965a8b99e869dddedac976
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-09-14 11:09:01 -0300

    requisito 10 done

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,28 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const base = employees.reduce((acc, actual) => {
+  acc.push({
+    id: actual.id,
+    fullName: `${actual.firstName} ${actual.lastName}`,
+    species: species.filter((specie) =>
+      actual.responsibleFor.includes(specie.id)).map((speSelec) => speSelec.name),
+    locations: species.filter((specie) =>
+      actual.responsibleFor.includes(specie.id)).map((speSelec2) => speSelec2.location),
+  });
+  return acc;
+}, []);
+
+console.log(base);
+
+function getEmployeesCoverage(obj) {
+  if (!obj) return base;
+  const { name, id } = obj;
+  if (base.find((emp) => emp.id === id)) return base.find((emp) => emp.id === id);
+  if (base.find((emp) => emp.fullName.includes(name))) {
+    return base.find((emp) => emp.fullName.includes(name));
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
