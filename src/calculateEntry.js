5b3db30fd42f5d07871f21037b3316512c49450f
Italo Sergio <italosergio@graduate.org>
2021-09-13 22:35:42 -0300

    Challange 6 - stage 3/8 check

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,33 +1,32 @@
-
 const data = require('../data/zoo_data');
 
-// const entrants1 = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
+const entrants1 = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
 function countEntrants(entrants) {
+  if (!entrants) return false;
+  if (!Object.values(entrants).length) return false;
+  return {
+    child: Object.values(entrants.filter((element) =>
+      element.age < 18)).length,
 
-  return !entrants ? false
-    : { 
-      child: Object.values(entrants.filter((element) =>
-        element.age < 18)).length,
+    adult: Object.values(entrants.filter((element) =>
+      element.age >= 18 && element.age < 50)).length,
 
-      adult: Object.values(entrants.filter((element) =>
-        element.age >= 18 && element.age < 50)).length,
-
-      senior: Object.values(entrants.filter((element) =>
-        element.age >= 50)).length,
-    };
+    senior: Object.values(entrants.filter((element) =>
+      element.age >= 50)).length,
+  };
 }
-console.log(countEntrants());
 
 function calculateEntry(entrants) {
   if (!countEntrants(entrants)) return 0;
 }
+console.log(countEntrants());
 
 module.exports = { calculateEntry, countEntrants };
