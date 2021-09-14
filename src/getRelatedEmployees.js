992c3b979829b25adca45e867f718e651ee2305f
Giovanni Duarte <gikigiio@gmail.com>
2021-09-14 18:05:57 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -10,46 +10,12 @@ function isManager(id) {
 }
 
 function getRelatedEmployees(managerId) {
-  const managerResult = isManager(managerId);
-  if (managerResult === false) {
+  if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const getResponsibilities = data.employees.find(
-    (elem) => elem.id === managerId,
-  ).responsibleFor;
-  console.log(getResponsibilities);
+  const getResponsabilities = data.employees.filter((elem) =>
+    elem.managers.includes(managerId));
+  return getResponsabilities.map((emp) => `${emp.firstName} ${emp.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
-
-// Deletar Abaixo
-
-// Implemente a função getRelatedEmployees
-// Considerando a boa prática de dividir o código em partes menores, apresentamos a função getRelatedEmployees em que você deverá dividí-la em duas funções:
-
-// 1 - isManager - que será responsável por verificar se uma pessoa colaboradora é gerente ou não. O retorno dessa função deve ser um booleano: true ou false;
-
-// 2 - getRelatedEmployees - que utiliza a primeira função para apresentar as seguintes saídas:
-
-// se for uma pessoa colaboradora gerente, deve retornar um array contendo os nomes das pessoas colaboradoras que ela é responsável;
-// se não for uma pessoa colaboradora gerente, deverá ser lançado um erro gerado com a função construtora Error da biblioteca padrão do JavaScript com a mensagem "O id inserido não é de uma pessoa colaboradora gerente!".
-
-// Exemplo:
-// throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-// Você pode ler mais sobre a função construtora Error, neste link.
-
-// Observações técnicas
-
-// Retorna um array contendo nome e sobrenome das pessoas colaboradoras gerenciadas por determinada pessoa com cargo de gerência.
-// Exemplo de output:
-
-// [ 'Burl Bethea', 'Ola Orloff', 'Emery Elser' ];
-// Dispara um erro com a mensagem: "O id inserido não é de uma pessoa colaboradora gerente!".
-// O que será avaliado
-
-// Retorna true se o id passado for de um gerente;
-// Retorna false se o id passado não for de um gerente;
-// Se o id passado for de um gerente, retorna um array contendo nome e sobrenome das pessoas colaboradoras que ela é responsável;
-// Se o id passado não for de um gerente, dispara um erro com a mensagem: "O id inserido não é de uma pessoa colaboradora gerente!".
-
-getRelatedEmployees(stephanieId);
