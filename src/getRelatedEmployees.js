3b8a928ea8d3ff1487a3f2dda043e548f3f9885d
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-14 18:45:48 -0300

    requisito-4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
+const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
+const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
+const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
+const managers = [stephanieId, olaId, burlId];
+
 function isManager(id) {
-  // seu código aqui
+  return managers.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) !== true) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const responsible = employees.filter((fEmploye) => fEmploye.managers.includes(managerId));
+  return responsible.map((mapName) => `${mapName.firstName} ${mapName.lastName}`);
 }
+
+// console.log(isManager());
 
 module.exports = { isManager, getRelatedEmployees };
