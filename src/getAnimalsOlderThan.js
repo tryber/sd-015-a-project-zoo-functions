6d517c28a18de128c0fdad2ca8fe5f7e2d55c428
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-09-14 11:14:21 -0300

    Requisito 2 - VQV

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const animals = data.species.filter((elem) => (elem.name === animal)).filter((ages) => ages.residents);
-  return animals.map((animalName) => animalName.residents.every((checkAge) => checkAge.age >= age));
+  const { residents } = data.species.find((elem) => elem.name === animal);
+  return residents.every((check) => check.age >= age);
 }
 console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
