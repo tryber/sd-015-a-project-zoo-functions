8924cf3f4bfbe6630dd9c05be6ad48147180c4d2
André <andre.bonaf@gmail.com>
2021-09-14 14:55:41 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    const objectReturn = {};
+    data.species.forEach((element) => {
+      objectReturn[element.name] = element.residents.length;
+    });
+    return objectReturn;
+  }
+  if (Object.keys(animal).length === 1) {
+    return data.species.find((species) => species.name === animal.specie).residents.length;
+  }
+  const sortByGender = data.species.find((species) => species.name === animal.specie);
+  return sortByGender.residents.filter((residents) => residents.sex === animal.gender).length;
 }
+
+// console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
