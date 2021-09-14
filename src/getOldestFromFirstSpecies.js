db6a995645046aa1a09ec06f78f8a49ef949d865
Braien <braienmp@outlook.com>
2021-09-14 13:35:35 -0300

    implementa a função getOldestFromFirstSpecies

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const firstSpecieId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const firstSpecie = data.species.find((specie) =>
+    specie.id === firstSpecieId).residents.reduce(((oldest, resident) =>
+    ((oldest.age < resident.age) ? resident : oldest)), { age: 0 });
+  return Object.values(firstSpecie);
 }
 
 module.exports = getOldestFromFirstSpecies;
