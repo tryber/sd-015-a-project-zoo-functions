37365785597d88bc7cc6c82620369c76e16a9e88
Emmanuelle Pereira <emma.persil@gmail.com>
2021-09-14 12:55:46 -0300

    Lint corrigido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  let dataSpecies = data.species;
-  return dataSpecies.filter((specie) => ids.includes(specie.id))
+  const dataSpecies = data.species;
+  return dataSpecies.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
