b6e0aec3b151f7a5eeb2168caa0aab5362f36be2
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-14 11:33:58 -0300

    Adicionando os requisitos 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,23 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // const listAnimals = { };
+  const listAnimals = { };
   if (animal === undefined) {
-    const value = data.species.map((element) => element.residents.length);
-    return value;
+    const value = species.map((element) => element.residents.length);
+    species.map((animals) => animals.name).forEach((element, index) => {
+      listAnimals[element] = value[index];
+    }); return listAnimals;
   }
   if (Object.values(animal).length === 1) {
-    const obj = Object.values(animal);
-    const filtterAnimal = data.species.find((element) => element.name === obj[0]);
-    const result = filtterAnimal.residents.length;
-    return result;
+    return species.find((element) => element.name === Object.values(animal)[0]).residents.length;
   }
-  const obj = Object.values(animal);
-  const filtterAnimal = data.species.find((element) => element.name === obj[0]);
-  return filtterAnimal.residents.filter((gender) => gender.sex === 'female');
+  if (Object.values(animal).length === 2 && animal.gender === 'female') {
+    const filtterAnimal = species.find((element) => element.name === Object.values(animal)[0]);
+    const female = filtterAnimal.residents.filter((gender) => gender.sex === 'female');
+    return female.length;
+  }
+  const filtterAnimal = species.find((element) => element.name === Object.values(animal)[0]);
+  const result = filtterAnimal.residents.filter((gender) => gender.sex === 'male');
+  return result.length;
 }
 
-console.log(countAnimals());
+console.log(countAnimals({ specie: 'bears', gender: 'female' }));
 
 module.exports = countAnimals;
 
