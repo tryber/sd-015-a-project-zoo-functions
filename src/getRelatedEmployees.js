ed2a98fec39ad65aac2315e05edbaa48e6fe9fb5
Gabriel <gncorreia@protonmail.com>
2021-09-13 23:39:54 -0300

    requisito 4 done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,21 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const managersIds = data.employees.map((employee) => employee.managers).flat(); // Estamos pegando os ids de todos os managers e colocando dentro de um array. Como o resultado vai dar varios arrays dentro de arrays, usamos o .flat() para colocar os resultados dentro de um único array.
+
+  return managersIds.includes(id);
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const employeeObject = data.employees.filter((employee) => employee.managers.includes(managerId));
+  const arrayOfNames = employeeObject.map((person) => `${person.firstName} ${person.lastName}`);
+  return arrayOfNames;
+  // Na verdade eu acho que tem que fazer o contrário. Ver se o id da pessoa está inserido na categoria "managers" dos empregados.
 }
-
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992')); // true
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,7 +5,7 @@ function getSpeciesByIds(...ids) {
 
   // Consultei o repositório do Apolo Wilker para resolver essa parte:
   // Link: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/9/files
-  return data.species.filter((animals) => ids.includes(animals.id)); // O includes é um método que procura se determinado conjunto de strings pode ser encontrado dentro de outra string, e irá retornar true, caso positivo, e false, caso negativo. Neste caso, estamos perguntando se os ids que estão dentro de animals estão presentes dentro do array, criado através do parâmetro rest, ...ids. Como estamos usando o filter, ele irá retornar um array contendo todos os animais cujo id está presente no array ...ids.
+  return data.species.filter((animals) => ids.includes(animals.id)); // O includes é um método que procura se determinado conjunto de strings pode ser encontrado dentro de outra string, e irá retornar true, caso positivo, e false, caso negativo. Neste caso, estamos perguntando se os ids que estão dentro de animals.id estão presentes dentro do array, criado através do parâmetro rest, ...ids. Como estamos usando o filter, ele irá retornar um array contendo todos os animais cujo id está presente no array ...ids.
 }
 
 module.exports = getSpeciesByIds;
