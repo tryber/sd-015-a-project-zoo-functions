0c19ba987f4429dfb3d2fc76f3f66108f2ad23fb
William <william.mk1@outlook.com>
2021-09-13 22:05:17 -0300

    .

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const objectReturn = {
+    child: entrants.filter((childs) => childs.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  }
+  return objectReturn;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const countEntrant = countEntrants(entrants);
+  return ((countEntrant.child * data.prices.child) +
+    (countEntrant.adult * data.prices.adult) +
+    (countEntrant.senior * data.prices.senior));
 }
 
 module.exports = { calculateEntry, countEntrants };
