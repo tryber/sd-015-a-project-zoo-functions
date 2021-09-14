de65f12b4d0745218110a052ed70e2bc3ce85084
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-14 16:24:45 -0300

    iniciando o projeto. VAMOS COM TUDO :rocket:

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
+  return species.filter((specie) => ids.includes(specie.id));
 }
+
+// console.log(species);
 
 module.exports = getSpeciesByIds;
