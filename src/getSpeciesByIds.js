6f2e95353aff612090a8d5242cda380cecbb23f4
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-14 19:10:03 -0300

    add req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -10,7 +10,16 @@ function getSpeciesByIds(...ids) {
     const filteredSpecies = data.species.filter((specie) => specie.id === id);
     return filteredSpecies;
   });
-  return selectedSepecies;
+
+  const finalResult = selectedSepecies.map((specie) => specie[0]);
+  return finalResult;
 }
 
+/* console.log(
+  getSpeciesByIds(
+    "0938aa23-f153-4937-9f88-4858b24d6bce",
+    "e8481c1d-42ea-4610-8e11-1752cfc05a46"
+  )
+);
+ */
 module.exports = getSpeciesByIds;
