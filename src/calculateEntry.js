8d763fa84dc8abe01ff3f912d8befbc5dc4383d7
Italo Sergio <italosergio@graduate.org>
2021-09-13 21:33:57 -0300

    Challange 6 - stage 1/8 check

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,27 @@
+
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const entrants1 = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
+function countEntrants(entrants) {
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+
+  return { 
+    child: Object.values(child).length,
+    adult: Object.values(adult).length,
+    senior: Object.values(senior).length,
+  };
+}
+console.log(countEntrants(entrants1));
 function calculateEntry(entrants) {
   // seu código aqui
 }
