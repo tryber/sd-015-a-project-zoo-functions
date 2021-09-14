8e973e93449646f40aec3b150d07302772e5c14c
Arthur <arthurteixeira0805@gmail.com>
2021-09-14 20:59:49 -0300

    4/10

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,3 +1,7 @@
+// Ref:
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/leonardo-felix-zoo-functions-project
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/pedro-guarize-zoo-functions-project
+
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+// Ref:
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/leonardo-felix-zoo-functions-project
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/pedro-guarize-zoo-functions-project
+
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((employee) => employee.managers.includes(managerId))
+    .map((empUnderMan) => `${empUnderMan.firstName} ${empUnderMan.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
