46038d12196d0ac9b887188f4340c8b6ca4617c0
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-09-14 18:12:21 -0300

    requsiitp

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,8 +2,10 @@ const data = require('../data/zoo_data');
 
 const { employees } = data;
 
-function getEmployeeByName(employeeName) { 
-  const dataEmployer = employees.find((element) => element.firstName.includes(employeeName) || element.lastName.includes(employeeName));
+function getEmployeeByName(employeeName) {
+  const dataEmployer = employees.find((element) =>
+    element.firstName.includes(employeeName)
+  || element.lastName.includes(employeeName));
 
   return !(dataEmployer) ? {} : dataEmployer;
 }
