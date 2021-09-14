5b1978418919295a868e69feb4d6fe52146add63
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 19:22:42 -0300

    ajuste do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,12 +8,12 @@ function countAnimals(animal) {
     data.species.forEach((specie) => { animalList[specie.name] = specie.residents.length; });
     return animalList;
   }
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
   const searchAnimal = species.find((resultAnimal) => resultAnimal.name === specie);
   const numberOfAnimals = searchAnimal.residents.length;
-  const searchGender = searchAnimal.residents.filter((resultAnimal) => resultAnimal.sex === gender);
+  const searchGender = searchAnimal.residents.filter((resultAnimal) => resultAnimal.sex === sex);
   const animalsByGender = searchGender.length;
-  return gender === undefined ? numberOfAnimals : animalsByGender;
+  return sex === undefined ? numberOfAnimals : animalsByGender;
 }
 
 module.exports = countAnimals;
