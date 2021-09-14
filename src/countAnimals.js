9d7fb22a9031c9854e8b4a7e3b07fa71346670e6
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-09-14 12:52:00 -0300

    primeiro requisito do requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  function criaObjeto(acumulador, especie) {
+    const acc = acumulador;
+    acc[especie.name] = especie.residents.length;
+    return acc;
+  }
+  if (animal === undefined) {
+    const Animais = data.species.reduce(criaObjeto, {});
+    return Animais;
+  }
+  if (animal.length === 1) {
+    return {};
+  }
+  if (animal.length === 2) {
+    return {};
+  }
 }
 
 module.exports = countAnimals;
