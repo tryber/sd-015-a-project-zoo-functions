5f5c47f14be8537550a8111022265fb240438120
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-14 18:23:12 -0400

    Soluçao do requisito 10 - project-zoo-functions

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
+const specie = require('../src/getSpeciesByIds');
+
+const { employees } = data;
+
+function getIndicatedEmployee(obj) {//Esta função retorna o objeto da pessoa colaboradora indicada no parâmetro(obj)
+  const employee = employees.find((employee) => {
+    const fullName = `${employee.firstName} ${employee.lastName}`;
+    return fullName.includes(obj.name) || employee.id.includes(obj.id);
+  });
+  return employee;
+}
+// console.log(getIndicatedEmployee({ id: 'id inválido' }))
+function answerToReturn(obj) {//Esta função retorna o obj criado a partir da pessoa colaboradora.
+  const indicatedEmployee = getIndicatedEmployee(obj);
+  let objToReturn = {
+    id: indicatedEmployee.id,
+    fullName: `${indicatedEmployee.firstName} ${indicatedEmployee.lastName}`,
+    species: specie(...indicatedEmployee.responsibleFor).map((animal) => animal.name),
+    locations: specie(...indicatedEmployee.responsibleFor).map((animal) => animal.location),
+  }
+  return objToReturn;
+}
+// console.log(answerToReturn({ name: 'Sharonda' }))
+
+function informationsOfAllEmployees() {//Função para retornar a informação de todos os employees caso nenhum parâmetro seja passado para a função principal(getEmployeesCoverage)
+  const arrayWithAllObjs = [];
+  employees.reduce((acc, curr) => {
+    acc = {
+      id: curr.id,
+      fullName: `${curr.firstName} ${curr.lastName}`,
+      species: specie(...curr.responsibleFor).map((animal) => animal.name),
+      locations: specie(...curr.responsibleFor).map((animal) => animal.location),
+    }
+    arrayWithAllObjs.push(acc);
+  }, {});
+  return arrayWithAllObjs;
+}
+// console.log(informationsOfAllEmployees())
 
 function getEmployeesCoverage(obj) {
   // seu código aqui
+  if (obj === undefined) return informationsOfAllEmployees();
+  if (getIndicatedEmployee(obj) === undefined) throw new Error('Informações inválidas');
+  return answerToReturn(obj);
 }
-// console.log(getEmployeesCoverage({ name: 'Sharonda' }))
+// console.log(getEmployeesCoverage())
 module.exports = getEmployeesCoverage;
