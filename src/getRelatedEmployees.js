120ba10e3f984a92cf42d38e749bb1098ed900fa
Davio Lopes <davioplp@gmail.com>
2021-09-14 01:15:23 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+ return employees.some((iten) => iten.managers.find((iten) => iten === id) === id);
 }
+//console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return employees.filter((iten) => iten.managers.find((iten) => iten === managerId))
+      .map((iten) => `${iten.firstName} ${iten.lastName}`);
+  }
 }
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
+
