496177d570d57e2066ba5d96c53638f8cca75ca6
Ismael Peccin <ismaelpeccin@gmail.com>
2021-09-13 22:15:23 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species } = require('../data/zoo_data');
+
 function countAnimals(animal) {
-  // seu código aqui
+  const objAnimal = {};
+  if (animal === undefined) {
+    species.forEach((item) => { objAnimal[item.name] = item.residents.length; });
+    return objAnimal;
+  }
+  const { specie, gender } = animal;
+
+  if (gender === undefined && specie !== undefined) {
+    return species.find((item) => item.name === specie).residents.length;
+  }
+  return species.find((item) =>
+    item.name === specie).residents.filter((gen) => gen.sex === gender).length;
 }
 
 module.exports = countAnimals;
