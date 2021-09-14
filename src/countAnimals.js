89ac4bf3afd38069b77acb3edf331f810586e957
GTmudin <testederealismo@gmail.com>
2021-09-14 15:32:54 -0300

    eslint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,11 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  hb
+function countAnimals(...animal) {
+  const resultado = species.filter(elemento => animal.includes(elemento));
+  return resultado;
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  const resultado = species.filter(elemento => ids.includes(elemento.id));
+  const resultado = species.filter((elemento) => ids.includes(elemento.id));
   return resultado;
 }
 
