d8c4fafe679a4cfadbe10ce8e4ff43fc2c0f240e
renatozr <renatozr07@gmail.com>
2021-09-14 18:35:24 -0300

    requisito 9

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-function countAnimals({ specie = 0, gender = 0 } = 0) {
+function countAnimals({ specie = 0, sex = 0 } = 0) {
   if (specie === 0) {
     return species.reduce((acc, cur) => {
       acc[cur.name] = cur.residents.length;
@@ -10,9 +10,8 @@ function countAnimals({ specie = 0, gender = 0 } = 0) {
     }, {});
   }
   const findSpecie = species.find((elm) => elm.name === specie).residents;
-  const slctdAnimals = gender === 0 ? findSpecie : findSpecie.filter((elm) => elm.sex === gender);
+  const slctdAnimals = sex === 0 ? findSpecie : findSpecie.filter((elm) => elm.sex === sex);
   return slctdAnimals.length;
 }
-countAnimals();
 
 module.exports = countAnimals;
