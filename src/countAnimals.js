ebcc1354aca3d658a8fa03eb5b55ff4b41d6a33f
João Cassanji <joaocassanji@gmail.com>
2021-09-14 18:35:02 -0300

    requisito 5 corrigido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,8 +3,8 @@ const data = require('../data/zoo_data');
 function countAnimals(animal) {
   const animals = {};
   if (animal === undefined) {
-    data.species.forEach(e  => { animals[e.name] = e.residents.length });
-    } else if (animal.gender === undefined) {
+    data.species.forEach(e => { animals[e.name] = e.residents.length; })
+  } else if (animal.gender === undefined) {
     const residentsObject = data.species.filter((e) => animal.specie.includes(e.name))[0].residents;
     return residentsObject.length;
   } else {
