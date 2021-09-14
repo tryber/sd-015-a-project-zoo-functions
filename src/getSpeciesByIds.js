23741c81034571cb0437a15b41e2c69629051756
Daniel Tavares <daniel1tavares@outlook.com>
2021-09-14 16:19:39 -0300

    iniciando requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui.
+function getSpeciesByIds(...ids) {
+  // src: https://trybecourse.slack.com/archives/C027T2VU8U8/p1631556965189500
+  const specieById = ids.map((id) => data.species.filter((specie) => specie.id === id));
+  return specieById;
 }
 
 module.exports = getSpeciesByIds;
+console.log(getSpeciesByIds(
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+  '0938aa23-f153-4937-9f88-4858b24d6bce',
+));
