e4e42eb45fc6df07c1161430c64ce422cd10e9ad
Júnior Zanoni <zanoni.odenir@gmail.com>
2021-09-14 17:37:25 -0300

    4 requisito

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,8 +5,16 @@ const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
 const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
 const managers = [stephanieId, olaId, burlId];
 
-const isManager = (id) => managers.some((elemento) => elemento === id);
+function isManager(id) {
+  return managers.some((elemento) => elemento === id);
+}
 
-const getRelatedEmployees = (managerId) => {};
+function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === true) {
+    const filtro = data.employees.filter((elemento) => elemento.managers[0] === managerId);
+    const filtroNomes = filtro.map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
+    return filtroNomes;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+}
 
 module.exports = { isManager, getRelatedEmployees };
