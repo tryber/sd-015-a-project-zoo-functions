368736e816d3bec2d31ad9b53fb5af2554c67861
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-14 09:01:02 -0300

    Adicionando os requisitos 9

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,23 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
+  // const listAnimals = { };
   if (animal === undefined) {
-    return 'Teste1';
+    const value = data.species.map((element) => element.residents.length);
+    return value;
   }
   if (Object.values(animal).length === 1) {
-    return 'fds';
+    const obj = Object.values(animal);
+    const filtterAnimal = data.species.find((element) => element.name === obj[0]);
+    const result = filtterAnimal.residents.length;
+    return result;
   }
-  return 'dasda';
+  const obj = Object.values(animal);
+  const filtterAnimal = data.species.find((element) => element.name === obj[0]);
+  return filtterAnimal.residents.filter((gender) => gender.sex === 'female');
 }
-console.log(countAnimals({ specie: 'bears' }));
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
 
