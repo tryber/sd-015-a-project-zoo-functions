b78ad7a444f53ba4957e01a50fdb2a909abe6175
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 18:18:46 -0300

    R5 rev123124

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,26 +4,23 @@ const geral = {};
 
 function assingGeral(x, y) { geral[x] = y.length; }
 const noInput = () => {
-    data.species.forEach(({ name, residents }) => assingGeral(name, residents));
-    return geral;
-}
+  data.species.forEach(({ name, residents }) => assingGeral(name, residents));
+  return geral;
+};
 
-
-const genderCheck = (animal) =>{
-  const { specie, gender } = animal
+const genderCheck = (animal) => {
+  const { specie, gender } = animal;
   const { residents } = data.species.find(({ name }) => name === specie);
   if (!gender) {
     return residents.length;
   }
   const residentsByGenre = residents.filter(({ sex }) => sex === gender);
   return residentsByGenre.length;
-}
+};
 
 function countAnimals(animal) {
   // seu código aqui
-  if(!animal) {
-   return noInput();
-  }
+  if (!animal) return noInput();
   return genderCheck(animal);
 }
 
