93dcdccefb8285df941645f2d3d4daf89e499ba9
João Cassanji <joaocassanji@gmail.com>
2021-09-14 18:02:24 -0300

    requisito 5 corrigido

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,16 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-    let animals = {};
-    if (animal === undefined) {
-        data.species.forEach((element) => animals[element.name] = element.residents.length);
-    } else if (animal.gender === undefined){
-        const animalResidentsObject = data.species.filter((element) => animal.specie.includes(element.name))[0].residents;
-        return animalResidentsObject.length;
-    } else {
-        const animalResidentsObject = data.species.filter((element) => animal.specie.includes(element.name))[0].residents;
-        return animalResidentsObject.filter((element) => element.sex === `${animal.gender}`).length;
-    } return animals;
+  let animals = {};
+  if (animal === undefined) {
+    data.species.forEach((element) => animals[element.name] = element.residents.length);
+  } else if (animal.gender === undefined){
+    const animalResidentsObject = data.species.filter((element) => animal.specie.includes(element.name))[0].residents;
+    return animalResidentsObject.length;
+  } else {
+    const animalResidentsObject = data.species.filter((element) => animal.specie.includes(element.name))[0].residents;
+    return animalResidentsObject.filter((element) => element.sex === `${animal.gender}`).length;
+  } return animals;
 }
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,14 +1,7 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-
-  const allEmployees = data.employees;
-  let allManagers = [];
-  allEmployees.forEach((element) => allManagers.push(element.managers));
-  return allManagers.find((element) => element == id);
 }
-
-console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
