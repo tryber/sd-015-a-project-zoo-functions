869ce8e614ea7c4e21c9255050597851559e6295
alvarogularte <alvaro.gularte@gmail.com>
2021-09-14 18:57:19 -0300

    Requirimment 6 completed

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,22 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const getAges = entrants.map((element) => element.age);
+  return ({
+    child: getAges.filter((child) => child < 18).length,
+    adult: getAges.filter((adult) => adult >= 18 && adult < 50).length,
+    senior: getAges.filter((senior) => senior >= 50).length,
+  });
+};
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const calculateEntry = (entrants) => {
+  const getEntrants = countEntrants(entrants);
+  return Object.keys(getEntrants).reduce(((acc, curr) =>
+    acc + getEntrants[curr] * prices[curr]), 0);
+};
 
 module.exports = { calculateEntry, countEntrants };
