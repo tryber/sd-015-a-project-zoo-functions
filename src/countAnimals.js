978fa3bc7aa941c98ff29112ac3a35e87b33c490
Omar Merljak <omerljak@gmail.com>
2021-09-14 19:42:36 -0300

    requisito countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(specie) {
-
+  const obj = {};
+  if (specie === undefined) {
+    data.species.forEach((animal) => { obj[animal.name] = animal.residents.length; });
+    return obj;
+  }
+  const residentSpecies = data.species.find((animal) => animal.name === specie.specie).residents;
+  if (specie.specie && specie.sex) {
+    return residentSpecies.filter((animal) => animal.sex === specie.sex).length;
+  }
+  return residentSpecies.length;
 }
 
 module.exports = countAnimals;
