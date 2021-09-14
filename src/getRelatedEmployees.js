5af51c0d761738f4004a25b5c266a08088b0eb06
Diego Brito <diegorbrito9@gmail.com>
2021-09-14 16:58:33 -0300

    Requisito 5 (check)

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,10 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
 }
 
+console.log(data.employees);
 module.exports = { isManager, getRelatedEmployees };
