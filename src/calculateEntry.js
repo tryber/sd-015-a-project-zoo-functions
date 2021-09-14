2dd5e410e93353478848249f0fb6c59fc9e1a1ca
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-14 01:40:30 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return [];
+
+  const senior = entrants.filter(({ age }) => age >= 50).length;
+  const adult = entrants.filter(({ age }) => age >= 18 && age < 50).length;
+  const child = entrants.filter(({ age }) => age < 18).length;
+
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  const totalVisitors = Object.entries(countEntrants(entrants));
+  const { prices } = data;
+  return totalVisitors.reduce((acc, curr) => acc + (prices[curr[0]] * curr[1]), 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
+
