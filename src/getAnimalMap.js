2ae8aea5806161948c18b64f699ef10d9eed3e99
Italo Sergio <italosergio@graduate.org>
2021-09-14 01:04:10 -0300

    Challange 7 - stage 1/7 (check)

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+  const animalsNE = data.species.filter((species) => species.location === 'NE');
+  const animalsNW = data.species.filter((species) => species.location === 'NW');
+  const animalsSE = data.species.filter((species) => species.location === 'SE');
+  const animalsSW = data.species.filter((species) => species.location === 'SW');
+
+  if (!options) {
+    return {
+      NE: animalsNE.map((element) => element.name),
+      NW: animalsNW.map((element) => element.name),
+      SE: animalsSE.map((element) => element.name),
+      SW: animalsSW.map((element) => element.name),
+    };
+  }
+  // console.log(animalsNE.map((element) => element.name));
 }
 
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
