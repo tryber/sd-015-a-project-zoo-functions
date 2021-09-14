a61ad440d79c0108dd9770926aa00c051e1f7e85
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 16:00:54 -0300

    Lint rev1

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,21 +2,19 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-  const geral = {}
+  const geral = {};
   if (!animal) {
-    data.species.forEach(({ name, residents }) => geral[name] = residents.length)
+    data.species.forEach(({ name, residents }) => geral[name] = residents.length);
     return geral;
-  } 
-  else {
-    const { specie , gender } = animal
-    if (!gender){
+  }
+  const { specie , gender } = animal;
+  if (!gender){
     const { residents } = data.species.find(({ name }) => name === specie);
     return residents.length;
-    }
-    const { residents } = data.species.find(({ name }) => name === specie); 
-    const residentsByGenre = residents.filter(({ sex }) => sex === gender);
-    return residentsByGenre.length;
   }
+  const { residents } = data.species.find(({ name }) => name === specie);
+  const residentsByGenre = residents.filter(({ sex }) => sex === gender);
+  return residentsByGenre.length;
 }
 
 module.exports = countAnimals;
