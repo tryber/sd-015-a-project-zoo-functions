b8cbebffc1f7caee2863b2251b65a5a48654229f
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-14 18:38:40 -0300

    fix lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,26 +1,25 @@
 const data = require('../data/zoo_data');
 
-function getSpecieByRegion(region) {
-  return (data.species.filter((specie) =>
-    (specie.location === region))).map((animal) => animal.name);
-}
+// function getSpecieByRegion(region) {
+//   return (data.species.filter((specie) =>
+//     (specie.location === region))).map((animal) => animal.name);
+// }
 
-function getAnimalsByRegion() {
-  const speciesRegions = { NE: [], NW: [], SE: [], SW: [] };
-  speciesRegions.NE = getSpecieByRegion('NE');
-  speciesRegions.NW = getSpecieByRegion('NW');
-  speciesRegions.SE = getSpecieByRegion('SE');
-  speciesRegions.SW = getSpecieByRegion('SW');
-  return speciesRegions;
-}
+// function getAnimalsByRegion() {
+//   const speciesRegions = { NE: [], NW: [], SE: [], SW: [] };
+//   speciesRegions.NE = getSpecieByRegion('NE');
+//   speciesRegions.NW = getSpecieByRegion('NW');
+//   speciesRegions.SE = getSpecieByRegion('SE');
+//   speciesRegions.SW = getSpecieByRegion('SW');
+//   return speciesRegions;
+// }
 
-function getAnimalsWithOptions(option) {
-  const species = getAnimalsByRegion();
-}
+// function getAnimalsWithOptions(option) {
+//   const species = getAnimalsByRegion();
+// }
 function getAnimalMap(options) {
   // seu código aqui
-  return getAnimalsByRegion();
+
 }
 
-console.log(getAnimalsByRegion());
 module.exports = getAnimalMap;
