90c4e97cb509301a9c301e2eb0d2a41e3f672f50
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-09-14 16:32:53 -0300

    req 3 e 4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,14 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const obj = {};
+  species.map((specie) => {
+    obj[specie.name] = specie.residents.length;
+    return true;
+  });
+  if (species === undefined) {
+    return obj;
+  }
 }
 
 module.exports = countAnimals;
