a4b3ce8e64227b65bbf3aa4313c0b6f7b75c75df
Gustavo Lima <lima_gus@outlook.com>
2021-09-13 23:40:52 -0300

    Requisito 6.1 - countEntrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
+
 function countEntrants(entrants) {
-  // seu código aqui
+  //  Array.filter retorna um array
+  return {
+    child: entrants.filter((count) => count.age < 18).length,
+    adult: entrants.filter((count) => count.age >= 18 && count.age < 50).length,
+    senior: entrants.filter((count) => count.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
