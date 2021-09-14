d2ceeeb5e79378e20d61480232567ddc2b6623e4
Eric Cruz <effolex@gmail.com>
2021-09-14 15:08:07 -0300

    nono requisito concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+const { species, employees } = data;
+
+function getOldestSpecies({ responsibleFor }) {
+  const { residents } = species.find(({ id }) => id === responsibleFor[0]);
+
+  const oldestResident = residents.reduce(
+    (older, resident) =>
+      ((resident.age > older.age) ? resident : older),
+    { age: 0 },
+  );
+
+  return Object.values(oldestResident);
+}
+
+function getOldestFromFirstSpecies(idGiven) {
+  const employee = employees.find(({ id }) => id === idGiven);
+  return getOldestSpecies(employee);
 }
 
 module.exports = getOldestFromFirstSpecies;
