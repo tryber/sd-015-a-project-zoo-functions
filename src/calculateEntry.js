0c64f566cff48e3bd79a49791630a42394be4cfb
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-09-14 09:29:49 -0300

    Requisito 4 e 9

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,8 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
-}
+  return {
+  
 
 function calculateEntry(entrants) {
   // seu código aqui
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((manager) => manager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  return data.employees
+    .filter((employeesManager) => employeesManager.managers.includes(managerId))
+    .map((name) => `${name.firstName} ${name.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
