2ef963112ad5a4242f6ac6b502da8e331e29ef0d
André <andre.bonaf@gmail.com>
2021-09-14 19:54:12 -0300

    Requisito 5 refeito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,36 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+const base = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+};
+
+function addZones(zone1, zone2, zone3, zone4) {
+  const animalsPerZone1 = data.species.filter((animals) => animals.location === zone1);
+  animalsPerZone1.forEach((animal) => {
+    base[zone1].push(animal.name);
+  });
+  const animalsPerZone2 = data.species.filter((animals) => animals.location === zone2);
+  animalsPerZone2.forEach((animal) => {
+    base[zone2].push(animal.name);
+  });
+  const animalsPerZone3 = data.species.filter((animals) => animals.location === zone3);
+  animalsPerZone3.forEach((animal) => {
+    base[zone3].push(animal.name);
+  });
+  const animalsPerZone4 = data.species.filter((animals) => animals.location === zone4);
+  animalsPerZone4.forEach((animal) => {
+    base[zone4].push(animal.name);
+  });
+  return base;
 }
+
+function getAnimalMap(options) {
+  if (options === undefined) return addZones('NE', 'NW', 'SE', 'SW');
+}
+
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
