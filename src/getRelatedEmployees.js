9867d61fdd57b672ff3cba415b5294ddeb673dfa
Leandro Basilio <leandroab07@gmail.com>
2021-09-14 01:01:48 -0300

    Adiciona requisito 1

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,10 +2,14 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  const resultado = data.employees.find((busca) => busca.id === id);
+  return resultado;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
 }
+
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // Fazer a função
+  const resultado = data.species.filter((especie) => ids.includes(especie.id));
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
