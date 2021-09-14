6d87af7fb9c7e781f3c94c2da2fb972cb0eecbbc
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-14 16:08:40 -0300

    corrige lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,14 +1,13 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
   if (ids.length === 0) {
     return [];
   }
-  const species = data.species;
-  //console.log(species);
-  const selectedSpecies = species.filter((specie) => {
-    for (let i = 0; i < ids.length; i++) {
+  
+  const selectedSpecies = data.species.filter((specie) => {
+    for (let i = 0; i < ids.length; i += 1) {
       if (specie.id === ids[i]) {
         return specie;
       }
@@ -20,8 +19,8 @@ function getSpeciesByIds(...ids) {
 // console.log(getSpeciesByIds());
 console.log(
   getSpeciesByIds(
-    "0938aa23-f153-4937-9f88-4858b24d6bce",
-    "533bebf3-6bbe-41d8-9cdf-46f7d13b62ae"
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'
   )
 );
 
