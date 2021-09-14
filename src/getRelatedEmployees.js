a5d021a56f9b984bf5f0538aa91f247e46207670
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-09-14 16:35:31 -0300

    requisito 3

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -5,13 +5,17 @@ const { employees } = data;
 function isManager(id) {
   return employees.some((element) => element.managers.includes(id));
 }
-console.log(isManager('stephanieId'));
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
+console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+console.log(isManager('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 function getRelatedEmployees(managerId) {
-  if(isManager() === true) {
-    return employees.map((element) => element.managers);
+  if (isManager(managerId) === true) {
+    return employees
+      .filter((element) => (element.managers.includes(managerId)))
+      .map((element2) => `${element2.firstName} ${element2.lastName}`);
   } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
-console.log(getRelatedEmployees('stephanieId'));
+console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
