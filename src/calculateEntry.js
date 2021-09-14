de994c26383219712b0124c1904ec587c73b3cfb
Ismael Peccin <ismaelpeccin@gmail.com>
2021-09-14 15:48:56 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,37 @@
 const data = require('../data/zoo_data');
 
+const { prices } = require('../data/zoo_data');
+
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+
+  entrants.forEach(({ age }) => {
+    if (age < 18) {
+      child += 1;
+      return;
+    }
+    if (age >= 50) {
+      senior += 1;
+      return;
+    }
+    adult += 1;
+  });
+
+  return { child, adult, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+
+  let { child, adult, senior } = countEntrants(entrants);
+
+  child *= prices.child;
+  adult *= prices.adult;
+  senior *= prices.senior;
+
+  return child + adult + senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
