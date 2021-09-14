0c3b2ed1ad314ac5d1ee76a9d5f7dcdb59e367e7
Eliana Nunes <elianadnunes@gmail.com>
2021-09-14 16:12:33 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,11 +2,19 @@ const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((value) => value.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const test = employees.filter((item) => item.managers.includes(managerId));
+
+    const namesOfEmployees = [];
+    test.forEach((employee) => namesOfEmployees.push(`${employee.firstName} ${employee.lastName}`));
+    return namesOfEmployees;
+  }
+
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
