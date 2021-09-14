2db749bec6813b578c594e32a1a6711fe8a83c5a
Renato Moreno <renats.mc@gmail.com>
2021-09-14 16:58:40 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,26 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  const child = entrants.filter((children) => children.age < 18).length;
+  const adult = entrants.filter((adults) => adults.age >= 18 && adults.age < 50).length;
+  const senior = entrants.filter((seniors) => seniors.age >= 50).length;
+  const visitants = { child, adult, senior };
+  return visitants;
+  // console.log(visitants);
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
+  // ref: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const amountVisitants = countEntrants(entrants);
+  const chidrenPrice = amountVisitants.child * data.prices.child;
+  const adultsPrice = amountVisitants.adult * data.prices.adult;
+  const seniorsPrice = amountVisitants.senior * data.prices.senior;
+  const total = chidrenPrice + adultsPrice + seniorsPrice;
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
