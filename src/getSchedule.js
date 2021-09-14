91843f5a4ee446f2aab210ec42c2e6b46051f3a8
Valdeci <moura97.valdeci@gmail.com>
2021-09-14 17:41:56 -0300

    Corrige erros de lint requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -7,7 +7,6 @@ const animals = species.map((element) => element.name);
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 const wichDay = (day) => {
-
   const anml = species.filter((an) => an.availability.some((element) => element === day));
 
   const hour = `Open from ${hours[day].open}am until ${hours[day].close}pm`;
@@ -15,8 +14,7 @@ const wichDay = (day) => {
   if (day === 'Monday') return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
 
   return { officeHour: hour, exhibition: anml.map((element) => element.name) };
-
-}
+};
 
 const zooSchedule = {
   Tuesday: wichDay('Tuesday'),
@@ -26,17 +24,15 @@ const zooSchedule = {
   Saturday: wichDay('Saturday'),
   Sunday: wichDay('Sunday'),
   Monday: wichDay('Monday'),
-}
+};
 
 function getSchedule(scheduleTarget = '') {
-
   if (!scheduleTarget) return zooSchedule;
 
   if (!animals.some((element) =>
-  element === scheduleTarget) &&
-  !weekDays.some((element) =>
-  element === scheduleTarget)) 
-  return zooSchedule;
+  element === scheduleTarget) 
+  && !weekDays.some((element) =>
+  element === scheduleTarget)) {return zooSchedule;}
 
   if (animals.some((element) =>
   element === scheduleTarget)) 
