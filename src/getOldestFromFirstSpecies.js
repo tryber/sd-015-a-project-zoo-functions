934b13d3321d389f2059f917982b1b0d8ee67207
William <william.mk1@outlook.com>
2021-09-14 20:17:03 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,7 +4,8 @@ function getOldestFromFirstSpecies(id) {
   const getEmployeeId = data.employees.find((employee) => employee.id === id);
   const getFirstRespAnimal = getEmployeeId.responsibleFor[0];
   const getResidents = (data.species.find((specie) => specie.id === getFirstRespAnimal)).residents;
-  const getOlderAnimal = getResidents.reduce((older, animals) => older.age > animals.age ? older : animals, 0);
+  const getOlderAnimal = getResidents.reduce((older, animals) =>
+    older.age > animals.age ? older : animals, 0);
   return Object.values(getOlderAnimal);
 }
 
