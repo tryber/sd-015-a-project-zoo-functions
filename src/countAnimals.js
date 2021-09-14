4509bf1d6837102661ce8b338da08bb9565514c1
fabiana <fabianamfranco@outlook.com>
2021-09-14 12:48:53 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const zoo = {};
+function cadaUm(especie) {
+  zoo[`${especie.name}`] = especie.residents.length;
+  return zoo;
+}
+
 function countAnimals(animal) {
-  // seu código aqui
+  // 1 - Sem parâmetros, retorna as espécies e sua quantidade
+  if (animal === undefined) {
+    data.species.forEach(cadaUm);
+    return zoo;
+  // 3 - recebendo como parâmetro um objeto com a chave 'specie' e 'gender', retorna um número, a quantidade de animais daquela espécie, no gênero selecionado
+  } if (animal.gender) {
+    const animais = data.species.find((element) => animal.specie === element.name);
+    const porSexo = animais.residents.filter((element) => animal.gender === element.sex);
+    return porSexo.length;
+  }
+  // 2 - recebendo como parâmetro um objeto com a chave 'specie', retorna um número, a quantidade de animais daquela espécie
+  const bicho = data.species.find((element) => animal.specie === element.name);
+  return bicho.residents.length;
 }
 
 module.exports = countAnimals;
