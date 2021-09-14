ee7cc74f2e7a8e2b7aa7b686b92d92a4c8a970ac
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-09-14 16:04:02 -0300

    primeiro commit vqv

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -4,7 +4,7 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   // seu código aqui
   const checkId = species
-  .filter((specie) => specie.id === ids);
+    .filter((specie) => specie.id === ids);
   return checkId;
 }
 
