c245f6bb547ced2900948f79307b54f69e0cd8ac
deobrafantinif <deboraferfan@gmail.com>
2021-09-14 02:53:08 -0300

    resolucao do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
+function countAnimals(specie) {
+  const numberAnimals = {};
+  if (specie === undefined) {
+    data.species.forEach((animal) => {
+      numberAnimals[animal.name] = animal.residents.length;
+    });
+    return numberAnimals;
+  }
 
+  const animalChoice = data.species.find((animal) => animal.name === specie.specie);
+
+  if (specie.gender !== undefined) {
+    const genderAnimalChoice = animalChoice.residents
+      .filter((animal) => animal.sex === specie.gender);
+    return genderAnimalChoice.length;
+  }
+
+  return animalChoice.residents.length;
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
