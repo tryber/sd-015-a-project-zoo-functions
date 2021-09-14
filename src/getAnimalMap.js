eba5866f45f2f93126fa90c1d2e2a35b6a99f2f3
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-09-14 19:13:27 -0300

    Lint issue fixed

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,17 +1,16 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap({ in}) {
-  if (!options) {
-    const { species } = data;
-    const NE = species.filter(({ location }) => location === 'NE').map((info) => info.name);
-    const NW = species.filter(({ location }) => location === 'NW').map((info) => info.name);
-    const SE = species.filter(({ location }) => location === 'SE').map((info) => info.name);
-    const SW = species.filter(({ location }) => location === 'SW').map((info) => info.name);
-    return { NE, NW, SE, SW };
-  }
-    
-}
+// function getAnimalMap(animal) {
+//   if (!options) {
+//     const { species } = data;
+//     const NE = species.filter(({ location }) => location === 'NE').map((info) => info.name);
+//     const NW = species.filter(({ location }) => location === 'NW').map((info) => info.name);
+//     const SE = species.filter(({ location }) => location === 'SE').map((info) => info.name);
+//     const SW = species.filter(({ location }) => location === 'SW').map((info) => info.name);
+//     return { NE, NW, SE, SW };
+//   }
+// }
 
-console.log(getAnimalMap());
+// console.log(getAnimalMap());
 
 // module.exports = getAnimalMap;
