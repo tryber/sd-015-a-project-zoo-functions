2d0f70118a764729232b9c0b2dedfd860175b7c4
Diego Brito <diegorbrito9@gmail.com>
2021-09-14 10:31:58 -0300

    Refatorando requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -4,8 +4,8 @@ function getAnimalsOlderThan(animal, age) {
   let aboveMinimumAge = true;
   data.species.forEach((animals) => {
     if (animals.name === animal) {
-      animals.residents.forEach((ages) => {
-        if (ages.age < age) aboveMinimumAge = false;
+      animals.residents.forEach((element) => {
+        if (element.age < age) aboveMinimumAge = false;
       });
     }
   });
