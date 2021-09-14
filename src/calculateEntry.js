b478b380a905cd7adce3176e578bcee9008ac4f3
Alex Sandro Sousa <alexsandro091092@gmail.com>
2021-09-14 10:55:54 -0300

    Adicionado Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {}
+function countEntrants(entrants) {
+  return {
+    child: entrants.filter((el) => el.age < 18).length,
+    adult: entrants.filter((el) => el.age >= 18 && el.age < 50).length,
+    senior: entrants.filter((el) => el.age >= 50).length,
+  };
+}
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const { prices } = data;
+  const person = countEntrants(entrants);
+  const childPrice = person.child * prices.child;
+  const adultPrice = person.adult * prices.adult;
+  const seniorPrice = person.senior * prices.senior;
+  return childPrice + adultPrice + seniorPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
