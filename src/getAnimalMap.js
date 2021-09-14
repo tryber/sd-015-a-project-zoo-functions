1d8bb0729e6e1c62e00949440a34855e74ffcecc
Braien <braienmp@outlook.com>
2021-09-14 09:24:49 -0300

    inicia implementaçao de getAnimalMap

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
+function getAnimalsByLocation(location, callback) {
+  return data.species.filter((specie) =>
+    (specie.location === location)).map((specie) => callback(specie));
+}
+
+function speciesName(specie) {
+  return specie.name;
+}
+
+function residentsName(specie) {
+  return { [specie.name]: specie.residents.map((resident) => resident.name) };
+}
+
+function sortedResidentsName(specie) {
+  return { [specie.name]: specie.residents.map((resident) => resident.name).sort() };
+}
+
+function getMap(param) {
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  const map = {};
+  locations.forEach((location) => {
+    (map[location] = getAnimalsByLocation(location, param));
+  });
+  return map;
+}
+
+function getAnimalMap(options = {}) {
   // seu código aqui
+  const { includeNames, sorted, sex } = options;
+  if (!includeNames || includeNames !== true) {
+    return getMap(speciesName);
+  }
+  return (sorted !== true) ? getMap(residentsName) : getMap(sortedResidentsName);
 }
 
 module.exports = getAnimalMap;
+
+console.table(getAnimalMap({ includeNames: true, sorted: true }));
