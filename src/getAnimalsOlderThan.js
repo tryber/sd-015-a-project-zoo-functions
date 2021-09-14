749f8ec82ecbc72668473f47549898c1ad8c34b4
Thiago <thiivieira@gmail.com>
2021-09-13 22:41:13 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const animais = species.find((element) => element.name === animal);
+  const animaisResidentes = animais.residents;
+  return animaisResidentes.every((element) => element.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
