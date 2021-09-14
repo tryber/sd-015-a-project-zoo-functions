20e000411b1d55f06cb696c26c7ee644131ce3e6
Eliana Nunes <elianadnunes@gmail.com>
2021-09-14 17:35:53 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,7 +2,20 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return species.reduce((acc, current) => {
+      acc[current.name] = current.residents.length;
+      return acc;
+    }, {});
+  }
+
+  const item = species.find((value) => value.name === animal.specie);
+
+  if (animal.specie && animal.gender) {
+    const animals = item.residents.filter((value) => value.sex === animal.gender);
+    return animals.length;
+  }
+  return item.residents.length;
 }
 
 module.exports = countAnimals;
