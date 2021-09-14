bee797647242113af44c9dfde8be68b689a8332e
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-14 17:48:30 -0300

    gender to sex

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,17 +4,17 @@ const data = require('../data/zoo_data');
 function countAnimals(obj) {
   // seu código aqui
   const everySpecie = {};
-  const gender = {};
+  const sex = {};
   species.forEach((specie) => {
     const { name, residents } = specie;
-    if (obj) gender[name] = residents.filter((resident) => resident.sex === obj.gender).length;
+    if (obj) sex[name] = residents.filter((resident) => resident.sex === obj.sex).length;
     everySpecie[name] = residents.length;
   });
 
   if (!obj) {
     return everySpecie;
-  } if (obj.gender) {
-    return gender[obj.specie];
+  } if (obj.sex) {
+    return sex[obj.specie];
   } return everySpecie[obj.specie];
 }
 
