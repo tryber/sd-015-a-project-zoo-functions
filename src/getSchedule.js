837804b99bf196b692c8410717a286b76f2cae49
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-14 18:39:53 -0300

    requisito 8 c/ lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,22 +1,35 @@
 const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function getScheduleByWeekDate(weekDate) {
+  const schedule = hours[weekDate];
+  const officeHour = (schedule.open === 0 && schedule.close === 0)
+    ? 'CLOSED' : `Open from ${schedule.open}am until ${schedule.close}pm`;
+  const exhibition = species
+    .filter((specie) => specie.availability.some((day) => day === weekDate))
+    .map((specie) => specie.name);
+
+  return { officeHour, exhibition: (exhibition.length) ? exhibition : 'The zoo will be closed!' };
+}
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
   const days = Object.keys(hours);
-  const animals = species.map((specie) => specie.name); 
+  const animals = species.map((specie) => specie.name);
 
   if (days.some((day) => day === scheduleTarget)) {
-
-  } else if (animals.some((animal) => animal === scheduleTarget)) {
-
-  } else {
-    
+    const result = {};
+    result[scheduleTarget] = getScheduleByWeekDate(scheduleTarget);
+    return result;
+  } if (animals.some((animal) => animal === scheduleTarget)) {
+    return species.find((specie) => (specie.name === scheduleTarget)).availability;
   }
-
-  console.log();
-  
+  const result = {};
+  Object.keys(hours).forEach((weekDate) => {
+    result[weekDate] = getScheduleByWeekDate(weekDate);
+  });
+  return result;
 }
 
-getSchedule();
+console.log(getSchedule('Monday'));
 module.exports = getSchedule;
