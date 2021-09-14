6ef522a76e0ebca4912fa4dc7a651bb28b584349
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-14 17:21:49 -0300

    Atualiza req 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,14 +8,14 @@ function countAnimals(animal = '') {
     const allAnimals = {};
     species.forEach((specie) => { allAnimals[specie.name] = specie.residents.length; });
     animalsCount = allAnimals;
-  } else if (animal.gender === undefined) {
+  } else if (animal.sex === undefined) {
     animalsCount = species
       .find((specie) => specie.name === animal.specie).residents.length;
   } else {
     const animalsBySpecie = species
       .find((specie) => specie.name === animal.specie);
     animalsCount = animalsBySpecie.residents
-      .filter((specie) => specie.sex === animal.gender).length;
+      .filter((specie) => specie.sex === animal.sex).length;
   }
   return animalsCount;
 }
