f1785eb9e8d19afa32932a990b21df0087d6a6cc
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-13 22:40:45 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
+// o seguinte código foi realizado com o auxílio das informações encontradas na página: https://dev.to/_bigblind/quick-tip-transform-an-array-into-an-object-using-reduce-2gh6
 
-function countAnimals(animal) {
-  // seu código aqui
+const defaultCountAnimals = data.species.reduce((acc, specie) => {
+  const { name, residents } = specie;
+  return {
+    ...acc, [name]: residents.length,
+  };
+}, {});
+
+function countAnimals(animalObj) {
+  if (animalObj === undefined) return defaultCountAnimals;
+
+  const { specie, gender } = animalObj;
+  if (Object.keys(animalObj).length === 1) return defaultCountAnimals[specie];
+
+  const animalData = data.species.find((animal) => specie === animal.name);
+  return animalData.residents.reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
 }
 
 module.exports = countAnimals;
