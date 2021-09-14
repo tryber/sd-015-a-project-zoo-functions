7460a96b39d779ca7ee304d38dfeb608096d3277
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 17:48:10 -0300

    finalizaçao do requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { SearchSource } = require('jest');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const animalList = {};
+  
+  if (!animal) {
+    data.species.forEach((specie) => {animalList[specie.name] = specie.residents.length;})
+    return animalList;
+  }
+  
+  const { specie, gender } = animal;
+  const searchAnimal = data.species.find((resultAnimal) => resultAnimal.name === specie);
+  const numberOfAnimals = searchAnimal.residents.length;
+  const searchGender = searchAnimal.residents.filter((resultAnimal) => resultAnimal.sex === gender);
+  const animalsByGender = searchGender.length;
+  return gender === undefined ? numberOfAnimals : animalsByGender;
 }
 
 module.exports = countAnimals;
