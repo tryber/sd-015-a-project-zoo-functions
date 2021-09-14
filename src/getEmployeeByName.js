3e62873354584b98cde6237fa80b5998034f54f4
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 12:00:15 -0300

    R3 refactored

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,7 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   // seu código aqui
   if (employeeName === undefined) return {};
-  return data.employees.find((employee) => employee.firstName === employeeName
-    || employee.lastName === employeeName);
+  return data.employees.find(({ firstName, lastName }) => [firstName, lastName].includes(employeeName));
 }
 
 module.exports = getEmployeeByName;
