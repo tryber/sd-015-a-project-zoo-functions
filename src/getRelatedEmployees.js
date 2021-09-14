2fe909327e717b04ea9b568897e9b802d0e4042f
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-14 17:32:20 -0300

    correçao de lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,11 +2,6 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  const { employees } = data;
-  const managers = [];
-  employees.forEach((aux) => managers.concat(aux.managers));
-  return managers.some((aux) => aux === id);
-
 }
 
 function getRelatedEmployees(managerId) {
