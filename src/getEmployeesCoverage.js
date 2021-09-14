a7c168b8b39877b8de5723aa76eefda706048a87
João Felipe P <jf.pelliccione@gmail.com>
2021-09-13 21:09:44 -0300

    Conclusão do requisito 10!

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,27 +1,63 @@
 const data = require('../data/zoo_data');
 
-/*
-function getSpecieLocation(whatSpecie) {
-  return data.species.find((specie) => specie.id === whatSpecie).location;
-}
+function specieIdToSpecieName(responsibleForArray) { // Função que troca 'specie.id' por 'specie.name', dos arrays 'employees.responsibleFor'.
+  const resultArray = [];
+  responsibleForArray.forEach((specie) => {
+    const thisTurnSpecieName = data.species.find((specie2) => specie2.id === specie).name;
+    resultArray.push(thisTurnSpecieName);
+  });
+  return resultArray;
+} // specieIdToSpecieName([snakesId, elephantsId]) --> [snakes, elephants]
 
-function employeesList() {
+function getEachSpecieLocation(responsibleForArray) { // Função que descobre a localização de cada espécie presente em um 'employee.responsibleFor'.
+  const resultArray = [];
+  responsibleForArray.forEach((specie) => {
+    const thisTurnLocation = data.species.find((specie2) => specie2.id === specie).location;
+    resultArray.push(thisTurnLocation);
+  });
+  return resultArray;
+} // getEachSpecieLocation([ottersId, giraffesId]) --> [SE, NE]
+
+function employeesList() { // Função que retorna um array com vários objetos. Cada um desses objetos contém informações específicas (id, fullname, species...), de um determinado 'employee'.
   let newObj = {};
   const resultArray = [];
 
   data.employees.forEach((employee) => {
-    newObj = { id: employee.id, fullName: `${employee.firstName} ${employee.lastName}`, species: employee.responsibleFor };
+    newObj = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: specieIdToSpecieName(employee.responsibleFor),
+      locations: getEachSpecieLocation(employee.responsibleFor),
+    };
+
     resultArray.push(newObj);
     newObj = {};
   });
   return resultArray;
 }
 
-console.log(employeesList());
+const allIds = employeesList().map((employee) => employee.id); // Constante que acumula a 'id', de cada um dos funcionários.
+
+function allNames() { // Função que retorna um array com o 'firstName' e 'lastName' de todos os 'employees'
+  const allNamesArray = [];
+  data.employees.forEach((employee) => {
+    allNamesArray.push(employee.firstName);
+    allNamesArray.push(employee.lastName);
+  });
+  return allNamesArray;
+}
 
 function getEmployeesCoverage(inputObj) {
-  // Escreva sua função aqui
+  if (inputObj === undefined) {
+    return employeesList();
+  } if (allNames().includes(inputObj.name)) {
+    return employeesList().find((employee) =>
+      Object.values(employee)[1].includes(inputObj.name));
+  } if (allIds.includes(inputObj.id)) {
+    return employeesList().find((employee) =>
+      Object.values(employee)[0].includes(inputObj.id));
+  }
+  throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
-*/
