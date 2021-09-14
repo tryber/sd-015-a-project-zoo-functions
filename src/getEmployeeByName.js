a82ed82644063b14d69be79532def745b11c84d1
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-09-13 22:10:26 -0300

    requisito 3, ajustando o lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,11 +1,12 @@
+/* eslint-disable max-len */
 const data = require('../data/zoo_data');
+
 function getEmployeeByName(employeeName) {
   if (!employeeName) {
     return {};
   }
   const nameAndSurname = data.employees.filter((employee) => employee.firstName === employeeName || employee.lastName === employeeName);
-  return nameAndSurname.pop()
-  
+  return nameAndSurname.pop();
 }
 console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
