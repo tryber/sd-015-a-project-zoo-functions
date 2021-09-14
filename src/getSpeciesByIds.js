fc8f675eb04d5225b4529f836b8066bb4515925d
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-09-14 16:02:38 -0300

    primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const checkId = species
+  .filter((specie) => specie.id === ids);
+  return checkId;
 }
 
 module.exports = getSpeciesByIds;
