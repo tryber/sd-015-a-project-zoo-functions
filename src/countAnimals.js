78688a927a0f5c9abb66c7ffb3f2a2e4de2b61e3
Adadezer <adadezer@gmail.com>
2021-09-14 16:09:04 -0300

    arrumando pull exercicio 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
-}
+  const obj = {}; // crio um objeto vazio
+  if (animal === undefined) { // se não tiver parâmetro
+    species.forEach((element) => { obj[element.name] = element.residents.length; }); // add no objeto vazio uma chave e um valor, q são o nome da 'raça', e a quantidade de animais de cada 'raça'
+    return obj;
+  }
 
-module.exports = countAnimals;
+  const especieResidents = species.find((element) => element.name === animal.specie).residents; // acho (find) o nome do animal em cada elemento, (isso me retorna o elemento todo, não a propriedade) e vejo nesse elemento, se o nome é igual ao animal passado como parametro ({ specie: 'penguins' }), sendo igual, vejo a propriedade residents dele
+
+  // if (animal.specie) {
+  //   return especieResidents.length; // achando esse elemento vejo a quantidade de animais q ele tem
+  // }
+
+  if (animal.specie && animal.gender) {
+    return especieResidents.filter((element) => element.sex.length);
+  }
+}
+console.log(countAnimals({ specie: 'penguins', gender: 'female' }));
+// module.exports = countAnimals;
