700fb72715c55a36bdecfdda012f13c62e52fe5f
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-14 18:20:59 -0300

    correçao requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,18 +1,18 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals({ specie = false, gender = false } = {}) {
-  if (!specie && !gender) {
+function countAnimals({ specie = false, sex = false } = {}) {
+  if (!specie && !sex) {
     const allAnimals = {};
     species.forEach((element) => { allAnimals[element.name] = element.residents.length; });
     return allAnimals;
   }
   const theAnimal = species.find((element) => element.name === specie).residents;
-  if (!gender) {
+  if (!sex) {
     return theAnimal.length;
   }
-  const animalNGender = theAnimal.filter((element) => element.sex === gender).length;
-  return animalNGender;
+  const animalNSex = theAnimal.filter((element) => element.sex === sex).length;
+  return animalNSex;
 }
 
 module.exports = countAnimals;
