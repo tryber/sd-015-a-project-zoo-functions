fc30ec8fdd5e89b6629c8fba2074fb6ce214c691
Augusto Alves <augusto.malves@yahoo.com.br>
2021-09-14 08:54:31 -0300

    add 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const primeiroAnimal = data.employees.find((employe) => employe.id === id).responsibleFor[0];
+  const animalVelho = data.species.find((velhote) => velhote.id === primeiroAnimal)
+  .residents.reduce((acc, curr) => (curr.age > acc.age ? curr : acc))
+  return [animalVelho.name, animalVelho.sex, animalVelho.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
