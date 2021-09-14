bc80e77bf435ab3fccb681d72cdd96c519769e35
João V. Dias <diasvictorj@gmail.com>
2021-09-14 19:05:42 -0300

    Sexto Requisito

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,10 +2,31 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
+  if (entrants === undefined || entrants.length === 0) {
+    return 0;
+  }
+  const child = entrants.filter((element) => element.age < 18);
+  const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
+  const senior = entrants.filter((element) => element.age >= 50);
+  return {
+    child: child.length, 
+    adult: adult.length,
+    senior: senior.length,
+  }
 }
 
-function calculateEntry(entrants) {
+function calculateEntry(countEntrants) {
   // seu código aqui
+  const toReturn = (countEntrants.child * 20.99) + (countEntrants.adult * 49.99) + (countEntrants.senior * 24.99);
+  return toReturn;
 }
-
+  const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+ console.log(calculateEntry(countEntrants(entrants))); 
 module.exports = { calculateEntry, countEntrants };
