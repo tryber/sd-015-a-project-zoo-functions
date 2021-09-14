b2d8f987f27b6e92a69e092bad929630a2e10373
Anderson-Zobel <anderson.zobels@gmail.com>
2021-09-14 18:54:13 -0300

    requirement 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((x) => x.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    return data.employees.filter((x) => x.managers.includes(managerId))
+      .map((x) => `${x.firstName} ${x.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
