80ef23d412f75d32ee0af4f1397a2f8ca93387cc
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 12:45:38 -0300

    ajustando erros para o avaliador

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,10 @@
-const { species } = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  if (ids.length == 0) return ids;
-  return data.species.filter((animalId) => ids.find((primeiro) => animalId.id === primeiro))
+  if (!ids) return ids;
+  return data.species.filter((animalId) => ids.find((first) => animalId.id === first));
 }
 
 module.exports = getSpeciesByIds;
