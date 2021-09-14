b3ba43dc264995e88421dba365655ec20b936028
Anderson-Zobel <anderson.zobels@gmail.com>
2021-09-14 15:34:52 -0300

    {}

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species.find((x) => x.name === animal).residents.every((y) => y.age >= age);
+  return data.species.find((x) => x.name === animal).residents.every((y) => y.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((animalId) => ids.includes(animalId.id));
+  return data.species.filter((animalId) => ids.includes(animalId.id));
 }
 module.exports = getSpeciesByIds;
