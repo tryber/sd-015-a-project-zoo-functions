1c276131d49c87890df9e6f58ec92ae4090e67df
Matheus <mathspa98@gmail.com>
2021-09-13 23:35:39 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals(animal = 'zero') {
+  if (animal !== 'zero') {
+    const a = data.species.find((elemento) => elemento.name === animal.specie);
+    const contaAnimais = (valor) => a.residents.filter((elemento) => elemento.sex === valor).length;
+    return animal.gender === undefined ? a.residents.length : contaAnimais(animal.gender);
+  }
+  return data.species.reduce((acc, curr) => {
+    acc[curr.name] = curr.residents.length;
+    return acc;
+  }, {});
 }
 
+console.log(countAnimals());
 module.exports = countAnimals;
