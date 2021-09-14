9569f71148c3417adf4d8aee4f698f3782172397
Tamyres <tamyrescaroline.oliveira@gmail.com>
2021-09-14 12:54:00 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(idInput) {
+  const employeeData = employees.find(({ id }) => id === idInput);
+  const { responsibleFor } = employeeData;
+  const foundAnimal = species.find(({ id }) => id === responsibleFor[0]);
+  const oldestAnimal = foundAnimal.residents.reduce((biggest, number) => {
+    if (biggest.age > number.age) {
+      return biggest;
+    }
+    return number;
+  }, 0);
+  return Object.values(oldestAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
