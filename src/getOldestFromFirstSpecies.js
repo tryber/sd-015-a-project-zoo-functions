b35cc6fbdeca58e373be6a567e9e626f68bb8ecf
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-14 14:50:53 -0400

    Soluçao do requisito 9 - Project-zoo-functions

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+function olderAnimalById(id) {
+  const { species } = data;
+  // const age = species.find((specie) => specie.id === id).residents[0];
+  const getOlderBySpecie = species.find((specie) => specie.id === id).residents
+    .reduce((older, curr) => ((curr.age >= older.age) ? curr : older), { age: 0 });
+  const olderAnimalOfSpecie = [];
+  olderAnimalOfSpecie.push(getOlderBySpecie.name, getOlderBySpecie.sex, getOlderBySpecie.age);
+  return olderAnimalOfSpecie
+}
+// console.log(olderAnimalById('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const { employees } = data;
+  const getFirstSpecieOfEmployee = employees.find((employee) => employee.id === id).responsibleFor[0];
+  return olderAnimalById(getFirstSpecieOfEmployee);
 }
-
+// console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'))
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,10 @@
 const data = require('../data/zoo_data');
+const { hours } = require('../data/zoo_data');
 
+function allHoursOfOperation(objHours) {
+  return Object.keys(objHours).reduce((acc, curr) => (acc+= curr), {})
+}
+console.log(allHoursOfOperation(hours))
 function getSchedule(scheduleTarget) {
   // seu código aqui
 }
