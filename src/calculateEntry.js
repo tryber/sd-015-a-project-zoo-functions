d2403fb7a672d30349228af58b82a85d0cafef9d
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-13 22:10:09 -0300

    Começa req 7, primeiro teste passa

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -14,9 +14,6 @@ function countEntrants(entrants) {
     //
   }, { child: 0, adult: 0, senior: 0 });
 }
-// adult: 49.99,
-// senior: 24.99,
-// child: 20.99,
 function calculateEntry(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) {
     return 0;
@@ -27,13 +24,4 @@ function calculateEntry(entrants) {
   const totalToPaid = (child * childPrice) + (senior * seniorPrice) + (adult * adultPrice);
   return totalToPaid;
 }
-const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-console.log(calculateEntry({}));
 module.exports = { calculateEntry, countEntrants };
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
+// { [specie.location]: specie.name })
+const [...species] = data.species;
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getSpeciesByLocation(speciesToSearch, location) {
+  const specie = speciesToSearch.filter((specieToSearch) => specieToSearch.location === location);
+  const arrayOfSpecies = [];
+  specie.forEach((element) => {
+    arrayOfSpecies.push(element.name);
+  });
+  return arrayOfSpecies;
 }
 
+function getAnimalMap(options) {
+  if (!options) {
+    const neSpecies = getSpeciesByLocation(species, 'NE');
+    const nwSpecies = getSpeciesByLocation(species, 'NW');
+    const seSpecies = getSpeciesByLocation(species, 'SE');
+    const swSpecies = getSpeciesByLocation(species, 'SW');
+    return {
+      NE: neSpecies,
+      NW: nwSpecies,
+      SE: seSpecies,
+      SW: swSpecies,
+    };
+  }
+  if (options.includeNames) {
+
+  }
+}
+//
+console.log(getAnimalMap({ includeNames: true }));
 module.exports = getAnimalMap;
