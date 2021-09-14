fd8fd882e88bac7071507d448e1e73794c9e7546
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-14 17:27:24 -0300

    adds requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,14 +1,18 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// IF THERE IS NO ARGUMENT
+const noArguments = () => {
+  const result = {};
+  species.forEach((specie) => {
+    result[specie.name] = specie.residents.length;
+  });
+  return result;
+};
+
+// MAIN FUNCTION
 function countAnimals(animal) {
-  if (!animal) {
-    const result = {};
-    species.forEach((specie) => {
-      result[specie.name] = specie.residents.length;
-    });
-    return result;
-  }
+  if (!animal) return noArguments();
 
   if (Object.keys(animal).length === 1) {
     return species
