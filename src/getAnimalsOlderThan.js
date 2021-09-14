41673b0753c282bf737472a738c15a3b82ca6f0a
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-14 20:59:09 -0300

    fix lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,10 +2,12 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const specie = data.species.filter((specie) => specie.name === animal);
-  let animals = specie.map((animal) => animal.residents);
-  animals = animals[0];
-  return animals.every((animal) => animal.age > age);
+  const filteredSpecie = data.species.filter(
+    (specie) => specie.name === animal
+  );
+  let animals = filteredSpecie.map((zooAnimal) => zooAnimal.residents);
+  [animals] = animals;
+  return animals.every((finalAnimal) => finalAnimal.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
