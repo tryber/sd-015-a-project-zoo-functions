769bbdaf9a8efc64c6b5a1b42482e0f0ece5ef00
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-14 19:21:20 -0300

    Requisito 2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,11 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  // const { species } = data;
+  // const numberSpecies = species.map((specie) => specie.residents.length);
+  // const nameSpecies = species.map((specie) => specie.name);
+  // const speciesReport = species.reduce((acc, specie, index, array) => Object.assign(acc, { nameSpecies[index]: numberSpecies[index]}) , {});
+// return speciesReport;
 }
 
 module.exports = countAnimals;
