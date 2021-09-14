3fb7eb847f7af27e3326413ef0e2a8e837cbabb7
Anderson-Zobel <anderson.zobels@gmail.com>
2021-09-14 15:08:13 -0300

    requirement 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((x) => x.firstName === employeeName || x.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
