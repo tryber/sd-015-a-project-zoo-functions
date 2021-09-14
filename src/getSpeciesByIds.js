e0c42d2edfbaa2eeeec6c3c7ea98072657603818
Pedro Henrique <pedrohrds05@hotmail.com>
2021-09-14 11:48:00 -0300

    Refatorando o requsito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return ids.map((id) => data.species.find((element) => element.id === id));
+  return ids.map((id) => data.species.find((element) => element.id.includes(id)));
 }
 
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
