0883f40963284a5e05e83a15f50f13fae8cd6b2a
Anderson Alencar <anderson23alencar@gmail.com>
2021-09-14 16:25:19 -0300

    Requisito 5 - countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    const numberOfAnimals = {};
+    species.forEach((spe) => { numberOfAnimals[spe.name] = spe.residents.length; });
+    return numberOfAnimals;
+  }
+  const { specie, gender } = animal;
+  const animalsForSpecie = species.find((spe) => spe.name === specie).residents;
+  const animalsOfGender = animalsForSpecie
+    .reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
+
+  return !gender ? animalsForSpecie.length : animalsOfGender;
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
