c7b1ab54fe6ebf4209f3b56302be315f8847316b
bruholanda <brunaholandacunha7@gmail.com>
2021-09-14 16:32:51 -0300

    Requirement 7; Draft

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,42 @@
 const data = require('../data/zoo_data');
 
+const objLocations = {
+  NE: [],
+  NW: [],
+  SE: [],
+  SW: [],
+};
+
+const getAnimalLocations = () => {
+  objLocations.NE = data.species.filter((value) => value.location === 'NE')
+    .map((value) => value.name);
+  objLocations.NW = data.species.filter((value) => value.location === 'NW')
+    .map((value) => value.name);
+  objLocations.SE = data.species.filter((value) => value.location === 'SE')
+    .map((value) => value.name);
+  objLocations.SW = data.species.filter((value) => value.location === 'SW')
+    .map((value) => value.name);
+};
+
+const arrayNames = () => {
+  return 0;
+};
+
+/* const sortArrayNames = () => {
+  const sorted = arrayNames();
+  sorted.sort();
+  return sorted;
+}; */
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) {
+    getAnimalLocations();
+    return objLocations;
+  }
+
+  if (options.includeNames) {
+    arrayNames();
+  }
 }
 
 module.exports = getAnimalMap;
