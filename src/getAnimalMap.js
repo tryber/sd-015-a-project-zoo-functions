f453e87076b2248ab17cdb06bd87e128deccb6a5
Italo Sergio <italosergio@graduate.org>
2021-09-14 02:49:51 -0300

    Challange 7 - stage 2/7 (check)

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,21 +1,41 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  const animalsNE = data.species.filter((species) => species.location === 'NE');
-  const animalsNW = data.species.filter((species) => species.location === 'NW');
-  const animalsSE = data.species.filter((species) => species.location === 'SE');
-  const animalsSW = data.species.filter((species) => species.location === 'SW');
+const animalsNE = data.species.filter((species) => species.location === 'NE');
+const animalsNW = data.species.filter((species) => species.location === 'NW');
+const animalsSE = data.species.filter((species) => species.location === 'SE');
+const animalsSW = data.species.filter((species) => species.location === 'SW');
+// let array = []
+// console.log(animalsNE.map((element) => ({ [element.name]: element.residents.map((element) => element.name) })));
 
-  if (!options) {
-    return {
-      NE: animalsNE.map((element) => element.name),
-      NW: animalsNW.map((element) => element.name),
-      SE: animalsSE.map((element) => element.name),
-      SW: animalsSW.map((element) => element.name),
-    };
-  }
-  // console.log(animalsNE.map((element) => element.name));
+const animalsPerLoc = () => ({
+  NE: animalsNE.map((element) => element.name),
+  NW: animalsNW.map((element) => element.name),
+  SE: animalsSE.map((element) => element.name),
+  SW: animalsSW.map((element) => element.name),
+});
+
+const animalsPerLocNames = () => ({
+  NE: animalsNE.map((element) =>
+    ({ [element.name]: element.residents
+      .map((animal) => animal.name) })),
+  NW: animalsNW.map((element) =>
+    ({ [element.name]: element.residents
+      .map((animal) => animal.name) })),
+  SE: animalsSE.map((element) =>
+    ({ [element.name]: element.residents
+      .map((animal) => animal.name) })),
+  SW: animalsSW.map((element) =>
+    ({ [element.name]: element.residents
+      .map((animal) => animal.name) })),
+});
+console.log(animalsPerLocNames());
+
+function getAnimalMap(options) {
+  if (!options) return animalsPerLoc();
+  if (options.includeNames) return animalsPerLocNames();
+
 }
 
-console.log(getAnimalMap());
+const options = { includeNames: true };
+console.log(getAnimalMap(options));
 module.exports = getAnimalMap;
