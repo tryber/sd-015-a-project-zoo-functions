155111097281614fe3f10db431dccc1554ac0b4e
João Cassanji <joaocassanji@gmail.com>
2021-09-14 09:34:41 -0300

    requisito 3 corrigido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species.filter((param) => animal.includes(param.name))
 }
+
+console.log(getAnimalsOlderThan('lions', 3))
 
 module.exports = getAnimalsOlderThan;
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {}
+  }
+  return data.employees.find((param) => employeeName.includes(param.firstName) || employeeName.includes(param.lastName));
 }
 
 module.exports = getEmployeeByName;
