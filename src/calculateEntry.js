7d91ba5567f370ec4338104a1d569e83214fc9a4
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-13 22:42:45 -0400

    Refatora funcao getRelatedEmployees do requisito 4

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -3,21 +3,24 @@ const data = require('../data/zoo_data');
 function separeteByAge(entrants, maxAge, minAge) {
   return entrants.filter((person) => person.age < maxAge && person.age >= minAge).length;
 }
+
 function countEntrants(entrants) {
   // seu código aqui
   let childAdultOrSenior = {
     child: separeteByAge(entrants, 18, 0),
     adult: separeteByAge(entrants, 50, 18),
-    senior: separeteByAge(entrants, 200, 50)
+    senior: separeteByAge(entrants, 200, 50),
   };
   return childAdultOrSenior;
 }
 // console.log(countEntrants([{fulano: 'fulano', age: 5}, {sicrano: 'sicrano', age: 18}, {beltrano: 'beltrano', age: 50}]));
 function calculateEntry(entrants) {
   // seu código aqui
-  if (entrants === undefined || Object.keys(entrants).length === 0) return 0
+  if (entrants === undefined || Object.entries(entrants).length === 0) return 0
   const getEntrants = countEntrants(entrants);
-  const priceToPay = ((getEntrants.child * 20.99) + (getEntrants.adult * 49.99) + (getEntrants.senior * 24.99));
+  const priceToPay = ((getEntrants.child * data.prices.child)
+  + (getEntrants.adult * data.prices.adult)
+  + (getEntrants.senior * data.prices.senior));
   return priceToPay;
 }
 // console.log(calculateEntry());
--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
   // seu código aqui
 }
