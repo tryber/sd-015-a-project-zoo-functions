099842cb5722a6478901ea4865f9ec906af78635
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-14 17:56:28 -0300

    requisito-3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const search = (fName) => fName.firstName === employeeName || fName.lastName === employeeName;
+  return employees.find(search);
 }
+
+// console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
