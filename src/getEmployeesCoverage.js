0f43a47e47a8bab4fe1b77fbbbe5d1c79bffd24b
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-14 15:37:51 -0400

    Corrie eslint do arquivo getOldestFromFirstSpecies - requisito 9

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const { employees } = data;
 
+function getEmployeesCoverage(obj) {
+  // seu código aqui
+  // if (obj.name) {
+  //   const teste = employees.find((employee) => employee.firstName === obj.name);
+  //   let realReturn = {
+  //     id: 1,
+  //     fullName: 1,
+  //     species: [],
+  //     locations: [],
+  //   };
+  //   return Object.keys(realReturn)
+  // }
+}
+// console.log(getEmployeesCoverage({ name: 'Sharonda' }))
 module.exports = getEmployeesCoverage;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -7,13 +7,14 @@ function olderAnimalById(id) {
     .reduce((older, curr) => ((curr.age >= older.age) ? curr : older), { age: 0 });
   const olderAnimalOfSpecie = [];
   olderAnimalOfSpecie.push(getOlderBySpecie.name, getOlderBySpecie.sex, getOlderBySpecie.age);
-  return olderAnimalOfSpecie
+  return olderAnimalOfSpecie;
 }
 // console.log(olderAnimalById('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'))
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const { employees } = data;
-  const getFirstSpecieOfEmployee = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const getFirstSpecieOfEmployee = employees.find((employee) => employee.id === id)
+    .responsibleFor[0];
   return olderAnimalById(getFirstSpecieOfEmployee);
 }
 // console.log(getOldestFromFirstSpecies('fdb2543b-5662-46a7-badc-93d960fdc0a8'))
