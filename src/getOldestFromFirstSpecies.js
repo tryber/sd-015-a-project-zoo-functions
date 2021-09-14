12435f049c0e328d32c2e006955587d49fb43005
Kelvin Pichinini <kelvinpichinini@gmail.com>
2021-09-14 19:05:20 -0300

    9º requisito, getOldestFromFirstSpecies, concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,13 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const animalId = data.employees.find((employee) => employee.id === id).responsibleFor[0];
+  const animalArray = data.species.find((specie) => specie.id === animalId).residents;
+  const sortedAnimalArray = animalArray.sort((a, b) => b.age - a.age);
+  const { age, name, sex } = sortedAnimalArray[0];
+  return [name, sex, age];
 }
 
 module.exports = getOldestFromFirstSpecies;
+
+getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
