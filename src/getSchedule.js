db34441064a9abd9252286827545cb5cd05dba0a
alexandrexpj <alexandrexpjr@hotmail.com>
2021-09-14 17:46:08 -0300

    inicio req 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,22 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  const days = Object.keys(hours);
+  const animals = species.map((specie) => specie.name); 
+
+  if (days.some((day) => day === scheduleTarget)) {
+
+  } else if (animals.some((animal) => animal === scheduleTarget)) {
+
+  } else {
+    
+  }
+
+  console.log();
+  
 }
 
+getSchedule();
 module.exports = getSchedule;
