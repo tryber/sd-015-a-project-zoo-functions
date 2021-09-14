a23641bac4584567616e28c48609fb6deb87e825
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 15:09:36 -0300

    mais um ajuste do avaliador

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,8 @@ const data = require('../data/zoo_data');
 function getEmployeeByName(employeeName) {
   // seu código aqui
   if (!employeeName) return {};
-  return data.employees.find((name) => name.lastName === employeeName || 
-  name.firstName === employeeName);
+  return data.employees.find((name) => name.lastName === employeeName
+  || name.firstName === employeeName);
 }
 
 module.exports = getEmployeeByName;
