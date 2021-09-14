7ab0a71df854a0ab02fc26902c5c478add561230
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-14 10:24:10 -0400

    Corrige eslint de ambiguidade utilizando arrow function + operador ternario, nos arquivos getRelatedEmployees e getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -5,8 +5,8 @@ const { species } = data;
 function getSpeciesByIds(...ids) {
   // seu código aqui
   return ids.reduce((acc, curr) =>
-    (ids === undefined) ? []
-      : acc.concat(species.find((element) => element.id === curr)), []);
+    ((ids === undefined) ? []
+      : acc.concat(species.find((element) => element.id === curr))), []);
 }
 // console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce', '01422318-ca2d-46b8-b66c-3e9e188244ed'));
 module.exports = getSpeciesByIds;
