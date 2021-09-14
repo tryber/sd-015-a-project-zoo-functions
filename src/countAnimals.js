7699220cb18b225e3a332cc2d34cd0f6e164d9a1
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-09-14 18:30:55 -0300

    Requisit 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+// Objec.fromEntries -> https://docs.w3cub.com/javascript/global_objects/object/fromentries, o entendimento dessa estrutura foi tirada do código VICTOR EMMANUEL OLIVEIRA FERNANDES DE ARAUJO.
 
 function countAnimals(animal) {
-  // seu código aqui
+  // seu código aqui.
+  if (!animal) {
+    const animals = Object.fromEntries(species.map((ani) => [ani.name, ani.residents.length]));
+    return animals;
+  }
+  const { specie: name, genger } = animal; // O paramentro recebido é um objeto com duas possibilidades, desestruturamos para conseguir ter acesso as keys desse objeto.
+  if (name && !genger) {
+    return species.find((oneAnimal) => oneAnimal.name === name).residents.length; // Uso do find é para satisfazer a primeira condição.
+  }
+  return species.find((onesAnimal) => onesAnimal.name === name)
+    .residents.filter((sexAnimal) => sexAnimal.sex === genger).length; // O Filter entrega um array com os elementos que satisfazem a condição.
 }
 
 module.exports = countAnimals;
