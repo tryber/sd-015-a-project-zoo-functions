9a9927580ec92e04a1222e8ef2e6654be3a63e02
Braien <braienmp@outlook.com>
2021-09-14 14:58:55 -0300

    inicia implementaçao de getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
+function getAnimals(day) {
+  return data.species.filter((specie) => specie.availability.some((elem) => elem === day)).map((animal) => animal.name);
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const weekDays = Object.keys(data.hours);
+  const schedule = {};
+  weekDays.forEach((day) => {
+    const weekDay = data.hours[day];
+    schedule[day] = (day !== 'Monday') ? {
+      officeHour: `Open from ${weekDay.open}am until ${weekDay.close}pm`,
+      exhibition: getAnimals(day),
+    } : {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!',
+    };
+  });
+  return schedule;
 }
 
 module.exports = getSchedule;
+
+// {
+//   'Tuesday': {
+//     'officeHour': 'Open from 8am until 6pm',
+//     'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
+//   },
+//   'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+// }
+
+console.log(getSchedule());
