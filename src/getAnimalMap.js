0e2faa432bf2fd85bab21ae29e2d7f5c9639238f
Alexsandro Xavier <alexsandro060299@outlook.com>
2021-09-14 12:19:30 -0300

    Começa req 8

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,14 +2,15 @@ const data = require('../data/zoo_data');
 // { [specie.location]: specie.name })
 const [...species] = data.species;
 
-function getSpeciesByLocation(speciesToSearch, location) {
+const getSpeciesByLocation = (speciesToSearch, location) => {
   const specie = speciesToSearch.filter((specieToSearch) => specieToSearch.location === location);
+  console.log(specie);
   const arrayOfSpecies = [];
   specie.forEach((element) => {
     arrayOfSpecies.push(element.name);
   });
   return arrayOfSpecies;
-}
+};
 
 function getAnimalMap(options) {
   if (!options) {
@@ -25,9 +26,8 @@ function getAnimalMap(options) {
     };
   }
   if (options.includeNames) {
-
   }
 }
 //
-console.log(getAnimalMap({ includeNames: true }));
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const { ...schedule } = data.hours;
+const [...species] = data.species;
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const scheduleDays = Object.keys(schedule);
+  const result = scheduleDays.map((day) => {
+    const speciesByDay = species
+      .filter((specie) => specie.availability.includes(day))
+      .map((specieByDay) => specieByDay.name);
+    if (day !== 'Monday') {
+      return {
+        [day]: `Open from ${schedule[day].open}am to ${schedule[day].close}pm`,
+        exhibition: speciesByDay,
+      };
+    }
+    return { [day]: ' CLOSED', exhibition: 'The zoo will be closed!' };
+  });
+  return result;
 }
+
+getSchedule();
 
 module.exports = getSchedule;
