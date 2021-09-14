67c14f2495a37cc5d54e655c95d5b5455f7a2937
Arthur Simões <arthursimoes155@gmail.com>
2021-09-13 21:29:18 -0300

    Refatora requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,13 +3,9 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const animal = employees.find((element) => element.id === id).responsibleFor[0];
-  const spe = species.find((specie) => specie.id === animal).residents;
-  const array = spe.map((element) => element.age);
-  const numberBig = array.reduce((acc, value) => Math.max(acc, value));
-  const teste = spe.find((element) => element.age === numberBig);
-  const oloko = [];
-  oloko.push(teste.name, teste.sex, teste.age);
-  return oloko;
+  const obj = species.find((specie) => specie.id === animal).residents;
+  const number = obj.map((element) => element.age).reduce((acc, value) => Math.max(acc, value));
+  return obj.reduce((a, { name, age, sex }) => (age === number ? a.concat(name, sex, age) : a), []);
 }
 
 console.log(getOldestFromFirstSpecies('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
