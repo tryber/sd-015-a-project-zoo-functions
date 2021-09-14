ac0fe274f010d2bb94f07efb15be00f9db766c7d
thomazj <thomaz.jefferson01@protonmail.com>
2021-09-14 10:43:53 -0300

    requisito 1 revLint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,11 +2,11 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-  if(ids === undefined) {
-    return []
+  if (ids === undefined) {
+    return [];
   }
-  const newArray = data.species.filter((animal) => ids.includes(animal.id))
-  return newArray
+  const newArray = data.species.filter((animal) => ids.includes(animal.id));
+  return newArray;
 }
 
 module.exports = getSpeciesByIds;
