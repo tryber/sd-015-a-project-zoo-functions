fa6124abb7961421a2b249e01d0c21b0b1121c8f
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 15:48:00 -0300

    R5 done

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,6 +2,21 @@ const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  const geral = {}
+  if (!animal) {
+    data.species.forEach(({ name, residents }) => geral[name] = residents.length)
+    return geral;
+  } 
+  else {
+    const { specie , gender } = animal
+    if (!gender){
+    const { residents } = data.species.find(({ name }) => name === specie);
+    return residents.length;
+    }
+    const { residents } = data.species.find(({ name }) => name === specie); 
+    const residentsByGenre = residents.filter(({ sex }) => sex === gender);
+    return residentsByGenre.length;
+  }
 }
 
 module.exports = countAnimals;
