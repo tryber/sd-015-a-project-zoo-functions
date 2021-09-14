64a732cb363f75c4bac7db3724c096028c5130f5
Lucas Loureiro <lucas-loureiro@outlook.com>
2021-09-14 15:40:05 -0300

    adds requirement 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const getOldestFromFirstSpecies = (id) => {
+  const animalId = employees
+    .find((employee) => employee.id === id).responsibleFor[0];
+  const oldestAnimal = species
+    .find((specie) => specie.id === animalId).residents
+    .reduce((oldest, current) => (current.age > oldest.age ? current : oldest));
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
+};
 
 module.exports = getOldestFromFirstSpecies;
+
+// console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
