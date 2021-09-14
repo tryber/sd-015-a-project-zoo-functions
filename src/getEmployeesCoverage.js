2122cc42239f088809d6c26ac4140822081249bf
João Pedro Aguiar Santos <jotapedrok@gmail.com>
2021-09-14 19:45:05 -0300

    requisito 10 - WE ARE THE CHAMPIONS, MY FRIEND!

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,57 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+// const x = [];
+// employees.find((element) => element.firstName === 'Ola').responsibleFor.forEach((bagui) => {
+//   x.push(species.find((coisa) => coisa.id === bagui));
+// });
+// console.log(x);
+
+const allEmployees = () => {
+  const people = [];
+  employees.forEach((employee) => {
+    const { id, firstName, lastName, responsibleFor } = employee;
+    const animals = [];
+    const region = [];
+    responsibleFor.forEach((animalId) => {
+      const theAnimal = species.find((animal) => animal.id === animalId);
+      animals.push(theAnimal.name);
+      region.push(theAnimal.location);
+    });
+    const obj = { id, fullName: `${firstName} ${lastName}`, species: animals, locations: region };
+    people.push(obj);
+  });
+  return people;
+};
+const idByName = (name) => {
+  if (employees.some((element) => element.firstName === name || element.lastName === name)) {
+    const all = allEmployees();
+    const person = employees.find((emp) => emp.firstName === name || emp.lastName === name);
+    console.log(person);
+    const idFounded = person.id;
+    return all.find((element) => element.id === idFounded);
+  }
+  throw new Error('Informações inválidas');
+};
+
+const byId = (id) => {
+  if (employees.some((element) => element.id === id)) {
+    return allEmployees().find((person) => person.id === id);
+  }
+  throw new Error('Informações inválidas');
+};
+
+function getEmployeesCoverage(options = {}) {
+  const { name = false, id = false } = options;
+  if (!name && !id) {
+    return allEmployees();
+  }
+  if (name) {
+    return idByName(name);
+  }
+  if (id) {
+    return byId(id);
+  }
 }
 
 module.exports = getEmployeesCoverage;
