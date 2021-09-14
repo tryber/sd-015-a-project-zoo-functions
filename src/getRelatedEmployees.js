d0127c2bd9032217aca6b53cfc15b4324075c94a
leonardo <leonardo.siepierski@outlook.com>
2021-09-14 13:56:50 -0300

    Metade do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,11 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  
 }
 
 module.exports = { isManager, getRelatedEmployees };
