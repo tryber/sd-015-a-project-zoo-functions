9e5548020d3ab7f8648cff830d15dbec64e560da
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-09-14 11:19:47 -0300

    requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (typeof employeeName === 'undefined') {
+    return { };
+  }
+  return data.employees.find((pessoa) => pessoa.firstName === employeeName || pessoa.lastName === employeeName);
 }
+
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
