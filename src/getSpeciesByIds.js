992eb35e70328849476f7fe61e1de0624e58cd7d
André <andre.bonaf@gmail.com>
2021-09-13 22:21:41 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
+  const mapObject = ids.map((id) => data.species.find((animal) => animal.id === id));
+  return mapObject;
 }
+
+console.log(getSpeciesByIds('533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'));
 
 module.exports = getSpeciesByIds;
