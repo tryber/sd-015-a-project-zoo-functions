88f4846ddd69d4223d5817aa7204d87c75c1564d
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-09-14 12:45:40 -0300

    requisito 04

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-  const filtrarAnimais = data.species.find((animalFiltrado) => animalFiltrado.name === animal);
-  return filtrarAnimais.residents.every((animalFiltrado) => animalFiltrado > age);
+  const filtrarAnimais = data.species.find((animalFiltrado) => animalFiltrado.name === animal).residents;
+  return filtrarAnimais.every((animalFiltrado) => animalFiltrado.age >= age);
 }
 module.exports = getAnimalsOlderThan;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  // verifica se contem o nome da pessoa na parte .managers de todos os outros funcionarios
+  return data.employees.some((funcionario) => funcionario.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const gerenteDe = employees.filter((funcionario) => funcionario.managers.includes(managerId));
+    return gerenteDe.map(({ firstName, lastName }) => `${firstName} ${lastName}`);
+  }
+  else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
