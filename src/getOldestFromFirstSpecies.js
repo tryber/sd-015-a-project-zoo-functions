4247a1a5f278237bed19d0a3146b6d73cb8650de
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 18:45:04 -0300

    R9 done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = employees.find((employee) => employee.id.includes(id));
+  return getOldestSpecies(employee);
+}
+
+function getOldestSpecies({ responsibleFor }) {
+  const { residents } = species.find((specie) => specie.id.includes(responsibleFor[0]));
+  const oldestResident = oldestResidentFn(residents);
+  return Object.values(oldestResident);
+}
+
+function oldestResidentFn(residents) {
+  return residents.reduce(
+    (older, resident) => ((resident.age > older.age) ? resident : older),
+    { age: 0 }
+  );
 }
 
 module.exports = getOldestFromFirstSpecies;
