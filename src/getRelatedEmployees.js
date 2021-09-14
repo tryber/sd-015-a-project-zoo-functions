1d25d3ee78115baab8a3627b4f57f289c31ad483
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-09-14 16:18:40 -0300

    Requisit 4 - VQV

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,12 +1,17 @@
 const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  return data.employees.some((elem) => elem.managers.id === id);
+  return employees.some((elem) => elem.managers.some((idmManeger) => idmManeger === id)); // A alternativa de usar um some detro de outro some conseguir acessando um projeto de um colega "Matheus Oliveira", nessa condição o some vai checar o valor da chave e não o array fornecido pelo elem.managers.
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId) === true) {
+    const manager = employees.filter((check) => check.managers.includes(managerId));
+    return manager.map((element) => `${element.firstName} ${element.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-
 module.exports = { isManager, getRelatedEmployees };
