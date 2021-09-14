7cd78a1d63a6a21aa1c60370a03610d62ff0b14b
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-14 18:16:37 -0300

    add getSchedule function

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getAnimalMap(options) {
-  // seu código aqui
+function getSpecieByRegion(region) {
+  return (data.species.filter((specie) =>
+    (specie.location === region))).map((animal) => animal.name);
 }
 
+function getAnimalsByRegion() {
+  const speciesRegions = { NE: [], NW: [], SE: [], SW: [] };
+  speciesRegions.NE = getSpecieByRegion('NE');
+  speciesRegions.NW = getSpecieByRegion('NW');
+  speciesRegions.SE = getSpecieByRegion('SE');
+  speciesRegions.SW = getSpecieByRegion('SW');
+  return speciesRegions;
+}
+
+function getAnimalsWithOptions(option) {
+  const species = getAnimalsByRegion();
+}
+function getAnimalMap(options) {
+  // seu código aqui
+  return getAnimalsByRegion();
+}
+
+console.log(getAnimalsByRegion());
 module.exports = getAnimalMap;
