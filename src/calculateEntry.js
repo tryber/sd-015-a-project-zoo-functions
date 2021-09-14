791d4ed508e4aa54853b3004b0ba821f8d6846a1
MuriloAmaral <muralha2000@gmail.com>
2021-09-14 13:45:15 -0300

    requisito ajustando .length

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,12 +2,12 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
-  const child = entrants.filter((entrant) => entrant.age < 18);
-  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
-  const senior = entrants.filter((entrant) => entrant.age >= 50);
-  return { child: child.length,
-    adult: adult.length,
-    senior: senior.length,
+  const child = entrants.filter((entrant) => entrant.age < 18).length;
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50).length;
+  const senior = entrants.filter((entrant) => entrant.age >= 50).length;
+  return { child,
+    adult,
+    senior,
   };
 }
 
