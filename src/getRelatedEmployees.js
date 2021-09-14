b38811f48905c0feece40a1533ce286bafc8242f
herzogcassia <cassiahherzog@gmail.com>
2021-09-14 17:48:57 -0300

    requisito 4 esse foi suado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  return employees.some((funcionario) => funcionario.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    const findFun = employees.filter((elemento) => elemento.managers.includes(managerId));
+    const fullNames = findFun.map((person) => `${person.firstName} ${person.lastName}`);
+    console.log(fullNames);
+    return fullNames;
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
