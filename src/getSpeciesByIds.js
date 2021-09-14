da8ec20dc2e80e59da972d0f871db926e70b23ac
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-14 15:54:56 -0300

    add req 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,29 @@
-const data = require("../data/zoo_data");
+const zoo_data = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  //
+  if (ids.length === 0) {
+    return [];
+  }
+  const species = data.species;
+  //console.log(species);
+  const selectedSpecies = species.filter((specie) => {
+    for (let i = 0; i < ids.length; i++) {
+      if (specie.id === ids[i]) {
+        return specie;
+      }
+    }
+  });
+  return selectedSpecies;
 }
+
+// console.log(getSpeciesByIds());
+console.log(
+  getSpeciesByIds(
+    '0938aa23-f153-4937-9f88-4858b24d6bce',
+    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'
+  )
+);
 
 module.exports = getSpeciesByIds;
