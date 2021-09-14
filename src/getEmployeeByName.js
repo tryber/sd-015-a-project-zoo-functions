c0f54389d04aebdda7ddf86728fabb86f4ac37c7
DevJunior21 <junior-wmw@hotmail.com>
2021-09-14 11:47:43 -0400

    correções no lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,11 +2,11 @@ const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  if (!employeeName){
-    return {}
+  if (!employeeName) {
+    return {};
   }
-  return employees.find((getName)=>
-  getName.firstName === employeeName || getName.lastName === employeeName)
+  return employees.find((getName) =>
+    getName.firstName === employeeName || getName.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,8 @@
 const data = require('../data/zoo_data');
 const { species } = require('../data/zoo_data');
 
-/* function getSpeciesByIds(...ids) {
-    const log = species.filter(({ id }) => ids.includes(id))
-    return log
-}  */
-
 function getSpeciesByIds(...ids) {
-  const log = species.filter(({ id }) => ids.some((id) => id === ids));
+  const log = species.filter(({ id }) => ids.includes(id));
   return log;
 }
 
