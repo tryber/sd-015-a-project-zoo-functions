fd81d2c0c35afbb9cc1f05847095484f96d0a92f
Braien <braienmp@outlook.com>
2021-09-14 17:46:22 -0300

    implementa a funçao getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,33 +1,42 @@
 const data = require('../data/zoo_data');
 
 function getAnimals(day) {
-  return data.species.filter((specie) => specie.availability.some((elem) => elem === day)).map((animal) => animal.name);
+  return data.species.filter((specie) => specie.availability.some((elem) =>
+    elem === day)).map((animal) => animal.name);
+}
+
+function getDailySchedule(weekDay, day) {
+  return (day !== 'Monday') ? {
+    officeHour: `Open from ${weekDay.open}am until ${weekDay.close}pm`,
+    exhibition: getAnimals(day),
+  } : {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+}
+
+function getScheduleByAnimal(day, schedule, scheduleTarget) {
+  if (day !== 'Monday') {
+    return schedule[day].exhibition.some((elem) => elem === scheduleTarget);
+  }
+  return false;
 }
 
 function getSchedule(scheduleTarget) {
   const weekDays = Object.keys(data.hours);
+  const animals = data.species.map((specie) => specie.name);
   const schedule = {};
   weekDays.forEach((day) => {
     const weekDay = data.hours[day];
-    schedule[day] = (day !== 'Monday') ? {
-      officeHour: `Open from ${weekDay.open}am until ${weekDay.close}pm`,
-      exhibition: getAnimals(day),
-    } : {
-      officeHour: 'CLOSED',
-      exhibition: 'The zoo will be closed!',
-    };
+    schedule[day] = getDailySchedule(weekDay, day);
   });
+  if (weekDays.some((day) => day === scheduleTarget)) {
+    return { [scheduleTarget]: schedule[scheduleTarget] };
+  }
+  if (animals.some((animal) => animal === scheduleTarget)) {
+    return weekDays.filter((day) => getScheduleByAnimal(day, schedule, scheduleTarget));
+  }
   return schedule;
 }
 
 module.exports = getSchedule;
-
-// {
-//   'Tuesday': {
-//     'officeHour': 'Open from 8am until 6pm',
-//     'exhibition': [ 'lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes' ],
-//   },
-//   'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
-// }
-
-console.log(getSchedule());
