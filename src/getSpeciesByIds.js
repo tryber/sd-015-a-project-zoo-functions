6c229c633db0c46606f1a7e9d082625f20728432
Emmanuelle Pereira <emma.persil@gmail.com>
2021-09-14 12:54:21 -0300

    getSpeciesByIds feita, passando localmente

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  let dataSpecies = data.species;
+  return dataSpecies.filter((specie) => ids.includes(specie.id))
 }
 
 module.exports = getSpeciesByIds;
