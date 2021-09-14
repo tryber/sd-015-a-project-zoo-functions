9a7d063916f466e5d79e3bbbf4c6131d8ef33526
Diego Brito <diegorbrito9@gmail.com>
2021-09-14 10:30:32 -0300

    Requisito 2 (check)

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,15 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
+  let aboveMinimumAge = true;
   data.species.forEach((animals) => {
     if (animals.name === animal) {
-      animals.residents.forEach((element) => {
-        if (element.age > age) {
-          return true;
-        }
+      animals.residents.forEach((ages) => {
+        if (ages.age < age) aboveMinimumAge = false;
       });
     }
   });
+  return aboveMinimumAge;
 }
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,9 +9,4 @@ function getSpeciesByIds(...ids) {
   });
   return animals;
 }
-
-getSpeciesByIds(
-  '0938aa23-f153-4937-9f88-4858b24d6bce',
-  '01422318-ca2d-46b8-b66c-3e9e188244ed',
-);
 module.exports = getSpeciesByIds;
