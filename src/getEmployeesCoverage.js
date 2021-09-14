b73e3f97d2835ca85a2bd0ace3fc8f4abf281860
Rafael <rafaelmoura7033@gmail.com>
2021-09-13 23:01:19 -0300

    Resolvendo erros de lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,16 +3,16 @@ const data = require('../data/zoo_data');
 const func = (fullName) => {
   const species = data.employees.find((element) => fullName.includes(element.firstName));
   const responsability = species.responsibleFor;
-  const objetoDosAnimais = data.species.filter((element) => responsability.includes(element.id))
-  const nomeDosAnimais = objetoDosAnimais.map((element) => element.name)
-  return nomeDosAnimais
-}
+  const objetoDosAnimais = data.species.filter((element) => responsability.includes(element.id));
+  const nomeDosAnimais = objetoDosAnimais.map((element) => element.name);
+  return nomeDosAnimais;
+};
 
 const funcDeLocalizar = (speciesNames) => {
-  let objetosPorSpecieName = data.species.filter((element) => speciesNames.includes(element.name));
-  const localizacoesDosObjs = objetosPorSpecieName.map((element) => element.location)
-  return localizacoesDosObjs
-}
+  const objsPorSpecieName = data.species.filter((element) => speciesNames.includes(element.name));
+  const localizacoesDosObjs = objsPorSpecieName.map((element) => element.location);
+  return localizacoesDosObjs;
+};
 
 const funcCondicional = (param, arrayTotal) => {
   const valorDoParam = Object.values(param)[0];
@@ -20,12 +20,13 @@ const funcCondicional = (param, arrayTotal) => {
   const paramCheioComId = arrayTotal.filter((element) => element.id.includes(valorDoParam));
   if (paramCheioComName.length > 0) {
     return paramCheioComName[0];
-  } 
+  }
+
   if (paramCheioComId.length > 0) {
     return paramCheioComId[0];
   }
   throw new Error('Informações inválidas');
-}
+};
 
 function getEmployeesCoverage(param) {
   const arrayTotal = [];
@@ -37,7 +38,7 @@ function getEmployeesCoverage(param) {
       fullName: fullName[index],
       species: func(fullName[index]),
       locations: funcDeLocalizar(func(fullName[index])),
-    })
+    });
   }
   if (param !== undefined) {
     return funcCondicional(param, arrayTotal);
