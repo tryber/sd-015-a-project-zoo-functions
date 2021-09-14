5a8b81f3086b27a8531bc5d7fab6e8aefd841cd1
Valdeci <moura97.valdeci@gmail.com>
2021-09-13 22:14:22 -0300

    Requisito 5

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const en = require('faker/lib/locales/en');
 const data = require('../data/zoo_data');
 
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
+
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.name
+  }
 }
 
 function calculateEntry(entrants) {
   // seu código aqui
 }
+
+console.log(countEntrants(entrants));
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const { species } = data;
+
+function countAnimals({specie, gender} = '') {
+  if (!specie) {
+    return species.reduce(((count, {name, residents: {length}}) => {
+      Object.assign(count, { [name]: length});
+      return count;
+    }), {});
+  }
+
+  if (!gender) {
+    return species.find((value) => value.name === specie).residents.length;
+  }
+
+  return species.find((anl) => anl.name  === specie).residents.filter((elm) => elm.sex === gender).length;
 }
 
 module.exports = countAnimals;
+
+// Referências: 
+// Sobre Object.assign: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Object/assign
+// fonte para (!x) usado nos if's: https://stackoverflow.com/questions/2559318/how-to-check-for-an-undefined-or-null-variable-in-javascript
+// Trecho específico do artigo: 
+// Note 2
+// This - even shorter - variant is not equivalent:
+
+// if ( !some_variable ) {
+// some_variable is either null, undefined, 0, NaN, false, or an empty string
+// }
+// so
+
+// if ( some_variable ) {
+// we don't get here if some_variable is null, undefined, 0, NaN, false, or ""
+// }
+// fonte sobre atruibuição em situações de desestruturação: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
