74c4033650eb7d871c43e427f017227dea363e45
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 12:30:22 -0300

    finalizaçao do requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) return {};
+  return data.employees.find((name) => name.lastName === employeeName || name.firstName === employeeName);
 }
 
 module.exports = getEmployeeByName;
