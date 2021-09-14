44b97fb9d689d50807a2b734685fcf5d7bf05231
Omar Merljak <omerljak@gmail.com>
2021-09-14 18:21:36 -0300

    lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-/* 
-const countAnimals = ((specie) => {
-species.reduce((acc, specie) => acc.name = { [specie.name]: specie.residents.length }), 0});
-console.log(countAnimals()) */
+
+function countAnimals(specie) {
+
+}
 
 module.exports = countAnimals;
