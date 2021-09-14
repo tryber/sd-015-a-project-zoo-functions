a454588a60345847b08823ec4a8c5c90157a7405
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-09-14 16:21:23 -0300

    requisito 4

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const findAnimal = data.species.find((element) => element.name === animal).residents;
-  // console.log(findAnimal);
-  return findAnimal.every((element) => element.age >= age);
+  const { residents } = data.species.find((element) => element.name === animal);
+  return residents.every((element) => element.age >= age);
 }
 
 console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
+
+// const findAnimal = data.species.find((element) => element.name === animal).residents;
+// return findAnimal.every((element) => element.age >= age);
+// }
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((elemento) => elemento.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
+  if (isManager(managerId) === true) {
+    const subordinados = employees.filter((elemento) => elemento.managers.includes(managerId));
+
+    return subordinados.map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+} console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = { isManager, getRelatedEmployees };
