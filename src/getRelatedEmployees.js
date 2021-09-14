26b0d04dcffc19a0754c0fa304a0ca7458053d1c
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 16:17:55 -0300

    finalizaçao requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
+  return data.employees.some((employee) => employee.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const searchingEmployee = data.employees.filter((employee) => employee.managers.includes(managerId));
+  return searchingEmployee.map((employee) => `${employee.firstName} ${employee.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
