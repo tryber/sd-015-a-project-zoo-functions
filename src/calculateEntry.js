91782462934a667ae2935910150492447cf1c99c
viniciusldn <viniciusldn0@gmail.com>
2021-09-14 17:55:12 -0300

    R6 done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,35 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let childCounter = 0;
+  let adultCounter = 0;
+  let seniorCounter = 0;
+
+  entrants.forEach(({ age }) => {
+    if (age < 18) childCounter += 1;
+    else if (age < 50) adultCounter += 1;
+    else seniorCounter += 1;
+  });
+  return {
+    adult: adultCounter,
+    child: childCounter,
+    senior: seniorCounter,
+  };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) return 0;
+
+  let { adult, child, senior } = countEntrants(entrants);
+  const { prices } = data;
+
+  adult *= prices.adult;
+  child *= prices.child;
+  senior *= prices.senior;
+  
+  const totalPrice = adult + child + senior;
+
+  return totalPrice;
 }
 
 module.exports = { calculateEntry, countEntrants };
