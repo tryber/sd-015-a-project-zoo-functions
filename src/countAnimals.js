89913293a962d4340ce0b99f0070322178096aa0
Joao Paulo Veloso <joaopaulox@outlook.com>
2021-09-14 13:27:15 -0300

    Requisito 2 do exercicio 5 pronto

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,27 @@
 const data = require('../data/zoo_data');
 
+function criaObjeto(acumulador, especie) {
+  const acc = acumulador;
+  acc[especie.name] = especie.residents.length;
+  return acc;
+}
 function countAnimals(animal) {
-  function criaObjeto(acumulador, especie) {
-    const acc = acumulador;
-    acc[especie.name] = especie.residents.length;
-    return acc;
-  }
   if (animal === undefined) {
     const Animais = data.species.reduce(criaObjeto, {});
     return Animais;
   }
-  if (animal.length === 1) {
-    return {};
+  if (Object.keys(animal).length === 1) {
+    console.log('entrou no especie');
+    const especie = animal.specie;
+    const bicho = data.species.find((elemento) => elemento.name === especie);
+    return bicho.residents.length;
   }
-  if (animal.length === 2) {
+  if (Object.keys(animal).length === 2) {
     return {};
   }
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'bears', gender: 'female' }));
 
 module.exports = countAnimals;
