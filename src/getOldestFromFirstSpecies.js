3aa762788e204110243b321808341a3734f1fe01
ricardo-zabir <ricardofonseca.zabir@hotmail.com>
2021-09-14 16:52:16 -0300

    Requisitos 6, 7 e 8

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const specificEmployee = data.employees.find((employee) => employee.id === id);
+  const firstSpecie = specificEmployee.responsibleFor[0];
+  const specie = data.species.find((eachSpecie) => eachSpecie.id === firstSpecie);
+  const sortedResidents = specie.residents.sort((resident, resident2) =>
+    resident2.age - resident.age);
+  return [sortedResidents[0].name, sortedResidents[0].sex, sortedResidents[0].age];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,38 @@
 const data = require('../data/zoo_data');
 
+const animals = data.species.map((specie) => specie.name);
+const days = Object.keys(data.hours);
+
+function allWeekAllAnimalsSchedule() {
+  const animalsAvailableOnDay = {};
+  days.forEach((day) => {
+    animalsAvailableOnDay[day] = {};
+    if (day !== 'Monday') {
+      const string = `Open from ${data.hours[day].open}am until ${data.hours[day].close}pm`;
+      animalsAvailableOnDay[day].officeHour = string;
+      animalsAvailableOnDay[day].exhibition = data.species.filter((specie) =>
+        (specie.availability.some((dayAvailable) => dayAvailable === day)
+       === true)).map((specie) => specie.name);
+    } else {
+      animalsAvailableOnDay[day].officeHour = 'CLOSED';
+      animalsAvailableOnDay[day].exhibition = 'The zoo will be closed!';
+    }
+  });
+  return animalsAvailableOnDay;
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (animals.some((animal) => animal === scheduleTarget)) {
+    const selectedAnimal = data.species.find((specie) => specie.name === scheduleTarget);
+    const arrayOfDays = selectedAnimal.availability;
+    return arrayOfDays;
+  }
+  if (days.some((day) => day === scheduleTarget)) {
+    const objectOfDay = {};
+    objectOfDay[scheduleTarget] = allWeekAllAnimalsSchedule()[scheduleTarget];
+    return objectOfDay;
+  }
+  return allWeekAllAnimalsSchedule();
 }
 
 module.exports = getSchedule;
