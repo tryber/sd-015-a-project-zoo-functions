894844495669a44d3b2c5a51dc74522b7fcbeb86
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-14 16:01:23 -0300

    fix conflicts

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
-const zoo_data = require('../data/zoo_data');
-const data = require('../data/zoo_data');
-=======
 const data = require("../data/zoo_data");
->>>>>>> e47f2c3b655b20b25c4e2fee2cd30d446bad03c0
 
 function getSpeciesByIds(...ids) {
   // seu código aqui
-<<<<<<< HEAD
   if (ids.length === 0) {
     return [];
   }
@@ -21,16 +15,13 @@ function getSpeciesByIds(...ids) {
     }
   });
   return selectedSpecies;
-=======
-  //
->>>>>>> e47f2c3b655b20b25c4e2fee2cd30d446bad03c0
 }
 
 // console.log(getSpeciesByIds());
 console.log(
   getSpeciesByIds(
-    '0938aa23-f153-4937-9f88-4858b24d6bce',
-    '533bebf3-6bbe-41d8-9cdf-46f7d13b62ae'
+    "0938aa23-f153-4937-9f88-4858b24d6bce",
+    "533bebf3-6bbe-41d8-9cdf-46f7d13b62ae"
   )
 );
 
