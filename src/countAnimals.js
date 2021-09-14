7bdf6716d4e42ffa3a58200ffbc8d2d2404b91a0
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-09-14 00:15:41 -0300

    feito o requisito 5.

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return species.reduce((objeto, elemento) =>
+      Object.assign(objeto, { [elemento.name]: elemento.residents.length }), {});
+  }
+  const { specie, gender } = animal;
+  if (!gender) {
+    return species.find((zooAnimal) => zooAnimal.name === specie).residents.length;
+  }
+  return species.find((zooAnimal) => zooAnimal.name === specie).residents
+    .filter((genreAnimal) => genreAnimal.sex === gender).length;
 }
 
 module.exports = countAnimals;
