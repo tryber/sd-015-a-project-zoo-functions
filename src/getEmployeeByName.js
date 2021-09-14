45cfbe307ecf3a50073edd98bab1067d4f0b1fce
Igor Campos Moreira <igorcampos200@gmail.com>
2021-09-14 20:21:55 -0300

    Reqisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getEmployeeByName(employeeName) {
-  // seu código aqui
-}
+const searchName = (employeeName) => {
+  const name1 = data.employees.find((search) => search.lastName === employeeName);
+  const name2 = data.employees.find((search) => search.firstName === employeeName);
+  return name1 || name2;
+};
+const getEmployeeByName = (employeeName) => {
+  let retorno = {};
+  if (employeeName !== undefined) {
+    retorno = searchName(employeeName);
+  } else {
+    retorno = {};
+  }
+  return retorno;
+};
 
 module.exports = getEmployeeByName;
