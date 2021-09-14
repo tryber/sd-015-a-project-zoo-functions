bf22f9c41a75b13b9c20035308fdd6b25e32618f
Rodrigo Lima <limarodrigoo@outlook.com>
2021-09-14 18:34:46 -0300

    add getOldestFromFirstSpecies function

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employeeInfo = employees.find((employee) => employee.id === id);
+  const firstAnimalResponsible = data.species.find((specie) => specie.id === employeeInfo
+    .responsibleFor[0]);
+  const oldestResident = firstAnimalResponsible.residents
+    .sort((resident1, resident2) => resident2.age - resident1.age);
+  return [oldestResident[0].name, oldestResident[0].sex, oldestResident[0].age];
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
