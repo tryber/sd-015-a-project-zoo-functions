2f4ded4d8fca2dc4ed24a90164799070d5ea896f
Guilherme Polippo <luispolippo@hotmail.com>
2021-09-14 16:14:45 -0300

    Corrigido requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -11,23 +11,21 @@ const listAnimals = () => {
 
 function countAnimals(animal = {}) {
   // seu código aqui
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
   const { species } = data;
 
-  if (gender === undefined && specie !== undefined) {
+  if (sex === undefined && specie !== undefined) {
     return species.find((specieElem) => specieElem.name === specie).residents
       .length;
   }
-  if (gender !== undefined) {
+  if (sex !== undefined) {
     const chosenSpicie = species.find(
       (specieElem) => specieElem.name === specie,
     );
-    return chosenSpicie.residents.filter((resident) => resident.sex === gender)
+    return chosenSpicie.residents.filter((resident) => resident.sex === sex)
       .length;
   }
   return listAnimals();
 }
-
-console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
