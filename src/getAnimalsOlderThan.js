57e44a6c385bd4dd3b74c9e022669a841978018f
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-09-14 11:14:09 -0300

    requisitos 01 e 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const filtrarAnimais = data.species.find((animalFiltrado) => animalFiltrado.name === animal);
+  return filtrarAnimais.residents.every((animalFiltrado) => animalFiltrado > age);
 }
-
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return species.filter((idAnimal) => ids.includes(idAnimal.id));
 }
+
+console.log(getSpeciesByIds());
 
 module.exports = getSpeciesByIds;
