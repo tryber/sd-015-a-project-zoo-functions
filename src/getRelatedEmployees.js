7c8f2883d53ff67b050298c5bf72dde667fe5e75
Leandro Basilio <leandroab07@gmail.com>
2021-09-14 18:30:19 -0300

    Adiciona requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -2,14 +2,22 @@ const data = require('../data/zoo_data');
 
 function isManager(id) {
   // seu código aqui
-  const resultado = data.employees.find((busca) => busca.id === id);
-  return resultado;
+  const managers = data.employees.some((manager) => manager.managers.includes(id));
+
+  return managers;
 }
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const subordinados = data.employees.filter((obj) => obj.managers.includes(managerId));
+  // console.log(subordinados);
+  return subordinados.map((obj) => `${obj.firstName} ${obj.lastName}`);
 }
 
-console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
+// console.log(getRelatedEmployees('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
 
 module.exports = { isManager, getRelatedEmployees };
