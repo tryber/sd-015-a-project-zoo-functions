d604ab74e350b22d5e2f9e8b8de44c863d98039c
Igor Campos Moreira <igorcampos200@gmail.com>
2021-09-14 16:34:52 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,21 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+const getSpeciesByIds =(...ids) => {
+  let retorno = [];
+  switch (ids.length) {
+    case 0:
+      return retorno;
+      break;
+    case 1:
+      retorno.push(data.species.find((idSearch) => idSearch.id === ids[0]));
+      return retorno;
+      break;
+    case 2:
+      retorno.push(data.species.find((idSearch) => idSearch.id === ids[0]));
+      retorno.push(data.species.find((idSearch) => idSearch.id === ids[1]));
+      return retorno;
+      break;
+  }
 }
 
 module.exports = getSpeciesByIds;
