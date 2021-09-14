07d4a3fe02f529d84f7328e7dbb7b73a3d535b88
bruholanda <brunaholandacunha7@gmail.com>
2021-09-14 16:46:20 -0300

    Requirement 9; Finished

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = employees.find((value) => value.id === id).responsibleFor[0];
+  const animalData = data.species.find((value) => value.id === animalId).residents;
+  const oldestAnimal = animalData.reduce((acc, cv) =>
+    ((acc.age > cv.age) ? acc : cv));
+
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
