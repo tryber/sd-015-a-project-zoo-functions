23e6a324c81d94b029763da0f982bb5357c36acb
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-09-14 19:33:57 -0300

    tentativa 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,21 @@
 const data = require('../data/zoo_data');
 
-function countAnimals({ specie, gender } = {}) {
+function countAnimals({ specie, sex } = {}) {
   // seu código aqui
   const arr = {};
-  if (!specie && !gender) {
+  if (!specie && !sex) {
     data.species.forEach((especie) => {
       arr[especie.name] = especie.residents.length;
     });
     return arr;
   }
-  if (!gender) {
+  if (!sex) {
     return data.species.find((qtdAnimals) => qtdAnimals.name === specie).residents
       .length;
   }
   return data.species
     .find((sexAnimals) => sexAnimals.name === specie)
-    .residents.filter((fe) => fe.sex === gender).length;
+    .residents.filter((fe) => fe.sex === sex).length;
 }
 console.log(countAnimals());
 module.exports = countAnimals;
