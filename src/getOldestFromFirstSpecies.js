0d067792e40e9664eb09f5315d7be836f2eed4c5
Rodrigo Moreira <rodrigossmoreira@hotmail.com>
2021-09-14 18:40:08 -0300

    correçao lint exercicio 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,9 +2,9 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   const firstEmployee = data.employees.find((element) => element.id === id).responsibleFor;
-  const specieAnimal = firstEmployee.find((animalResponsible) => animalResponsible = firstEmployee[0]);
-  const animalValues = data.species.find((animais) => 
-  animais.id === specieAnimal);
+  const specieAnimal = firstEmployee.find((animalResponsibal) => firstEmployee[0]);
+  const animalValues = data.species.find((animais) =>
+    animais.id === specieAnimal);
   const animalOld = animalValues.residents.sort((a, b) => b.age - a.age);
   return Object.values(animalOld[0]);
 }
