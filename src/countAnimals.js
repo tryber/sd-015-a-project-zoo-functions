cf2a2fcf63d54ea9fc19837bb7eb857f0f1a8b9b
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-14 17:05:22 -0300

    quinto requisito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countAnimals({ specie, gender } = {}) {
+  const allAnimals = {};
+  if (!specie && !gender) {
+    data.species.forEach((especie) => {
+      allAnimals[especie.name] = especie.residents.length;
+    });
+    return allAnimals;
+  }
+  if (!gender) {
+    return data.species.find((animalQtd) => animalQtd.name === specie).residents
+      .length;
+  }
+  return data.species
+    .find((specieSex) => specieSex.name === specie)
+    .residents.filter((femMal) => femMal.sex === gender).length;
 }
-
+console.log(countAnimals());
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -11,5 +11,4 @@ function getRelatedEmployees(managerId) {
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
-// console.log(isManager('0e7b460e-acf4-4e17-bcb3-ee472265db83'));
 module.exports = { isManager, getRelatedEmployees };
