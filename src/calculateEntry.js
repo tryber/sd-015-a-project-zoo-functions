541ee81a5e178a65a4020ddbcfba6829049d618f
gabriel <gabriel.f.takaki@gmail.com>
2021-09-14 17:13:58 -0300

    requisito 4 ao 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,20 +1,19 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) =>  ({
+const countEntrants = (entrants) => ({
   child: entrants.filter((find) => find.age < 18).length,
   adult: entrants.filter((find) => find.age >= 18 && find.age < 50).length,
   senior: entrants.filter((find) => find.age >= 50).length,
   // seu código aqui
 });
 
-
 const calculateEntry = (entrants) => {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
-  return (countEntrants(entrants).child * prices.child)+
-  (countEntrants(entrants).adult* prices.adult)+
-  (countEntrants(entrants).senior * prices.senior);
+  return (countEntrants(entrants).child * prices.child) 
+  +(countEntrants(entrants).adult * prices.adult)
+  +(countEntrants(entrants).senior * prices.senior);
   // seu código aqui
-}
+};
 
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,16 +1,18 @@
-const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
-const { especies } = data;
+// const { species } = require('../data/zoo_data');
 
-function countAnimals({ specie = 0, genero = 0 } = 0) {
-  if (specie === 0) {
-    return species.reduce((acc, val) => {
-      acc[val.name] = val.residents.length;
-      return acc;
-    }, {});
-  }
-  return 
-  // seu código aqui
-}
+// const data = require('../data/zoo_data');
 
-module.exports = countAnimals;
+// const { especies } = data;
+
+// function countAnimals({ specie = 0, genero = 0 } = 0) {
+//   if (specie === 0) {
+//     return species.reduce((acc, val) => {
+//       acc[val.name] = val.residents.length;
+//       return acc;
+//     }, {});
+//   }
+//   return 
+//   // seu código aqui
+// }
+
+// module.exports = countAnimals;
