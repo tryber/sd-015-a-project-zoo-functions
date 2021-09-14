74d57746218460543ce83505c450e0f7bc32ddfc
Breno da Cunha Campanha <comosaberonline@gmail.com>
2021-09-14 15:50:06 -0400

    commit apenas para dar checkout para master

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,19 +1,7 @@
 const data = require('../data/zoo_data');
 
-// const { employees } = data;
-
 function getEmployeesCoverage(obj) {
   // seu código aqui
-  // if (obj.name) {
-  //   const teste = employees.find((employee) => employee.firstName === obj.name);
-  //   let realReturn = {
-  //     id: 1,
-  //     fullName: 1,
-  //     species: [],
-  //     locations: [],
-  //   };
-  //   return Object.keys(realReturn)
-  // }
 }
 // console.log(getEmployeesCoverage({ name: 'Sharonda' }))
 module.exports = getEmployeesCoverage;
