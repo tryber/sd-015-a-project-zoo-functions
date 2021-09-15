7166bb326c62b2630ab6e7050e1d8fd780a2983f
MuriloAmaral <muralha2000@gmail.com>
2021-09-15 20:52:34 -0300

    requisito 9, pq o 7 e o 8 sao dificeis demais

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    
+  }
 }
 
 module.exports = getAnimalMap;
