d9adb30f5bf1c99f08092dbb9d40079ef4dbd1b8
alvarogularte <alvaro.gularte@gmail.com>
2021-09-15 14:25:02 -0300

    Change gender for sex

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,10 +7,10 @@ const countAnimals = (animal) => {
     species.forEach((elem) => { getAnimals[elem.name] = elem.residents.length; });
     return getAnimals;
   }
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
   const animals = species.find((element) => element.name === specie).residents;
-  const genderCount = animals.reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
-  return gender === undefined ? animals.length : genderCount;
+  const sexCount = animals.reduce((acc, curr) => (curr.sex === sex ? acc + 1 : acc), 0);
+  return sex === undefined ? animals.length : sexCount;
 };
 
 module.exports = countAnimals;
