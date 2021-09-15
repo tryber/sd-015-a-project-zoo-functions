5e2acfee891917a8d259202e62a40f61defa93ea
Lygia Dias <lygiiadias@gmail.com>
2021-09-15 15:31:39 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals() {
-  // seu código aquii
+function countAnimals(animalPam) {
+  const objAnimals = {};
+  if (!animalPam) {
+    species.forEach((anim) => { objAnimals[anim.name] = anim.residents.length; });
+    // Vai percorrer o array de species a adcionar no objeto vazio o nome e a quantidade de animais.
+    return objAnimals;
+  }
+  const { specie, gender } = animalPam;
+
+  if (specie !== undefined && gender === undefined) {
+    return species.find((anim) => anim.name === specie).residents.length;
+  }
+  return species.find((anim) => anim.name === specie).residents
+    .filter((sex) => sex.sex === gender).length;
 }
-
 module.exports = countAnimals;
