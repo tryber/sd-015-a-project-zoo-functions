d97574dfbd93d37cf30e922b7214cf611d670430
Felipe <felllypes@hotmail.com>
2021-09-15 01:09:41 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,4 @@
 const data = require('../data/zoo_data');
+// Vergani95 repositório consultado: https://github.com/tryber/sd-014-a-project-zoo-functions/blob/62d6605aa95edfdbe5b26ee02336af95dedd365a/src/zoo.js
+console.log(data.species.name)
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
-
-module.exports = getAnimalsOlderThan;
