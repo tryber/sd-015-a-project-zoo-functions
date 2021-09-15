2748b93545627ea9f74265f97da7887574a199f8
Leandro Basilio <leandroab07@gmail.com>
2021-09-14 21:40:02 -0300

    Adiciona requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animaisQuantidade = {};
+  if (animal === undefined) {
+    data.species.map((obj) => { animaisQuantidade[obj.name] = obj.residents.length; });
+
+    return animaisQuantidade;
+  } if (Object.keys(animal).length === 1) {
+    const quantidade = data.species.filter((obj) => obj.name.includes(Object.values(animal)));
+    const resultado = quantidade[0].residents.length;
+    return resultado;
+  }
+
+  const quantidadeObj = data.species.find((obj) => obj.name === Object.values(animal)[0]);
+  const sex = Object.values(animal)[1];
+  const quantidade = quantidadeObj.residents.filter((obj) => obj.sex === sex).length;
+  console.log(quantidade);
+  return quantidade;
 }
+
+// countAnimals({ specie: 'bears', gender: 'male' });
 
 module.exports = countAnimals;
