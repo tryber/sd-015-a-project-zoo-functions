e7004dcce0e0b860001f165f595debb6382893a2
Italo Sergio <italosergio@graduate.org>
2021-09-15 06:36:25 -0300

    Challange 2 (check) - [code clear]

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,7 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  return species
+  return data.species
     .find((element) => element.name === animal)
     .residents
     .every((elemento) => elemento.age > age);
