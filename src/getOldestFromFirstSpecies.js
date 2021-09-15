defddcc7f861bfe50f29ab4ff1007b6c65096ff9
Valdeci <moura97.valdeci@gmail.com>
2021-09-14 22:06:50 -0300

    erros de lint rqst 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,12 @@
 const data = require('../data/zoo_data');
+
 const { species, employees } = data;
 
 function getOldestFromFirstSpecies(id) {
-  if (!id) { return `Erro!` };
+  if (!id) { return 'Erro!'; }
   const firstId = employees.find((element) => element.id === id).responsibleFor[0];
   const animal = species.filter((element) => element.id === firstId);
-  return Object.values(animal[0].residents.reduce((acc, curr)=> acc.age > curr.age ? acc:curr));
+  return Object.values(animal[0].residents.reduce((acc, curr) => acc.age > curr.age ? acc : curr));
 }
 
 module.exports = getOldestFromFirstSpecies;
