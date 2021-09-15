5b3b5250ca0be20be56f12cb4810cfa9be7960a7
Davio Lopes <davioplp@gmail.com>
2021-09-15 01:34:29 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,8 +2,18 @@ const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (animal === undefined) {
-    species.forEach((animals) => console.log(`${animals.name} : ${animals.residents.length}`));
-  }
+    const animals = {};
+    species.forEach((iten) => { animals[iten.name] = iten.residents.length; });
+    return animals;
+  } 
+   if (animal.gender === undefined) {
+    const numAni = species.find((animals) => animals.name === animal.specie);
+    return numAni.residents.length;
+  } 
+   else {
+    const numAni = species.find((animals) => animals.name === animal.specie)
+    return numAni.residents.filter((iten) => iten.sex === animal.gender).length;
+  } 
 }
-countAnimals();
+console.log(countAnimals());
 module.exports = countAnimals;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -7,5 +7,5 @@ function getSpeciesByIds(...ids) {
   });
   return animals;
 }
-console.log(getSpeciesByIds());
+console.log(getSpeciesByIds('bb2a76d8-5fe3-4d03-84b7-dba9cfc048b5'));
 module.exports = getSpeciesByIds;
