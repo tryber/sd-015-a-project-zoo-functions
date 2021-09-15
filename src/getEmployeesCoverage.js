c9133f978000019b9d07b6b4b19295c4dca4f26e
deobrafantinif <deboraferfan@gmail.com>
2021-09-15 00:38:43 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const workerInfo = ({ id, firstName, lastName, responsibleFor }) => ({
+  id,
+  fullName: `${firstName} ${lastName}`,
+  species: responsibleFor.map((animal) => {
+    const theAnimal = data.species.find((specie) => specie.id === animal);
+    return theAnimal.name;
+  }),
+  locations: responsibleFor.map((animal) => {
+    const theAnimal = data.species.find((specie) => specie.id === animal);
+    return theAnimal.location;
+  }),
+});
+
+function getEmployeesCoverage(idOrName) {
+  if (!idOrName) {
+    return data.employees.map((employee) => workerInfo(employee));
+  }
+
+  const { name, id } = idOrName;
+  const workerSelected = data.employees
+    .find((worker) => worker.firstName === name || worker.lastName === name || worker.id === id);
+
+  if (!workerSelected) {
+    throw new Error('Informações inválidas');
+  }
+
+  return workerInfo(workerSelected);
 }
 
 module.exports = getEmployeesCoverage;
