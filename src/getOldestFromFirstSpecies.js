56384654bec2e59772792551b2d8e37b5a4ae523
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-15 02:31:01 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
+const { employees } = data;
+const { species } = data;
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = employees.find((emp) => emp.id === id);
+  const animalSpecie = species.find((specie) => specie.id === employee.responsibleFor[0]);
+  const oldest = animalSpecie.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr));
+  return Object.values(oldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
