a8242da1750a8598af9dece32daf5250da727a34
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-14 21:58:39 -0300

    ajuste de lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,19 +1,15 @@
 const data = require('../data/zoo_data');
 
- const { species } = data;
+const { species } = data;
 const { employees } = data;
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
   const employeeId = employees.find((aux) => aux.id === id);
-  // return employeeId;
   const animal = species.find((aux) => aux.id === employeeId.responsibleFor[0]);
-  // return animal;
-  const oldestAnimal = animal.residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr), {});
+  const oldestAnimal = animal.residents.reduce((acc, curr) =>
+    (acc.age > curr.age ? acc : curr), {});
   return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
-
-// [nome, sexo, idade]
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'))
