0ca701bbbc78cf0ef52141d196e76519258db239
gabriela-veloso <gabrielaveloso.engmateriais@gmail.com>
2021-09-15 14:23:32 -0300

    Requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,43 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  // seu código aqui
-}
+const fullSchedule = () => {
+  const completeInfo = {};
+  Object.keys(data.hours).forEach((elem) => {
+    const days = data.hours[elem];
+    if (elem === 'Monday') {
+      completeInfo[elem] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    } else {
+      completeInfo[elem] = {
+        officeHour: `Open from ${days.open}am until ${days.close}pm`,
+        exhibition: data.species
+          .filter((anim) => anim.availability.includes(elem))
+          .map((x) => x.name),
+      };
+    }
+  });
+  return completeInfo;
+};
+
+const animalOrDay = (scheduleTarget, day, animal) => {
+  if (animal) {
+    return data.species.find((anim) => anim.name === scheduleTarget)
+      .availability;
+  }
+  if (day) {
+    return { [scheduleTarget]: fullSchedule()[scheduleTarget] };
+  }
+};
+
+const getSchedule = (scheduleTarget) => {
+  const day = Object.keys(data.hours).includes(scheduleTarget);
+  const animal = data.species.some((elem) => elem.name === scheduleTarget);
+  if (animal || day) {
+    return animalOrDay(scheduleTarget, day, animal);
+  }
+  return fullSchedule();
+};
 
 module.exports = getSchedule;
