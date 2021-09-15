f8749ace3173b90b2804f064c4ac4053e26431fe
João V. Dias <diasvictorj@gmail.com>
2021-09-15 11:57:41 -0300

    Sexto requisito correçoes eslint 2

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
   // seu código aqui
-  if (entrants !== undefined || Object.values(entrants).length !==0) {
+  if (entrants !== undefined || Object.values(entrants).length !== 0) {
     const child = entrants.filter((element) => element.age < 18);
     const adult = entrants.filter((element) => element.age >= 18 && element.age < 50);
     const senior = entrants.filter((element) => element.age >= 50);
@@ -23,12 +23,12 @@ function calculateEntry(entrants) {
     return toReturn;
   } return 0;
 }
- const entrants = [
+const entrants = [
   { name: 'Lara Carvalho', age: 5 },
   { name: 'Frederico Moreira', age: 5 },
   { name: 'Pedro Henrique Carvalho', age: 5 },
   { name: 'Maria Costa', age: 18 },
   { name: 'Núbia Souza', age: 18 },
   { name: 'Carlos Nogueira', age: 50 },
-]; 
+];
 module.exports = { calculateEntry, countEntrants };
