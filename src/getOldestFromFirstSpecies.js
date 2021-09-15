fcdaa3fa6a2b83b5cd7cf6431c0726054c3e6ab7
Valdeci <moura97.valdeci@gmail.com>
2021-09-14 22:20:24 -0300

    corrige 101 para menos de 100 caracteres

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,7 +6,7 @@ function getOldestFromFirstSpecies(id) {
   if (!id) { return 'Erro!'; }
   const firstId = employees.find((element) => element.id === id).responsibleFor[0];
   const animal = species.filter((element) => element.id === firstId);
-  return Object.values(animal[0].residents.reduce((acc, curr) => (acc.age > curr.age ? acc : curr)));
+  return Object.values(animal[0].residents.reduce((acc, cur) => (acc.age > cur.age ? acc : cur)));
 }
 
 module.exports = getOldestFromFirstSpecies;
