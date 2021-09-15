41c04a9b2d6056e6bbb7a5802840de81fda2700c
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 21:09:31 -0300

    finalizaçao do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,9 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const visitors = {
+    child: entrants.filter((people) => people.age < 18).length,
+    adult: entrants.filter((people) => people.age >= 18 && people.age < 50).length,
+    senior: entrants.filter((people) => people.age >= 50).length
+  }
+  return visitors;
 }
 
 function calculateEntry(entrants) {
+  if (!entrants || Object.keys(entrants).length === 0) return 0;
+  const infoVisitors = countEntrants(entrants);
+  const totalSum = ((infoVisitors.child * data.prices.child) 
+  + (infoVisitors.adult * data.prices.adult) + (infoVisitors.senior * data.prices.senior))
+  return totalSum;
 }
 
 module.exports = { calculateEntry, countEntrants };
