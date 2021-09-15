9125e8ac233e0f71a22806a965df0007bce6413c
carla <carlabazevedoa@gmail.com>
2021-09-15 10:41:59 -0300

    ok  getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const ageAnimal = data.species.find((species) => species.name === animal);
+  return ageAnimal.residents.every((residAge) => residAge.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
