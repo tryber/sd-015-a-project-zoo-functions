1e0691843e9c54afd00dc49b9c940c950acb7f69
Caio Lima <caiojlimah@gmail.com>
2021-09-14 21:41:58 -0300

    Refatorando getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,46 +1,27 @@
 const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function checkingIfIsDayOrAnimal(dayAnimal) {
+function checkingIfIsDayOrAnimal(param) {
   const ani = species.map((e) => e.name);
   const days = Object.keys(hours);
-  if (days.some((e) => e === dayAnimal)) {
-    return {
-      [dayAnimal]: {
-        officeHour: `Open from ${hours[dayAnimal].open}am until ${hours[dayAnimal].close}pm`,
-        exhibition: species.filter((e) => e.availability.includes(dayAnimal)).map((e) => e.name),
-      },
-    };
+  if (days.some((e) => e === param)) {
+    return { [param]: {
+      officeHour: `Open from ${hours[param].open}am until ${hours[param].close}pm`,
+      exhibition: species.filter((e) => e.availability.includes(param)).map((e) => e.name) } };
   }
-  if (ani.some((e) => e === dayAnimal)) {
-    return species.find((e) => e.name === dayAnimal).availability;
-  }
+  if (ani.some((e) => e === param)) return species.find((e) => e.name === param).availability;
   return false;
 }
 
-function ifIsMonday() {
-  return {
-    Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' },
-  };
-}
+const monday = () => ({ Monday: { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' } });
 
-function showAllSchedule() {
-  const days = Object.keys(hours);
-  return Object.assign(days.reduce((acc, day) => {
-    Object.assign(acc, checkingIfIsDayOrAnimal(day));
-    return acc;
-  }, {}), ifIsMonday());
-}
+const showAllSchedule = () => Object.assign(Object.keys(hours).reduce((acc, day) =>
+  Object.assign(acc, checkingIfIsDayOrAnimal(day)), {}), monday());
 
 function getSchedule(scheduleTarget = false) {
-  const isDayOrAnimal = checkingIfIsDayOrAnimal(scheduleTarget);
-  if (scheduleTarget === 'Monday') {
-    return ifIsMonday();
-  }
-  if (!isDayOrAnimal) {
-    return showAllSchedule();
-  }
-  return isDayOrAnimal;
+  if (scheduleTarget === 'Monday') return monday();
+  if (!checkingIfIsDayOrAnimal(scheduleTarget)) return showAllSchedule();
+  return checkingIfIsDayOrAnimal(scheduleTarget);
 }
 
 module.exports = getSchedule;
