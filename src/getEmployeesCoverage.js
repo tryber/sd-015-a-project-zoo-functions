87c0f923f405a6155269011b3e4325811794c099
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-09-15 11:34:21 -0300

    feito uma refatoração no codigo da função 10.

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -17,10 +17,25 @@ function returnArrayModify() { // função auxiliar que vai formatar um array.
       fullName: `${element.firstName} ${element.lastName}`,
       species: getId(...element.responsibleFor).map((animal) => animal.name), // função que encontra o id
       locations: getId(...element.responsibleFor).map((animal) => animal.location),
+      // o getId é uma função reaproveitada, olhar na linha 1.
     };
     arrayToReturn.push(obj); // empurra o array para um vaizio e retorna ele.
   });
   return arrayToReturn; // retorna o objeto formatado
+}
+
+function getEmployeeByName(worker) {
+  // quando passar o nome no parametro ele vai procurar no objeto que foi feito e verificar no parametro, se tiver retorna
+  // o objeto selecionado, para isso ele faz a verificação no parametro da função.
+  const arrayCompleto = returnArrayModify(); // função que retorna o array modificado da linha 11
+  const findWorkerByName = arrayCompleto.find((element) => element.fullName.includes(worker.name));
+  return findWorkerByName;
+}
+
+function getEmployeeById(workerId) { // a mesma coisa nessa função ele compara o id do parametro com o id do objeto.
+  const arrayCompleto = returnArrayModify(); // função que retorna o array modificado da linha 11
+  const findWorkerId = arrayCompleto.find((element) => element.id === workerId.id);
+  return findWorkerId;
 }
 
 function getEmployeesCoverage(colaborationPeople) { // função principal que faz verificações.
@@ -29,12 +44,14 @@ function getEmployeesCoverage(colaborationPeople) { // função principal que fa
   if (!colaborationPeople) return arrayCompleto; // se não tiver parametro retorna o array completo
   // se tiver parametro ele vai procurar nesse objeto que está no parametro da função se o nome está lá
   // se tiver ele retorna o objeto completo.
-  if (arrayCompleto.find((element) => element.fullName.includes(colaborationPeople.name))) {
-    return arrayCompleto.find((element) => element.fullName.includes(colaborationPeople.name));
+  const employeeName = getEmployeeByName(colaborationPeople); // função que acha o nome da pessoa colaboradora linha 26
+  if (employeeName) {
+    return employeeName;
   }
   // se tiver parametro no caso o id do array for o mesmo que o objeto retorna o objeto
-  if (arrayCompleto.find((element) => element.id === colaborationPeople.id)) {
-    return arrayCompleto.find((element) => element.id === colaborationPeople.id);
+  const employeeId = getEmployeeById(colaborationPeople); // função que acha o Id da pessoa linha 32.
+  if (employeeId) {
+    return employeeId;
   }
   throw new Error('Informações inválidas'); // se o parametro for errado lança uma mensagem de error.
 }
