c24aee18b66a2f2442ba49d225d3510acb557dbc
Italo Sergio <italosergio@graduate.org>
2021-09-15 06:34:19 -0300

    Challange 1 (check) - [code clear]

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,27 +1,11 @@
-/* ### 1. Implemente a função `getSpeciesByIds`
-
-Esta função é responsável pela busca das espécies de animais por id. Ela retorna um array contendo as espécies referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
-
-**Observações técnicas**
-
-- O parâmetro desta função pode ser alterado para atender ao requisito proposto.
-
-**O que será avaliado**
-
-- Caso receba nenhum parâmetro, necessário retornar um array vazio;
-- Ao receber como parâmetro um único id, retorna um array com a espécie referente à esse id;
-- Ao receber mais de um id, retorna um array com as espécies referentes aos ids. */
-
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   const result = [];
   ids.forEach((elementIds) =>
-    result.push(species.find((elementSpecies) =>
+    result.push(data.species.find((elementSpecies) =>
       elementSpecies.id === elementIds)));
   return result;
 }
 
-// console.log(getSpeciesByIds('89be95b3-47e4-4c5b-b687-1fabf2afa274', 'baa6e93a-f295-44e7-8f70-2bcdc6f6948d'));
 module.exports = getSpeciesByIds;
