88eef199393d94c6eb01197cb58f3ee9ed4f0765
Italo Sergio <italosergio@graduate.org>
2021-09-15 06:37:57 -0300

    Challange 3 (check) - [code clear]

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,23 +1,10 @@
-/* ### 3. Implemente a função `getEmployeeByName`
-
-Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
-
-**O que será avaliado**
-
-- Sem parâmetros, retorna um objeto vazio
-- Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
-- Quando provido o último nome do funcionário, retorna o objeto do funcionário
-
---- */
-
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   return employeeName
-    ? employees.find((element) =>
+    ? data.employees.find((element) =>
       element.firstName === employeeName || element.lastName === employeeName)
     : {};
 }
-// console.log(getEmployeeByName('Nigel'));
+
 module.exports = getEmployeeByName;
