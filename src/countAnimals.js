d8f4ccb496534c3c1e1338603fe79c21b2630353
Luan Vitor <luan_vittor@hotmail.com>
2021-09-14 22:46:46 -0300

    Fixing README

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,13 +8,13 @@ function countAnimals(animal) {
     return obj;
     // nessa condicional caso o parametro seja vazio, faco um for each para pegar todas as especies e colocar em um array criado anteriormente
   }
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
   // desestruturei os parametro para acessa-los de forma independente
   const animal2 = species.find((elemento) => elemento.name === specie).residents;
   // atribui a animal2 o objeto que condiz com o parametro, porem resgatei o array residents
-  const residentes = animal2.reduce((acc, element) => (element.sex === gender ? acc + 1 : acc), 0);
+  const residentes = animal2.reduce((acc, element) => (element.sex === sex ? acc + 1 : acc), 0);
   // usei o array resgatado acima para contar quantos animaisi tinha pelo sexo.
-  return gender === undefined ? animal2.length : residentes;
+  return sex === undefined ? animal2.length : residentes;
   // caso nao fosse passado qual genero ele quer, retorna o length do array de residentes, caso contrario  retorna a rinha 16.
 }
 
