70547698dc40f0b02e27e812d4f4925578334b53
douglas oliveira <douglasgama0@gmail.com>
2021-09-14 21:11:56 -0300

    ajuste para o avaliador

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -4,16 +4,16 @@ function countEntrants(entrants) {
   const visitors = {
     child: entrants.filter((people) => people.age < 18).length,
     adult: entrants.filter((people) => people.age >= 18 && people.age < 50).length,
-    senior: entrants.filter((people) => people.age >= 50).length
-  }
+    senior: entrants.filter((people) => people.age >= 50).length,
+  };
   return visitors;
 }
 
 function calculateEntry(entrants) {
   if (!entrants || Object.keys(entrants).length === 0) return 0;
   const infoVisitors = countEntrants(entrants);
-  const totalSum = ((infoVisitors.child * data.prices.child) 
-  + (infoVisitors.adult * data.prices.adult) + (infoVisitors.senior * data.prices.senior))
+  const totalSum = ((infoVisitors.child * data.prices.child)
+  + (infoVisitors.adult * data.prices.adult) + (infoVisitors.senior * data.prices.senior));
   return totalSum;
 }
 
