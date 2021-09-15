09f929aa57fef48aa485364a00b6050bdc0eb630
Italo Sergio <italosergio@graduate.org>
2021-09-15 06:21:47 -0300

    Challange 8 (check)

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,6 +1,10 @@
 const data = require('../data/zoo_data');
 
-const daysExibitions = () => {
+const isAnimal = (param) => data.species.map((animal) => animal.name).includes(param);
+
+const isDay = (param) => Object.keys(data.hours).includes(param);
+
+const weekExibitions = () => {
   const obj = {};
   Object.keys(data.hours).forEach((day) => {
     if (day !== 'Monday') {
@@ -21,25 +25,19 @@ const daysExibitions = () => {
   return obj;
 };
 
-const daysAnimals = (day) => ({
+const animalExibitions = (animalName) => data.species
+  .find((animal) => animal.name === animalName).availability;
+
+const dayExibition = (day) => ({
   [day]: {
-    officeHour: daysExibitions()[day].officeHour,
-    exhibition: daysExibitions()[day].exhibition,
+    officeHour: weekExibitions()[day].officeHour,
+    exhibition: weekExibitions()[day].exhibition,
   },
 });
 
-const isAnimal = (param) => data.species.map((animal) => animal.name).includes(param);
-const isDay = (param) => Object.keys(data.hours).includes(param);
-
-const animalDays = (animalName) => data.species
-  .find((animal) => animal.name === animalName).availability;
-
 function getSchedule(schedule) {
-  if (!schedule || (!isAnimal(schedule) && !isDay(schedule))) return daysExibitions();
-  return isAnimal(schedule) ? animalDays(schedule) : daysAnimals(schedule);
+  if (!schedule || (!isAnimal(schedule) && !isDay(schedule))) return weekExibitions();
+  return isAnimal(schedule) ? animalExibitions(schedule) : dayExibition(schedule);
 }
 
-console.log(getSchedule('Wednesday'));
-// console.log(data.species.filter((element) => element.availability.includes('Tuesday')).map((element) => element.name));
-// console.log(data.hours.Tuesday.open);
 module.exports = getSchedule;
