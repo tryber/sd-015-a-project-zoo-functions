19a996e79b28b9dd9346dbd039e751eb05d4fd22
Junio <junioalves2005@hotmail.com>
2021-09-15 14:21:22 -0300

    metade do requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,18 +1,28 @@
-// const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// function countEntrants(entrants) {
+const { adult, child, senior } = prices;
 
-// let aa = countEntrants([
-//   { name: 'name.findName()', age: 5 },
-//   { name: 'name.findName()', age: 5 },
-//   { name: 'name.findName()', age: 5 },
-//   { name: 'name.findName()', age: 18 },
-//   { name: 'name.findName()', age: 18 },
-//   { name: 'name.findName()', age: 50 },
-// ])
+const entrant = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 
-// function calculateEntry(entrants) {
-//   // seu código aqui ee
-// }
+function countEntrants(entrants) {
+  const childAge = entrants.filter((x) => x.age < 18);
+  const adultAge = entrants.filter((y) => y.age >= 18 && y.age < 50);
+  const seniorAge = entrants.filter((z) => z.age >= 50);
+  return { adult: adultAge.length, child: childAge.length, senior: seniorAge.length };
+}
+console.log(countEntrants(entrant));
+function calculateEntry(entrants) {
+  if (!entrants || Object.values(entrants).length === 0) {
+    return 0;
+  }
+}
 
-// module.exports = { calculateEntry, countEntrants };
+module.exports = { calculateEntry, countEntrants };
