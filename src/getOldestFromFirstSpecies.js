63da997f96ffbc7eabd6512e8ebc2dfad7a50eb2
Italo Sergio <italosergio@graduate.org>
2021-09-15 08:19:53 -0300

    Challange 9 (check)

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
 
+  const animalEmployee = data.employees
+    .find((person) => person.id === id).responsibleFor[0];
+
+  const firstSpecieResidents = data.species
+    .find((animals) => animals.id === animalEmployee).residents;
+
+  const residentsSort = firstSpecieResidents
+    .sort((young, old) => old.age - young.age);
+
+  const oldAnimal = Object.values(residentsSort[0]);
+
+  return oldAnimal;
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
