d0fa4d2d6dece84c431b31097c30a75fb8ca6df8
uauditore <fauditore2912@gmail.com>
2021-09-15 18:50:14 -0300

    requisitos 8 e 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionario = employees.find((elemento) => elemento.id === id);
+  const animais = species.find((elemento) => elemento.id === funcionario.responsibleFor[0]).residents
+  const resultado = animais.reduce((acumulador, elemento) => (acumulador.age > elemento.age ? acumulador : elemento), 0);
+  return [resultado.name, resultado.sex, resultado.age]
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,9 +1,45 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  if (!undefined) {
-    
+function horasDoDia(aberto, fechado) {
+  if ( aberto === 0 && fechado === aberto) {
+    return 'CLOSED'
+  } else {
+    return `Open from ${aberto}am until ${fechado}pm`
   }
 }
-
+function animalDaqueleDia(dia) {
+  const dias = data.species.filter((element) => element.availability.includes(dia))
+  return dias.map((emenlento) => emenlento.name)
+}
+function verificarODia(dia) {
+  if (dia !== 'Monday') {
+   return animalDaqueleDia(dia)
+  } else {
+    return 'The zoo will be closed!'
+  }
+}
+function calendario (...array) {
+  const resultado = {};
+  array.forEach((elemento) => {
+    resultado[elemento] = {
+      officeHour: horasDoDia(hours[elemento].open, hours[elemento].close),
+      exhibition: verificarODia(elemento)
+    }
+  })
+  return resultado
+}
+function getSchedule(scheduleTarget) {
+  let resultado = {};
+  const semana = Object.keys(hours)
+  const animais = species.map((elemento) => elemento.name)
+  if (scheduleTarget === undefined || (!semana.includes(scheduleTarget) && !animais.includes(scheduleTarget))) {
+    resultado = calendario(...semana)
+  } else if (semana.includes(scheduleTarget)) {
+    resultado = calendario(scheduleTarget)
+  } else {
+    resultado = species.find((elemento) => elemento.name === scheduleTarget).availability
+  } 
+  return resultado
+}
 module.exports = getSchedule;
