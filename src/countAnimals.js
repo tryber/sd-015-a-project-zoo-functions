80c1390c416492e1e9251d386ba565e86f8835cd
Caio Lima <caiojlimah@gmail.com>
2021-09-14 21:19:39 -0300

    Ajustando "gender" para "sex" do countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,18 +1,21 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function countAnimals({ specie = undefined, gender = false } = {}) {
-  if (specie === undefined) {
+function countAnimals({ specie = undefined, sex = false } = {}) {
+  if (!specie) {
     return species.reduce((acumulator, { name, residents }) => {
       Object.assign(acumulator, { [name]: residents.length });
       return acumulator;
     }, {});
   }
-  if (!gender) {
+  if (!sex) {
     return species.find((elem) => elem.name === specie).residents.length;
   }
   return species.find((elem) => elem.name === specie).residents.filter((e) =>
-    e.sex === gender).length;
+    e.sex === sex).length;
 }
+
+console.log(countAnimals({ specie: 'giraffes' }));
+console.log(countAnimals({ specie: 'bears', sex: 'female' }));
 
 module.exports = countAnimals;
