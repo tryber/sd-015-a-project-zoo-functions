8719416cecdb9f93557d7c7affbc81718d13aa80
Thiago <thiivieira@gmail.com>
2021-09-14 21:55:26 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, { name, residents }) => {
+      Object.assign(acc, { [name]: residents.length });
+      return acc;
+    }, {});
+  }
+
+  const { specie, gender } = animal;
+  if (!gender) {
+    return data.species.find((element) => element.name === specie).residents
+      .length;
+  }
+  return data.species
+    .find((element) => element.name === specie)
+    .residents.filter((element) => element.sex === gender).length;
 }
 
 module.exports = countAnimals;
