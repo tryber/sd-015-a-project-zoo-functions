efb551e92a953ce69ddae62f417c3ef158dbbb4c
Vitor Diorio <vitor.diorio@yahoo.com.br>
2021-09-14 22:58:49 -0300

    requisito 7 + pont e virgula na linha 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { hours } = data;
+const { species } = data;
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (!scheduleTarget) {
+    return Object.keys(hours).reduce((acc, curr) => {
+      acc[curr] = { officeHour: `open from ${hours[curr].open}am until ${hours[curr].close}pm` };
+      acc[curr] = { exhibition: species.map((specie) => specie.availability.includes(curr)) };
+    }, {});
+  }
 }
 
 module.exports = getSchedule;
