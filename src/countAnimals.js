dd5bd7d9255ff0891d580a2e044b06ed2763e78c
Arthur <arthurteixeira0805@gmail.com>
2021-09-15 20:05:08 -0300

    5/10

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
+// Ref:
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/leonardo-felix-zoo-functions-project
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/pedro-guarize-zoo-functions-project
+
 const data = require('../data/zoo_data');
+
+const { species } = data;
 
 function countAnimals(animal) {
   // seu código aqui
+  if (!animal) {
+    return species.reduce((acc, curr) => {
+      acc[curr.name] = curr.residents.length;
+      return acc;
+    }, {});
+  }
+  const generic = species.find((spec) => spec.name === animal.specie).residents;
+  const unGender = generic.length;
+  const gender = generic.reduce((acc, curr) => (curr.sex === animal.gender ? acc + 1 : acc), 0);
+  return animal.gender === undefined ? unGender : gender;
 }
 
 module.exports = countAnimals;
