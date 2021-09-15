bc37de49b62ff266b07005982bf908227a7b42cc
Alex <alex_cubas1.0@hotmail.com>
2021-09-14 21:11:49 -0300

    exercicio 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,34 @@
+const { hours, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+const arrayHours = Object.keys(hours);
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (scheduleTarget === undefined || scheduleTarget !== hours || scheduleTarget !== species.name) {
+    const days = arrayHours.reduce((acc, curr, i, a) => {
+      if (i !== a.length - 1) {
+        acc[curr] = {
+          officeHour: `Open from ${hours[curr].open}am until ${hours[curr].close}pm`,
+          exhibition: species.filter((param) =>
+            param.availability.includes(curr)).map((elem) =>
+            elem.name),
+        };
+        return acc;
+      }
+      acc[curr] = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+      return acc;
+    }, {});
+    return days;
+  }
 }
+
+function dayMonday(scheduleTarget) {
+  if (scheduleTarget === 'Monday') {
+    return `{
+      'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+    }`;
+  }
+}
+
+// console.log(dayMonday());
 
 module.exports = getSchedule;
