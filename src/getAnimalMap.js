20bd3ec5fcf484f67760da0ac986055f983d1d11
Thiago Monteiro <thiagoacmonteiro@outlook.com>
2021-09-14 21:52:10 -0300

    Exercise 9 solved

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,16 +1,19 @@
 const data = require('../data/zoo_data');
 
-// function getAnimalMap(animal) {
-//   if (!options) {
-//     const { species } = data;
-//     const NE = species.filter(({ location }) => location === 'NE').map((info) => info.name);
-//     const NW = species.filter(({ location }) => location === 'NW').map((info) => info.name);
-//     const SE = species.filter(({ location }) => location === 'SE').map((info) => info.name);
-//     const SW = species.filter(({ location }) => location === 'SW').map((info) => info.name);
-//     return { NE, NW, SE, SW };
-//   }
-// }
+// Function to return the species by location if no parameters is assined
+function getAnimalsByLocation() {
+  const { species } = data;
+  const NE = species.filter(({ location }) => location === 'NE').map((info) => info.name);
+  const NW = species.filter(({ location }) => location === 'NW').map((info) => info.name);
+  const SE = species.filter(({ location }) => location === 'SE').map((info) => info.name);
+  const SW = species.filter(({ location }) => location === 'SW').map((info) => info.name);
+  return { NE, NW, SE, SW };
+}
 
-// console.log(getAnimalMap());
+function getAnimalMap(options) {
 
-// module.exports = getAnimalMap;
+}
+
+getAnimalsByLocation();
+
+module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalIdFound = data.employees.find((info) => info.id === id).responsibleFor[0];
+  const residentsFound = data.species.find((specie) => specie.id === animalIdFound).residents;
+  const oldestAnimal = residentsFound.sort((animalA, animalB) => animalB.age - animalA.age)[0];
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
