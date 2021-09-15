c5c7d736f550ac1227b32a515d644446b466d3d1
Gustavo Lima <lima_gus@outlook.com>
2021-09-15 20:13:57 -0300

    Ajustes no requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,16 +1,6 @@
 const data = require('../data/zoo_data');
 
-// const entrants = [
-//   { name: 'Lara Carvalho', age: 5 },
-//   { name: 'Frederico Moreira', age: 5 },
-//   { name: 'Pedro Henrique Carvalho', age: 5 },
-//   { name: 'Maria Costa', age: 18 },
-//   { name: 'Núbia Souza', age: 18 },
-//   { name: 'Carlos Nogueira', age: 50 },
-// ];
-
 function countEntrants(entrants) {
-  //  Array.filter retorna um array
   return {
     child: entrants.filter((count) => count.age < 18).length,
     adult: entrants.filter((count) => count.age >= 18 && count.age < 50).length,
@@ -26,8 +16,7 @@ function calculateEntry(entrants) {
   const childPayment = visitants.child * data.prices.child;
   const adultPayment = visitants.adult * data.prices.adult;
   const seniorPayment = visitants.senior * data.prices.senior;
-  const totalPayment = childPayment + adultPayment + seniorPayment;
-  return totalPayment;
+  return childPayment + adultPayment + seniorPayment;
 }
 
 module.exports = { calculateEntry, countEntrants };
