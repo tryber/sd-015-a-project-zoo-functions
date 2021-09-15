f64876826d2e0df8b5b998122231805e300b5d3b
Italo Sergio <italosergio@graduate.org>
2021-09-15 03:44:46 -0300

    Challange 8 - stage 1/5 (check)

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,30 @@
 const data = require('../data/zoo_data');
 
+const animalsDays = () => {
+  const obj = {};
+  Object.keys(data.hours).forEach((day) => {
+    if (day !== 'Monday') {
+      const time = data.hours[day];
+      obj[day] = {
+        officeHour: `Open from ${time.open}am until ${time.close}pm`,
+        exhibition: data.species
+          .filter((animal) => animal.availability.includes(day))
+          .map((animal) => animal.name),
+      };
+    } else {
+      obj[day] = {
+        officeHour: 'CLOSED',
+        exhibition: 'The zoo will be closed!',
+      };
+    }
+  });
+  return obj;
+};
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) return animalsDays();
 }
 
+console.log(getSchedule());
+// console.log(data.species.filter((element) => element.availability.includes('Tuesday')).map((element) => element.name));
+// console.log(data.hours.Tuesday.open);
 module.exports = getSchedule;
