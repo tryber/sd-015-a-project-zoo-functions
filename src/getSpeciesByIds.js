92f010e727c9a947b12bddcebbe109b7fde3aa47
carla <carlabazevedoa@gmail.com>
2021-09-15 20:51:48 -0300

    ajuste de lint getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
-const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) { return ids.map((id) => 
-data.species.find((species) => id === species.id));
+function getSpeciesByIds(...ids) {
+  return ids.map((id) =>
+    data.species.find(((specie) => id === specie.id)));
 }
 
 module.exports = getSpeciesByIds;
