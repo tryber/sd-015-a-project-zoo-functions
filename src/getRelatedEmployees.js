7e566e609082054a2416cfd626928f27798f315f
Daniel Tavares <daniel1tavares@outlook.com>
2021-09-16 11:31:47 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,25 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const isIdManager = data.employees.some((employee) => employee.managers.includes(id));
+  return isIdManager;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const verifyId = isManager(managerId);
+  if (verifyId) {
+    const employeeManaged = data.employees.filter(
+      (employee) => employee.managers.includes(managerId),
+    );
+    const employees = employeeManaged.map(
+      (employee) => `${employee.firstName} ${employee.lastName}`,
+    );
+    return employees;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
