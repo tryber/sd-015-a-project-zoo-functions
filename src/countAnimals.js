2e7b454f1e77e28a82be1e9285e2c4873e443015
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-16 12:22:32 -0300

    adequa lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,15 +1,15 @@
 const data = require('../data/zoo_data');
 
 const allAnimals = {
-  'lions': 4,
-  'tigers': 2,
-  'bears': 3,
-  'penguins': 4,
-  'otters': 4,
-  'frogs': 2,
-  'snakes': 2,
-  'elephants': 4,
-  'giraffes': 6,
+  lions: 4,
+  tigers: 2,
+  bears: 3,
+  penguins: 4,
+  otters: 4,
+  frogs: 2,
+  snakes: 2,
+  elephants: 4,
+  giraffes: 6,
 };
 
 function countAnimals(animal) {
@@ -21,7 +21,8 @@ function countAnimals(animal) {
   if (!animal.sex) {
     return filteredAnimal.residents.length;
   }
-  const sexFilteredResidents = filteredAnimal.residents.filter((resident) => resident.sex === animal.sex);
+  const sexFilteredResidents = filteredAnimal.residents.filter((resident) => 
+    resident.sex === animal.sex);
   return sexFilteredResidents.length;
 }
 
