84fd33b54a7d09c7d673b69234012b992c3d188b
Davio Lopes <davioplp@gmail.com>
2021-09-15 23:17:36 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,8 +1,8 @@
 const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
   const entrant = { adult: 0, child: 0, senior: 0 };
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
   entrants.forEach((iten) => {
     if (iten.age < 18) entrant.child += 1;
     if (iten.age >= 18 && iten.age < 50) entrant.adult += 1;
@@ -33,4 +33,4 @@ console.log(calculateEntry([
   { name: 'asda', age: 18 },
   { name: 'asda', age: 50 },
 ]));
-module.exports = { calculateEntry, countEntrants }; 
+module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -5,15 +5,15 @@ function countAnimals(animal) {
     const animals = {};
     species.forEach((iten) => { animals[iten.name] = iten.residents.length; });
     return animals;
-  } 
-   if (animal.gender === undefined) {
+  }
+  if (animal.gender === undefined) {
     const numAni = species.find((animals) => animals.name === animal.specie);
-    return numAni.residents.length;
-  } 
-   else {
+    console.log(numAni.residents.length); 
+  }
+  else {
     const numAni = species.find((animals) => animals.name === animal.specie)
     return numAni.residents.filter((iten) => iten.sex === animal.gender).length;
-  } 
+  }
 }
 console.log(countAnimals());
 module.exports = countAnimals;
