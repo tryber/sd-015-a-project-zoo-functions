a3eeba47e38141aaa067f9113e97c8432d203821
Italo Sergio <italosergio@graduate.org>
2021-09-16 04:11:00 -0300

    Challange 6 (check) - [fix] [refactored]

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,19 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  if (!entrants || ({})) return false;
-  return {
-    child: Object.values(entrants.filter((element) =>
-      element.age < 18)).length,
-    adult: Object.values(entrants.filter((element) =>
-      element.age >= 18 && element.age < 50)).length,
-    senior: Object.values(entrants.filter((element) =>
-      element.age >= 50)).length,
-  };
-}
+const countEntrants = (obj) => ({
+  child: Object.values(obj.filter((element) =>
+    element.age < 18)).length,
+  adult: Object.values(obj.filter((element) =>
+    element.age >= 18 && element.age < 50)).length,
+  senior: Object.values(obj.filter((element) =>
+    element.age >= 50)).length,
+});
 
 function calculateEntry(entrants) {
-  if (!countEntrants(entrants)) return 0;
+  if (!entrants || !Object.keys(entrants).length) return 0;
   const childPay = countEntrants(entrants).child * data.prices.child;
   const adultPay = countEntrants(entrants).adult * data.prices.adult;
   const seniorPay = countEntrants(entrants).senior * data.prices.senior;
