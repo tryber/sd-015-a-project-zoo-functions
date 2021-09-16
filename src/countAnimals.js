769dbfffe14991cf68ebd1cd3736895ca55b251b
Thiago <thiivieira@gmail.com>
2021-09-15 21:34:35 -0300

    Requisito 5 arrumando GitHub

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,14 +8,14 @@ function countAnimals(animal) {
     }, {});
   }
 
-  const { specie } = animal;
-  if (!specie) {
+  const { specie, sex } = animal;
+  if (!sex) {
     return data.species.find((element) => element.name === specie).residents
       .length;
   }
   return data.species
     .find((element) => element.name === specie)
-    .residents.filter((element) => element.sex === specie.sex).length;
+    .residents.filter((element) => element.sex === sex).length;
 }
 
 module.exports = countAnimals;
