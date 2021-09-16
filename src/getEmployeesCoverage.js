1e36486224d855a2213fb154314087e097c2646c
Italo Sergio <italosergio@graduate.org>
2021-09-16 05:47:35 -0300

    Challange 10 - 1/5 (check)

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,22 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const specieIdName = (id) => {
+  return data.species.find((specie) => specie.id === id).name;
+};
 
+const specieIdLocation = (id) => {
+  return data.species.find((specie) => specie.id === id).location
+}
+function getEmployeesCoverage(options) {
+  if (!options) {
+    return data.employees.map((employee) => ({
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: employee.responsibleFor.map((specieId) => specieIdName(specieId)),
+      locations: employee.responsibleFor.map((specieId) => specieIdLocation(specieId)),
+    }));
+  }
+}
+console.log(getEmployeesCoverage());
 module.exports = getEmployeesCoverage;
