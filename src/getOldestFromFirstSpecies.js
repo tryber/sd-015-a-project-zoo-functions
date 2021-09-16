408e4f86d67e8e38ec479e978ecb44c55d00423e
Raphael Taglialegna <raphael.trcosta@gmail.com>
2021-09-15 21:31:34 -0300

    Requisit 9 - VQV voando

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  // seu código aqui.
+  const getAnimalsEmployees = data.employees.find((elementId) =>
+    elementId.id === id).responsibleFor[0];
+  const getAnimals = data.species.find((elementAnimals) =>
+    getAnimalsEmployees.includes(elementAnimals.id)).residents;
+
+  const oldAnimal = getAnimals.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr), 0); // Essa compração usando o reduce foi tirada a logica do site :https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects?rq=1 resposta do  juliangonzalez.
+  return Object.values(oldAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
