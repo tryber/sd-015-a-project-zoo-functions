8cb35093b9de038431938bc4bf59c57c83055eaa
carla <carlabazevedoa@gmail.com>
2021-09-15 22:48:31 -0300

    countAnimals ok

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    return data.species.reduce((object, specie) =>
+      Object.assign(object, { [specie.name]: specie.residents.length }), {});
+  }
+  if (animal.specie && animal.sex) {
+    return data.species.find((specie) =>
+      specie.name === animal.specie).residents.filter((resident) =>
+      resident.sex === animal.sex).length;
+  }
+  if (animal.specie) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
 }
 
 module.exports = countAnimals;
