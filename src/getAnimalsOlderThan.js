4a03a848c5ad214bb81b42efccbfdc1bab77efe2
MatheusBurthon91 <matatiaspatrizio@hotmail.com>
2021-09-15 21:03:53 -0300

    feita refatoração no codigo de uma função.

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,6 @@
 const { species } = require('../data/zoo_data');
-const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-  return species.find((specie) => specie.name === animal) // aqui ele faz a procura do animal
+const getAnimalsOlderThan = (animal, age) =>
+  species.find((specie) => specie.name === animal)
     .residents.every((resident) => resident.age > age);
-  // aqui ele traz um booleano que faz a verificação que se a idade passada no parametro é menor do que a dos
-  // animais residentes.
-}
-
 module.exports = getAnimalsOlderThan;
