e0a15ed0cb46ab30b80807a8e00339871c637fe4
Renato Moreno <renats.mc@gmail.com>
2021-09-16 20:44:25 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,6 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((worker) => worker.id === id);
+  const specieResponsible = data.species
+    .find((specie) => specie.id === employee.responsibleFor[0]).residents;
+  const ageOrdering = specieResponsible.sort((first, second) => second.age - first.age)[0];
+  const animalIdentification = [ageOrdering.name, ageOrdering.sex, ageOrdering.age];
+  return animalIdentification;
 }
 
 module.exports = getOldestFromFirstSpecies;
