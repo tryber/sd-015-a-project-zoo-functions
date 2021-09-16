8473e8722c274cdfc4be3f8642397134a1ed18de
Daniel Tavares <daniel1tavares@outlook.com>
2021-09-16 10:46:20 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // src: https://trybecourse.slack.com/archives/C027T2VU8U8/p1631556965189500
-  const specieById = data.species.filter((specie) => specie.id === ids);
+function getSpeciesByIds(...ids) {
+  const specieById = data.species.filter((specie) => ids.includes(specie.id));
   return specieById;
 }
 
