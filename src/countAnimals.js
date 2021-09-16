d04905a80efd8bcd79ef8a410188da6f59400a34
Thiago Lordello <thiagolordello@hotmail.com>
2021-09-16 11:54:52 -0300

    5a questao v4

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,12 +4,12 @@ Observações técnicas
 
 Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
 Com o argumento { specie: 'penguins' }, retorna um número, a quantidade de pinguins no zoológico;
-Com o argumento { specie: 'giraffes', gender: 'female' }, retorna um número, a quantidade de girafas do gênero feminino.
+Com o argumento { specie: 'giraffes', sex: 'female' }, retorna um número, a quantidade de girafas do gênero feminino.
 O que será avaliado
 
 Sem parâmetros, retorna as espécies e sua quantidade;
 Recebendo como parâmetro um objeto com a chave specie, retorna um número, a quantidade de animais daquela espécie;
-Recebendo como parâmetro um objeto com a chave specie e gender, retorna um número, a quantidade de animais daquela espécie, no gênero selecionado.  */
+Recebendo como parâmetro um objeto com a chave specie e sex, retorna um número, a quantidade de animais daquela espécie, no gênero selecionado.  */
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
@@ -22,13 +22,13 @@ function countAnimals(animal) {
     }, {});
     return animalsObj;
   }
-  const { specie, gender } = animal;
-  if (!gender) { // entra se tiver somente o nome da especie { specie: 'penguins' }
+  const { specie, sex } = animal;
+  if (!sex) { // entra se tiver somente o nome da especie { specie: 'penguins' }
     const animSelected = species.find((especie) => especie.name === specie);
     return animSelected.residents.length;
   }
   const animSelected = species.find((especie) => especie.name === specie); // entra aqui se tiver passando duas duas chaves no objeto da countAnimals
-  const resultado = animSelected.residents.filter((girafa) => girafa.sex === animal.gender).length;
+  const resultado = animSelected.residents.filter((girafa) => girafa.sex === animal.sex).length;
   return resultado;
 }
 
