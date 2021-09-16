659e8f2477ef67c515abd0f46de71e599afc83ff
João Cassanji <joaocassanji@gmail.com>
2021-09-16 12:23:54 -0300

    requisito 6 corrigido

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,14 +1,5 @@
 const data = require('../data/zoo_data');
 
-const entrantsXX = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 const entrantsList = {
   child: 0,
   adult: 0,
@@ -54,8 +45,8 @@ function calculateEntry(entrants) {
   if (entrants === undefined || Object.keys(entrants).length === 0) {
     return 0;
   }
-countEntrants(entrants);
-return refreshTotalValue();
+  countEntrants(entrants);
+  return refreshTotalValue();
 }
 
 module.exports = { calculateEntry, countEntrants };
