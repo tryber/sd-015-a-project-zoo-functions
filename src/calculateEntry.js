fc1680c36bac38601cd3cf136ae2a19c75f888e0
Davio Lopes <davioplp@gmail.com>
2021-09-15 23:10:51 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,7 @@
 const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-   if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0; 
+  if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
   const entrant = { adult: 0, child: 0, senior: 0 };
   entrants.forEach((iten) => {
     if (iten.age < 18) entrant.child += 1;
@@ -22,9 +22,9 @@ console.log(countEntrants([
 function calculateEntry(entrants) {
   if (typeof entrants === 'undefined' || Object.keys(entrants).length === 0) return 0;
   const entrant = countEntrants(entrants);
-  return (entrant.adult * prices.adult) + (entrant.senior * prices.senior) + (entrant.child * prices.child);
+  return (entrant.adult * prices.adult) +
+    (entrant.senior * prices.senior) + (entrant.child * prices.child);
 }
-
 console.log(calculateEntry([
   { name: 'asda', age: 5 },
   { name: 'asda', age: 5 },
