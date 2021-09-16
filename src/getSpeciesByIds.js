b7c3731dceeee85925453ac01bc117d52d48107c
Ricardo Anjos <ricardoanj.14@gmail.com>
2021-09-15 22:42:44 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // iniciando
-}
+function getSpeciesByIds(...ids) {
+  if (ids.length === 0) return [];
 
+  const objects = ids.map((id) => data.species.find((animal) => animal.id === id));
+  return objects;
+}
 module.exports = getSpeciesByIds;
