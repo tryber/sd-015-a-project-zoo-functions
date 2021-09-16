27834ce641046842a95df419b03a04138cc3a30b
Emily Carvalho <nilsen.loli.emily@gmail.com>
2021-09-16 19:36:57 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const infoEmployees = employees.find((employe) => employe.id === id);
+  const animals = species.find((creature) => creature.id
+    === infoEmployees.responsibleFor[0]).residents;
+  const infoAnimal = animals.reduce((older, now) => ((now.age > older.age) ? now : older));
+  return [infoAnimal.name, infoAnimal.sex, infoAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
