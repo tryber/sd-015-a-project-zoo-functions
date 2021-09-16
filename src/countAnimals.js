c6a0e0dcdfd8e9539aa9f371de1d680b314299c7
Caio César <caiocesarmcruz@gmail.com>
2021-09-16 02:32:54 -0300

    requisito 6

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+const path = data.species;
+
+function countAnimals(...animal) {
+  if (animal.length === 0) {
+    return path.map((el) => `${el.name}: ${el.residents.length}`);
+  }
+  if (path.name.includes(animal)) {
+    return 'jurubeba';
+  }
 }
+
+console.log(countAnimals({ specie: 'penguins' }));
 
 module.exports = countAnimals;
