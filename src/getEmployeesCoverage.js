e72621e96febff070c14993621d5eaa6bd1c0632
Arthur <arthurteixeira0805@gmail.com>
2021-09-16 16:11:45 -0300

    testing

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,12 @@
+// Ref:
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/leonardo-felix-zoo-functions-project
+// https://github.com/tryber/sd-015-a-project-zoo-functions/tree/pedro-guarize-zoo-functions-project
+
 const data = require('../data/zoo_data');
 
 function getEmployeesCoverage() {
   // seu código aqui
+  
 }
 
 module.exports = getEmployeesCoverage;
