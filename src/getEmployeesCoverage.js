413b50d0575b769c76df4f7bce07fa626b15cd71
Arthur <arthurteixeira0805@gmail.com>
2021-09-16 20:29:27 -0300

    8/10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -3,9 +3,30 @@
 // https://github.com/tryber/sd-015-a-project-zoo-functions/tree/pedro-guarize-zoo-functions-project
 
 const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const structure = (employ) => (
+  {
+    id: employ.id,
+    fullName: `${employ.firstName} ${employ.lastName}`,
+    species: employ.responsibleFor.map((creature) =>
+      species.find((spec) => spec.id === creature).name),
+    locations: employ.responsibleFor.map((creature) =>
+      species.find((spec) => spec.id === creature).location),
+  }
+);
+
+const employeeAll = () => employees.map((employ) => (structure(employ)));
+const uniqueEmp = (obj) => structure(obj);
+
+const getEmployeesCoverage = (nameID) => {
   // seu código aqui
-}
+  if (nameID === undefined) return employeeAll();
+  const { name, id } = nameID;
+  const getEmp = employees.find((employ) => employ.firstName === name || employ.lastName === name
+  || employ.id === id);
+  if (getEmp === undefined) throw new Error('Informações inválidas');
+  return uniqueEmp(getEmp);
+};
 
 module.exports = getEmployeesCoverage;
