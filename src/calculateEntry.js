e19a508bae4793f41bb71485da5922948c9b9f3c
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-16 02:59:13 -0300

    requisito-6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { prices } = data;
+
 function countEntrants(entrants) {
-  // seu código aqui
+  return entrants.reduce((res, cont) => {
+    if (cont.age < 18) {
+      res.child += 1;
+    }
+    if (cont.age >= 18 && cont.age < 50) {
+      res.adult += 1;
+    }
+    if (cont.age >= 50) {
+      res.senior += 1;
+    }
+    return res;
+  }, { child: 0, adult: 0, senior: 0 });
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  const priceChild = count.child * prices.child;
+  const priceAdult = count.adult * prices.adult;
+  const priceSenior = count.senior * prices.senior;
+  return priceChild + priceAdult + priceSenior;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,7 @@ const data = require('../data/zoo_data');
 
 const { species } = data;
 
-function getSpeciesByIds(...ids) {
+function getSpeciesByIds(...ids) { // ... nos permite representar um número indefinido de argumentos como um array
   return species.filter((specie) => ids.includes(specie.id));
 }
 
