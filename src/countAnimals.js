27d7df513948eafd72e1831a5b996a94c37f2367
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-16 03:33:00 -0300

    requisito-5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  let { specie, gender } = '';
+  if (animal) {
+    ({ specie, gender } = animal);
+  } else {
+    return species.reduce((res, cur) => {
+      res[cur.name] = cur.residents.length;
+      return res;
+    }, {});
+  }
+  let findAnimals;
+
+  if (specie) findAnimals = species.find(({ name }) => name === specie);
+  if (gender) {
+    findAnimals = findAnimals.residents.filter(({ sex }) => sex === gender);
+    return findAnimals.length;
+  }
+
+  return findAnimals.residents.length;
 }
+
+console.log(countAnimals({ specie: 'bears', gender: 'female' }));
 
 module.exports = countAnimals;
