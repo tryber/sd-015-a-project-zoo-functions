31d62ef01510c4d7ea348fefdcc32a5d882f2db1
Leandro Basilio <leandroab07@gmail.com>
2021-09-17 03:35:19 -0300

    Adiciona parte da funcionalidade do requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,37 @@
 const data = require('../data/zoo_data');
 
+function getDay(day) {
+  const objDay = {};
+
+  if (data.hours[day].open === 0) {
+    objDay.officeHour = 'CLOSED';
+    objDay.exhibition = 'The zoo will be closed!';
+
+    return objDay;
+  }
+
+  const hours = data.hours[day];
+  const animals = data.species.filter((obj) => obj.availability.includes(day));
+
+  objDay.officeHour = `Open from ${hours.open}am until ${hours.close}pm`;
+  objDay.exhibition = animals.map((obj) => obj.name);
+
+  return objDay;
+}
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (scheduleTarget === undefined) {
+    const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+    const resultado = {};
+
+    days.map((obj) => {
+      resultado[obj] = getDay(obj);
+    });
+
+    return resultado;
+  }
 }
 
 module.exports = getSchedule;
