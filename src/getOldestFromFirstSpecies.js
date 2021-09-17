b6ad3a6f6557c764c6c453bf552deb8b647e01be
Fabiana Franco <fabianamfranco@outlook.com>
2021-09-17 19:49:24 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,33 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  let animaisPessoa = data.employees.map((pessoa) => {
-    if(id === pessoa.id) {
-      
-    }})
-  // os animais do id
-  //  .forEach em todos esses amimais
-  //  .find o mais velho
-  let maisVelho = data.species.find()
-  
+  let animaisPessoa = data.employees.find((pessoa) => pessoa.id === id).responsibleFor;
+  let oAnimal = data.species.find((animal) => animal.id === animaisPessoa[0]).residents;
+  let porIdade = oAnimal.sort((a, b) => b.age - a.age);
+  let oBicho = porIdade[0];
+  let resposta = [oBicho.name, oBicho.sex, oBicho.age];
+  return resposta;  
 }
 
 module.exports = getOldestFromFirstSpecies;
-
-// getOldestFromFirstSpecies(employeeId)
-// const stephanieId = '9e7d4524-363c-416a-8759-8aa7e50c0992';
-// const olaId = 'fdb2543b-5662-46a7-badc-93d960fdc0a8';
-// const burlId = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-
-// 'passado o id de um funcionário, encontra a primeira espécie de animal gerenciado pelo funcionário, e retorna um array com nome, sexo e idade do animal mais velho dessa espécie'
-
-// const expected = [
-//   ['Maxwell', 'male', 15],
-//   ['Maxwell', 'male', 15],
-//   ['Margherita', 'female', 10],
-//   ['Bill', 'male', 6],
-//   ['Margherita', 'female', 10],
-//   ['Margherita', 'female', 10],
-//   ['Shu', 'female', 19],
-//   ['Maxwell', 'male', 15],
-// ];
