9725c0fa90f10911448699bca20b25455f0cbfc7
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-09-17 16:03:26 -0300

    requisito 5

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,16 @@
 const data = require('../data/zoo_data');
 
-function isManager(id) {
-  // seu código aqui
-}
+const isManager = (id) => data.employees.some((is) => is.managers.includes(id));
 
 function getRelatedEmployees(managerId) {
   // seu código aqui
+  if (isManager(managerId)) {
+    const employeers = data.employees.filter((emp) => emp.managers.includes(managerId));
+    const relatedemployers = employeers
+      .map((related) => `${related.firstName} ${related.lastName}`);
+    return relatedemployers;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
