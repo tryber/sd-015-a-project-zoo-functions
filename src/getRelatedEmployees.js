03556f9e14bfe2a4e0a79ccda9ed6c541a380235
Bruno Miranda <brunompe@gmail.com>
2021-09-17 14:58:25 -0400

    requirement 04 - done

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+// const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Primeiro = Busco em data a key employees.
+// Segunda - Dentro de employees, busco dentro das keys managers se existe algum valor igual ao passado no parametro id.
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((person) => person.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const person = data.employees.filter((peps) => peps.managers.includes(managerId));
+  return person.map((peps) => `${peps.firstName} ${peps.lastName}`);
 }
 
 module.exports = { isManager, getRelatedEmployees };
