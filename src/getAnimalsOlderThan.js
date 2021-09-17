4fd45d14a4913fe6f4d3c4643032a68f7293769c
Bruno Miranda <brunompe@gmail.com>
2021-09-17 13:49:04 -0400

    requiremet 02 - done

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+// Primeiro busquei os residentes que tem o mesmo nome passado como parametro da função
+// depois busquei todos o elementos que atendiam a restrição de idade passado como parametro da função
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const { residents } = data.species.find((specie) => specie.name === animal);
+  // console.log(findAnimals);
+  const requiredAnimals = residents.every((subject) => subject.age > age);
+  return requiredAnimals;
 }
+
+getAnimalsOlderThan('bears', 4);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,7 @@
 const data = require('../data/zoo_data');
 
+// o Param Rest se dá para podermos colocar quantos IDs forem necessários
+// Primeiro eu filtrei os Dados por especie que contem o ID que foi colocado como parametro da função.
 function getSpeciesByIds(...ids) {
   return data.species.filter((species) => ids.includes(species.id));
 }
