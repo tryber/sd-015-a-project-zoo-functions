b89dc65639cb60d192c9a612636fcdc99fa69eac
André <andre.bonaf@gmail.com>
2021-09-17 11:33:23 -0300

    Requisito 7 AAAAAAAAAAAAA FINALMENTE

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,36 +1,70 @@
 const data = require('../data/zoo_data');
 
-const base = {
-  NE: [],
-  NW: [],
-  SE: [],
-  SW: [],
+const { species } = data;
+
+const localeAnimals = () => {
+  const animalsLocated = {};
+
+  animalsLocated.NE = (species.filter((allAnimals) => allAnimals.location === 'NE')
+    .map((animals) => animals.name));
+  animalsLocated.NW = (species.filter((allAnimals) => allAnimals.location === 'NW')
+    .map((animals) => animals.name));
+  animalsLocated.SE = (species.filter((allAnimals) => allAnimals.location === 'SE')
+    .map((animals) => animals.name));
+  animalsLocated.SW = (species.filter((allAnimals) => allAnimals.location === 'SW')
+    .map((animals) => animals.name));
+
+  return animalsLocated;
 };
 
-function addZones(zone1, zone2, zone3, zone4) {
-  const animalsPerZone1 = data.species.filter((animals) => animals.location === zone1);
-  animalsPerZone1.forEach((animal) => {
-    base[zone1].push(animal.name);
-  });
-  const animalsPerZone2 = data.species.filter((animals) => animals.location === zone2);
-  animalsPerZone2.forEach((animal) => {
-    base[zone2].push(animal.name);
-  });
-  const animalsPerZone3 = data.species.filter((animals) => animals.location === zone3);
-  animalsPerZone3.forEach((animal) => {
-    base[zone3].push(animal.name);
-  });
-  const animalsPerZone4 = data.species.filter((animals) => animals.location === zone4);
-  animalsPerZone4.forEach((animal) => {
-    base[zone4].push(animal.name);
-  });
-  return base;
-}
+const sortedAndSex = (options, animals) => {
+  const names = animals.residents.filter((sex) => sex.sex === options.sex)
+    .map((resident) => resident.name);
+  if (options.sorted) return names.sort();
+  return names;
+};
+
+const mapNames = (options, animals) => {
+  const names = animals.residents.map((resident) => resident.name);
+  if (Object.keys(options).length === 3) return sortedAndSex(options, animals);
+  if (options.sorted) return names.sort();
+  if (options.sex === 'male') {
+    return animals.residents.filter((sex) => sex.sex === 'male')
+      .map((resident) => resident.name);
+  }
+  if (options.sex === 'female') {
+    return animals.residents.filter((sex) => sex.sex === 'female')
+      .map((resident) => resident.name);
+  }
+  return names;
+};
+
+const animalsSorted = (options) => {
+  const animalsLocated = {};
+  animalsLocated.NE = (species.filter((allAnimals) => allAnimals.location === 'NE')
+    .map((animals) => ({
+      [animals.name]: mapNames(options, animals),
+    })));
+  animalsLocated.NW = (species.filter((allAnimals) => allAnimals.location === 'NW')
+    .map((animals) => ({
+      [animals.name]: mapNames(options, animals),
+    })));
+  animalsLocated.SE = (species.filter((allAnimals) => allAnimals.location === 'SE')
+    .map((animals) => ({
+      [animals.name]: mapNames(options, animals),
+    })));
+  animalsLocated.SW = (species.filter((allAnimals) => allAnimals.location === 'SW')
+    .map((animals) => ({
+      [animals.name]: mapNames(options, animals),
+    })));
+  return animalsLocated;
+};
 
 function getAnimalMap(options) {
-  if (options === undefined) return addZones('NE', 'NW', 'SE', 'SW');
+  if (!options || !options.includeNames) return localeAnimals();
+  if (options.includeNames) return animalsSorted(options);
 }
 
-console.log(getAnimalMap());
+// console.log(util.inspect(getAnimalMap({ includeNames: true, sex: 'female', sort: true }), false, null));
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+function mappedDays() {
+  const newObj = data.species.map((species) => {});
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (!scheduleTarget) return mappedDays;
 }
 
 module.exports = getSchedule;
