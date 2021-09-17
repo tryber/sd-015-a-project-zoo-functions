b0a36c92a8988ec4c7abcef32130609103116c84
André <andre.bonaf@gmail.com>
2021-09-17 11:36:05 -0300

    começo 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,11 +1,7 @@
 const data = require('../data/zoo_data');
 
-function mappedDays() {
-  //
-}
-
 function getSchedule(scheduleTarget) {
-  if (!scheduleTarget) return mappedDays;
+  //
 }
 
 module.exports = getSchedule;
