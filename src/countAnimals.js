eadc5aee8b8f70f5b91d8f4bf763bf40ecadac96
Anderson-Zobel <anderson.zobels@gmail.com>
2021-09-17 16:29:53 -0300

    requirement 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const allSpecies = {};
+    data.species.forEach((x) => {
+      allSpecies[x.name] = x.residents.length;
+    });
+    return allSpecies;
+  }
+  const { specie, sex } = animal;
+  if (!sex) {
+    return data.species.find((x) =>
+      x.name === specie).residents.length;
+  }
+  return data.species.find((x) =>
+    x.name === specie).residents.filter((x) =>
+    x.sex === sex).length;
 }
-
 module.exports = countAnimals;
