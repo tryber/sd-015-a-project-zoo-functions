a5027d282abd6659db73aa563bfb6b288afbd1f3
Regiane Santos <re.cfs235@gmail.com>
2021-09-17 01:13:15 -0300

    Requisito 5 - countAnimals corrigindoErroTestes

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -13,11 +13,11 @@ function countAnimals(animal) {
     // Sem parâmetros, retorna todas as espécies e suas quantidades.
   }
 
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
   const AnimalSpecie = species.find((elemento) => elemento.name === specie).residents;
   // AnimalSpecie é o array com todos os residentes da especie passada como parametro specie.
-  const AnimalGender = AnimalSpecie.reduce((acc, curr) => (curr.sex === gender ? acc + 1 : acc), 0);
-  return gender === undefined ? AnimalSpecie.length : AnimalGender;
+  const AnimalGender = AnimalSpecie.reduce((acc, curr) => (curr.sex === sex ? acc + 1 : acc), 0);
+  return sex === undefined ? AnimalSpecie.length : AnimalGender;
   // Quando o argumento é só a { specie: 'penguins' }, retorna o tamanho do array AnimalSpecie que é a quantidade de pinguins no zoológico;
   // Quando são duas chaves { specie: 'giraffes', sex: 'female' }, retorna a variavel AnimalGender que é a quantidade de girafas do gênero feminino.
 }
