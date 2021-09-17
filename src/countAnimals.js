df534eb978df096a1888363f882e3a1bdaa50555
Caio César <caiocesarmcruz@gmail.com>
2021-09-16 23:07:59 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,15 +2,19 @@ const data = require('../data/zoo_data');
 
 const path = data.species;
 
-function countAnimals(...animal) {
-  if (animal.length === 0) {
-    return path.map((el) => `${el.name}: ${el.residents.length}`);
+function countAnimals(animal) {
+  if (!animal) {
+    const red = (acc, cur) => ({ ...acc, [cur.name]: cur.residents.length });
+    return path.reduce(red, {});
+    // usei como inspiração função reduce de vídeo
+    // link: https://www.youtube.com/watch?v=cVx-nMahAjk
   }
-  if (path.name.includes(animal)) {
-    return 'jurubeba';
-  }
+  const findAnimal = path.find((el) => el.name === animal.specie);
+  if (findAnimal && animal.sex === undefined) {
+    return findAnimal.residents.length;
+  } return findAnimal.residents.filter((el) => el.sex === animal.sex).length;
 }
 
-console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals());
 
 module.exports = countAnimals;
