a88f114bb83d6959862ed471254da88329de51a1
carla <carlabazevedoa@gmail.com>
2021-09-16 21:49:32 -0300

    calculateEntry ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const adults = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const seniors = entrants.filter((entrant) => entrant.age >= 50);
+
+  return { child: child.length, adult: adults.length, senior: seniors.length };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const countEntrant = countEntrants(entrants);
+  return Object.keys(countEntrant).reduce((priceTotal, key) =>
+    priceTotal + countEntrant[key] * data.prices[key], 0);
 }
 
 module.exports = { calculateEntry, countEntrants };
