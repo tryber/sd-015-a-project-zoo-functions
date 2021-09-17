0db43e7faa6265ccd8b877d5822f08dffe0dcf1d
arturbacilla <artur.bacilla1@gmail.com>
2021-09-17 12:15:27 -0300

    Requisite 5 - Count animals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,22 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal !== undefined) {
+    const { specie, sex } = animal;
+    const specieResidents = species.find((whichAnimal) =>
+      whichAnimal.name === specie).residents;
+    console.log(specieResidents);
+    return sex === undefined
+      ? specieResidents.length
+      : specieResidents.filter((whatAnimals) => whatAnimals.sex === sex).length;
+  }
+  const result = {};
+  species.forEach((key) => {
+    result[key.name] = key.residents.length;
+  });
+  return result;
 }
 
 module.exports = countAnimals;
