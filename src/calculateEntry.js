643f9d6204b5102eb97b846e3a54dc0d734308cd
Thiago Lordello <thiagolordello@hotmail.com>
2021-09-17 18:55:06 -0300

    6a questao

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -10,14 +10,22 @@ function countEntrants(entrants) {
     if (element.age >= 18 && element.age < 50) { countAdult += 1; }
 
     if (element.age >= 50) countSenior += 1;
-    return 0;
   });
+  // console.log({ child: countChild, adult: countAdult, senior: countSenior });
   return { child: countChild, adult: countAdult, senior: countSenior };
 }
 
 function calculateEntry(entrants) {
-  if (!entrants) return 0;
-  if (entrants.args === undefined) return 0;
+  if (entrants === undefined) return 0;
+  if (entrants.length === undefined) return 0;
+  const pessoas = countEntrants(entrants); // Aqui está o pulo do gato. Recebe os valores de pessoas calculado acima.
+  const { child, adult, senior } = pessoas;
+  const totChild = child * data.prices.child;
+  const totAdult = adult * data.prices.adult;
+  const totSenior = senior * data.prices.senior;
+  const totalEntries = totAdult + totChild + totSenior;
+  console.log(totalEntries);
+  return totalEntries;
 }
 const entrants = [
   { name: 'Lara Carvalho', age: 5 },
@@ -28,6 +36,5 @@ const entrants = [
   { name: 'Carlos Nogueira', age: 50 },
 ];
 countEntrants(entrants);
-
 calculateEntry(entrants);
 module.exports = { calculateEntry, countEntrants };
