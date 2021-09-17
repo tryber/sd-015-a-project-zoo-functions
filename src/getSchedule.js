b3f3dfd8eae240309cfe67f0d2e5b61fd849c84d
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-17 11:21:37 -0300

    lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -9,7 +9,7 @@ function daySchedule(chosenDay) {
     return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
   }
 
-  let exhibitionAnimals = [];
+  const exhibitionAnimals = [];
   data.species.forEach((specie) => {
     if (specie.availability.includes(chosenDay)) {
       exhibitionAnimals.push(specie.name);
@@ -22,29 +22,30 @@ function daySchedule(chosenDay) {
   };
 }
 
-function getSchedule(scheduleTarget) {
-  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-  const animals = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'];
-  const allDaysSchedule = {
-    Monday: daySchedule('Monday'),
-    Tuesday: daySchedule('Tuesday'),
-    Wednesday: daySchedule('Wednesday'),
-    Thursday: daySchedule('Thursday'),
-    Friday: daySchedule('Friday'),
-    Saturday: daySchedule('Saturday'),
-    Sunday: daySchedule('Sunday'),
-  };
+const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+// eslint-disable-next-line max-len
+const animals = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'];
+const allDaysSchedule = {
+  Monday: daySchedule('Monday'),
+  Tuesday: daySchedule('Tuesday'),
+  Wednesday: daySchedule('Wednesday'),
+  Thursday: daySchedule('Thursday'),
+  Friday: daySchedule('Friday'),
+  Saturday: daySchedule('Saturday'),
+  Sunday: daySchedule('Sunday'),
+};
 
+function getSchedule(scheduleTarget) {
   if (!scheduleTarget) {
     return allDaysSchedule;
-  } else if (!(weekDays.includes(scheduleTarget) || animals.includes(scheduleTarget))) {
+  } if (!(weekDays.includes(scheduleTarget) || animals.includes(scheduleTarget))) {
     return allDaysSchedule;
-  } else if(animals.includes(scheduleTarget)) {
+  } if (animals.includes(scheduleTarget)) {
     return animalSchedule(scheduleTarget);
-  } 
+  }
   return {
     [scheduleTarget]: daySchedule(scheduleTarget),
-  }
+  };
 }
 
 module.exports = getSchedule;
