7d386668844a56dfc6a69b27d913cbc68e93f9a5
Leandro Basilio <leandroab07@gmail.com>
2021-09-17 08:36:24 -0300

    Corrige ESlint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -2,14 +2,6 @@ const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
-  const NE = data.species.filter((obj) => obj.location === 'NE');
-  const NW = data.species.filter((obj) => obj.location === 'NW');
-  const SE = data.species.filter((obj) => obj.location === 'SE');
-  const SW = data.species.filter((obj) => obj.location === 'SW');
-
-  const resultado = NE.forEach((obj, index) => obj[index]);
-
-  return resultado;
 }
 
 console.log(getAnimalMap());
