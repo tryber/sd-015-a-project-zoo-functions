15cb76c0d713d06c171fcee744454e8021b9712c
Jonas Candia <jonas57.jc@gmail.com>
2021-09-17 15:17:31 -0300

    tentando primeiro requisito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  const findId = data.species.find((i) => ids.includes(i.id));
+  return findId;
 }
 
 module.exports = getSpeciesByIds;
