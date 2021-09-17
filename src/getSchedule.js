cc8a926e97bd52a507422575a5f25b48a5b804d3
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-17 11:09:04 -0300

    requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const weekDays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+  const animals = ['lions', 'tigers', 'bears', 'penguins', 'otters', 'frogs', 'snakes', 'elephants', 'giraffes'];
+  const allDaysSchedule = {
+    Monday: daySchedule('Monday'),
+    Tuesday: daySchedule('Tuesday'),
+    Wednesday: daySchedule('Wednesday'),
+    Thursday: daySchedule('Thursday'),
+    Friday: daySchedule('Friday'),
+    Saturday: daySchedule('Saturday'),
+    Sunday: daySchedule('Sunday'),
+  };
+
+  if (!scheduleTarget) {
+    return allDaysSchedule
+  } else if (!(weekDays.includes(scheduleTarget) || animals.includes(scheduleTarget))) {
+    return allDaysSchedule
+  } else if(animals.includes(scheduleTarget)) {
+    return animalSchedule(scheduleTarget)
+  } 
+  return {
+    [scheduleTarget]: daySchedule(scheduleTarget),
+  }
+}
+
+function animalSchedule(animalName) {
+  return data.species.find((specie) => specie.name === animalName).availability;
+}
+
+function daySchedule(chosenDay) {
+  if (chosenDay === 'Monday') {
+    return { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+  }
+
+  let exhibitionAnimals = [];
+  data.species.forEach((specie) => {
+    if (specie.availability.includes(chosenDay)) {
+      exhibitionAnimals.push(specie.name);
+    }
+  });
+
+  return {
+      officeHour: `Open from ${data.hours[chosenDay].open}am until ${data.hours[chosenDay].close}pm`,
+      exhibition: exhibitionAnimals,
+    };
 }
 
 module.exports = getSchedule;
