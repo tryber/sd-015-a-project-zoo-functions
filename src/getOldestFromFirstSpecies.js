17f3590250ace1babee8549d82ee603f24c6f360
Davio Lopes <davioplp@gmail.com>
2021-09-17 02:52:27 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -7,16 +7,10 @@ const responsibleFor = (id) =>
     .map((iten) => iten.responsibleFor[0]);
 
 const oldest = (id) => {
-  const allAnimals =species.find((iten) => iten.id === responsibleFor(id)[0])
+  const allAnimals = species.find((iten) => iten.id === responsibleFor(id)[0])
     .residents.sort((a, b) => b.age - a.age);
-  return [allAnimals[0].name, allAnimals[0].sex, allAnimals[0].age];
-}
+  return [allAnimals[0].name, allAnimals[0].sex, allAnimals[0].age];}
 
 console.log(oldest('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = oldest;
-
-
-
-
-
