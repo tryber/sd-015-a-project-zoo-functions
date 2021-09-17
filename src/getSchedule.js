02b3fdf2cb0eb1a6aaa64ba65ce810d065246cec
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-17 00:15:42 -0300

    requisito 08

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,23 +1,50 @@
-// const { hours, species } = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
 
-// function openZoo() {
-//   if (!scheduleTarget) {
-//     return Object.keys(hours).reduce((acumulador, currenteValue) => Object.assign(acumulador, {
-//       [currenteValue]: {
-//         officeHour:
-//           `Open from ${hours[currenteValue].open}am until ${hours[currenteValue].close}pm`,
-//         exhibition: species.filter((animals) =>
-//           animals.availability.includes(currenteValue)).map((nameAnimals) =>
-//           nameAnimals.name),
-//       },
-//     }, moday()), {});
-//   }
-// };
+const exibitionAnimal = (currenteValue) => {
+  let resposta;
+  if (currenteValue === 'Monday') {
+    resposta = 'The zoo will be closed!';
+  } else {
+    resposta = species.filter((animals) =>
+      animals.availability.includes(currenteValue)).map((nameAnimals) =>
+      nameAnimals.name);
+  }
+  return resposta;
+};
 
-// const monday = () => ({ monday: {officeHour: 'CLOSED', exhibition: 'The zoo will be closed!'}});
+const officeHour = (currenteValue) => {
+  let resposta;
+  if (currenteValue === 'Monday') {
+    resposta = 'CLOSED';
+  } else {
+    resposta = `Open from ${hours[currenteValue].open}am until ${hours[currenteValue].close}pm`;
+  }
+  return resposta;
+};
+
+function scheduleZoo() {
+  const result = Object.keys(hours).reduce((acc, currenteValue) => {
+    acc[currenteValue] = { officeHour: officeHour(currenteValue),
+      exhibition: exibitionAnimal(currenteValue) };
+    return acc;
+  }, {});
+  return result;
+}
+
+const calender = (params) => species.find((elemento) => elemento.name === params).availability;
 
 function getSchedule(scheduleTarget) {
-  //
+  const day = Object.keys(hours);
+  const animals = species.map((element) => element.name);
+  if (!scheduleTarget || (!day.includes(scheduleTarget) && !animals.includes(scheduleTarget))) {
+    return scheduleZoo();
+  }
+  if (day.includes(scheduleTarget)) {
+    const resposta = {};
+    resposta[scheduleTarget] = scheduleZoo()[scheduleTarget];
+    return resposta;
+  }
+  return calender(scheduleTarget);
 }
-console.log(getSchedule());
+
 module.exports = getSchedule;
