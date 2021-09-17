896c90e35d8ab27dad48cef61ae3da31635f0fcc
Bruno Miranda <brunompe@gmail.com>
2021-09-17 17:03:27 -0400

    requirement 06 - done

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,34 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      child += 1;
+    }
+    if (element.age >= 18 && element.age < 50) {
+      adult += 1;
+    }
+    if (element.age >= 50) {
+      senior += 1;
+    }
+  });
+  return { adult, child, senior };
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  let { child, adult, senior } = countEntrants(entrants);
+  const { prices } = data;
+  child *= prices.child;
+  adult *= prices.adult;
+  senior *= prices.senior;
+  const total = (child + adult + senior);
+  return total;
 }
 
 module.exports = { calculateEntry, countEntrants };
