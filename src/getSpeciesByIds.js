477542e9152f4f900c2a8f834d5e9e8f8ea3af32
devgeorge <glalmeida.dev@gmail.com>
2021-09-17 14:52:55 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,6 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-// seu código aqui  
+function getSpeciesByIds(...ids) {
+  return data.species.filter((specie) => ids.includes(specie.id));
 }
-console.log('vamos com tudo');
 module.exports = getSpeciesByIds;
