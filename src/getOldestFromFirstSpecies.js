fc8beae94012727c5481fabf2c94a9aa6a4f3d90
Igor Campos Moreira <igorcampos200@gmail.com>
2021-09-17 00:06:50 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,28 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const maxAge = (animal) => {
+  let maAge = 0;
+  let animalSelected = {};
+  const retorno = [];
+  animal.filter((animals) => {
+    if (animals.age > maAge) {
+      maAge = animals.age;
+      animalSelected = animals;
+    }
+    return animalSelected;
+  });
+  retorno.push(animalSelected.name);
+  retorno.push(animalSelected.sex);
+  retorno.push(animalSelected.age);
+  return retorno;
+};
+const getSpecie = (id) => {
+  const animal = data.species.find((search) => search.id === id);
+  return maxAge(animal.residents);
+};
+const getOldestFromFirstSpecies = (id) => {
+  const employe = data.employees.find((search) => search.id === id);
+  return getSpecie(employe.responsibleFor[0]);
+};
 
 module.exports = getOldestFromFirstSpecies;
