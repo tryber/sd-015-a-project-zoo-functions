a8a80987dadb79bb3052f123578935aafaee75a2
Bruno Miranda <brunompe@gmail.com>
2021-09-17 13:56:13 -0400

    requirement 03 - done

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
+// Primeiro - Teste se foi passado alguma coisa como parametro da função!
+// Segundo - Se foi passado algo no parametro, busca nos dados a primeira key que seja igual ao firstName ou lastName;
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const person = data.employees.find((element) =>
+    element.firstName === employeeName || element.lastName === employeeName);
+  return person;
 }
 
 module.exports = getEmployeeByName;
