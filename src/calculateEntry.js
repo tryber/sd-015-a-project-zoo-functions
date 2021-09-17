782547f16febd1bdc5c803b67b26328ebf3ed915
arturbacilla <artur.bacilla1@gmail.com>
2021-09-17 17:48:14 -0300

    Requisite 6: Count entrants

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  return {
+    child: entrants.filter((person) => person.age < 18).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    senior: entrants.filter((person) => person.age >= 50).length,
+  };
 }
 
 function calculateEntry(entrants) {
