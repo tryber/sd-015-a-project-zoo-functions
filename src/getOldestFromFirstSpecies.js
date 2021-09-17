733b09ed7bb4a12e88f527093d290a260e0912c5
Leonardo Ramos <oliveira.leonardo3004@gmail.com>
2021-09-16 22:27:54 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const filteredEmployee = data.employees.find((employee) =>
+    employee.id === id);
+
+  const filteredSpecie = data.species.find((animals) =>
+    animals.id === filteredEmployee.responsibleFor[0]);
+
+  const ageOfOldestAnimal = filteredSpecie.residents.map((animal) =>
+    animal.age).sort((a, b) => b - a )[0];
+
+  const oldestAnimal = filteredSpecie.residents.find((animal) =>
+    animal.age === ageOfOldestAnimal);
+
+  return [oldestAnimal.name, oldestAnimal.sex, ageOfOldestAnimal];
 }
 
 module.exports = getOldestFromFirstSpecies;
