e301c0b731982729a8fa2b7b01a52b34708f22ac
Emerson Oliveira <emerson.contato@gmail.com>
2021-09-17 13:43:01 -0300

    Requisito 9 inserido

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,6 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const categoryLocation = {
+  NE: species.filter((animals) => animals.location === 'NE').map((specie) => specie.name),
+  NW: species.filter((animals) => animals.location === 'NW').map((specie) => specie.name),
+  SE: species.filter((animals) => animals.location === 'SE').map((specie) => specie.name),
+  SW: species.filter((animals) => animals.location === 'SW').map((specie) => specie.name),
+};
+
 function getAnimalMap(options) {
+  if (!options) return categoryLocation;
 
 }
 
