82999ad068af90bc6461e60603a3107915093e3c
devgeorge <glalmeida.dev@gmail.com>
2021-09-17 15:14:05 -0300

    Requisito 2

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { residents } = data.species.find((animalSpecie) => animalSpecie.name === animal);
+  return residents.every((resident) => resident.age > age);
 }
 
 module.exports = getOldestFromFirstSpecies;
