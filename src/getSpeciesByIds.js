55b784fb2e3043f1c06f9fda14ce1ed38d8b8d8c
antoniofortisneto <antoniofortis@gmail.com>
2021-09-18 05:39:21 -0300

    primeiro exercicio depois de uma noite de revisao hehe

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  return species.filter((identification) => ids.includes(identification.id));
 }
 
 module.exports = getSpeciesByIds;
+/* o filter retorna vazio se nao receber parametro */
