d325dd140f4ac6f75b9f7366f5bb8d4fcbb4fd6f
Monte <you@example.com>
2021-09-18 14:33:26 -0300

    requisito 5 teste2 lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -14,5 +14,6 @@ function countAnimals(animal) {
   const sexCounter = animalSpecie.reduce((acc, curr) => (curr.sex === sex ? acc + 1 : acc), 0);
   return sex === undefined ? animalSpecie.length : sexCounter;
 }
+// função encontrada na pull da colega emanuelle, não estava conseguindo consertar o problema de não rodar no avalidor remoto, decidi colocar uma função de um colega n pra ver se roda
 
 module.exports = countAnimals;
