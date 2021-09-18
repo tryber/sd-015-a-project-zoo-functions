9a47592972ef0a6b36044c9f4d0d9f42ce657911
Daniel Tavares <daniel1tavares@outlook.com>
2021-09-18 09:38:52 -0300

    requisito 5.1 correçoes

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 function countAnimals(animal) {
   if (animal === undefined) {
     // src: https://github.com/tryber/sd-015-a-project-zoo-functions/pull/9/commits/cf2a2fcf63d54ea9fc19837bb7eb857f0f1a8b9b
-    // precisava ver como retornar um array de forma correta
+    // precisava ver como retornar um object de forma correta
     const allAnimal = {};
     data.species.forEach((specie) => {
       allAnimal[specie.name] = specie.residents.length;
@@ -21,5 +21,3 @@ function countAnimals(animal) {
 }
 
 module.exports = countAnimals;
-
-console.log(countAnimals({ specie: 'bears', sex: 'female' }));
