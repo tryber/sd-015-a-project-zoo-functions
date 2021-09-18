724248b339e48170e234317882e7f054d45af3d6
devgeorge <glalmeida.dev@gmail.com>
2021-09-18 11:19:19 -0300

    Consertando erros

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,8 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const { residents } = data.species.find((animalSpecie) => animalSpecie.name === animal);
-  return residents.every((resident) => resident.age > age);
+  
 }
 
 module.exports = getOldestFromFirstSpecies;
