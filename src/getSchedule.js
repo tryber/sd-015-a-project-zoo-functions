995deef1789e8370371f6f63dad67948db951bcc
Monte <you@example.com>
2021-09-18 18:21:02 -0300

    requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,42 +1,56 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
-  function horarios(abre, fecha) {
-    return abre === fecha && abre === 0 ? 'CLOSED' : `Open from ${abre}am until ${fecha}pm`;
-  }
+function hoursDay(open, close) {
+  return open === close && open === 0 ? 'CLOSED' : `Open from ${open}am until ${close}pm`;
+}
+// a funcao hoursDay retorna se estadara aberto e o horario dado os parametros
 
-  function acharAnimais(dia) {
-    return data.species.filter((animal) => animal.availability.includes(dia)).map((bixo) => {
-      const retornoMap = bixo.name;
-      return retornoMap;
-    });
-  }
+function animalOfTheDay(dia) {
+  return data.species.filter((element) => element.availability.includes(dia))
+    // passo um filter em species para verificar quais elementos estao disponiveis no dia passado
+    .map((element2) => element2.name);
+  // .map cria uma array com o nome dos animais q estao disponiveis no dia passado como parametro
+}
+// a funcao acima retorna o nome dos animais q estao disponives para o dia passado como parametro
 
-  const animaisExibidos = (dia) => (dia !== 'Monday' ? acharAnimais(dia) : 'The zoo will be closed!');
+function availableDay(dia) {
+  return (dia !== 'Monday' ? animalOfTheDay(dia) : 'The zoo will be closed!');
+}
+// chama a funcao que virifica o dia passado e retorna se estara fechado
 
-  function contruirCalendario(...day) {
-    const resposta = {};
-    day.forEach((dia) => {
-      resposta[dia] = {
-        officeHour: horarios(data.hours[dia].open, data.hours[dia].close),
-        exhibition: animaisExibidos(dia),
-      };
-    });
-    return resposta;
+function fullSchedule(...day) {
+  const retorno = {};
+  day.forEach((dia) => {
+    retorno[dia] = {
+      // coloco a chave '[dia]' para ser criada a cada elemento do day
+      officeHour: hoursDay(data.hours[dia].open, data.hours[dia].close),
+      // chamo a funcao hoursday com o parametro da hora do dia correspondente do elemento
+      exhibition: availableDay(dia),
+      // chamo a funcao para verificar qual animal esta disponivel no dia (day)
+    };
+  });
+  return retorno;
+}
+// nessa funcao imprimi a schedule padrao da semana
+
+function getSchedule(param) {
+  let retorno = {};
+  const daysOfWeek = Object.keys(data.hours);
+  // crio uma array para armazenar os dias
+  const animals = data.species.map((element) => element.name);
+  // crio outra array para armazenar todas espcies
+  if (param === undefined || (!daysOfWeek.includes(param) && !animals.includes(param))) {
+    retorno = fullSchedule(...daysOfWeek);
+    // se o valor for nulo ou nao incluir um dos dias da semana e nao incluir nome de animar ele retorna o calentario completo
+    // e chama a funcao de monstar calendario com o parametro do dia da semana
+  } else if (daysOfWeek.includes(param)) {
+    retorno = fullSchedule(param);
+    // se o parametro possuir um dos dias da semana ele chama a funcao de criar o calendario com o parametro passado
+  } else {
+    retorno = data.species.find((element) => element.name === param).availability;
+    // caso passe o nome de um animal, retorna apenas o array com os dias das semana que esse animal esta disponivel
   }
-  function disponibleDay(dayName) {
-    let resposta = {};
-    const diasDaSemana = Object.keys(data.hours);
-    const animais = data.species.map((animal) => animal.name);
-    if (dayName === undefined || (!diasDaSemana.includes(dayName) && !animais.includes(dayName))) {
-      resposta = contruirCalendario(...diasDaSemana);
-    } else if (diasDaSemana.includes(dayName)) {
-      resposta = contruirCalendario(dayName);
-    } else {
-      resposta = data.species.find((bixo) => bixo.name === dayName).availability;
-    }
-    return resposta;
-  }
+  return retorno;
 }
 
 module.exports = getSchedule;
