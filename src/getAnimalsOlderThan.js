a811cdc67dc646ea4eaa414ab5c1ef3c272e6ac3
MoisésFigueira <moisesmichael@gmail.com>
2021-09-18 12:03:08 -0300

    Lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,8 @@ const data = require('../data/zoo_data');
 
 // www.youtube.com/watch?v=szXPNFjVKzI&t=125s, mentoria e git Pedro Guarize, every couse.
 
-const getAnimalsOlderThan = (animal, age) => species.find((nameAnimal) => nameAnimal.name === animal).residents
+const getAnimalsOlderThan = (animal, age) => species
+  .find((nameAnimal) => nameAnimal.name === animal).residents
   .every((veryAge) => veryAge.age > age);
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (...item) => species.filter(({ id }) => item.includes (id));
+// www.youtube.com/watch?v=bdLkAm12pgI, mentorias e Git Pedro Guariza
+
+const getSpeciesByIds = (...item) => species.filter(({ id }) => item.includes(id));
 
 module.exports = getSpeciesByIds;
-
-// https://www.youtube.com/watch?v=bdLkAm12pgI
-//mentorias, Git Pedro Guariza
