d8b52645a905ac85d73ca2e5c39fba3ce1a4fc3a
Thiago <thiivieira@gmail.com>
2021-09-18 16:59:44 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const speciesId = (epeopleW) => data.species
+  .filter((element) => epeopleW.responsibleFor
+    .includes(element.id));
+
+const objPeople = data.employees.map((e) => ({
+  id: e.id,
+  fullName: `${e.firstName} ${e.lastName}`,
+  species: speciesId(e).map((animal) => animal.name),
+  locations: speciesId(e).map((local) => local.location),
+}));
+
+const pegarEmployee = ({ name, id }) => data.employees
+  .find((e) => e.id === id || e.firstName === name || e.lastName === name);
+
+function getEmployeesCoverage(param) {
+  if (!param) {
+    return objPeople;
+  }
+  const employes = pegarEmployee(param);
+  if (!employes) throw new Error('Informações inválidas');
+  return objPeople.find((e) => e.id === employes.id);
 }
 
 module.exports = getEmployeesCoverage;
