a18bc5c1150ac4f535a22d557c8eb118e5f0ce75
Monte <you@example.com>
2021-09-18 14:28:33 -0300

    requisito 5 teste2

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,18 +2,17 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if (animal === undefined) {
-    const allAnimals = {};
-    species.forEach((creature) => { allAnimals[creature.name] = creature.residents.length; });
+  if (typeof animal === 'undefined') {
+    const allAnimals = { };
+    species.forEach((targetAnimal) => {
+      allAnimals[targetAnimal.name] = targetAnimal.residents.length;
+    });
     return allAnimals;
   }
-  const { specie, gender } = animal;
-  const findAnimals = species.find((creature) => creature.name === specie);
-  const population = findAnimals.residents.length;
-  const findGender = findAnimals.residents.filter((creature) => creature.sex === gender);
-  const popGender = findGender.length;
-
-  return gender === undefined ? population : popGender;
+  const { specie, sex } = animal;
+  const animalSpecie = species.find((tgAnimal) => tgAnimal.name === specie).residents;
+  const sexCounter = animalSpecie.reduce((acc, curr) => (curr.sex === sex ? acc + 1 : acc), 0);
+  return sex === undefined ? animalSpecie.length : sexCounter;
 }
-// testando o avaliador
+
 module.exports = countAnimals;
