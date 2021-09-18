770dc0f97821dbd8d74de474ce7174e9e696c764
Monte <you@example.com>
2021-09-18 11:41:17 -0300

    requisito 2 lint

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   return data.species.find((animalObj) =>
-  animalObj.name === animal)
-  .residents.every((resident) => resident.age > age);
+    animalObj.name === animal)
+    .residents.every((resident) => resident.age > age);
 }
 
 module.exports = getAnimalsOlderThan;
