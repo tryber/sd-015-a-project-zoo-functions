a418cd0ab66778889f047759c978626453b91045
Monte <you@example.com>
2021-09-17 23:50:39 -0300

    requisito 1 lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,6 +2,6 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   return ids.map((id) => data.species.find((specie) => specie.id === id));
-};
+}
 
 module.exports = getSpeciesByIds;
