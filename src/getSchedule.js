cf5fd205d133adb2a05a3eeba758642eff0fbfa6
Anderson Alencar <anderson23alencar@gmail.com>
2021-09-18 16:22:42 -0300

    Requisito 8 - getSchedule

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,34 @@
-const data = require('../data/zoo_data');
+const { hours, species } = require('../data/zoo_data');
+
+const days = Object.keys(hours);
+
+const getAllDays = () => {
+  // const days = Object.keys(hours);
+
+  const allDays = days.reduce((acc, day) => {
+    acc[day] = {
+      officeHour: `Open from ${hours[day].open}am until ${hours[day].close}pm`,
+      exhibition: species.filter((spe) => spe.availability.includes(day)).map((ani) => ani.name),
+    };
+    acc.Monday = { officeHour: 'CLOSED', exhibition: 'The zoo will be closed!' };
+    return acc;
+  }, {});
+
+  return allDays;
+};
+
+const getChosenDay = (day) => ({ [day]: getAllDays()[day] });
+
+const daysExhibOfSingleAnimal = (animal) => (species
+  .find((spe) => spe.name === animal).availability);
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const nameAnimals = species.map((spe) => spe.name);
+
+  if (days.includes(scheduleTarget)) return getChosenDay(scheduleTarget);
+  if (nameAnimals.includes(scheduleTarget)) return daysExhibOfSingleAnimal(scheduleTarget);
+
+  return getAllDays();
 }
 
 module.exports = getSchedule;
