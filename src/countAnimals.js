5147d5d8095bfae713574bc647b80b4262b13ed0
ZekaEu <zeka.surf@hotmail.com>
2021-09-18 20:01:41 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,14 @@
-const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  const animals = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => { animals[name] = residents.length; });
+    return animals;
+  }
+  const specieTotal = species.find((specie) => specie.name === animal.specie).residents;
+  if (animal.gender) return specieTotal.filter((specie) => specie.sex === animal.gender).length;
+  return specieTotal.length;
 }
 
 module.exports = countAnimals;
