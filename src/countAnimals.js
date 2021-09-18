7e000c5228ca3fb6bb26edcc37255d77a6031c4b
João V. Dias <diasvictorj@gmail.com>
2021-09-18 14:43:35 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,25 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const objectToReturn = {};
+    species.forEach(element => {
+      const animalName = element.name;
+      const numberAnimals = element.residents.length;
+      objectToReturn[`${animalName}`] = numberAnimals;
+    });
+    return objectToReturn;
+  }
+  const { specie, sex } = animal;
+  const findAnimal = species.find((element) => element.name === specie);
+  if (sex) {
+    return findAnimal['residents'].filter((element) => element.sex === sex).length;
+  }
+  else {
+    return findAnimal.residents.length;
+  }
 }
-
+ /* console.log(countAnimals({ specie: 'bears', sex: 'female' }));  */
 module.exports = countAnimals;
