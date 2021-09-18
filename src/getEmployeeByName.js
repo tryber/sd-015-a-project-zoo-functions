4697138252d5fbaeede1f44c77198282b04842de
Monte <you@example.com>
2021-09-18 12:04:15 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) return {};
+  return data.employees.find((fullName) => fullName.firstName === employeeName
+  || fullName.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
