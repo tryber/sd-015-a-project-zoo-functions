251d054975061e0f567e58140340618df6454b31
Omar Merljak <omerljak@gmail.com>
2021-09-17 22:22:33 -0300

    requisito getRelatedEmployess

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -3,10 +3,10 @@ const data = require('../data/zoo_data');
 const getSpeciesByIds = require('./getSpeciesByIds');
 
 function getOldestFromFirstSpecies(id) {
-  const employee = employees.find((funcionario) => funcionario.id === id);
-  const animal = getSpeciesByIds(employee.responsibleFor[0]);
+  const employee = employees.find((funcionario) => funcionario.id === id);// busca o primeiro funcionário que satifaz
+  const animal = getSpeciesByIds(employee.responsibleFor[0]);// chama função utilizada no requisito 1 com o parametro employee.responsibleFor em sua primeira posição(ajuda do colega FATnando)
 
-  return Object.values(animal[0].residents.sort((a, b) => b.age - a.age)[0]);
+  return Object.values(animal[0].residents.sort((a, b) => b.age - a.age)[0]);// retorna os valores do objeto que recebe o resultado trazido pela const animal de forma organizada(sort)
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const employeeNames = data.employees.reduce((acc, curr) => acc.concat(curr.managers), []);// armazena qual funcionario tem FUNÇÃO manager
+  return employeeNames.some((managerId) => managerId === id);// retorna se algum funcionário recebe a função de gerenciar algum animal
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === true) { // se isManager recebe manegerId for verdadeiro
+    return data.employees.reduce((acc, curr) => { // retorna o funcionário que satisfaz
+      if (curr.managers.includes(managerId) === true) { // se o funcionário corrente tiver incluso em suas funções 'managerId'
+        const fullName = `${curr.firstName} ${curr.lastName}`;// const faz a concatenação para formar o nome completo
+        return acc.concat(fullName);// retorna o funcionário atual pelo seu nome completo
+      }
+      return acc;// retorna um funcionário ..mas não satisfaz a condição
+    }, []);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');// emite msg de erro quando não satisfaz a condição
 }
-
+// com dificuldade de desenvolver o código..o raciocínio é entendido.. código baseado no da colega Tamyres
 module.exports = { isManager, getRelatedEmployees };
