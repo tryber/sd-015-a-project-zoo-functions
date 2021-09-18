dc9e57e7f4fa80e8d44c902f6daa9967e6311b89
devgeorge <glalmeida.dev@gmail.com>
2021-09-18 11:14:08 -0300

    Requisito 2 certo

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,7 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const { residents } = data.species.find((especieAnimal) => especieAnimal.name === animal);
+  const { residents } = data.species.find((Specie) => Specie.name === animal);
   return residents.every((resident) => resident.age > age);
 }
 
