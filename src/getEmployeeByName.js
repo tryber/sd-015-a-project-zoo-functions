8f22fea9f5cc018b40b922c13ffc7d404c8d3e5b
MoisésFigueira <moisesmichael@gmail.com>
2021-09-18 13:03:35 -0300

    lint

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,10 +1,11 @@
 const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  if (!employeeName)
-    return {};
-  return employees.find((byName) => 
+//www.youtube.com/playlist?list=PLYgzkrmJnLwo-pTURSTti_ljhjWB9BF7- , mentoria git Leo e pedro
+
+function getEmployeeByName(employeeName) {
+  if (!employeeName) return {};
+  return employees.find((byName) =>
   byName.firstName === employeeName || byName.lastName === employeeName);
 }
 module.exports = getEmployeeByName;
