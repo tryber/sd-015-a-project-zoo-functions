87c0055734b4796d16cd7ac46f65df3834ecd6b3
MoisésFigueira <moisesmichael@gmail.com>
2021-09-18 11:56:59 -0300

    Exercises 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// www.youtube.com/watch?v=szXPNFjVKzI&t=125s, mentoria e git Pedro Guarize, every couse.
+
+const getAnimalsOlderThan = (animal, age) => species.find((nameAnimal) => nameAnimal.name === animal).residents
+  .every((veryAge) => veryAge.age > age);
 
 module.exports = getAnimalsOlderThan;
