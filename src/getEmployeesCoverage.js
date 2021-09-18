33a364435c305a5cd9ed97e940f15ce2f4b1f03c
Apolo wilker <wilkerapolocarvalho@gmail.com>
2021-09-18 17:02:00 -0300

    Requisito 10 / 4

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,43 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function locationAndNamesAnimals(idAnimaisValidos) {
+  // id animal
+  return species.reduce((listNames, animal) => {
+    if (idAnimaisValidos.includes(animal.id)) {
+      listNames.listAnimals.push(animal.name);
+      listNames.listLocationAnimal.push(animal.location);
+    }
+    return listNames;
+  }, { listAnimals: [], listLocationAnimal: [] });
+}
+
+function employeesDates() {
+  return employees.reduce((listOfEmployees, employee) => {
+    const listAnimals = locationAndNamesAnimals(employee.responsibleFor);
+    const dadosFuncionario = {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: listAnimals.listAnimals,
+      locations: listAnimals.listLocationAnimal,
+    };
+    const list = [...listOfEmployees, dadosFuncionario];
+    return list;
+  }, []);
+}
+console.log(employeesDates());
+
+function getEmployeesCoverage(information) {
+  if (!information) {
+    return employeesDates();
+  }
+  const { id, name } = information;
+  if (employeesDates().find((employee) => employee.id === id)) {
+    return employeesDates().find((employee) => employee.id === id);
+  }
+  if (employeesDates().find((employee) => employee.fullName.includes(name))) {
+    return employeesDates().find((employee) => employee.fullName.includes(name));
+  }
 }
 
 module.exports = getEmployeesCoverage;
