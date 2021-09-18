c07f4912f0dbeb739393e98ac618c6c7f61e796f
Emerson Oliveira <emerson.contato@gmail.com>
2021-09-18 16:19:59 -0300

    Pull realizado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,9 +7,9 @@ function countAnimals(animal) {
     const allSpecies = {};
     species.forEach((nameSpec) => { allSpecies[nameSpec.name] = nameSpec.residents.length; });
     return allSpecies;
-  } if (animal.gender) {
+  } if (animal.sex) {
     const animals = species.find((animalName) => animal.specie === animalName.name);
-    const sex = animals.residents.filter((maleFemale) => animal.gender === maleFemale.sex);
+    const sex = animals.residents.filter((maleFemale) => animal.sex === maleFemale.sex);
     return sex.length;
   }
   const nameOfAnimal = species.find((elem) => animal.specie === elem.name);
