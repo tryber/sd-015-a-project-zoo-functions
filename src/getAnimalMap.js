c7605aa75595dcf9d85634432d9a5f3f6b97cd64
renatozr <renatozr07@gmail.com>
2021-09-17 23:35:07 -0300

    requisito 7

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,11 +1,58 @@
 const data = require('../data/zoo_data');
 
-// const { species } = data;
+const { species } = data;
 
-// const filterLocation = (location) => species.filter((elm) => elm.location === location);
+function getSpeciesByLocations(locations) {
+  const result = {};
+  locations.forEach((loc) => {
+    result[loc] = species.filter((spc) => loc === spc.location).map((sSpc) => sSpc.name);
+  });
+  return result;
+}
 
-function getAnimalMap(options) {
-  //
+function getNamesByLocations(locations) {
+  const result = {};
+  locations.forEach((loc) => {
+    result[loc] = species.filter((spc) => loc === spc.location).map((sSpc) => {
+      const objSpcResidents = {};
+      objSpcResidents[sSpc.name] = sSpc.residents.map((rsd) => rsd.name);
+      return objSpcResidents;
+    });
+  });
+  return result;
+}
+
+function getNamesByLocationsAndSex(locations, sex) {
+  const result = {};
+  locations.forEach((loc) => {
+    result[loc] = species.filter((spc) => loc === spc.location).map((sSpc) => {
+      const objSpcResidents = {};
+      objSpcResidents[sSpc.name] = sSpc.residents.filter((rsd) => rsd.sex === sex)
+        .map((rsd) => rsd.name);
+      return objSpcResidents;
+    });
+  });
+  return result;
+}
+
+function getAnimalMap({ includeNames = 0, sex, sorted } = 0) {
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  if (includeNames === 0) {
+    return getSpeciesByLocations(locations);
+  }
+  let result = getNamesByLocations(locations);
+  if (sex) {
+    result = getNamesByLocationsAndSex(locations, sex);
+  }
+  if (sorted) {
+    locations.forEach((loc) => {
+      result[loc].forEach((elm) => {
+        const firstKey = Object.keys(elm)[0];
+        elm[firstKey].sort();
+      });
+    });
+  }
+  return result;
 }
 
 module.exports = getAnimalMap;
