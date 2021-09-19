542eab6d4d801a06fcf55f3d231187322fec7a8b
devgeorge <glalmeida.dev@gmail.com>
2021-09-19 11:10:02 -0300

    Requisito 5.0

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,14 +1,14 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((employee) => employee.managers.includes(id));
+  return data.employees.some((employees) => employees.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
   if (isManager(managerId)) {
-    return data.employees.filter((emplyee) => (
-      emplyee.managers.includes(managerId)
-    )).map((employee) => `${employee.firstName} ${employee.lastName}`);
+    return data.employees.filter((emplyees) => (
+      emplyees.managers.includes(managerId)
+    )).map((employees) => `${employees.firstName} ${employees.lastName}`);
   }
   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
