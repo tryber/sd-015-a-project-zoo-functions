83f24e9e9f814e91c218e2d50a189256cae202b9
MoisésFigueira <moisesmichael@gmail.com>
2021-09-19 15:46:11 -0300

    lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,9 +1,12 @@
 const data = require('../data/zoo_data');
 
+// developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce - monitoria sabado luiz sobre nome reduce
+
 function getOldestFromFirstSpecies(id) {
   const responsibleEmployee = data.employees.find((employee) => employee.id === id).responsibleFor;
   const animal = data.species.find((specie) => specie.id === responsibleEmployee[0]).residents;
-  return Object.values(animal.reduce((previousValue, currentValue) => ((previousValue.age < currentValue.age) ? currentValue : previousValue)));
+  return Object.values(animal.reduce((previousValue, currentValue) =>
+  ((previousValue.age < currentValue.age) ? currentValue : previousValue)));
 }
 
 module.exports = getOldestFromFirstSpecies;
