8798856aa46045ae5e8bc734c4b5b8ffffb4642b
Eliana Nunes <elianadnunes@gmail.com>
2021-09-19 13:32:29 -0300

    Requisito 5 correcoes. Agora vai! kk

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,15 +7,20 @@ function countAnimals(animal) {
       acc[current.name] = current.residents.length;
       return acc;
     }, {});
+  } if (!animal.sex) {
+    const { residents } = species.find((specie) => specie.name === animal.specie);
+    return residents.length;
+  } if (animal.specie && animal.sex) {
+    const findAnimals = species.find(
+      (value) => value.name === animal.specie,
+    ).residents;
+    const findAnimalsGender = findAnimals.filter(
+      (value) => value.sex === animal.sex,
+    );
+    return findAnimalsGender.length;
   }
-
-  const item = species.find((value) => value.name === animal.specie);
-
-  if (animal.specie && animal.gender) {
-    const animals = item.residents.filter((value) => value.sex === animal.gender);
-    return animals.length;
-  }
-  return item.residents.length;
 }
+
+// Referência correçoes na função: [Cassia Elen] https://github.com/tryber/sd-015-a-project-zoo-functions/pull/105/commits/e4ad02e58127e70b981fba23a717db7f016e6fc0
 
 module.exports = countAnimals;
