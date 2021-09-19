c246d73130f3a97a2951945eb86eb3fbd100a796
Vini_Vergani <vinivergani95@gmail.com>
2021-09-19 20:13:07 -0300

    Adiciona requisito 1 - lint

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,8 +3,8 @@ const data = require('../data/zoo_data');
 function getSpeciesByIds(...ids) {
   if (ids.length === 0) {
     return [];
-  };
+  }
   return data.species.filter((findId) => ids.includes(findId.id));
-};
+}
 
 module.exports = getSpeciesByIds;
