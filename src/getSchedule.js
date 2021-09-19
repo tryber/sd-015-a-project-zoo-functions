2c2ff142f2d725f48892d22387087f651f94823b
Leandro Basilio <leandroab07@gmail.com>
2021-09-18 22:38:24 -0300

    Adiciona requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -19,20 +19,42 @@ function getDay(day) {
   return objDay;
 }
 
+function getDayWithName(animal) {
+  const dataAnimal = data.species.filter((specie) => specie.name === animal);
+
+  return dataAnimal.map((obj) => obj.availability)[0];
+}
+
+function getObjWithDay(day) {
+  const resultado = {};
+  resultado[day] = getDay(day);
+  return resultado;
+}
+
+function getObjWithArray() {
+  const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
+
+  const resultado = {};
+
+  days.map((obj) => {
+    resultado[obj] = getDay(obj);
+    return resultado;
+  });
+  return resultado;
+}
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
-  if (scheduleTarget === undefined) {
-    const days = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 'Monday'];
-
-    const resultado = {};
-
-    days.map((obj) => {
-      resultado[obj] = getDay(obj);
-      return resultado;
-    });
-
-    return resultado;
+  if (data.species.some((obj) => obj.name === scheduleTarget)) {
+    return getDayWithName(scheduleTarget);
+  } if (scheduleTarget === undefined) {
+    return getObjWithArray();
+  } if (Object.keys(data.hours).some((obj) => obj.includes(scheduleTarget))) {
+    return getObjWithDay(scheduleTarget);
   }
+  return getObjWithArray();
 }
+
+console.log(getSchedule('Monday'));
 
 module.exports = getSchedule;
