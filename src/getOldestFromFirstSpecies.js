9a4a2c1b44ed3ad10282fba03062d3053d919e73
Matheus Muniz <matheusmuniz100@hotmail.com>
2021-09-19 16:52:43 -0300

    requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,7 +2,12 @@ const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-
+  const filtrarFuncionario = data.employees.find((funcionario) => funcionario.id === id);
+  const filtrarAnimal = data.species.find((esp) => esp.id === filtrarFuncionario.responsibleFor[0]);
+  const animalMaisVelho = filtrarAnimal.residents.sort((a, b) => b.age - a.age);
+  return Object.values(animalMaisVelho[0]);
 }
+
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 
 module.exports = getOldestFromFirstSpecies;
