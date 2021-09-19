05201f5302da8c519d0e6941271f60f53f778122
João V. Dias <diasvictorj@gmail.com>
2021-09-19 19:29:46 -0300

    Nono Requisito

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
   // seu código aqui
+  if (options === undefined) {
+  const toReturn = species.sort((a, b) => a.location - b.location);
+  console.log(toReturn);
+  }
 }
+console.log(getAnimalMap());
 
 module.exports = getAnimalMap;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
-}
+  const findEmployee = employees.find((elementId) =>
+    elementId.id === id).responsibleFor[0];
 
+  const findAnimals = species.find((elementAnimals) =>
+    findEmployee.includes(elementAnimals.id)).residents;
+
+  const oldAnimal = findAnimals.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr), 0); // Essa compração usando o reduce foi tirada a logica do site :https://stackoverflow.com/questions/4020796/finding-the-max-value-of-an-attribute-in-an-array-of-objects?rq=1 resposta do  juliangonzalez.
+  return Object.values(oldAnimal);
+}
 module.exports = getOldestFromFirstSpecies;
