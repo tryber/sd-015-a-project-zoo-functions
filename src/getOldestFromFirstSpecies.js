b30f4bc6a8643606d3dc7e13b948e18352a0f818
MoisésFigueira <moisesmichael@gmail.com>
2021-09-19 15:47:25 -0300

    lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -6,7 +6,7 @@ function getOldestFromFirstSpecies(id) {
   const responsibleEmployee = data.employees.find((employee) => employee.id === id).responsibleFor;
   const animal = data.species.find((specie) => specie.id === responsibleEmployee[0]).residents;
   return Object.values(animal.reduce((previousValue, currentValue) =>
-  ((previousValue.age < currentValue.age) ? currentValue : previousValue)));
+    ((previousValue.age < currentValue.age) ? currentValue : previousValue)));
 }
 
 module.exports = getOldestFromFirstSpecies;
