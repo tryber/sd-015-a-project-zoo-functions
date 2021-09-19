6c669fb4dfe1898e84919842a1f5310d4f720b99
arturbacilla <artur.bacilla1@gmail.com>
2021-09-19 13:30:09 -0300

    Requisite 6: Calculate entry values

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,16 +1,25 @@
-const data = require('../data/zoo_data');
+const data = require("../data/zoo_data");
 
 function countEntrants(entrants) {
   return {
     child: entrants.filter((person) => person.age < 18).length,
-    adult: entrants.filter((person) => person.age >= 18 && person.age < 50).length,
+    adult: entrants.filter((person) => person.age >= 18 && person.age < 50)
+      .length,
     senior: entrants.filter((person) => person.age >= 50).length,
   };
 }
 
 function calculateEntry(entrants) {
-  const { prices: { adult: adultPrice, senior: seniorPrice, child: childPrice}} = data;
-  console.log(`adult:${adultPrice}, senior:${seniorPrice}, child:${childPrice}`);
+  let response = 0;
+  if (entrants === undefined || Object.keys(entrants).length === 0) return 0;
+  const { prices: { adult: adultPrice, senior: seniorPrice, child: childPrice } } = data;
+  const {
+    child: childCnt = 0,
+    adult: adultCnt = 0,
+    senior: seniorCnt = 0,
+  } = countEntrants(entrants);
+  response += childCnt * childPrice + adultCnt * adultPrice + seniorCnt * seniorPrice;
+  return response;
 }
 
 module.exports = { calculateEntry, countEntrants };
