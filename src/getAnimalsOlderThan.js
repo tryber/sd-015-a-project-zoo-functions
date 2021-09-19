42744230a6fc1786db98ad0d9058856b8d9c9138
Vini_Vergani <vinivergani95@gmail.com>
2021-09-19 20:29:29 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const getAnimal = data.species.find((findAnimal) => findAnimal.name === animal);
+  return getAnimal.residents.every((animalAge) => animalAge.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
