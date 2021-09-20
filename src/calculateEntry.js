0ad485e3acdd4e7a129e6a8a12e716e72e351b35
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-09-20 18:22:51 -0300

    Requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const inicialEntrants = {
+    child: entrants.filter((child) => child.age < 18).length,
+    adult: entrants.filter((adult) => adult.age >= 18 && adult.age < 50).length,
+    senior: entrants.filter((senior) => senior.age >= 50).length,
+  };
+  return inicialEntrants;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof entrants === 'undefined' || Object.values(entrants).length === 0) return 0;
+  const peopleEntrants = countEntrants(entrants);
+  const { child, adult, senior } = peopleEntrants;
+  const { prices } = data;
+  return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
 module.exports = { calculateEntry, countEntrants };
