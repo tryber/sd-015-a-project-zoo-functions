d6198e0ac9d8d8244266dbfcbd45bc11e18d27f8
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-09-20 19:31:06 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (typeof animal === 'undefined') {
+    const speciesTotais = {}; // cria um objeto vazio
+    data.species.forEach((specie) => {
+      speciesTotais[specie.name] = specie.residents.length; // atribui a chave nome especie para a quantidade da especie que tem esse nome
+    });
+    return speciesTotais;
+  }
+
+  const totalPorSpecie = data.species.find((obj) => obj.name === animal.specie).residents;
+
+  if (animal.specie && animal.sex) {
+    return totalPorSpecie.filter((obj) => obj.sex === animal.sex).length;
+  }
+
+  return totalPorSpecie.length;
 }
 
 module.exports = countAnimals;
