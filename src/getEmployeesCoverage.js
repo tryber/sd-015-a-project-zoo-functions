bfcc8a228794869b85cb7cccfcc1d2308b407f3a
Regiane Santos <re.cfs235@gmail.com>
2021-09-20 13:18:49 -0300

    Requisito 10 - getEmployessCoverage saiuuuuu Aleluia Aleluia

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const EmployeeData = (employee) => ({
+  id: employee.id,
+  fullName: `${employee.firstName} ${employee.lastName}`,
+
+  species: employee.responsibleFor
+    .map((animal) => species
+      .find((specie) => specie.id === animal).name),
+
+  locations: employee.responsibleFor
+    .map((animal) => species
+      .find((specie) => specie.id === animal).location),
+});
+
+const getEmployeesCoverage = (idOrName) => {
+  if (idOrName === undefined) {
+    const allEmployeesData = employees.map(EmployeeData);
+    return allEmployeesData;
+    // sem parâmetros, retorna uma lista com os dados de todos os funncionários
+  }
+  const { name, id } = idOrName;
+  const emplo = employees.find((employee) => employee.firstName === name
+  || employee.lastName === name || employee.id === id);
+  // Buscando o nome ou id especifico, jogando na variavel emplo o objeto em questão
+
+  if (emplo === undefined) {
+    throw new Error('Informações inválidas');
+    // Se o nome ou id não forem validos retorna um erro
+  }
+  return EmployeeData(emplo);
+};
 
 module.exports = getEmployeesCoverage;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  if (typeof scheduleTarget === 'undefined') {
+    const AllAnimals = species.reduce((accumulator, currentValue) => {
+      accumulator[currentValue.name] = currentValue.residents.length;
+      // Cria uma chave com o nome do animal
+      return accumulator;
+    }, {});
+    return AllAnimals;
+    // Sem parâmetros, retorna todas as espécies e suas quantidades.
+  }
 }
 
 module.exports = getSchedule;
