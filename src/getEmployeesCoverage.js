27bbf43d001083361a7037420441857a25b692c6
herzogcassia <cassiahherzog@gmail.com>
2021-09-20 17:47:53 -0300

    correção do lint requisto 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function selectAnimal(key, empregado) {
+  return species.filter((bicho) =>
+    empregado.responsibleFor.includes(bicho.id)).map((xablau) => xablau[key]);
+}
+
+function getEmployeesById() {
+  return employees.map((empregado) => {
+    const returnMap = {
+      id: empregado.id,
+      fullName: `${empregado.firstName} ${empregado.lastName}`,
+      species: selectAnimal('name', empregado),
+      locations: selectAnimal('location', empregado),
+    };
+    return returnMap;
+  });
+}
+function getEmployeesCoverage(employee) {
+  const arrayName = getEmployeesById();
+  if (!employee) {
+    return arrayName;
+  }
+  const resultado = arrayName.find((nome) =>
+    nome.fullName.includes(employee.name) || nome.id.includes(employee.id));
+  if (!resultado) {
+    throw new Error('Informações inválidas');
+  }
+  return resultado;
 }
 
 module.exports = getEmployeesCoverage;
