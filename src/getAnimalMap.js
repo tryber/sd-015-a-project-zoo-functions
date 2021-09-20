7b20b32ce9cfca746447c0bf6d254371b7d5de27
herzogcassia <cassiahherzog@gmail.com>
2021-09-20 18:01:15 -0300

    correção no lint

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,13 +1,12 @@
 const data = require('../data/zoo_data');
-const { location } = data;
 
 function getAnimalMap(options) {
-  if (!options) {
-    const allAnimalsForlocal = options.filter((animal) => animal.location).length;
-    allAnimalsForlocal.reduce((accumulator, currenteValue) )
-    console.log(allAnimalsForlocal);
-  }
-  // seu código aqui
+  // if (!options) {
+  //   const allAnimalsForlocal = options.filter((animal) => animal.location).length;
+  //   allAnimalsForlocal.reduce((accumulator, currenteValue) )
+  //   console.log(allAnimalsForlocal);
+  // }
+  // // seu código aqui
 }
 
 module.exports = getAnimalMap;
