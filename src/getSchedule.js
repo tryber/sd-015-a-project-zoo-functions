810b29e002137fbb3f6fbd6928bdf2eef5ab3a13
Gabriel Moda <gabriel-moda@hotmail.com.br>
2021-09-20 13:32:12 -0300

    Arrumando erro de lint das refatoradas e incluindo a 10

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,35 +1,7 @@
-const { time } = require('faker');
-const { hours, species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
-// const obj = {};
-// function allInfo() {
-//   Object.keys(hours).forEach((elementHour) => {
-//     const time = hours[elementHour];
-//     if (elementHour !== 'Monday') {
-//       obj[elementHour] = {
-//         officeHour: `Open from ${time.open}am until ${time.close}pm`,
-//         exhibition: species.filter((animal) => animal.availability.includes(elementHour))
-//           .map((element) => element.name),
-//       };
-//     } else {
-//       obj[elementHour] = { officeHour: 'CLOSED',
-//         exhibition: 'The zoo will be closed!',
-//       };
-//     }
-//     return obj;
-//   });
+function getSchedule(scheduleTarget) {
 
-//   function getSchedule(scheduleTarget) {
-//     const animal = (getHourAnimal) => species.find((animals) => animals.name === scheduleTarget);
-//   } 
-//   return allInfo();
+}
 
-
-// return {
-//   [Object.keys(hours)[0]]: {
-//     officeHour: `Open from ${hours.Tuesday.open}am until ${hours.Tuesday.close}pm`,
-//     exhibition: species.filter((tuesday) => tuesday.availability.includes('Tuesday'))
-//       .map((element) => element.name),
-//   },
-// };
 module.exports = getSchedule;
