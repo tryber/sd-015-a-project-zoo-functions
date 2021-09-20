343073d1637490b9d80da8840c86266480f15c94
Daniel Tavares <daniel1tavares@outlook.com>
2021-09-20 16:29:38 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((entrant) => entrant.age < 18);
+  const childObject = { child: child.length };
+  const adult = entrants.filter((entrant) => entrant.age >= 18 && entrant.age < 50);
+  const adultObject = { adult: adult.length };
+  const senior = entrants.filter((entrant) => entrant.age >= 50);
+  const seniorObject = { senior: senior.length };
+  return Object.assign(adultObject, childObject, seniorObject);
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  } { 
+    const people = countEntrants(entrants);
+    const priceAdult = people.adult * prices.adult;
+    const priceSenior = people.senior * prices.senior;
+    const priceChild = people.child * prices.child;
+    return priceAdult + priceSenior + priceChild;
+  }
 }
 
 module.exports = { calculateEntry, countEntrants };
