4a6fcaf582bef79a19f8b25279331688fa7f47ba
uauditore <fauditore2912@gmail.com>
2021-09-20 12:10:18 -0300

    acabando porem falta arrumar o maravilhoso lint

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function defaultt() {
+  const resultado = employees.map((elemento) => ({
+    id: elemento.id,
+    fullName: `${elemento.firstName} ${elemento.lastName}`,
+    species: elemento.responsibleFor.map((elemento2) => species.find((elemento3) => elemento3.id === elemento2).name),
+    locations: elemento.responsibleFor.map((elemento2) => species.find((elemento3) => elemento3.id === elemento2).location),
+  }))
+  return resultado
+}
+function getEmployeesCoverage(parametro) {
+  if (!parametro) {
+    return defaultt();
+  }
+  const {id, name} = parametro;
+  if (employees.find((parametro2) => (parametro2.firstName === name || parametro2.lastName === name || parametro2.id === id))) {
+  const objeto = employees.find((parametro3) => (parametro3.firstName === name || parametro3.lastName === name || parametro3.id === id))
+  return {
+    id: objeto.id,
+    fullName: `${objeto.firstName} ${objeto.lastName}`,
+    species: objeto.responsibleFor.map((elemento2) => species.find((elemento3) => elemento3.id === elemento2).name),
+    locations: objeto.responsibleFor.map((elemento2) => species.find((elemento3) => elemento3.id === elemento2).location),
+  };
+}
+throw new Error('Informações inválidas');
 }
 
 module.exports = getEmployeesCoverage;
