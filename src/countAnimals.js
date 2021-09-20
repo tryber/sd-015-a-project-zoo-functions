740afe24a2d2b816f7c4afcf70658cf83c509d42
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-20 17:24:47 -0300

    test countAnimals

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
-}
+  /* const result = {}; */
+  if (!animal) {
+    const result = species.map((specie) => (
+      { [specie.name]: specie.residents.length }));
+      return result.sort((a, b) => a[0] - b[0]);
+  }
+  const { specie, gender } = animal;
 
+  if (specie && gender) {
+    const specieFind = species.find((aux) => aux.name === specie);
+    const genderFIlter = specieFind.residents.filter((aux) => aux.sex === gender);
+    return genderFIlter.length;
+  }
+
+  return species.find((aux) => aux.name === specie).residents.length;
+
+}
+console.log(countAnimals());
 module.exports = countAnimals;
+ 
