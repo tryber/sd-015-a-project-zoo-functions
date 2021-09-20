ef0de206550b56f95114ff8806bb4d0a541f02c8
leonardo <leonardo.siepierski@outlook.com>
2021-09-20 18:05:06 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,30 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function createObject(object) {
+  const animals = object.responsibleFor.map((id) => species.find((animal) => animal.id === id).name);
+  return {
+    id: object.id,
+    fullName: `${object.firstName} ${object.lastName}`,
+    species: animals,
+    locations: animals.map((animal) => species.find((location) => location.name === animal).location)
+  };
+}
+
+const verify = (employee, identity) => employee.id === identity.id || employee.firstName === identity.name || employee.lastName === identity.name
+
+function getEmployeesCoverage(entry) {
+    let coverage = {};
+    if (entry === undefined) {
+      coverage = employees.map((element) => createObject(element));
+    } else {
+      const employee = employees.find((eachEmployee) => verify(eachEmployee, entry));
+      if (employee !== undefined) {
+        coverage = createObject(employee);
+      } else {
+        throw new Error('Informações inválidas');
+      }
+    }
+    return coverage;
 }
 
 module.exports = getEmployeesCoverage;
