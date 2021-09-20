d134ecb6edbdece770e6b3094b6619d2caad8f97
Regiane Santos <re.cfs235@gmail.com>
2021-09-20 13:22:46 -0300

    Corrigindo o erro do commit anterior Requisito 10 eita estresse

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,15 +1,6 @@
 const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
-  if (typeof scheduleTarget === 'undefined') {
-    const AllAnimals = species.reduce((accumulator, currentValue) => {
-      accumulator[currentValue.name] = currentValue.residents.length;
-      // Cria uma chave com o nome do animal
-      return accumulator;
-    }, {});
-    return AllAnimals;
-    // Sem parâmetros, retorna todas as espécies e suas quantidades.
-  }
 }
 
 module.exports = getSchedule;
