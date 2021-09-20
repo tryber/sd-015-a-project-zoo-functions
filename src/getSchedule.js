dcad6093477fa58014f8d9c9523ac6956bccf752
Junio <junioalves2005@hotmail.com>
2021-09-20 15:00:20 -0300

    arrumando Lint

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,38 @@
 const data = require('../data/zoo_data');
 
+
+function getOfficeHour(scheduleTarget) {
+  if (scheduleTarget === 'Tuesday' || scheduleTarget === 'Wednesday') {
+    return 'Open from 8am until 6pm';
+  }
+  if (scheduleTarget === 'Thursday' || scheduleTarget === 'Friday') {
+    return 'Open from 10am until 8pm';
+  }
+  if (scheduleTarget === 'Saturday') {
+    return 'Open from 8am until 10pm';
+  }
+  else {
+    return 'Open from 8am until 8pm';
+  }
+}
+
+function getAnimals(animals) {
+  if (animals.length !== 0) {
+    return {
+      [scheduleTarget]: {
+        officeHour: getOfficeHour(scheduleTarget),
+        exhibition: animals,
+      }
+    }
+  }
+  let animal = data.species.find((y) => y.name === scheduleTarget);
+
+  if (animal !== undefined) {
+    return animal.availability;
+  }
+  return getTodosOsDias();
+}
+
 function getSchedule(scheduleTarget) {
   if (scheduleTarget === 'Monday') {
     return {
@@ -9,62 +42,29 @@ function getSchedule(scheduleTarget) {
       },
     };
   }
-
-  function getTodosOsDias() {
-
-    const Tuesday = getSchedule('Tuesday');
-    const Wednesday = getSchedule('Wednesday');
-    const Thursday = getSchedule('Thursday');
-    const Friday = getSchedule('Friday');
-    const Saturday = getSchedule('Saturday');
-    const Sunday = getSchedule('Sunday');
-
-    return [
-      Tuesday,
-      Wednesday,
-      Thursday,
-      Friday,
-      Saturday,
-      Sunday
-    ]
-  }
-  function getOfficeHour(scheduleTarget) {
-    if (scheduleTarget === 'Tuesday' || scheduleTarget === 'Wednesday') {
-      return 'Open from 8am until 6pm';
-    }
-    if (scheduleTarget === 'Thursday' || scheduleTarget === 'Friday') {
-      return 'Open from 10am until 8pm';
-    }
-    if (scheduleTarget === 'Saturday') {
-      return 'Open from 8am until 10pm';
-    }
-    else {
-      return 'Open from 8am until 8pm';
-    }
-  }
-
-
-  function getAnimals(animals) {
-    if (animals.length !== 0) {
-      return {
-        [scheduleTarget]: {
-          officeHour: getOfficeHour(scheduleTarget),
-          exhibition: animals,
-        }
-      }
-    }
-    let animal = data.species.find((y) => y.name === scheduleTarget);
-
-    if (animal !== undefined) {
-      return animal.availability;
-    }
-    return getTodosOsDias();
-  }
-
   const animals = data.species.filter((x) => x.availability.includes(scheduleTarget))
     .map((y) => y.name);
 
   return getAnimals(animals);
+}
+
+function getTodosOsDias() {
+
+  const Tuesday = getSchedule('Tuesday');
+  const Wednesday = getSchedule('Wednesday');
+  const Thursday = getSchedule('Thursday');
+  const Friday = getSchedule('Friday');
+  const Saturday = getSchedule('Saturday');
+  const Sunday = getSchedule('Sunday');
+
+  return [
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday,
+    Sunday
+  ]
 }
 
 module.exports = getSchedule;
