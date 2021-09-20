2431ea11f723a91cc1416639984bd7af05e9e099
leonardo <leonardo.siepierski@outlook.com>
2021-09-20 16:34:35 -0300

    Requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -4,7 +4,7 @@ function getOldestFromFirstSpecies(id) {
   const firstAnimal = employees.find((employee) => employee.id === id).responsibleFor[0];
   const allAnimals = species.find((specie) => specie.id === firstAnimal).residents;
   const oldestAnimal = allAnimals.reduce((highestAge, animal) => {
-    const oldest = highestAge.age > animal.age ? highestAge : animal;
+    const oldest = highestAge > animal.age ? highestAge : animal;
     return oldest;
   }, 0);
   return Object.values(oldestAnimal);
