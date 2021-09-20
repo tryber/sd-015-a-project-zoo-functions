a822223f6ee38f92802c44b43a9488210dd4e67c
Arthur Lisboa <lisboaarthur@hotmail.com>
2021-09-20 10:36:10 -0300

    adiciona requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
-}
+const getNameOrId = (no, nomeid) => {
+  const veri = no.id === nomeid.id || no.firstName === nomeid.name || no.lastName === nomeid.name;
+  return veri;
+};
+
+const criarObjeto = (obj) => {
+  const pet = obj.responsibleFor.map((id) => data.species.find((bicho) => bicho.id === id).name);
+  const dados = {
+    id: obj.id,
+    fullName: `${obj.firstName} ${obj.lastName}`,
+    species: pet,
+    locations: pet.map((animal) => data.species.find((local) => local.name === animal).location),
+  };
+  return dados;
+};
+
+const getEmployeesCoverage = (idName) => {
+  let resposta = {};
+  if (!idName) {
+    resposta = data.employees.map((obj) => criarObjeto(obj));
+    return resposta;
+  }
+  const empregado = data.employees.find((func) => getNameOrId(func, idName));
+  if (empregado) {
+    resposta = criarObjeto(empregado);
+    return resposta;
+  }
+  if (!empregado) throw new Error('Informações inválidas');
+  return resposta;
+};
 
 module.exports = getEmployeesCoverage;
