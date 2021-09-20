f323568c3cd53a07a8cbc54c4b8018afa7e1adc7
Bruno Miranda <brunompe@gmail.com>
2021-09-20 13:47:40 -0400

    requirement 09 - done

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const caredSpecies = data.employees.find((person) => person.id === id).responsibleFor[0]; // Quero encontrar a primeira especie cuidada para funcionário passado pelo ID;
+  const animals = data.species.find((animal) => animal.id === caredSpecies).residents;
+  const older = animals.reduce((acc, curr) => ((acc.age > curr.age ? acc : curr)));
+  // console.log(older);
+  return Object.values(older);
 }
+getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992');
 
 module.exports = getOldestFromFirstSpecies;
