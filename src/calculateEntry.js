78660af49fdf8c20673ff9c97dc8cffbe70671bd
Guilherme Zobel <guilherme.zobel@gmail.com>
2021-09-19 21:44:03 -0300

    requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const senior = entrants.filter((elemento) => elemento.age >= 50).length;
+  const adult = entrants.filter((elemento) => elemento.age >= 18 && elemento.age < 50).length;
+  const child = entrants.filter((elemento) => elemento.age < 18).length;
+  return {
+    senior,
+    adult,
+    child,
+  };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = '') {
+  if (!entrants.length) return 0;
+  const customers = countEntrants(entrants);
+  const { senior, adult, child } = customers;
+  const totalValues = (senior * prices.senior) + (adult * prices.adult) + (child * prices.child);
+  return totalValues;
 }
 
 module.exports = { calculateEntry, countEntrants };
