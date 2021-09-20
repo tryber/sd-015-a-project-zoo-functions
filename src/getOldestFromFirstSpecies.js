bce88d6c47418e1a307a6b314b151d361dd503d4
Junio <junioalves2005@hotmail.com>
2021-09-20 14:34:13 -0300

    arrumando erros do Lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,20 +5,20 @@ function getOldestFromFirstSpecies(id) {
   const funcionarios = data.employees.find((funcionario) => funcionario.id === id);
   const animal = species.find((specie) => funcionarios.responsibleFor.includes(specie.id));
 
-  let retorno = {
+  const retorno = {
     value: 0,
     name: '',
-    sex: ''
-  }
+    sex: '',
+  };
 
-  for (element of animal.residents) {
+  for (let element of animal.residents) {
     if (element.age > retorno.value) {
       retorno.value = element.age;
       retorno.name = element.name;
       retorno.sex = element.sex;
     }
   }
-      return [retorno.name, retorno.sex, retorno.value];
+  return [retorno.name, retorno.sex, retorno.value];
 }
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -2,65 +2,65 @@ const data = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
     if (scheduleTarget === 'Monday') {
-      return {
-        [scheduleTarget]: {
-          officeHour: 'CLOSED',
-          exhibition: 'The zoo will be closed!'
+        return {
+            [scheduleTarget]: {
+                officeHour: 'CLOSED',
+                exhibition: 'The zoo will be closed!'
+            }
         }
-      }
     }
-  
-  
+
+
     let animals = data.species.filter((x) => x.availability.includes(scheduleTarget)).map(y => y.name);
-  
+
     if (animals.length !== 0) {
-      return {
-        [scheduleTarget]: {
-          officeHour: getOfficeHour(scheduleTarget),
-          exhibition: animals
+        return {
+            [scheduleTarget]: {
+                officeHour: getOfficeHour(scheduleTarget),
+                exhibition: animals
+            }
         }
-      }
-  
+
     }
     let animal = data.species.find((y) => y.name === scheduleTarget);
-  
+
     if (animal !== undefined) {
-      return animal.availability;
+        return animal.availability;
     }
     return getTodosOsDias()
-  }
-  
-  function getOfficeHour(scheduleTarget) {
+}
+
+function getOfficeHour(scheduleTarget) {
     if (scheduleTarget === 'Tuesday' || scheduleTarget === 'Wednesday') {
-      return 'Open from 8am until 6pm'
+        return 'Open from 8am until 6pm'
     }
     if (scheduleTarget === 'Thursday' || scheduleTarget === 'Friday') {
-      return 'Open from 10am until 8pm'
+        return 'Open from 10am until 8pm'
     }
     if (scheduleTarget === 'Saturday') {
-      return 'Open from 8am until 10pm'
+        return 'Open from 8am until 10pm'
     }
     else {
-      return 'Open from 8am until 8pm'
+        return 'Open from 8am until 8pm'
     }
-  }
-  
-  function getTodosOsDias() {
-  
+}
+
+function getTodosOsDias() {
+
     const Tuesday = getSchedule('Tuesday');
     const Wednesday = getSchedule('Wednesday');
     const Thursday = getSchedule('Thursday');
     const Friday = getSchedule('Friday');
     const Saturday = getSchedule('Saturday');
     const Sunday = getSchedule('Sunday');
-  
+
     return [
-      Tuesday,
-      Wednesday,
-      Thursday,
-      Friday,
-      Saturday,
-      Sunday
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
     ]
-  }
+}
 module.exports = getSchedule;
