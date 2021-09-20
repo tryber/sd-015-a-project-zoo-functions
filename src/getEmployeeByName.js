3ab5754cb563bd4c6728a4c08d537662c7851e4f
arturbacilla <artur.bacilla1@gmail.com>
2021-09-20 12:06:49 -0300

    Modified function getEmployeeByName to include search by id aswell

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  return !employeeName ? {} : data.employees.find((element) =>
-    element.firstName === employeeName || element.lastName === employeeName);
+  return !employeeName
+    ? {}
+    : data.employees.find(
+      (element) =>
+        element.firstName === employeeName
+          || element.lastName === employeeName
+          || element.id === employeeName,
+    );
 }
 
 module.exports = getEmployeeByName;
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const getEmployeeByName = require('./getEmployeeByName');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function fullCoverage() {
+  
+}
+
+function getEmployeesCoverage(givenParameter) {
+  if (!givenParameter || Object.keys(givenParameter).length === 0) {
+    // no param, fullCoverage
+  } else {
+  const { name: givenName = undefined, id: givenID = undefined } = givenParameter;
+  }
 }
 
 module.exports = getEmployeesCoverage;
