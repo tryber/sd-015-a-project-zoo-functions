3497516f79bbae4b814f018aa5db0c186fbdef4a
djsilva <coxcorp@gmail.com>
2021-09-20 19:06:36 -0300

    requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,10 @@
-const data = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
-}
+// function getAnimalsOlderThan(animal, age) {
+//   // seu código aqui
+// }
+const getAnimalsOlderThan = (animal, age) => species.find((unit) =>
+  unit.name === animal).residents.every((unit) => unit.age >= age);
 
 module.exports = getAnimalsOlderThan;
