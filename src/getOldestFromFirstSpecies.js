8a6fe1dc21c6e8171a886ffb4f1d06af879c8ad9
Junio <junioalves2005@hotmail.com>
2021-09-20 14:24:16 -0300

    concluindo requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,24 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcionarios = data.employees.find((funcionario) => funcionario.id === id);
+  const animal = species.find((specie) => funcionarios.responsibleFor.includes(specie.id));
+
+  let retorno = {
+    value: 0,
+    name: '',
+    sex: ''
+  }
+
+  for (element of animal.residents) {
+    if (element.age > retorno.value) {
+      retorno.value = element.age;
+      retorno.name = element.name;
+      retorno.sex = element.sex;
+    }
+  }
+      return [retorno.name, retorno.sex, retorno.value];
 }
 
 module.exports = getOldestFromFirstSpecies;
