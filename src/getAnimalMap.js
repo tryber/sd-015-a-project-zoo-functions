7a5b27cffd439654ab149db58ad551f4fdb1ae8e
Felipe <felllypes@hotmail.com>
2021-09-20 08:11:17 -0300

    8 requisitos

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,8 +1,5 @@
-const { species } = require('../data/zoo_data');
-const { residents } = require('../data/zoo_data');
+function getAnimalMap(options) {
 
- function getAnimalMap(options) {
-  
-  }
+}
 console.log(getAnimalMap({ includeNames: true }));
-module.exports = getAnimalMap; 
+module.exports = getAnimalMap;
