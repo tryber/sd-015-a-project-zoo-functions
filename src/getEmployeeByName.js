1dbd08f8fc974e6af2ee3735d030a714909d8b54
SaraRabelo <rabelorsara@gmail.com>
2021-09-20 17:44:19 -0300

    3o correção linha 5  Requisito ok

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   if (employeeName !== undefined) {
-    return data.employees.find((element) => element.firstName === employeeName || element.lastName === employeeName);
+    return data.employees.find((element) => 
+    element.firstName === employeeName || element.lastName === employeeName);
   }
   return {};
 }
