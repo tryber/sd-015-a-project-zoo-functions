ba270d60e3bb7c09aa983d11b67ef960525e2c24
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-20 19:21:31 -0300

    mais teste

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,10 +1,6 @@
 const data = require('../data/zoo_data');
-// const {employees} = data;
 
 function getEmployeesCoverage(info) {
   // seu código aqui
-  // const { name, id } = info;
-  /*  if (!info) {
-} */
 }
 module.exports = getEmployeesCoverage;
