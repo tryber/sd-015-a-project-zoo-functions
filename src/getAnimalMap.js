e10e151b77a105dcc919b10d4585757a69eaa123
Marcos Valdecy <mvaldecy11@gmail.com>
2021-09-20 15:24:36 -0300

    getRelatedEmployees

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function getAnimalMap(options) {
   // seu código aqui
+  // sem parametro
+  const neAnimals = species.filter((aux) => aux.location === 'NE');
+  const nwAnimals = species.filter((aux) => aux.location === 'NW');
+  const seAnimals = species.filter((aux) => aux.location === 'SE');
+  const swAnimals = species.filter((aux) => aux.location === 'SW');
+  // return neAnimals;
+  return {
+    NE: neAnimals.map((aux) => `${aux.name}`),
+    NW: nwAnimals.map((aux) => `${aux.name}`),
+    SE: seAnimals.map((aux) => `${aux.name}`),
+    SW: swAnimals.map((aux) => `${aux.name}`),
+  };
 }
 
 module.exports = getAnimalMap;
+
+/*    NE: ['lions', 'giraffes'],
+      NW: ['tigers', 'bears', 'elephants'],
+      SE: ['penguins', 'otters'],
+      SW: ['frogs', 'snakes'], */
+console.log(getAnimalMap());
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,10 +1,21 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
   // seu código aqui
+  const managers = employees.map((aux) => `${aux.managers}`);
+  return managers.some((aux) => aux.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else {
+    return employees
+      .filter((aux) => aux.managers.includes(managerId))
+      .map((aux) => `${aux.firstName} ${aux.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
