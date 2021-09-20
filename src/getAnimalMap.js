68d32eb22619db976d6476da9a173d72b17632f0
herzogcassia <cassiahherzog@gmail.com>
2021-09-20 12:53:49 -0300

    requisito 9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,6 +1,12 @@
 const data = require('../data/zoo_data');
+const { location } = data;
 
 function getAnimalMap(options) {
+  if (!options) {
+    const allAnimalsForlocal = options.filter((animal) => animal.location).length;
+    allAnimalsForlocal.reduce((accumulator, currenteValue) )
+    console.log(allAnimalsForlocal);
+  }
   // seu código aqui
 }
 
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { employees, species } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const employee1 = employees.find((employee) => employee.id === id).responsibleFor;
+  const getAnimalResposable = species.find((specie) => specie.id === employee1[0]).residents;
+  const getOlderAnimal = getAnimalResposable.reduce((acc, current) =>
+    ((acc.age > current.age) ? acc : current));
+  return Object.values(getOlderAnimal);
 }
 
 module.exports = getOldestFromFirstSpecies;
