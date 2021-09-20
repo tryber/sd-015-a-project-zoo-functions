f0dccb348aa045838f94966034df903dcf733b96
arturbacilla <artur.bacilla1@gmail.com>
2021-09-20 19:45:02 -0300

    Requisite 10 - Finished

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -8,25 +8,33 @@ function getAnimalLocation(whichAnimal) {
     animal.name === whichAnimal || animal.id === whichAnimal).location;
 }
 
+function getEmployeeID(param) {
+  return employees.filter((what) =>
+    what.id === param || what.firstName === param || what.lastName === param)[0].id;
+}
+
+function convertAnimalId(animalID) {
+  return species.find((specie) => specie.id === animalID).name;
+}
+
 function fullCoverage() {
-  const fullCoverage = employees.map((key) => ({
+  return employees.map((key) => ({
     id: key.id,
     fullName: `${key.firstName} ${key.lastName}`,
-    species: key.responsibleFor,
+    species: key.responsibleFor.map((currSpecie) => convertAnimalId(currSpecie)),
+    locations: key.responsibleFor.map((thatSpecie) => getAnimalLocation(thatSpecie)),
   }));
-  return fullCoverage;
 }
 
 function getEmployeesCoverage(givenParameter) {
   if (!givenParameter || Object.keys(givenParameter).length === 0) {
-    // no param, fullCoverage
-  } else {
-    const givenKey = Object.values(givenParameter)[0];
-    const thatEmployee = getEmployeeByName(givenKey);
-    if (thatEmployee === undefined) throw new Error('Informações inválidas');
-    console.log(fullCoverage());
-    // console.log((`givenKey: ${givenKey} and employee: ${thatEmployee}`));
+    return fullCoverage();
   }
+  const givenKey = Object.values(givenParameter)[0];
+  const thatEmployee = getEmployeeByName(givenKey);
+  if (thatEmployee === undefined) throw new Error('Informações inválidas');
+  const completeCoverage = fullCoverage();
+  return completeCoverage.find((person) => person.id === getEmployeeID(givenKey));
 }
 
 module.exports = getEmployeesCoverage;
