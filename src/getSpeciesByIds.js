26969250bb5ba55505a2a427c7296468cf9ca764
djsilva <coxcorp@gmail.com>
2021-09-20 16:43:27 -0300

    requisito 01 atualização

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,9 +1,10 @@
-// const data = require('../data/zoo_data');
-const { species } = require('../data/zoo_data');
+const data = require('../data/zoo_data');
 
 // function getSpeciesByIds(ids) {
-  // seu código aqui
-  const getSpeciesByIds = (...ids) => species.filter((unit) => ids.includes(unit.id));
+// seu código aqui
+const getSpeciesByIds = (...ids) =>
+  data.species.filter((unit) =>
+    ids.includes(unit.id));
 // }
 
 module.exports = getSpeciesByIds;
