55186f9dad60310c4509cd65a5ef7b71c2030d3f
LuisFillipeSoares <jobluisfillipe@gmail.com>
2021-09-20 15:52:38 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,17 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((employ) => employ.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const verificaManager = isManager(managerId);
+
+  if (verificaManager) {
+    const employ = data.employees.filter((objeto) => objeto.managers.includes(managerId));
+    return employ.map((obj) => `${obj.firstName} ${obj.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
