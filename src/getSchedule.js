33fa1d5467eb4e819bc92e9c7bb2bb64fa6ca943
arturbacilla <artur.bacilla1@gmail.com>
2021-09-19 23:28:26 -0300

    Requisite 8: Basic logic

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+const { species, hours } = data;
+const daysOfWeek = Object.keys(hours);
+
+function getSpeciesList() {
+  const speciesList = [];
+  species.forEach((specie) => {
+    speciesList.push(specie.name);
+  });
+  return speciesList;
+}
+
+function getHour(whichDay) {
+  const hourOfDay = hours[whichDay];
+  return hourOfDay;
+}
+
+function completeSchedule() {
+  const fullSchedule = daysOfWeek.map((day) =>
+    ({ [day]: { officeHour: `Open from ${getHour(day).open}am until ${getHour(day).close}pm` } }));
+  // const result = fullSchedule.reduce((a, v) => ({ a, [v]: v }), {});
+  return typeof (fullSchedule);
+  // console.log(fullSchedule);
+}
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  console.log(completeSchedule());
 }
 
 module.exports = getSchedule;
