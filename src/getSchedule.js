d7e21d811b9aed17862d92b03c5b28ce32b7fd31
arturbacilla <artur.bacilla1@gmail.com>
2021-09-20 01:29:12 -0300

    Requisite 8: Finished all functions

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -16,16 +16,49 @@ function getHour(whichDay) {
   return hourOfDay;
 }
 
+// const sumNumbers = (array) => array.filter(getEven).reduce(sumPair)
+function getAnimalsOfDay(weekDay) {
+  const result = [];
+  const specieResidents = species.filter((specie) => specie.availability.includes(weekDay));
+  specieResidents.forEach((element) => {
+    result.push(element.name);
+  });
+  return result;
+}
+
 function completeSchedule() {
-  const fullSchedule = daysOfWeek.map((day) =>
-    ({ [day]: { officeHour: `Open from ${getHour(day).open}am until ${getHour(day).close}pm` } }));
-  // const result = fullSchedule.reduce((a, v) => ({ a, [v]: v }), {});
-  return fullSchedule[0];
-  // console.log(fullSchedule);
+  const fullSchedule = daysOfWeek.map((day) => ({
+    [day]: {
+      officeHour: `Open from ${getHour(day).open}am until ${
+        getHour(day).close
+      }pm`,
+      exhibition: getAnimalsOfDay(day),
+    },
+  })).reduce((acc, item) => {
+    const key = Object.keys(item)[0];
+    acc[key] = item[key];
+    return acc;
+  }, {});
+  fullSchedule.Monday.officeHour = 'CLOSED';
+  fullSchedule.Monday.exhibition = 'The zoo will be closed!';
+  return fullSchedule;
 }
 
 function getSchedule(scheduleTarget) {
-  console.log(completeSchedule());
+  let result;
+  if (daysOfWeek.includes(scheduleTarget)) {
+    const fullSchedule = completeSchedule();
+    result = { [scheduleTarget]: fullSchedule[scheduleTarget] };
+    // console.log(`its an weekday: ${scheduleTarget}`);
+  } else if (getSpeciesList().includes(scheduleTarget)) {
+    result = species.find((whichAnimal) =>
+      whichAnimal.name === scheduleTarget).availability;
+  } else {
+    result = completeSchedule();
+  }
+  // console.log(scheduleTarget);
+  // console.log(result);
+  return result;
 }
 
 module.exports = getSchedule;
