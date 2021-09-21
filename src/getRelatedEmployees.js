b468dd77ad4d022afe0aacf85ce1dc56f12727ab
Vini_Vergani <vinivergani95@gmail.com>
2021-09-21 11:56:33 -0300

    Adiciona requisito 4 - lint

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,15 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  return data.employees.some((itsAManager) => itsAManager.managers.includes(id))
+  return data.employees.some((itsAManager) => itsAManager.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  if (isManager(managerId) === false){
+  if (isManager(managerId) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
-  const employeesManaged = data.employees.filter((manager) => manager.managers.includes(managerId));
-  const peopleManaged = employeesManaged.map((employee) => `${employee.firstName} ${employee.lastName}`);
+  const empManaged = data.employees.filter((manager) => manager.managers.includes(managerId));
+  const peopleManaged = empManaged.map((employee) => `${employee.firstName} ${employee.lastName}`);
   return peopleManaged;
 }
 
