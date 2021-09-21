3ea2bdfc80ef7270d4eff7c979433ba8916f0a42
Lucas Felipe Ribeiro <luc.felipe42@gmail.com>
2021-09-20 23:27:53 -0300

    req 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getSchedule(scheduleTarget) {
+const horario = (dia) => {
+  const funcionamento = `Open from ${hours[dia].open}am until ${hours[dia].close - 12}pm`;
+  return funcionamento;
+};
+
+function getSchedule(dayName) {
   // seu código aqui
+  const arr = Object.keys(hours);
+  const dias = {};
+  if (dayName === undefined) {
+    for (let i = 0; i < arr.length - 1; i += 1) {
+      dias[arr[i]] = horario(arr[i]);
+    }
+    dias.Monday = 'CLOSED';
+    return dias;
+  }
+  if (dayName === 'Monday') {
+    dias.Monday = 'CLOSED';
+    return dias;
+  }
+  dias[dayName] = horario(dayName);
+  return dias;
 }
 
 module.exports = getSchedule;
