f92cacf62efe658d91ef5b57e9fd72d0af6505a8
Seu nome <mgabiss10@gmail.com>
2021-09-21 11:48:12 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,18 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((employSome) => employSome.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    const collaborators = employees.filter((employFilter) =>
+      employFilter.managers.includes(managerId));
+    const result = collaborators.map((employMap) => `${employMap.firstName} ${employMap.lastName}`);
+    return result;
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
