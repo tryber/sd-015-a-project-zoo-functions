c1afc650ee8fb08bc2d4bb4a6f70ae44a781ae41
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-21 11:03:22 -0300

    ajuste

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -14,4 +14,5 @@ function countAnimals(animal) {
   const genderReport = specieReport.filter((resident) => resident.sex === animal.gender);
   return genderReport.length;
 }
+
 module.exports = countAnimals;
