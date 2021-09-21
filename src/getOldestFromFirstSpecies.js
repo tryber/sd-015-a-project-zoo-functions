cccda49bcb7d6aaa90d6a5c0459be11f7cf35025
Vini_Vergani <vinivergani95@gmail.com>
2021-09-21 10:25:31 -0300

    Adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const emplyee = data.employees.find((worker) => worker.id === id);
+  const resident = data.species.find((findSpecie) => emplyee.responsibleFor[0] === findSpecie.id);
+  const sortOlder = resident.residents.sort((a, b) => b.age - a.age)[0];
+  return Object.values(sortOlder);
 }
 
 module.exports = getOldestFromFirstSpecies;
