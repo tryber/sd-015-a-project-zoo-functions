e06a9f9bfb59a56aa0aab1342d085f3543ea5805
antoniofortisneto <antoniofortis@gmail.com>
2021-09-21 05:37:54 -0300

    9 concluida

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
+// const { species } = require('../data/zoo_data');
 
 function countAnimals(animal) {
   // seu código aqui
+
 }
 
 module.exports = countAnimals;
--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const worker = employees.find((workerM) => workerM.id === id).responsibleFor;
+  const beast = species.find((anim) => anim.id === worker[0]).residents;
+  const oldBeast = beast.reduce((accumulator, curentValue) =>
+    ((accumulator.age > curentValue.age) ? accumulator : curentValue));
+  return Object.values(oldBeast);
 }
 
 module.exports = getOldestFromFirstSpecies;
+// para formular a linha 9 esse site me ajudou https://ichi.pro/pt/o-que-faz-o-ponto-de-interrogacao-significa-no-codigo-javascript-58536023095136
+// e este tbm https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce //
+// na linha 6 acho o funcionario e na linha 7 identifico o primeiro animal, linha 8 e 9 compararo idades, linha 10 basicamente mostro o que a funçao pediu//
