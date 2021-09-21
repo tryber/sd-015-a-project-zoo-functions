027a94b03fc9ab5bd090a99d8a4479b7b9b8b6e1
Raynan <raynanfernandes@yahoo.com.br>
2021-09-21 11:45:46 -0300

    Requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
 function countAnimals(animal) {
   // seu código aqui
+  const animals = {};
+  if (!animal) {
+    species.forEach(({ name, residents }) => {
+      animals[name] = residents.length;
+    });
+    return animals;
+  }
+  const animalSelected = species.find((spec) => spec.name === animal.specie);
+
+  if (animal.gender) {
+    const sexSelected = animalSelected.residents
+      .filter((resident) => resident.sex === animal.gender);
+    return sexSelected.length;
+  }
+  return animalSelected.residents.length;
 }
 
 module.exports = countAnimals;
