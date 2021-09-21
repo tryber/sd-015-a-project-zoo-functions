adc1aafd25243f62f85b63b11f6519bee5b90916
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-20 22:44:57 -0300

    add req 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,6 +2,16 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  const employeeArr = data.employees.filter((employee) => {
+    return (
+      employee.firstName === employeeName || employee.lastName === employeeName
+    );
+  });
+  return employeeArr[0];
 }
+console.log(getEmployeeByName());
 
 module.exports = getEmployeeByName;
