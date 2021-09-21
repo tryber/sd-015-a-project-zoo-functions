d54c5864a148f518bf0ddc3f724485210f6b2b12
Vitor <tutornews2@gmail.com>
2021-09-21 00:17:57 -0300

    Adiciona requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const vars = species.find((objeto) => objeto.name === animal);
+  return vars.residents.every((checar) => checar.age >= age);
 }
 
 module.exports = getAnimalsOlderThan;
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,7 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  return species.filter((animalId) => ids.includes(animalId.id));
+  // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+  return species.filter((idBusca) => ids.includes(idBusca.id));
 }
 
 module.exports = getSpeciesByIds;
