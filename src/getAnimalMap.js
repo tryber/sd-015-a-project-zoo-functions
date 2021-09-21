2faf28f64deb327181b0fa91b876f9466b759734
antoniofortisneto <antoniofortis@gmail.com>
2021-09-21 12:12:03 -0300

    agora foiiiiiiiiiiii

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const aniMap = {
+  NW: [],
+  NE: [],
+  SW: [],
+  SE: [],
+};
+data.species.map((beast) => aniMap[beast.location].push(beast.name));
+
 function getAnimalMap(options) {
   // seu código aqui
+  if (!options) {
+    return aniMap;
+  }
+  if (options.includeNames === true) {
+    return 0;
+  }
 }
-
 module.exports = getAnimalMap;
