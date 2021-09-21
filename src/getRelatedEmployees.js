69dc1b3d0b7b28509dfaaba18289a0517576d125
Vitor <tutornews2@gmail.com>
2021-09-21 07:54:35 -0300

    Adiciona requisito 4.

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  // verifica se o id que for passado aqui está incluso em em.managers = em.managers.includes(id)
+  return employees.some((em) => em.managers.includes(id));
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId)) {
+    // filtra todos os objetos que tem managerId e joga em um array
+    const objetoNome = employees.filter((names) => names.managers.includes(managerId));
+    // retorna uma array com template literal fistName e lastName do arrray com objetos objetoNome
+    return objetoNome.map((ob) => `${ob.firstName} ${ob.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -3,6 +3,7 @@ const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
   // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/String/includes
+  // filter cria um array vazio, caso não exista um elemento que satisfaça[sem paremetro] ele devolve o array vazio(pois todos os outros elemetos foram rejeitados).
   return species.filter((idBusca) => ids.includes(idBusca.id));
 }
 
