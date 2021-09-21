1a2e7de4894e642bc310854518c274e4f66d1704
GTmudin <testederealismo@gmail.com>
2021-09-21 12:36:21 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,37 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function novosDados() {
+  return employees.map((elemento) => ({
+    id: elemento.id,
+    fullName: `${elemento.firstName} ${elemento.lastName}`,
+    species: elemento.responsibleFor.map((elemen) => species.find((elemen2) => elemen2.id === elemen)
+      .name),
+    locations: elemento.responsibleFor.map((elemen) => species.find((elemen2) => elemen2.id === elemen)
+      .location),
+  }));
 }
+
+function getEmployeesCoverage(arg) {
+  if (!arg) {
+    return novosDados();
+  }
+  const { id, name } = arg;
+
+  if (employees.find((rg) => (rg.firstName === name || rg.lastName === name || rg.id === id))) {
+    const ray = employees.find((rg) => (rg.firstName === name || rg.lastName === name || rg.id === id));
+    return {
+      id: ray.id,
+      fullName: `${ray.firstName} ${ray.lastName}`,
+      species: ray.responsibleFor.map((ele) => species.find((ele2) => ele2.id === ele)
+        .name),
+      locations: ray.responsibleFor.map((ele) => species.find((ele2) => ele2.id === ele)
+        .location),
+    };
+  }
+  throw new Error('Informações inválidas');
+}
+
+console.log(getEmployeesCoverage({ name: 'Sharonda' }));
 
 module.exports = getEmployeesCoverage;
