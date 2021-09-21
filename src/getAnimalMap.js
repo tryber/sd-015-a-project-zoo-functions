93af1a00c841d698fd25ade8e181f638bfdaefa5
thomazj <thomaz.jefferson01@protonmail.com>
2021-09-21 13:50:13 -0300

    requisito 9

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,44 @@
 const data = require('../data/zoo_data');
+const { species } = data
 
 function getAnimalMap(options) {
   // seu código aqui
+  if(options === undefined) {
+    return especieAnimalLocaliza()
+  } 
 }
 
+function especieAnimalLocaliza() {
+  const areasObjeto = {
+    NE: species.filter((element) => element.location.includes('NE')).map((element) => element.name),
+    NW: species.filter((element) => element.location.includes('NW')).map((element) => element.name),
+    SE: species.filter((element) => element.location.includes('SE')).map((element) => element.name),
+    SW: species.filter((element) => element.location.includes('SW')).map((element) => element.name)
+  }
+  return areasObjeto;
+}  
+
+function criarObjetoNome() {
+  const NE = species.filter((element) => element.location.includes('NE')).map((element) => element.name);
+  
+  const objetoReposta = {
+    NE
+  }
+  
+  console.log(objetoReposta)
+}
+
+criarObjetoNome()
+
+
+
 module.exports = getAnimalMap;
+
+
+
+// species.filter((element) => element.location.includes('NE')).map((element) => element.residents)
+
+// function ordenarElementos(elemento) {
+//   const objeto = Object.values(elemento).sort()
+//   return objeto
+// }
