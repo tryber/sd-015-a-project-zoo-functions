4c3ab1745f45853da3bc82a316e27914fd3ae233
Vini_Vergani <vinivergani95@gmail.com>
2021-09-21 11:23:06 -0300

    Adiciona requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,29 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const visitors = { child: 0, adult: 0, senior: 0};
+   visitors.child = entrants.filter((visitor) => visitor.age < 18).length;
+   visitors.senior = entrants.filter((visitor) => visitor.age >= 50).length;
+   visitors.adult = entrants.filter((visitor) => visitor.age >=18 && visitor.age < 50).length;
+return visitors;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+if(entrants === undefined || Object.keys(entrants).length === 0) {
+  return 0;
+}
+  const visitors = { child: 0, adult: 0, senior: 0};
+  visitors.child = entrants.filter((visitor) => visitor.age < 18).length;
+  visitors.senior = entrants.filter((visitor) => visitor.age >= 50).length;
+  visitors.adult = entrants.filter((visitor) => visitor.age >=18 && visitor.age < 50).length;
+
+  const childPrice = visitors.child * prices.child;
+  const adultPrice = visitors.adult * prices.adult;
+  const seniorPrice = visitors.senior * prices.senior;
+
+  const totalEntry = childPrice + adultPrice + seniorPrice;
+  return totalEntry;
 }
 
 module.exports = { calculateEntry, countEntrants };
