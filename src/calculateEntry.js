64fd8f62c3f9ed97c12656783548bc78f3ab44ad
Douglas <douglascoelho31@hotmail.com>
2021-09-21 13:25:42 -0300

    requisito 6 e 10 ok

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,10 +1,23 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
+  const child = entrants.filter((criancinha) => criancinha.age < 18).length;
+  const adult = entrants.filter((adulto) => adulto.age >= 18 && adulto.age < 50).length;
+  const senior = entrants.filter((idosso) => idosso.age >= 50).length;
+  return { child, adult, senior };
   // seu código aqui
 }
 
 function calculateEntry(entrants) {
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  { const child = entrants.filter((criancinha) => criancinha.age < 18).length;
+    const adult = entrants.filter((maiorIda) => maiorIda.age >= 18 && maiorIda.age < 50).length;
+    const senior = entrants.filter((idosso) => idosso.age >= 50).length;
+    return (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
+  }
   // seu código aqui
 }
 
--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,38 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+function fazendo() {
+  return data.employees.map((elemento) => ({
+    id: elemento.id,
+    fullName: `${elemento.firstName} ${elemento.lastName}`,
+    species: elemento.responsibleFor.map((elementod) => species.find((elementot) => elementot
+      .id === elementod).name),
+    locations: elemento.responsibleFor.map((elementod) => species.find((elementot) => elementot
+      .id === elementod).location),
+  }));
 }
 
+function getEmployeesCoverage(esperando) {
+  if (!esperando) {
+    return fazendo();
+  }
+  const { id, name } = esperando;
+  if (employees.find((ach) => (ach.firstName === name || ach.lastName === name || ach.id === id))) {
+    const objeto = employees.find((ach) => (ach.firstName === name || ach.lastName === name || ach
+      .id === id));
+    return {
+      id: objeto.id,
+      fullName: `${objeto.firstName} ${objeto.lastName}`,
+      species: objeto.responsibleFor.map((elemento) => species.find((elementod) => elementod
+        .id === elemento).name),
+      locations: objeto.responsibleFor.map((elemento) => species.find((elementod) => elementod
+        .id === elemento).location),
+    };
+  }
+  throw new Error('Informações inválidas');
+  // seu código aqui
+}
+console.log(getEmployeesCoverage({ id: 'c1f50212-35a6-4ecd-8223-f835538526c2' }));
+
 module.exports = getEmployeesCoverage;
+// coleguinha luan me deu esse help para execucao desta!
