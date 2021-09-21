c72fea273fb2fb9dd49aa82c6e6987597914aeec
phillipe <rodrigues153@gmail.com>
2021-09-21 20:54:55 -0300

    adiciona requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,15 +2,10 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...param) {
-  const animalSelecionado = species.filter((element) => {
-     if (element.id === param) {
-       return element;
-     }
-    if (param === undefined) return ['a'];
-   });
- return animalSelecionado;
-}
-// getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
- console.log(getSpeciesByIds());
+  console.log(param);
+  const animalSelecionado = species.filter((element) => param.includes(element.id));
+  return animalSelecionado;
+}// getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
+console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce',
+  'e8481c1d-42ea-4610-8e11-1752cfc05a46'));
 module.exports = getSpeciesByIds;
-'0938aa23-f153-4937-9f88-4858b24d6bce'
