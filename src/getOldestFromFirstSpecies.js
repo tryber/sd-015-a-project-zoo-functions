8c90224019cd6323b69592522a0859cc62c43114
Vitor <tutornews2@gmail.com>
2021-09-21 13:47:18 -0300

    Adiciona requisito 9.

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,12 @@
+const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// repositorio do José Carlos consultado nessa parte  - encurtador.com.br/EFH48
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const funcio = employees.find((func) => func.id === id);
+  const animals = species.find((animal) => animal.id === funcio.responsibleFor[0]).residents;
+  const getOldest = animals.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr));
+  return Object.values(getOldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
