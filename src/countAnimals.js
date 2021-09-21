aa81a6118ae496b30b4a972684d9600ff5607b56
GTmudin <testederealismo@gmail.com>
2021-09-20 21:39:20 -0300

    teste gender/sex

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -2,22 +2,24 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-
+  const tods = {};
+  if (arguments.length === 0) {
+    species.forEach((elemento) => {
+      tods[`${elemento.name}`] = elemento.residents.length;
+    });
+    return tods;
+  }
+  if (Object.keys(animal).length === 1) {
+    const argAnimal = species.find((elemento) => elemento.name === animal.specie);
+    return argAnimal.residents.length;
+  }
+  const argGenderAnimal = species.find((elemento) => elemento.name === animal.specie);
+  const selecGender = argGenderAnimal.residents.filter((elemento) => elemento.sex === animal.sex);
+  return selecGender.length;
 }
 
-console.log(countAnimals({ specie: 'penguins' }));
+console.log(countAnimals({ specie: 'elephants', sex: 'male' }));
 
 module.exports = countAnimals;
 
-// Esta função é responsável por contabilizar a quantidade de animais de cada espécie.
-
-// Observações técnicas
-
-// Se nenhum argumento for passado, retorna um objeto cujo o nome de cada espécie é uma chave desse objeto, e o total de animais dessa espécie é o seu valor;
-// Com o argumento { specie: 'penguins' }, retorna um número, a quantidade de pinguins no zoológico;
-// Com o argumento { specie: 'giraffes', sex: 'female' }, retorna um número, a quantidade de girafas do gênero feminino.
-// O que será avaliado
-
-// Sem parâmetros, retorna as espécies e sua quantidade;
-// Recebendo como parâmetro um objeto com a chave specie, retorna um número, a quantidade de animais daquela espécie;
-// Recebendo como parâmetro um objeto com a chave specie e sex, retorna um número, a quantidade de animais daquela espécie, no gênero selecionado.
+// const todosOsAnimais = species.map((elemento) => `${elemento.name} ${elemento.residents.length}`);
