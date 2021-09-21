83b1e9b8eabdcb863e9c66b2a91ef1642c938a51
GTmudin <testederealismo@gmail.com>
2021-09-20 21:43:57 -0300

    eslint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,25 +1,7 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // let child = 0;
-  // let adult = 0;
-  // let senior = 0;
 
-  // for (let index = 0; index < entrants.length; index += 1) {
-  //   if (arguments.length === 0 || arguments[0] === {}) {
-  //     return 0;
-  //   }
-
-  //   if (entrants[index].age < 18) {
-  //     child += 1;
-  //   } else if (entrants[index].age >= 18 && entrants[index].age < 50) {
-  //     adult += 1;
-  //   } else {
-  //     senior += 1;
-  //   }
-  // }
-
-  // return (`{ "child": ${child}, "adult": ${adult}, "senior": ${senior} }`);
 }
 
 function calculateEntry(entrants) {
@@ -27,3 +9,23 @@ function calculateEntry(entrants) {
 }
 
 module.exports = { calculateEntry, countEntrants };
+
+// let child = 0;
+// let adult = 0;
+// let senior = 0;
+
+// for (let index = 0; index < entrants.length; index += 1) {
+//   if (arguments.length === 0 || arguments[0] === {}) {
+//     return 0;
+//   }
+
+//   if (entrants[index].age < 18) {
+//     child += 1;
+//   } else if (entrants[index].age >= 18 && entrants[index].age < 50) {
+//     adult += 1;
+//   } else {
+//     senior += 1;
+//   }
+// }
+
+// return (`{ "child": ${child}, "adult": ${adult}, "senior": ${senior} }`);
