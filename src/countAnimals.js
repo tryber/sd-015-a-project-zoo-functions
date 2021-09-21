c3bcec34595b648c5e80111ead72702e94262b95
SaraRabelo <rabelorsara@gmail.com>
2021-09-21 11:18:26 -0300

    Requisito 5 OK

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,33 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
+function countName(animal) {
+  let namespecie = {};
+  if (animal.sex === undefined) {
+    const relSpecie = data.species.find((element) => element.name === animal.specie);
+    namespecie = relSpecie.residents.length;
+    return (namespecie);
+  }
+  const filterResidents = data.species.find((element) => (element.name === animal.specie));
+  const arrayResidentes = filterResidents.residents;
+  let qte = 0;
+  arrayResidentes.forEach((element, index) => {
+    if (element.sex === animal.sex) {
+      qte += 1;
+    }
+  });
+  return qte;
 }
 
+function countAnimals(animal) {
+  const namespecie = {};
+  if (animal === undefined) {
+    data.species.forEach(({ name, residents }) => {
+      namespecie[name] = residents.length;
+    });
+    console.log(namespecie);
+    return (namespecie);
+  }
+  return countName(animal);
+}
+countAnimals({ specie: 'bears', sex: 'female' });
 module.exports = countAnimals;
