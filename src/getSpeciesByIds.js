181097170db4d60194b715817328b59e56537af6
Seu nome <mgabiss10@gmail.com>
2021-09-21 11:04:22 -0300

    Requisito 1 refeito

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,7 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getSpeciesByIds(...ids) {
-  // eslint-disable-next-line no-undef
   return species.filter((specie) => ids.includes(specie.id));
 }
 
