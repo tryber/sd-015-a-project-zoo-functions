14d774614205dda4fb392aac6712167d78adcd17
Ricardo Anjos <ricardoanj.14@gmail.com>
2021-09-21 11:45:57 -0300

    requisto 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+function getAnimalsOlderThan(animal, minAge) {
+  const { residents } = data.species.find((species) => species.name === animal);
+  return residents.every((resident) => residents.age >= minAge);
 }
 
 module.exports = getAnimalsOlderThan;
