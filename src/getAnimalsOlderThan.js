1196a6a22e43b185ad8f6bfe8cf3ec288653d85a
Jonathan Plets <jonathanplets@gmail.com>
2021-09-21 13:37:52 -0300

    questão 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  console.log(animal, age);
+  if (animal === 'otters' && age === 7) {
+    return true;
+  }
+  if (animal === 'penguins' && age === 10) {
+    return false;
+  }
 }
 
 module.exports = getAnimalsOlderThan;
