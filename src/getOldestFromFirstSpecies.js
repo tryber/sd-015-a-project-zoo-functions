debc3882ed1344ad7e29e7b1dedf6c2ebdc231e0
Douglas <douglascoelho31@hotmail.com>
2021-09-21 13:40:25 -0300

    requisito 9 ok

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,19 @@
+// const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
+  const pegarId = data.employees.find((elemento) => elemento.id === id).responsibleFor[0];
+  const pegarAnimal = data.species.find((elemento) => elemento.id === pegarId);
+  const animais = pegarAnimal.residents;
+  const animaisAntigos = animais.reduce((acumulador, elemento) => {
+    if (acumulador === null || elemento.age > acumulador) return elemento.age;
+    return acumulador;
+  }, null);
+  const pegandoAnimais = animais.filter((elemento) => elemento.age === animaisAntigos)[0];
+  const {name, sex, age } = pegandoAnimais;
+  return [name,sex, age];
   // seu código aqui
 }
+// amigo andrey de novais me ajudou nessa
 
 module.exports = getOldestFromFirstSpecies;
