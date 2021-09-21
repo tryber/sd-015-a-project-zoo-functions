3c3aa6a20be139f7b2f040d5853c2d0c4f67e78a
Seu nome <mgabiss10@gmail.com>
2021-09-21 11:01:29 -0300

    Requisito 1

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,11 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  const every = species.every((animals, ages) => animals.residents.age >= ages);
+  return every;
 }
+
+console.log(getAnimalsOlderThan('others', 7));
 
 module.exports = getAnimalsOlderThan;
