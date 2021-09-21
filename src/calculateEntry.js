09cffe42a7c874532634525dfc9d3d61b1ad6f02
Vini_Vergani <vinivergani95@gmail.com>
2021-09-21 11:25:22 -0300

    Adiciona requisito 6 - lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -2,21 +2,21 @@ const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const visitors = { child: 0, adult: 0, senior: 0};
-   visitors.child = entrants.filter((visitor) => visitor.age < 18).length;
-   visitors.senior = entrants.filter((visitor) => visitor.age >= 50).length;
-   visitors.adult = entrants.filter((visitor) => visitor.age >=18 && visitor.age < 50).length;
-return visitors;
+  const visitors = { child: 0, adult: 0, senior: 0 };
+  visitors.child = entrants.filter((visitor) => visitor.age < 18).length;
+  visitors.senior = entrants.filter((visitor) => visitor.age >= 50).length;
+  visitors.adult = entrants.filter((visitor) => visitor.age >= 18 && visitor.age < 50).length;
+  return visitors;
 }
 
 function calculateEntry(entrants) {
-if(entrants === undefined || Object.keys(entrants).length === 0) {
-  return 0;
-}
-  const visitors = { child: 0, adult: 0, senior: 0};
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const visitors = { child: 0, adult: 0, senior: 0 };
   visitors.child = entrants.filter((visitor) => visitor.age < 18).length;
   visitors.senior = entrants.filter((visitor) => visitor.age >= 50).length;
-  visitors.adult = entrants.filter((visitor) => visitor.age >=18 && visitor.age < 50).length;
+  visitors.adult = entrants.filter((visitor) => visitor.age >= 18 && visitor.age < 50).length;
 
   const childPrice = visitors.child * prices.child;
   const adultPrice = visitors.adult * prices.adult;
