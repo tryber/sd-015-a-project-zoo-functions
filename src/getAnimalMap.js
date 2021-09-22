15c5779226b92a2b4cf4afebf2dcfc3c6cc2da8c
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2021-09-22 15:19:48 -0300

    requisito 10

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,49 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+
+const receiveLocation = (location) => {
+  return species
+    .filter((locationAnimal) => locationAnimal.location === location)
+    .map((arrayAnimals) => arrayAnimals.name);
+};
+// const receiveLocation2 = (location) => {
+//   return species
+//     .filter((locationAnimal) => locationAnimal.location === location)
+//     .reduce((atual, current) => , [])
+// };
+// console.log(receiveLocation2('NE'));
+
+const allLocations = () => {
+  const locations = ['NE', 'NW', 'SE', 'SW'];
+  return locations.reduce((objectLocations, currentLocation) => {
+    const finalObject = objectLocations;
+    finalObject[currentLocation] = receiveLocation(currentLocation);
+    return finalObject;
+  }, {});
+};
+// const newobjs = receiveLocation('NE').reduce((objectAnimals, currentAnimal) => {
+//   const objctFinal = objectAnimals;
+//   objctFinal.push({
+//     [currentAnimal]: species.find((element) => element.name === currentAnimal),
+//   });
+//   return objctFinal;
+// }, []);
+
+// const locationWithName = () => {
+//   const locations = ['NE', 'NW', 'SE', 'SW'];
+//   return locations.reduce((objectLocations, currentLocation) => {
+//     const finalObject = objectLocations;
+//     finalObject[currentLocation] = receiveLocation(currentLocation).reduce(() =>);
+//     return finalObject;
+//   }, {});
+// }
+
 function getAnimalMap(options) {
-  // seu código aqui
+  if (!options) return allLocations();
+
+  // if(options.includeNames === true)
 }
 
+console.log(getAnimalMap());
 module.exports = getAnimalMap;
