98e58b7fbac43bf693333f2e583b46453645729c
LHenriquerios <henriquerios4@gmail.com>
2021-09-21 21:52:00 -0300

    Reuisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    return data.species.reduce((acc, { name, residents }) => {
+      Object.assign(acc, { [name]: residents.length });
+      return acc;
+    }, {});
+  }
+  if (!animal.gender) {
+    return data.species.find((specie) => specie.name === animal.specie).residents.length;
+  }
+  if (animal.gender) {
+    return data.species.find((specie) =>
+      specie.name === animal.specie).residents.filter((resident) =>
+      resident.sex === animal.gender).length;
+  }
 }
 
 module.exports = countAnimals;
