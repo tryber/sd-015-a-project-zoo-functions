2e5b988f2b9e20abd58943ee5efc56771c0a4c57
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-23 08:40:26 -0300

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -8,21 +8,14 @@ function isManager(id) {
   return verifyId;
 }
 // const id = '0e7b460e-acf4-4e17-bcb3-ee472265db83';
-function getRelatedEmployees(managerId, id) {
-  // if (isManager(id)) {
-//     const manager = employees.find((employee) => employee.managers.find((manager) => manager === id));
-//     console.log(manager.responsibleFor);
-//     const { responsibleFor } = manager;
-// const nameEmployees = responsibleFor.map((under) => {
-//   const a = under;
-//   employees.find((employee) => employee.id === a).firstName
-// });
-//   console.log(nameEmployees);
-  // return nameEmployees;
-  // return manager.responsibleFor;
-  // }
-  // console.log(nameEmployees);
-  //   throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+function getRelatedEmployees(managerId) {
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const underManager = employees.filter((employee) => employee.managers
+    .find((manager) => manager === managerId));
+  const namesUnderManager = underManager.map((under) => `${under.firstName} ${under.lastName}`);
+  return namesUnderManager;
 }
 
 module.exports = { isManager, getRelatedEmployees };
