178788365fb359d6387d580d4e66ec4b7086f885
Ricardo Anjos <ricardoanj.14@gmail.com>
2021-09-23 18:33:23 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (!animal) {
+    const animals = {};
+    data.species.forEach((specieName) => {
+      animals[specieName.name] = specieName.residents.length;
+    });
+    return animals;
+  }
+  const { specie, gender } = animal;
+  if (!gender) {
+    return data.species.find((specieName) => specieName
+      .name === specie).residents.length;
+  }
+  return data.species.find((specieName) => specieName.name
+  === specie).residents.filter((specieSex) => specieSex.sex === gender).length;
 }
 
 module.exports = countAnimals;
