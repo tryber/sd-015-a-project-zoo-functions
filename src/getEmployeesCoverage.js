0f388f21efa8fc49f1cc52ba9128a5ab1e1315ce
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-09-24 16:21:46 -0300

    requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,33 @@
-const data = require('../data/zoo_data');
+const { employees, species } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
+const oneemployeer = (employ) => ({
+  id: employ.id,
+  fullName: `${employ.firstName} ${employ.lastName}`,
+  species: employ.responsibleFor.map((animal) => species.find((spec) => spec.id === animal).name),
+  locations: employ.responsibleFor.map((animal) => species
+    .find((spec) => spec.id === animal).location),
+});
+
+const everyBody = () =>
+  employees.map((emplo) => ({
+    id: emplo.id,
+    fullName: `${emplo.firstName} ${emplo.lastName}`,
+    species: emplo.responsibleFor.map((anim) => species.find((spec) => spec.id === anim).name),
+    locations: emplo.responsibleFor.map((anim) => species
+      .find((spec) => spec.id === anim).location),
+  }));
+
+function getEmployeesCoverage(nameOrid) {
   // seu código aqui
+  if (!nameOrid) return everyBody();
+
+  const { name, id } = nameOrid;
+  const employeer = employees
+    .find((info) => info.id === id || info.firstName === name || info.lastName === name);
+
+  if (!employeer) throw new Error('Informações inválidas');
+
+  return oneemployeer(employeer);
 }
 
 module.exports = getEmployeesCoverage;
