76032733e221ba86c634bc4677c0f42352db16f0
Emmanuelle Pereira <emma.persil@gmail.com>
2021-09-25 16:29:55 -0300

    Corrige lint

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,13 +1,13 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const { employees, species} = data;
+  const { employees, species } = data;
   const employeesID = employees.find((empl) => empl.id === id);
   const responsible = employeesID.responsibleFor[0];
   const specie = species.find((animal) => animal.id === responsible);
-  const getOldest = specie.residents.reduce((oldest, current) => 
+  const getOldest = specie.residents.reduce((oldest, current) =>
     (oldest.age > current.age ? oldest : current));
-  
+
   return Object.values(getOldest);
 }
 
