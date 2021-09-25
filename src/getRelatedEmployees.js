2818ee1666793fe1a9256233545a1c1f502e7b0f
SaraRabelo <rabelorsara@gmail.com>
2021-09-25 20:02:58 -0300

    4a questão - Refatorando

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -7,24 +7,21 @@ function isManager(id) {
     if ((gerente[index].managers.length !== 0) && (!saida)) {
       const existe = gerente[index].managers;
       saida = existe.some((elemento) => elemento === id);
-    };
-  };
+    }
+  }
   return saida;
-};
+}
 
 function getRelatedEmployees(managerId) {
   arrayColabora = [];
-
   if (isManager(managerId)) {
     const gerente = data.employees
     for (let index = 0; index < gerente.length; index += 1) {
-      if (gerente[index].managers.length !== 0) {
         for (let index1 = 0; index1 < gerente[index].managers.length; index1 += 1) {
           if (gerente[index].managers[index1] === managerId) {
             arrayColabora.push(`${gerente[index].firstName} ${gerente[index].lastName}`);
           }
         }
-      }
     }
   }
   else {
@@ -32,8 +29,8 @@ function getRelatedEmployees(managerId) {
   }
   return(arrayColabora);
 }
-//getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
-console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
+//console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
+//console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 
 
 
