b90e9a72ce5a45b69337bae9823b76d054e55f3b
SaraRabelo <rabelorsara@gmail.com>
2021-09-25 16:34:56 -0300

    4a Questão Solucionada

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+ 
+  const gerente = data.employees
+  for (let index = 0; index < gerente.length; index += 1){
+    if (gerente[index].managers.length !== 0) {
+      for (let index1 = 0; index1 < gerente[index].managers.length; index1 += 1) {
+        if (gerente[index].managers[index1] === id) {
+          return true;
+        } 
+      }
+    }
+  }
+  return false
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  arrayColabora = [];
+ 
+  if (isManager(managerId)) {
+    const gerente = data.employees
+    for (let index = 0; index < gerente.length; index += 1){
+      if (gerente[index].managers.length !== 0) {
+        for (let index1 = 0; index1 < gerente[index].managers.length; index1 += 1) {
+          if (gerente[index].managers[index1] === managerId) {
+            arrayColabora.push(`${gerente[index].firstName} ${gerente[index].lastName}`);
+          } 
+        }
+      }
+    }  
+  }  
+  else {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return(arrayColabora);
 }
+getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
+
+
 
 module.exports = { isManager, getRelatedEmployees };
