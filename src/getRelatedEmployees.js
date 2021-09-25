28da36013aba538428e8ac560c840fb11419abac
SaraRabelo <rabelorsara@gmail.com>
2021-09-25 19:49:42 -0300

    4a questão - Refatorando

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,23 +1,21 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-
-  const gerente = data.employees
+  const gerente = data.employees;
+  let saida = false;
   for (let index = 0; index < gerente.length; index += 1) {
-    if (gerente[index].managers.length !== 0) {
-      for (let index1 = 0; index1 < gerente[index].managers.length; index1 += 1) {
-        if (gerente[index].managers[index1] === id) {
-          return true;
-        }
-      }
+    if ((gerente[index].managers.length !== 0) && (!saida)) {
+      let existe = gerente[index].managers;
+      saida = existe.some((elemento) => elemento === id);
+      
     }
   }
-  return false;
+  return saida;
 }
 
 function getRelatedEmployees(managerId) {
   arrayColabora = [];
-  
+
   if (isManager(managerId)) {
     const gerente = data.employees
     for (let index = 0; index < gerente.length; index += 1) {
@@ -35,7 +33,8 @@ function getRelatedEmployees(managerId) {
   }
   return(arrayColabora);
 }
-getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
+//getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992');
+console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 
 
 
