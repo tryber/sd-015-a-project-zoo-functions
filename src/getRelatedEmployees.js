9adcf50e551896410e1e4c7657fc502ef338ca07
SaraRabelo <rabelorsara@gmail.com>
2021-09-25 20:50:52 -0300

    4a questão - Refatorando

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -12,24 +12,26 @@ function isManager(id) {
   return saida;
 }
 
-function getRelatedEmployees(managerId) {
-  let arrayColabora = [];
-  if (isManager(managerId)) {
-    const gerente = data.employees;
-    gerente.forEach((elemento, index) => {
-      for (let index1 = 0; index1 < elemento.managers.length; index1 += 1) {
-        const aaa = elemento.managers;
-        aaa.forEach((elemento1) => {
-          if (elemento1 === managerId)     
-            arrayColabora.push(`${gerente[index].firstName} ${gerente[index].lastName}`);
+function colaboraGerente(managerId) {
+  const arrayColabora = [];
+  const gerente = data.employees;
+  gerente.forEach((elemento, index) => {
+    for (let index1 = 0; index1 < elemento.managers.length; index1 += 1) {
+      const aaa = elemento.managers;
+      aaa.forEach((elemento1) => {
+        if (elemento1 === managerId)     
+          arrayColabora.push(`${gerente[index].firstName} ${gerente[index].lastName}`);
         })
-      }
-    });
-  }
-   else {
-    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  }
-  return (arrayColabora);
+    }
+  });
+  return arrayColabora;
+}
+function getRelatedEmployees(managerId) {
+  if (isManager(managerId)) {
+    return colaboraGerente(managerId);
+  } else {
+      throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } 
 }
  console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 // console.log(isManager('9e7d4524-363c-416a-8759-8aa7e50c0992'))
