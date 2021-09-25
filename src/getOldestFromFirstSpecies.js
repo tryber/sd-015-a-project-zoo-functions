864387c601f9c6b437c08ce3a937bb683e156a69
Emmanuelle Pereira <emma.persil@gmail.com>
2021-09-25 16:28:08 -0300

    Requisito 9 feito, passando localmente

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const { employees, species} = data;
+  const employeesID = employees.find((empl) => empl.id === id);
+  const responsible = employeesID.responsibleFor[0];
+  const specie = species.find((animal) => animal.id === responsible);
+  const getOldest = specie.residents.reduce((oldest, current) => 
+    (oldest.age > current.age ? oldest : current));
+  
+  return Object.values(getOldest);
 }
 
 module.exports = getOldestFromFirstSpecies;
