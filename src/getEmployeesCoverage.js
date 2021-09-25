15298186550e522a2956be6613345ed03d27c1b0
Emmanuelle Pereira <emma.persil@gmail.com>
2021-09-25 18:27:47 -0300

    Requisito 10 feito, passando localmente

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,34 @@
-const data = require('../data/zoo_data');
+const { species, employees } = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const findEmployee = (person) => {
+  if (person.name) {
+    return employees.find((a) =>
+      a.firstName === person.name || a.lastName === person.name);
+  }
+
+  if (person.id) {
+    return employees.find((a) =>
+      person.id === a.id);
+  }
+};
+
+const standardObj = (obj) => ({
+
+  id: obj.id,
+  fullName: `${obj.firstName} ${obj.lastName}`,
+  species: species.filter((specie) => obj.responsibleFor
+    .includes(specie.id)).map((anim) => anim.name),
+  locations: species.filter((specie) => obj.responsibleFor
+    .includes(specie.id)).map((anim) => anim.location),
+
+});
+
+function getEmployeesCoverage(person) {
+  if (!person) {
+    return employees.map((a) => standardObj(a));
+  }
+  if (!findEmployee(person)) throw new Error('Informações inválidas');
+  return standardObj(findEmployee(person));
 }
 
 module.exports = getEmployeesCoverage;
