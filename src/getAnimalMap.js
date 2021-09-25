809221a1dfd82ff5050962614a3256ed12337e08
SaraRabelo <rabelorsara@gmail.com>
2021-09-25 17:29:53 -0300

    Tentando ignorar o commit 7a questão não está pronto.

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,6 +1,7 @@
 const data = require('../data/zoo_data');
 
-function resultNameAnimal(localanimal, nomeanimal) {}
+function resultNameAnimal(localanimal, nomeanimal) 
+//{}
 
 getAnimalMap({ includeNames: true });
 module.exports = getAnimalMap;
