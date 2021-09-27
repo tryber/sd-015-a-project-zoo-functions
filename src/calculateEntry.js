359967cc9dc3f5fb04f4d2481ad9eef6d7ec830f
SaraRabelo <rabelorsara@gmail.com>
2021-09-27 14:23:38 -0300

    6a Questão solucionada

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  const qteAdult = entrants.filter((element) => element.age < 18);
-  const qteChild = entrants.filter((child) => (child.age >= 18) && (child.age < 50));
-  const qteSenior = entrants.filter((senior) => (senior.age >= 50));
+  let somaad = 0;
+  let somach = 0;
+  let somasn = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18) somach += 1;
+    if (element.age >= 18 && element.age < 50) {
+      somaad += 1;
+    } else somasn += 1;
+  });
+
   const contagem = {
-    adult: qteAdult.reduce((somaAdult) => somaAdult + qteAdult.age),
-    child: qteChild.reduce((somaChild) => somaChild + qteAdult.age),
-    senior: qteSenior.reduce((somaSenior) => somaSenior + qteAdult.age),
+    adult: somaad,
+    child: somach,
+    senior: somasn - somach,
   };
   return (contagem);
 }
@@ -16,22 +23,11 @@ function calculateEntry(entrants) {
   if ((entrants === undefined) || (Object.values(entrants).length === 0)) {
     return 0;
   }
-  const totalVisitantes = countEntrants();
-  const precoAdult = data.species.prices * totalVisitantes.contagem.adult;
-  const precoChild = data.species.prices * totalVisitantes.contagem.child;
-  const precoSenior = data.species.prices * totalVisitantes.contagem.senior;
+  const totalVisitantes = countEntrants(entrants);
+  const precoAdult = data.prices.adult * totalVisitantes.adult;
+  const precoChild = data.prices.child * totalVisitantes.child;
+  const precoSenior = data.prices.senior * totalVisitantes.senior;
   return precoAdult + precoChild + precoSenior;
 }
-
-/* const entrants = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-countEntrants(entrants);
-calculateEntry(entrants); */
 
 module.exports = { calculateEntry, countEntrants };
