72adf7fb9b665be5234f49a4dfc0f9d6f00bb909
biizerrah <biizerrah@outlook.com>
2021-09-27 01:10:30 -0300

    requisito 5

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,5 +1,5 @@
 const data = require('../data/zoo_data');
-// const { species } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // código
