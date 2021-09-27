04b06c1c10afa02b6a4df4db02f82351af4c20e3
Ricardo Anjos <ricardoanj.14@gmail.com>
2021-09-27 20:36:02 -0300

    esqueci de commitar

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,31 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
+// const entrants = [
+//   { name: 'Lara Carvalho', age: 5 },
+//   { name: 'Frederico Moreira', age: 5 },
+//   { name: 'Pedro Henrique Carvalho', age: 5 },
+//   { name: 'Maria Costa', age: 18 },
+//   { name: 'Núbia Souza', age: 18 },
+//   { name: 'Carlos Nogueira', age: 50 },
+// ];
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const child = entrants.filter((person) => person.age < 18).length;
+  const adult = entrants.filter((person) => person.age >= 18 && person.age < 50).length;
+  const senior = entrants.filter((person) => person.age >= 50).length;
+  return {
+    child,
+    adult,
+    senior,
+  };
 }
 
-function calculateEntry(entrants) {
-  // seu código aqui
+function calculateEntry(entrants = '') {
+  if (!entrants.length) return 0;
+  const countEntrant = countEntrants(entrants);
+  const { child, adult, senior } = countEntrant;
+  const totalValue = (child * prices.child) + (adult * prices.adult) + (senior * prices.senior);
+  return totalValue;
 }
-
+console.log(calculateEntry);
 module.exports = { calculateEntry, countEntrants };
--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,20 +1,20 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  if (!animal) {
+  if (animal === undefined) {
     const animals = {};
     data.species.forEach((specieName) => {
       animals[specieName.name] = specieName.residents.length;
     });
     return animals;
   }
-  const { specie, gender } = animal;
-  if (!gender) {
+  const { specie, gender: sex } = animal;
+  if (sex === undefined) {
     return data.species.find((specieName) => specieName
       .name === specie).residents.length;
   }
-  return data.species.find((specieName) => specieName.name
-  === specie).residents.filter((specieSex) => specieSex.sex === gender).length;
+  return data.species.find((specieName2) => specieName2.name
+  === specie).residents.filter((specieSex) => specieSex.sex === sex).length;
 }
 
 module.exports = countAnimals;
