8d04c3142b6d010181cfb550ecdc0f2c15181879
biizerrah <biizerrah@outlook.com>
2021-09-27 11:38:46 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -3,7 +3,7 @@ const { species } = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   const animals = species.find((specie) => specie.name === animal);
-  return animals.residents.every((resident) => resident.age >= age);
+  const residentAge = animals.residents.every((resident) => resident.age >= age);
+  return residentAge;
 }
-
 module.exports = getAnimalsOlderThan;
