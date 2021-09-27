512e81390e91a03e330f843a917334805210d20b
Vitor <tutornews2@gmail.com>
2021-09-27 09:40:03 -0300

    Adiciona comentario requesito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -5,6 +5,9 @@ const data = require('../data/zoo_data');
 function getOldestFromFirstSpecies(id) {
   const funcio = employees.find((func) => func.id === id);
   const animals = species.find((animal) => animal.id === funcio.responsibleFor[0]).residents;
+  // console.log(animals);
+  // video que me ajudou a entender reduce https://www.youtube.com/watch?v=O_bSjsLga48
+  // o acumulador passa a ser aquilo que foi retornado.
   const getOldest = animals.reduce((acc, curr) => ((acc.age > curr.age) ? acc : curr));
   return Object.values(getOldest);
 }
