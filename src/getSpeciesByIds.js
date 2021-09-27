c01f7f214c8c24a448f9120ee523581839377759
miguel-alexandre <sauloj@protonmail.com>
2021-09-27 14:30:11 -0300

    recomeçando o projeto

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+function getSpeciesByIds(...ids) {
   // seu código aqui
+  const { species } = data;
+
+  return ids.map((id) => (species.find((speciesId) => speciesId.id === id)));
 }
 
 module.exports = getSpeciesByIds;
