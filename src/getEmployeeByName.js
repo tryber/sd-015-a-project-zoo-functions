ca205a447360c7de65dc0cbc8b7995f7044f0eb5
biizerrah <biizerrah@outlook.com>
2021-09-27 11:56:48 -0300

    requisito 5

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
-// const { employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
+  if (!employeeName) {
+    return { };
+  } return employees.find((worker) => worker.firstName === employeeName
+  || worker.lastName === employeeName);
 }
 
 module.exports = getEmployeeByName;
