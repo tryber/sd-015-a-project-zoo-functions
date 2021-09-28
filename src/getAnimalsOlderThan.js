988e34de66b214513ebf0ddc49ff2ca459f329bf
Henrique_Veras <theveras1@gmail.com>
2021-09-28 17:42:41 -0300

    adiciona comentarios no arquivo getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,8 +1,10 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
+// Função que verifica se a idade de todos os animais da especie do parametro "animal" é maior que o valor de "age"
 function getAnimalsOlderThan(animal, age) {
+  // Encontra a especie no array de especies:
   const newTarget = species.find((target) => target.name === animal);
+  // Verifica se todos os elemntos possuem a idade maior que o valor de "age"
   const animalAgeCheck = newTarget.residents.every((animalAge) => animalAge.age >= age);
   return animalAgeCheck;
 }
