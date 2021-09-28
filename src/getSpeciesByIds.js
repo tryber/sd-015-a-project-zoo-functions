b5c15878bc37e3e4b4e39c804cacdb837c09d41e
Henrique_Veras <theveras1@gmail.com>
2021-09-28 18:22:45 -0300

    Adiciona comentarios ao arquivo getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,11 +1,13 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
+// Função que encontra o animal com base em seu ID, e retorna o objeto dentro de um array:
 function getSpeciesByIds(...ids) {
+  // Se nenhum valor for passado como parametro:
   const result = [];
   if (!ids) {
     return result;
   }
+  // Se um ou mais valores forem passados como parametros:
   const findAnimal = () => ids.forEach((id) => {
     const newAnimal = species.filter((specie) => specie.id === id);
     result.push(...newAnimal);
