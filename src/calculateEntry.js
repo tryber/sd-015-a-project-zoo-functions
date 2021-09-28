85534e99d4be3756bff7e146a7955af2bae8780f
Henrique_Veras <theveras1@gmail.com>
2021-09-28 19:09:29 -0300

    Adiciona comentarios no arquivo calculateEntry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,22 +1,29 @@
 const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
-
+// Função que categoriza cada pessoa em uma das três catergorias de visitantes:
 function countEntrants(entrants) {
-  if (!entrants || Object.keys(entrants).length === 0) { return 0; }
+  // Se nenhum valor foi passado como parametro ou um objeto vazio:
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  // Pega apenas o valor da idade de cada visitante:
   const ageCheck = entrants.map((person) => person.age);
-  const finalObject = { child: 0, adult: 0, senior: 0 };
+  const finalObject = {};
+  // Object destructuring para diminuir o codigo:
   let { child, adult, senior } = finalObject;
   child = ageCheck.filter((age) => age < 18).length;
   adult = ageCheck.filter((age) => age >= 18 && age < 50).length;
   senior = ageCheck.filter((age) => age >= 50).length;
-  return ({ child, adult, senior });
+  return { child, adult, senior };
 }
-
+// Função para calcular o valor maximo da entrada de visitantes:
 function calculateEntry(entrants) {
+  // Se nenhum valor foi passado como parametro ou um objeto vazio:
   if (!entrants || Object.keys(entrants).length === 0) {
     return 0;
   }
-  const { child, adult, senior } = countEntrants(entrants);
+  const results = countEntrants(entrants);
+  const { child, adult, senior } = results;
   return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
