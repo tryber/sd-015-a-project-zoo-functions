5ba569de7bb7876b0cf4f99d286d729545237b72
lipegois <luizz3.14@hotmail.com>
2021-09-27 21:17:10 -0500

    Requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
-}
+function getSpeciesByIds(...ids) {
+  const result = [];
 
+  if (ids === undefined) {
+    return result;
+  }
+
+  const algIds = (id) => {
+    const selectedSpecies = data.species.find((specie) => specie.id === id);
+    result.push(selectedSpecies);
+  };
+
+  ids.forEach(algIds);
+  return result;
+}
 module.exports = getSpeciesByIds;
