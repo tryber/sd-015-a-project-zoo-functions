cdd4bbc50e22e2f9963ec2d2dab229d91da06988
thomazj <thomaz.jefferson01@protonmail.com>
2021-09-28 16:04:17 -0300

    requisito 8.1

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,32 +1,7 @@
 const data = require('../data/zoo_data');
 
-const { species } = data;
-
 function getAnimalMap(options) {
   // seu código aqui
-}
-
-function listaTodasEspecies() {
-  const objetoPadrão = {
-    NE: species.filter((infoGeral) =>
-      infoGeral.location.includes('NE')).map((especie) => especie.name),
-    NW: species.filter((infoGeral) =>
-      infoGeral.location.includes('NW')).map((especie) => especie.name),
-    SE: species.filter((infoGeral) =>
-      infoGeral.location.includes('SE')).map((especie) => especie.name),
-    SW: species.filter((infoGeral) =>
-      infoGeral.location.includes('SW')).map((especie) => especie.name),
-  };
-  return objetoPadrão;
-}
-
-function listaTodasEspecieseNome() {
-  const listaEspecies = listaTodasEspecies();
-
-  const objetoResultado = listaEspecies.NE.map((elemento) => {
-    return {elemento}
-  });
-  return objetoResultado
 }
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,31 @@
 const data = require('../data/zoo_data');
 
+function semParametro() {
+  const { species } = data;
+  const { hours } = data;
+  const resultado = {};
+  const filterHours = Object.entries(hours);
+
+  filterHours.forEach((valor) => {
+    const filtrarAnimaisNome = species.filter((objeto) => objeto.availability.includes(valor[0]));
+    const arrayAnimais = filtrarAnimaisNome.map((elemento) => elemento.name);
+
+    resultado[valor[0]] = {
+      officeHour: `Open from ${valor[1].open}am until ${valor[1].close}pm`,
+      exhibition: arrayAnimais,
+    };
+  });
+  return resultado;
+}
+
 function getSchedule(scheduleTarget) {
   // seu código aqui
+  if (scheduleTarget === undefined) {
+    const retorno = semParametro();
+    retorno.Monday.exhibition = 'The zoo will be closed!';
+    retorno.Monday.officeHour = 'CLOSED';
+    return retorno;
+  }
 }
 
 module.exports = getSchedule;
