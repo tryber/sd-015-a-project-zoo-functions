5cd9d27a449cd8fc627c7e8bf8e3328f7aa5e2b1
Jonas Candia <jonas57.jc@gmail.com>
2021-09-28 14:31:28 -0300

    terceiro requisito

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,14 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
+// Retirado do github: https://github.com/tryber/sd-015-a-project-zoo-functions/blob/maria-gabriela-zoo-functions-project/src/getEmployeeByName.js.
+
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const name = employees.find(({ firstName, lastName }) =>
+    employeeName === firstName || employeeName === lastName);
+  if (name === undefined) return {};
+
+  return name;
 }
 
 module.exports = getEmployeeByName;
