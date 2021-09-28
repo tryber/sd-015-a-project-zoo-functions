e978f41e6cbc5241ba53e39de3b8c33a9f961a39
phillipe <rodrigues153@gmail.com>
2021-09-27 23:59:48 -0300

    Teste do requisito 5 apos git pull

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,17 +1,17 @@
 const data = require('../data/zoo_data');
 const { hours } = require('../data/zoo_data');
 
-
 function getSchedule(scheduleTarget) {
   if (!scheduleTarget) {
     const clone = hours;
     const x = Object.keys(clone);
     const obj = {};
     x.forEach((element) => {
-      obj[element]=(`Open from ${hours.Thursday.open} until ${hours.Thursday.close}`); 
+      obj[element] = (`Open from ${hours.Thursday.open} until ${hours.Thursday.close}`);
       console.log(obj);
     });
   }
+  return null;
 }
 console.log(getSchedule());
 
