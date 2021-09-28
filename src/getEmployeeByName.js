b0a189a7942e4362dc0b19152cfef811494b59f2
Henrique_Veras <theveras1@gmail.com>
2021-09-28 17:44:51 -0300

    Adiciona comentarios no arquivo getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,6 +1,6 @@
 const data = require('../data/zoo_data');
 const { employees } = require('../data/zoo_data');
-
+// Função que busca o empregado no array "employees" usando como parametro o nome ou o sobrenome:
 function getEmployeeByName(Name) {
   if (!Name) {
     return {};
