bd1f3872482ea8d95f09261ad878b11e67c5d983
Henrique_Veras <theveras1@gmail.com>
2021-09-28 17:38:36 -0300

    correçao do lint

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -8,17 +8,14 @@ function countAnimals(animal) {
     // Para cada especie de animal, será criada uma chave com o Nome da especie e valor correspondente a quantidade de animais da especie:
     const allAnimals = () => species.forEach((animals) => {
       const newKey = ({ [animals.name]: animals.residents.length });
-      // chave criada "newKey" é inserida no objeto vazio "objectOfAnimals"
       Object.assign(objectOfAnimals, newKey);
     });
     allAnimals();
     return objectOfAnimals;
     // Se existe mais de uma chave no objeto-parametro "animal":
   } if (Object.keys(animal).length > 1) {
-    // Procura no array "species" pela especie de mesmo nome que o valor da chave "specie" em "animal"
     const animalTarget = species.find((target) => target.name === animal.specie);
-    // Filtra o "animalTarget" para que retorne somente o numero animais do mesmo sexo que o valor da chave "sex" em "animal"
-    return animalTarget.residents.filter((target) => target.sex === animal.sex).length;
+    return animalTarget.residents.filter((target) => animal.sex === target.sex).length;
   }
   // Se apenas uma chave existir no objeto-parametro:
   const animalTarget = species.find((target) => target.name === animal.specie);
