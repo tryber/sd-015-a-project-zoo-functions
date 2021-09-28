cd2366e0920d96efef81f7b4ff2350a59cb33983
Henrique_Veras <theveras1@gmail.com>
2021-09-28 00:15:29 -0300

    correçao exercicio calculate entry

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -20,13 +20,4 @@ function calculateEntry(entrants) {
   return child * prices.child + adult * prices.adult + senior * prices.senior;
 }
 
-const entrants1 = [
-  { name: 'Lara Carvalho', age: 5 },
-  { name: 'Frederico Moreira', age: 5 },
-  { name: 'Pedro Henrique Carvalho', age: 5 },
-  { name: 'Maria Costa', age: 18 },
-  { name: 'Núbia Souza', age: 18 },
-  { name: 'Carlos Nogueira', age: 50 },
-];
-
 module.exports = { calculateEntry, countEntrants };
