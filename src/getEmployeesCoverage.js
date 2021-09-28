ce4680b3d03d752703a5aaa764b3e526353e76dd
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-27 21:44:07 -0300

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,50 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const { species } = data;
+const { employees } = data;
+
+function getAllemployeesCoverage() {
+  const speciesByEmployees = employees.map((employee) => employee.responsibleFor);
+  const speciesNameByEmployees = speciesByEmployees.map((arraySpecies) => arraySpecies
+    .map((specie) => species.find((spc) => spc.id === specie).name));
+  const speciesLocations = speciesByEmployees.map((array) => array.map((specie) => species
+    .find((spc) => spc.id === specie).location));
+  const employeesCoverage = employees.map((employee, index) => (
+    {
+      id: employee.id,
+      fullName: `${employee.firstName} ${employee.lastName}`,
+      species: speciesNameByEmployees[index],
+      locations: speciesLocations[index],
+    }
+  ));
+  return employeesCoverage;
+}
+
+function getEachemployeeCoverage(employee) {
+  const employeeCoverage = employees.find((emp) => emp.id === employee.id
+    || emp.lastName === employee.name || emp.firstName === employee.name);
+  if (employeeCoverage === undefined) {
+    throw new Error('Informações inválidas');
+  }
+  const speciesEmployee = employeeCoverage.responsibleFor
+    .filter((specie) => species.find((spc) => specie));
+  const speciesNameEmp = speciesEmployee
+    .map((specie) => species.find((spc) => spc.id === specie).name);
+  const speciesLocation = speciesEmployee
+    .map((specie) => species.find((spc) => spc.id === specie).location);
+  return {
+    id: employeeCoverage.id,
+    fullName: `${employeeCoverage.firstName} ${employeeCoverage.lastName}`,
+    species: speciesNameEmp,
+    locations: speciesLocation,
+  };
+}
+
+function getEmployeesCoverage(employee) {
+  if (employee === undefined) {
+    return getAllemployeesCoverage();
+  }
+  return getEachemployeeCoverage(employee);
 }
 
 module.exports = getEmployeesCoverage;
