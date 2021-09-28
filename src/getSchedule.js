fff3f65faffe37bcd63c6cd38f5c7ed56167c2b9
Vini_Vergani <vinivergani95@gmail.com>
2021-09-27 21:24:39 -0300

    Adiciona requisito 8 \o/ - lint 1

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,43 +1,44 @@
 const { TestScheduler } = require('jest');
 const data = require('../data/zoo_data');
+
 const simpleWay = {
-  'Tuesday': {
-    'officeHour': 'Open from 8am until 6pm',
-    'exhibition': ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
+  Tuesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['lions', 'tigers', 'bears', 'penguins', 'elephants', 'giraffes'],
   },
-  'Wednesday': {
-    'officeHour': 'Open from 8am until 6pm',
-    'exhibition': ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
+  Wednesday: {
+    officeHour: 'Open from 8am until 6pm',
+    exhibition: ['tigers', 'bears', 'penguins', 'otters', 'frogs', 'giraffes'],
   },
-  'Thursday': {
-    'officeHour': 'Open from 10am until 8pm',
-    'exhibition': ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
+  Thursday: {
+    officeHour: 'Open from 10am until 8pm',
+    exhibition: ['lions', 'otters', 'frogs', 'snakes', 'giraffes'],
   },
-  'Friday': {
+  Friday: {
     'officeHour': 'Open from 10am until 8pm',
     'exhibition': ['tigers', 'otters', 'frogs', 'elephants', 'giraffes'],
   },
-  'Saturday': {
-    'officeHour': 'Open from 8am until 10pm',
-    'exhibition': [
+  Saturday: {
+    officeHour: 'Open from 8am until 10pm',
+    exhibition: [
       'lions', 'tigers',
       'bears', 'penguins',
       'otters', 'frogs',
       'snakes', 'elephants',
     ],
   },
-  'Sunday': {
-    'officeHour': 'Open from 8am until 8pm',
-    'exhibition': ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
+  Sunday: {
+    officeHour: 'Open from 8am until 8pm',
+    exhibition: ['lions', 'bears', 'penguins', 'snakes', 'elephants'],
   },
-  'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+  Monday: { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
 };
 const forMonday = {
-  'Monday': { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
+  Monday: { 'officeHour': 'CLOSED', 'exhibition': 'The zoo will be closed!' },
 };
 const simpleArray = [];
 simpleArray.push(simpleWay);
-//Para a criação desta função usei de inspiração o código do Vitor Diorio visto que eu estava com dificuldades de implementala funcionalmente na parte que buscava as species- disponivel em https://github.com/tryber/sd-015-a-project-zoo-functions/pull/93/commits/199f680d891c0c976fd2ca25b4410d10774eccd4
+//  Para a criação desta função usei de inspiração o código do Vitor Diorio visto que eu estava com dificuldades de implementala funcionalmente na parte que buscava as species- disponivel em https://github.com/tryber/sd-015-a-project-zoo-functions/pull/93/commits/199f680d891c0c976fd2ca25b4410d10774eccd4
 
 function findDay (day) {
   const finder = {};
@@ -58,11 +59,10 @@ function getSchedule(scheduleTarget) {
     return findDay(scheduleTarget);
   }
   if (data.species.some((animal) => animal.name === scheduleTarget)) {
-    const findSpecie = data.species.find((animal) => animal.name === scheduleTarget)
+    const findSpecie = data.species.find((animal) => animal.name === scheduleTarget);
     return findSpecie.availability;
-  } else {
+  }  
     return simpleWay;
-  }
 }
 
 module.exports = getSchedule;
