115ae51b7c5d5f868e79e4a40f4c16abe8489d16
Jonathan Plets <jonathanplets@gmail.com>
2021-09-28 16:09:26 -0300

    questao4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,19 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const pessoaGerente = data.employees.some((elemento) => elemento.managers.includes((id)));
+  return pessoaGerente;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const ehGerente = isManager(managerId);
+
+  if (ehGerente) {
+    return employees.filter((elemento) => elemento.managers.includes(managerId))
+      .map((elemento) => `${elemento.firstName} ${elemento.lastName}`);
+  }
+  throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
