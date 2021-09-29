c40c6ba756f70dad42db68a53b262614d05a05a7
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-29 15:42:43 -0300

    Requisito 10 - Parcial

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,40 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
+
+const arrayLocations = ['NE', 'NW', 'SE', 'SW'];
+const animalMap = arrayLocations.map((location) => species.filter((specie) => specie.location === location).map((obj) => obj.name));
 
 function getAnimalMap(options) {
-  // seu código aqui
+  if (options === undefined) {
+    const objAnimalMap = animalMap.map((animal, index) => {
+      const obj = {};
+      obj[arrayLocations[index]] = animal;
+      return obj;
+    });
+    const animalReduced = objAnimalMap.reduce((acc, obj) => Object.assign(acc, obj), {});
+    return animalReduced;
+  }
+  
+  const residentMap = animalMap.map((animal) => animal.map((spc) => {
+    const specieResidents = species.find((specie) => specie.name === spc).residents;
+    if (options.sorted) {
+      return specieResidents.sort((a, b) => a.name > b.name ? 1 : -1);
+    }
+    return specieResidents;
+  }).map((array, index) => {
+    const obj = {};
+    obj[animal[index]] = array.map((resident) => resident.name);
+    return obj;
+}));
+  
+  const objtResidentMap = residentMap.map((animal, index) => {
+    const obj = {};
+    obj[arrayLocations[index]] = animal;
+      return obj;
+  });
+  const residentReduced = objtResidentMap.reduce((acc, obj) => Object.assign(acc, obj), {});
+  return residentReduced;
 }
+
 
 module.exports = getAnimalMap;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
+const { hours } = data;
+const { species } = require('../data/zoo_data');
 
 function getSchedule(scheduleTarget) {
   // seu código aqui
+//   const daysWeek = Object.entries(hours);
+// console.log(daysWeek);
+// const testeMap = daysWeek.map((day) => species.filter((specie) => specie.availability.find((spc) => spc === day[0])));
+// const enxuga = testeMap.map((test) => test.map((tst) => tst.name));
+// console.log(enxuga);
+
 }
 
 module.exports = getSchedule;
