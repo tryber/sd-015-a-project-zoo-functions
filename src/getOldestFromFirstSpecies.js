15d1ee60c68a9410edcf8463f0bfb62c9df65546
Renato Adorno <renatowillian.2000@gmail.com>
2021-09-29 20:12:56 -0300

    requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const findAnimal = employees.find((employe) => employe.id === id).responsibleFor[0]; // armazena id animal
+  const findSpecie = species.find((specie) => specie.id === findAnimal);
+  const oldestSpecie = findSpecie.residents.reduce((res, cur) => {
+    if (cur.age > res.age) return cur;
+    return res;
+  }); //  encontra especie mais velha { name: '-----', sex: '-----', age: ----- }
+  return Object.values(oldestSpecie); // converte para array somente com valores
 }
+
+// getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1');
 
 module.exports = getOldestFromFirstSpecies;
