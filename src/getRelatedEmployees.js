c100e93b99c377a1f63f4aba02a420a80466c3a0
lipegois <luizz3.14@hotmail.com>
2021-09-29 11:34:12 -0500

    Requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const verifyEmployees = data.employees.some((person) => {
+    const verifyManagers = person.managers.some((manager) => manager === id);
+    return verifyManagers;
+  });
+
+  return verifyEmployees;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) !== false) {
+    const algEmployees = (employee) => employee.managers.some((manager) => manager === managerId);
+    const employees = data.employees.filter((algEmployees));
+    const names = employees.map((person) => `${person.firstName} ${person.lastName}`);
+
+    return names;
+  } throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
 }
 
 module.exports = { isManager, getRelatedEmployees };
