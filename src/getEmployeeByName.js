2ba8c1aeac67b4e9e377017166e74b3d2c8192bc
miguel-alexandre <sauloj@protonmail.com>
2021-09-29 18:02:44 -0300

    getEmployeeByName

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return employees.find(
+    (param) =>
+      param.firstName === employeeName || param.lastName === employeeName,
+  );
 }
 
 module.exports = getEmployeeByName;
