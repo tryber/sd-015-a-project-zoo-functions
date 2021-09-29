9e2c2c59cc7c1a7369d088d33c38b5c261f72323
miguel-alexandre <sauloj@protonmail.com>
2021-09-29 17:07:07 -0300

    Consertando o lint 4

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -2,12 +2,11 @@
 //  Ela retorna um array contendo as espécies
 //  referentes aos ids passados como parâmetro, podendo receber um ou mais ids.
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 // espalha o array
 function getSpeciesByIds(...ids) {
-  const { species } = require('../data/zoo_data');
-
   // seu código aqui
-  return species.filter((specie) => ids.includes(specie.id))
+  return species.filter((specie) => ids.includes(specie.id));
 }
 
 module.exports = getSpeciesByIds;
