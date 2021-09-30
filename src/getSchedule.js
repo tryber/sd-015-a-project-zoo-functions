4fb41598047879f08b96a9bfebc1967cc2dbf78e
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-30 19:58:50 -0300

    Requisito 8

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,43 +1,52 @@
 const data = require('../data/zoo_data');
 
-// const { hours } = data;
-// const { species } = require('../data/zoo_data');
-// const daysWeek = Object.entries(hours);
-// const testeMap = daysWeek.map((day) => species.filter((specie) => specie.availability.find((spc) => spc === day[0])));
-// const animalExhibitionbByDay = testeMap.map((test) => test.map((tst) => tst.name));
+const { hours } = data;
+const { species } = data;
+const daysWeek = Object.entries(hours);
+const testeMap = daysWeek.map((day) => species
+  .filter((specie) => specie.availability.find((spc) => spc === day[0])));
+const animalExhibitionbByDay = testeMap.map((test) => test.map((tst) => tst.name));
 
-//  FUNÇÃO COMEÇA AQUI
+function getAnimalSchedule() {
+  const animalSchedule = daysWeek.map(((element, index) => element[0], (element, index) => {
+    const obj = {};
+    if (element[0] === 'Monday') {
+      obj.officeHour = 'CLOSED';
+      obj.exhibition = 'The zoo will be closed!';
+      return [element[0], obj];
+    }
+    obj.officeHour = `Open from ${element[1].open}am until ${element[1].close}pm`;
+    obj.exhibition = animalExhibitionbByDay[index];
+    return [element[0], obj];
+  }));
+  return animalSchedule;
+}
 
-// const animalSchedule2 = daysWeek.map(((element, index) => element[0], (element, index) => {
-//   const obj = {};
-//   if (element[0] === 'Monday') {
-//     obj['officeHour'] = 'CLOSED';
-//     obj['exhibition'] = 'The zoo will be closed!';
-//     return [element[0], obj];
-//   }
-//   obj['officeHour'] = `Open from ${element[1].open}am until ${element[1].close}pm`;
-//   obj['exhibition'] = animalExhibitionbByDay[index];
-//   return [element[0], obj];
-// }))
-
-//   const animalScheduleByDay = animalSchedule2.filter((day) => day[0] === scheduleTarget);
-//   console.log(animalScheduleByDay);
-//   console.log(Object.keys(hours).includes(scheduleTarget));
-//   if (Object.keys(hours).includes(scheduleTarget)) {
-//     const intoObjAnimalSchedule = animalScheduleByDay.reduce((acc, animal) => {
-//       acc[animal[0]] = animal[1];
-//       return acc;
-//     }, {});
-//     return intoObjAnimalSchedule;
-//   }
-//     const intoObjAnimalSchedule = animalSchedule2.reduce((acc, animal) => {
-//       acc[animal[0]] = animal[1];
-//       return acc;
-//     }, {});
-//     return intoObjAnimalSchedule;
+const intoObjAnimalSchedule = getAnimalSchedule().reduce((acc, animal) => {
+  const a = animal[0];
+  const b = animal[1];
+  acc[a] = b;
+  return acc;
+}, {});
+const speciesName = species.map((specie) => specie.name);
 
 function getSchedule(scheduleTarget) {
+  const animalScheduleByDay = getAnimalSchedule().filter((day) => day[0] === scheduleTarget);
+  if (Object.keys(hours).includes(scheduleTarget)) {
+    const intoObjAnimalSchedule2 = animalScheduleByDay.reduce((acc, animal) => {
+      const r = animal[0];
+      const f = animal[1];
+      acc[r] = f;
+      return acc;
+    }, {});
+    return intoObjAnimalSchedule2;
+  }
+  const speciesFilter = species.filter((spc) => spc.name === scheduleTarget);
 
+  if (speciesName.includes(scheduleTarget)) {
+    return speciesFilter[0].availability;
+  }
+  return intoObjAnimalSchedule;
 }
 
 module.exports = getSchedule;
