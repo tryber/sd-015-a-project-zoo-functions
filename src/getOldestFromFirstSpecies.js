0a394b6aeb2a1458c396e352629216a921f24d9d
Jonathan Plets <jonathanplets@gmail.com>
2021-09-30 00:50:44 -0300

    questao 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,10 @@
 const data = require('../data/zoo_data');
-const { employees, species } = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
   // console.log(id);
-  const funcionarios = employees.find((funcionario) => funcionario.id === id);
+  const funcionarios = data.employees.find((funcionario) => funcionario.id === id);
   const animais = funcionarios.responsibleFor.find((animal) => animal);
-  const animalObj = species.find((animal) => animal.id === animais);
+  const animalObj = data.species.find((animal) => animal.id === animais);
   const animalOlder = animalObj.residents.sort((a, b) => b.age - a.age);
   return Object.values(animalOlder[0]);
 }
