315106922e2dfe07b5b619e0727a70e65ead9125
lipegois <luizz3.14@hotmail.com>
2021-09-29 20:43:52 -0500

    Requisito 10

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-function getEmployeesCoverage() {
-  // seu código aqui
+const info = data.employees.map((pessoa) => {
+  const infoObject = {
+    id: pessoa.id,
+    fullName: `${pessoa.firstName} ${pessoa.lastName}`,
+    species: pessoa.responsibleFor.map((IdAnimal) =>
+      data.species.find((element) => element.id === IdAnimal).name),
+    locations: pessoa.responsibleFor.map((IdAnimal) =>
+      data.species.find((element) => element.id === IdAnimal).location),
+  };
+  return infoObject;
+});
+
+function getEmployeesCoverage(param = info) {
+  const verify = info.find((element) =>
+    element.fullName.includes(param.name) || element.id === param.id);
+  if (param.name || param.id) {
+    if (verify === undefined) {
+      throw new Error('Informações inválidas');
+    }
+    return verify;
+  }
+  return param;
 }
 
 module.exports = getEmployeesCoverage;
