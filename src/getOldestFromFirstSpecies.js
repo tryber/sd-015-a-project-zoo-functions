d45ee8ece897bc7f0d3a8fc468f01431fe20fbc4
Lucas Barbosa <lukeb.trybe@gmail.com>
2021-09-30 17:15:19 -0300

    adiciona requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+function getOldestAnimal(specieResidents) {
+  let oldestAnimalAge = 0;
+  let oldestAnimal;
+  for (let i = 0; i < specieResidents.length; i += 1) {
+    if (specieResidents[i].age > oldestAnimalAge) {
+      oldestAnimalAge = specieResidents[i].age;
+      oldestAnimal = specieResidents[i];
+    }
+  }
+  return oldestAnimal;
+}
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
+  const employee = data.employees.find((employeeData) => employeeData.id === id);
+  const firstSpecieId = employee.responsibleFor[0];
+  const firstSpecieResidents = data.species.find(
+    (specie) => specie.id === firstSpecieId,
+  ).residents;
+  const oldestAnimal = getOldestAnimal(firstSpecieResidents);
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 }
 
 module.exports = getOldestFromFirstSpecies;
