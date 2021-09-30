02dacf4c3deb4a6586631819f91fc5d5e3244e92
rafaelmedeiross <rafael.medeiros@msn.com>
2021-09-30 16:59:24 -0300

    Requisito 7 final

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,14 +1,15 @@
 const data = require('../data/zoo_data');
 
 const { species } = data;
+const initialSpecies = species;
 
 const arrayLocations = ['NE', 'NW', 'SE', 'SW'];
-const animalMap = arrayLocations.map((location) => species
+const animalMap = arrayLocations.map((location) => initialSpecies
   .filter((specie) => specie.location === location)
   .map((obj) => obj.name));
 
 function getSpecieResidents(options, spc) {
-  const specieResidents = species
+  const specieResidents = initialSpecies
     .find((specie) => specie.name === spc).residents;
   if (options.sex) {
     const residentsFiltered = specieResidents
@@ -18,21 +19,14 @@ function getSpecieResidents(options, spc) {
   return specieResidents;
 }
 
-function getSortedAnimals(animal, options) {
-  const sortAnimals = animal.map((spc) => {
-    if (options.sorted) {
-      const sortResidents = getSpecieResidents(options, spc)
-        .sort((a, b) => a.name > b.name);
-      return sortResidents;
-    }
-    return getSpecieResidents(options, spc);
-  });
-  return sortAnimals;
+function getSortedAnimals(spc, options) {
+  const residents = getSpecieResidents(options, spc);
+  return [...residents].sort((a, b) => a.name > b.name);
 }
 
 function getResidentMap(options) {
-  const residentMap = animalMap.map((animal) => getSortedAnimals(animal, options)
-    .map((array, index) => {
+  const residentMap = animalMap.map((animal) => animal
+    .map((spc) => getSpecieResidents(options, spc)).map((array, index) => {
       const obj = {};
       obj[animal[index]] = array.map((resident) => resident.name);
       if (options.includeNames) {
@@ -43,22 +37,39 @@ function getResidentMap(options) {
   return residentMap;
 }
 
-function turnIntoObject(value, index) {
+function turnIntoObject(property, value, index) {
   const obj = {};
-  obj[arrayLocations[index]] = value;
+  obj[property[index]] = value;
   return obj;
+}
+
+function getSortedResidentMap(options) {
+  const sortedResidentMap = animalMap.map((animal) => animal
+    .map((spc) => getSortedAnimals(spc, options)).map((array, index) => {
+      const value = array.map((resident) => resident.name);
+      if (options.includeNames) {
+        return turnIntoObject(animal, value, index);
+      }
+      return Object.keys(turnIntoObject(animal, value, index))[0];
+    }));
+  return sortedResidentMap;
 }
 
 function getAnimalMap(options) {
   if (options === undefined) {
     const objAnimalMap = animalMap
-      .map((animal, index) => turnIntoObject(animal, index));
-    const animalMapReduced = objAnimalMap
-      .reduce((acc, obj) => Object.assign(acc, obj), {});
+      .map((animal, index) => turnIntoObject(arrayLocations, animal, index));
+    const animalMapReduced = objAnimalMap.reduce((acc, obj) => Object.assign(acc, obj), {});
     return animalMapReduced;
   }
-  const objtResidentMap = getResidentMap(options)
-    .map((animal, index) => turnIntoObject(animal, index));
+  let objtResidentMap = getResidentMap(options)
+    .map((animal, index) => turnIntoObject(arrayLocations, animal, index));
+
+  if (options.sorted) {
+    objtResidentMap = getSortedResidentMap(options)
+      .map((animal, index) => turnIntoObject(arrayLocations, animal, index));
+  }
+
   const residentReduced = objtResidentMap
     .reduce((acc, obj) => Object.assign(acc, obj), {});
   return residentReduced;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,14 +1,42 @@
 const data = require('../data/zoo_data');
+
 // const { hours } = data;
 // const { species } = require('../data/zoo_data');
+// const daysWeek = Object.entries(hours);
+// const testeMap = daysWeek.map((day) => species.filter((specie) => specie.availability.find((spc) => spc === day[0])));
+// const animalExhibitionbByDay = testeMap.map((test) => test.map((tst) => tst.name));
+
+//  FUNÇÃO COMEÇA AQUI
+
+// const animalSchedule2 = daysWeek.map(((element, index) => element[0], (element, index) => {
+//   const obj = {};
+//   if (element[0] === 'Monday') {
+//     obj['officeHour'] = 'CLOSED';
+//     obj['exhibition'] = 'The zoo will be closed!';
+//     return [element[0], obj];
+//   }
+//   obj['officeHour'] = `Open from ${element[1].open}am until ${element[1].close}pm`;
+//   obj['exhibition'] = animalExhibitionbByDay[index];
+//   return [element[0], obj];
+// }))
+
+//   const animalScheduleByDay = animalSchedule2.filter((day) => day[0] === scheduleTarget);
+//   console.log(animalScheduleByDay);
+//   console.log(Object.keys(hours).includes(scheduleTarget));
+//   if (Object.keys(hours).includes(scheduleTarget)) {
+//     const intoObjAnimalSchedule = animalScheduleByDay.reduce((acc, animal) => {
+//       acc[animal[0]] = animal[1];
+//       return acc;
+//     }, {});
+//     return intoObjAnimalSchedule;
+//   }
+//     const intoObjAnimalSchedule = animalSchedule2.reduce((acc, animal) => {
+//       acc[animal[0]] = animal[1];
+//       return acc;
+//     }, {});
+//     return intoObjAnimalSchedule;
 
 function getSchedule(scheduleTarget) {
-  // seu código aqui
-//   const daysWeek = Object.entries(hours);
-// console.log(daysWeek);
-// const testeMap = daysWeek.map((day) => species.filter((specie) => specie.availability.find((spc) => spc === day[0])));
-// const enxuga = testeMap.map((test) => test.map((tst) => tst.name));
-// console.log(enxuga);
 
 }
 
