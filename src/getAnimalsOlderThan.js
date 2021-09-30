f535939b98a5d9e7065eb2d233b9116f45e402e1
george lucas <glalmeida.dev@gmail.com>
2021-09-30 12:18:52 -0300

    Requisito 2 compartilhado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-function getAnimalsOlderThan(animals, age) {
-
+function getAnimalsOlderThan(nameSpecie, minimunAge) {
+    const residents = data.species.find((param) => {param.name === nameSpecie});
+    return residents.every((param1) => param1.age > minimunAge);
 }
-
+console.log(getAnimalsOlderThan('lions',10));
 module.exports = getAnimalsOlderThan;
