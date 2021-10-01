2f819873a970b2da8fb331f3ced38b54d5743d5d
miguel-alexandre <sauloj@protonmail.com>
2021-10-01 20:54:41 -0300

    getAnimalsOlderThan

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,10 @@ const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
-}
+    const findTheAnimal = data.species.find((correct) => correct.name === animal);
+    const ageCorrect = findTheAnimal.residents.every((allAnimals) => allAnimals.age >= age);
+    return ageCorrect;
+  }
+
 
 module.exports = getAnimalsOlderThan;
