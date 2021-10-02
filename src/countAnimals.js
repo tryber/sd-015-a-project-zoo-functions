daf71e6a29dc43c125ef3f9d5744c7be813a383e
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-10-02 14:55:57 -0300

    Requisito 05 - att

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -7,13 +7,13 @@ function countAnimals(animal) {
     species.forEach((animal2) => { obj[animal2.name] = animal2.residents.length; });
     return obj;
   }
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
 
-  if (specie !== undefined && gender === undefined) {
+  if (specie !== undefined && sex === undefined) {
     return species.find((animal3) => animal3.name === specie).residents.length;
   }
   return species.find((animal4) => animal4.name === specie).residents
-    .filter((sex) => sex.sex === gender).length;
+    .filter((sexCreature) => sexCreature.sex === sex).length;
 }
 
 module.exports = countAnimals;
