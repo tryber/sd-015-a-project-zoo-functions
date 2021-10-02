03f0c315b8f98d2fe347f838a45a643d029c4151
Gabriel-Araujo11 <gabriel_arsi@hotmail.com>
2021-10-02 15:11:53 -0300

    Requisito 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+function getOldestFromFirstSpecies(idss) {
+  const laborer = data.employees.find((e) => e.id === idss);
+  const currAnimal = data.species.find((animal) => animal.id === laborer.responsibleFor[0]);
+  const div = currAnimal.residents.sort((a1, a2) => {
+    if (a1.age > a2.age) return 1;
+    if (a1.age < a2.age) return -1;
+    return 0;
+  });
+  const oldest = div[div.length - 1];
+  const done = [oldest.name, oldest.sex, oldest.age];
+  return done;
 }
-
 module.exports = getOldestFromFirstSpecies;
