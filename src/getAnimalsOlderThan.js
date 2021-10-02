dacfe43d10cf18fbcdec94035cd5fa80efba497a
miguel-alexandre <sauloj@protonmail.com>
2021-10-01 21:40:22 -0300

    getAnimalsOlderThan lintado 8

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,3 +1,4 @@
+// Crie uma função que verifica se todos os animais de uma espécie possuem a idade mínima especificada
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
