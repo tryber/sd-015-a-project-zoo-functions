aff5d6681f24dcaadcef48b480e6c3171b765c77
miguel-alexandre <sauloj@protonmail.com>
2021-10-01 21:01:47 -0300

    getAnimalsOlderThan lintado 3

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,11 +1,10 @@
 // Crie uma função que verifica se todos os animais de uma espécie possuem a idade mínima especificada
-
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
   const findTheAnimal = data.species.find((correct) => correct.name === animal);
   const ageCorrect = findTheAnimal.residents.every((allAnimals) => allAnimals.age >= age);
   return ageCorrect;
 }
-module.exports = getAnimalsOlderThan;
+
+module.exports = getAnimalsOlderTh
