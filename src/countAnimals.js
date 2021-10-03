6892b485f68eb63313cb19110b6e6f7eb6abbda2
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-10-03 18:59:25 -0300

    requisito 5 mudado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -4,23 +4,24 @@ const data = require('../data/zoo_data');
 // https://github.com/tryber/sd-015-a-project-zoo-functions/pull/12/commits/52d0a899afe68fc53467a395e2dd8e45786bbeb7
 
 function countAnimals(animal) {
-  // seu código aqui
-  const animalsName = {};
-
   if (!animal) {
-    data.species.forEach((ele) => {
-      animalsName[ele.name] = ele.residents.length;
+    const totalAnimals = {};
+    data.species.forEach((specie) => {
+      totalAnimals[specie.name] = specie.residents.length;
     });
-    return animalsName;
+    return totalAnimals;
   }
 
-  const { specie, gender } = animal;
+  const { specie, sex } = animal;
 
-  const Animals = data.species.find((e) => e.name === specie).residents;
+  if (!sex) {
+    const searchSpecie = data.species.find((spc) => spc.name === specie);
+    return searchSpecie.residents.length;
+  }
 
-  const residents = Animals.reduce((acc, el) => (el.sex === gender ? acc + 1 : acc), 0);
-
-  return gender === undefined ? Animals.length : residents;
+  const searchSpecie2 = data.species.find((spc) => spc.name === specie);
+  const searchSpecie2Sex = searchSpecie2.residents.filter((resident) => resident.sex === sex);
+  return searchSpecie2Sex.length;
 }
 
 module.exports = countAnimals;
