2c089fe2ed3e15fa93356bd3b2e50650278fcf7e
Gabriel Henrique <G3brielhfsouza@outlook.com>
2021-10-03 18:31:58 -0300

    requisito 5 modificado e consertado

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
+// consultado o repositorio de alex cubas
+// https://github.com/tryber/sd-015-a-project-zoo-functions/pull/12/commits/52d0a899afe68fc53467a395e2dd8e45786bbeb7
+
 function countAnimals(animal) {
   // seu código aqui
+  const animalsName = {};
+
+  if (!animal) {
+    data.species.forEach((el) => {
+      animalsName[el.name] = el.residents.length;
+    });
+    return animalsName;
+  }
+
+  const { specie, gender } = animal;
+
+  const Animals = data.species.find((e) => e.name === specie).residents;
+
+  const residents = Animals.reduce((acc, el) => (el.sex === gender ? acc + 1 : acc), 0);
+
+  return gender === undefined ? Animals.length : residents;
 }
 
 module.exports = countAnimals;
