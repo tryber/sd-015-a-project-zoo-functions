95956ed0db2564a284ba620dc43da2c6f4a846c8
Guilherme Pavinato <guilherme.pavinato@gmail.com>
2021-10-04 00:12:07 -0300

    requisito 1- getSpeciesByIds

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // seu código aqui
+function getSpeciesByIds(...ids) {
+  if (ids === undefined) {
+    return [];
+  }
+  const resultado = ids.map((id) => species.find((specie) => specie.id === id));
+  return resultado;
 }
 
 module.exports = getSpeciesByIds;
